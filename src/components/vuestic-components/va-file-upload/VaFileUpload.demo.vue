c3907aeb9717f000c8b3409fb400757681e028e4
Nastassia Danilova <nastassia.danilova@epicmax.co>
2019-09-03 13:33:10 +0300

    feat: va-file-upload default file list

--- src/components/vuestic-components/va-file-upload/VaFileUpload.demo.vue
+++ src/components/vuestic-components/va-file-upload/VaFileUpload.demo.vue
@@ -17,6 +17,10 @@
     <VbCard title="disabled" tyle="width: 100%">
       <va-file-upload v-model="basic" disabled/>
     </VbCard>
+    <VbCard title="default files list">
+      <va-file-upload v-model="defaultFileList"/>
+      <va-file-upload v-model="defaultFileList" type="gallery"/>
+    </VbCard>
     <VbCard title="colors" style="width: 100%">
       <va-file-upload v-model="basic" color="info"/>
       <va-file-upload v-model="gallery" type="gallery" color="info"/>
@@ -34,6 +38,16 @@ export default {
       basic: [],
       single: [],
       gallery: [],
+      defaultFileList: [
+        {
+          name: 'xxx.png',
+          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        },
+        {
+          url: 'https://via.placeholder.com/150',
+          size: 15000,
+        },
+      ],
     }
   },
 }
