d81dd983289641a145c696dd97920278507353c6
sidneyarida <sidneyarida@gmail.com>
2019-09-08 13:53:46 +0200

    fix: select option (#14)

--- src/components/vuestic-components/va-select/VaSelect.vue
+++ src/components/vuestic-components/va-select/VaSelect.vue
@@ -322,7 +322,7 @@ export default {
 
       if (this.multiple) {
         this.valueProxy = isSelected
-          ? value.filter(optionSelected => option !== optionSelected)
+          ? value.filter(optionSelected => !this.compareOptions(option, optionSelected))
           : [...value, option]
         this.$refs.dropdown.updatePopper()
       } else {
