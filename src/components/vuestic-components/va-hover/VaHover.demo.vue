a6dc2a16e32f6c388b1e86a9cdf8550c1d78050e
Dmitry Mamyrev <work.whitelight@gmail.com>
2019-10-05 17:50:28 +0300

    [#37] demo upgrade; disabled prop check

--- src/components/vuestic-components/va-hover/VaHover.demo.vue
+++ src/components/vuestic-components/va-hover/VaHover.demo.vue
@@ -2,15 +2,25 @@
   <VbDemo>
     <VbCard>
       <va-hover v-slot:default="{ hover }">
-        <va-card title="Slot wrapper" :color="hover ? 'warning' : 'info'" >
+        <va-card title="Default slot content" :color="hover ? 'warning' : 'primary'" >
           Hover over me!
         </va-card>
       </va-hover>
       <br>
-      <va-hover v-model="h">
-        <va-card title="Binding through v-model" :color="h ? 'warning' : 'info'">
+      <va-hover v-model="vModelHover">
+        <va-card title="Binding through v-model" :color="vModelHover ? 'warning' : 'primary'">
           Hover over me
         </va-card>
+      </va-hover>
+      <br>
+      <va-hover disabled v-model="vModelHoverDisabled">
+        <va-card title="Binding through v-model, disabled component" :color="vModelHoverDisabled ? 'warning' : 'primary'">
+          Hover disabled
+        </va-card>
+      </va-hover>
+      <br>
+      <va-hover v-slot:default="{ hover }">
+        <div style="background: linear-gradient(to right, rgb(102, 229, 181), rgb(41, 224, 105)); height: 100px; border-radius: 6px; margin: auto; text-align:center;"> Div example: hover = {{ hover }}</div>
       </va-hover>
     </VbCard>
   </VbDemo>
@@ -28,7 +38,8 @@ export default {
   },
   data () {
     return {
-      h: false,
+      vModelHover: false,
+      vModelHoverDisabled: false,
     }
   },
 }
