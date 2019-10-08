c2c7c5ca9171503a130ae85a46f6828d5b73cd52
Kuchuk Andrey <kreezag@gmail.com>
2019-10-08 18:41:19 +0300

    add align prop to breadcrumbs. update demo of breadcrumbs

--- src/components/vuestic-components/va-breadcrumbs/VaBreadcrumbs.demo.vue
+++ src/components/vuestic-components/va-breadcrumbs/VaBreadcrumbs.demo.vue
@@ -1,14 +1,31 @@
 <template>
-  <va-card>
-    <va-breadcrumbs></va-breadcrumbs>
-  </va-card>
+  <VbDemo>
+    <VbCard title="Align">
+      <div style="width: 500px;">
+      Left:
+        <va-breadcrumbs align="left"></va-breadcrumbs>
+        <br /><br />
+        Center:
+        <va-breadcrumbs align="center"></va-breadcrumbs>
+        <br /><br />
+        Right:
+        <va-breadcrumbs align="right"></va-breadcrumbs>
+        <br /><br />
+      </div>
+    </VbCard>
+  </VbDemo>
 </template>
 
 <script>
-import VaCard from '../va-card/VaCard'
 import VaBreadcrumbs from './VaBreadcrumbs'
 
 export default {
-  components: { VaBreadcrumbs, VaCard },
+  components: {
+    VaBreadcrumbs,
+  },
+  data () {
+    return {
+    }
+  },
 }
 </script>
--- src/components/vuestic-components/va-breadcrumbs/VaBreadcrumbs.vue
+++ src/components/vuestic-components/va-breadcrumbs/VaBreadcrumbs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="va-breadcrumbs">
+  <div class="va-breadcrumbs" :class="computedClass">
     <span v-for="(item) in items" :key="item" class="va-breadcrumbs__item">
       <span class="va-breadcrumbs__item__text">{{item}}</span>
       <span class="va-breadcrumbs__item__separator">/</span>
@@ -10,12 +10,29 @@
 <script>
 export default {
   name: 'va-breadcrumbs',
+  props: {
+    align: {
+      type: String,
+      default: 'left',
+      required: false,
+    },
+  },
   data () {
     return {
-      items: [ 'one', 'two', 'tree' ],
+      items: [ 'One', 'Two', 'Three' ],
     }
   },
-  computed: {},
+  computed: {
+    computedClass () {
+      return {
+        'va-breadcrumbs--left': !this.align || this.align === 'left',
+        'va-breadcrumbs--right': this.align === 'right',
+        'va-breadcrumbs--center': this.align === 'center',
+        'va-breadcrumbs--between': this.align === 'between',
+        'va-breadcrumbs--around': this.align === 'around',
+      }
+    },
+  },
 }
 </script>
 
@@ -24,13 +41,35 @@ export default {
   display: flex;
   width: 100%;
   justify-content: center;
+
+  &.va-breadcrumbs--left {
+    justify-content: flex-start;
+  }
+
+  &.va-breadcrumbs--right {
+    justify-content: flex-end;
+  }
+
+  &.va-breadcrumbs--center {
+    justify-content: center;
+  }
+}
+
+.va-breadcrumbs__item {
+  display: inline-flex;
 }
 
 .va-breadcrumbs__item__separator {
+  padding: 0 0.5rem;
+  display: inline-flex;
+
   .va-breadcrumbs__item:last-child & {
     display: none;
   }
+}
 
+.va-breadcrumbs__item__text {
+  display: inline-flex;
 }
 
 </style>
