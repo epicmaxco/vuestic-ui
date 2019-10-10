e3492cae5deb7b49acbc2c0c6652167213b42d30
Dzmitry Mamyrau <whitel1ght@users.noreply.github.com>
2019-10-10 15:57:02 +0300

    [#37] hover component (#68)
    * [#37] hover component
    
    * [#37] demo upgrade; disabled prop check
    
    * [#37] hover component docs added
    
    * minor fixes

---
+++ src/components/vuestic-components/va-hover/VaHover.demo.vue
@@ -0,0 +1,40 @@
+<template>
+  <VbDemo>
+    <VbCard title="slot access" refresh>
+      <va-hover v-slot:default="{ hover }">
+        <div style="background-color: #babfc2">{{ hover }}</div>
+      </va-hover>
+    </VbCard>
+    <VbCard title="v-model access">
+      <va-hover v-model="value">
+        <div style="background-color: #babfc2">{{ value }}</div>
+      </va-hover>
+      <input type="checkbox" v-model="value">
+    </VbCard>
+    <VbCard title="disabled">
+      <va-hover disabled v-slot:default="{ hover }">
+        <div style="background-color: #babfc2">slot - {{ hover }}</div>
+      </va-hover>
+      <va-hover disabled v-model="value">
+        <div style="background-color: #babfc2">value - {{ valueDisabled }}</div>
+      </va-hover>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+
+import VaHover from './VaHover'
+
+export default {
+  components: {
+    VaHover,
+  },
+  data () {
+    return {
+      value: false,
+      valueDisabled: false,
+    }
+  },
+}
+</script>
