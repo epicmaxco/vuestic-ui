54c7a4072e20b58ac7350263a2eb6653103946bd
Yauheni Prakopchyk <ontrew@gmail.com>
2019-10-17 09:51:20 +0300

    add vuepress + move docs (#92)

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Vuestic UI docs',
+  description: 'Vuestic UI docs',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Avatar', link: '/components/va-avatar.md' },
+      { text: 'Badge', link: '/components/va-badge.md' },
+      { text: 'Button', link: '/components/va-button.md' },
+    ],
+    sidebar: 'auto',
+  },
+}
