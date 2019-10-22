bcc85491832a60bbec33367ceb938169e92166f3
Dzmitry Mamyrau <whitel1ght@users.noreply.github.com>
2019-10-22 16:12:20 +0300

    Feat/va progress bars (#83)
    * circular progress bar sizes added
    
    * progress bar docs, demo, new props
    
    * unnecessary font-size removed
    
    * documentation addition
    
    * radius switched to diameter
    
    * large and small presets added to horizontal progress bar
    
    * minor
    
    * large preset font size

--- src/components/vuestic-components/va-progress-bar/progress-types/VaProgressCircle.vue
+++ src/components/vuestic-components/va-progress-bar/progress-types/VaProgressCircle.vue
@@ -1,22 +1,14 @@
 <template>
-  <div
-    class="va-progress-circle"
-    :class="{
-      'va-progress-circle--indeterminate': indeterminate,
-    }"
-  >
-    <svg
-      class="va-progress-circle__progress-bar"
-      viewBox="21.25 21.25 42.5 42.5"
-    >
+  <div class="va-progress-circle" ref="progress" :style="computedStyle" :class="computedClass">
+    <svg class="va-progress-circle__progress-bar" viewBox="0 0 40 40">
       <circle
         class="va-progress-circle__overlay"
-        cx="42.5"
-        cy="42.5"
+        cx="50%"
+        cy="50%"
         :r="radius"
         fill="none"
         :stroke="colorComputed"
-        stroke-width="2.5"
+        :stroke-width="computedThickness"
         :stroke-dasharray="dasharray"
         :stroke-dashoffset="dashoffset"
       />
@@ -34,15 +26,45 @@ import { ColorThemeMixin } from '../../../../services/ColorThemePlugin'
 export default {
   name: 'va-progress-circle',
   mixins: [progressMixin, ColorThemeMixin],
+  props: {
+    size: {
+      type: [Number, String],
+      default: 40,
+      validator: (value) => {
+        return typeof value === 'number' || value.toString().match(/rem|em|ex|pt|pc|mm|cm|px/)
+      },
+    },
+    thickness: {
+      type: Number,
+      default: 3,
+    },
+  },
   computed: {
     radius () {
-      return 20
+      return 20 - (20 * this.thickness / 100)
     },
     dasharray () {
       return 2 * Math.PI * this.radius
     },
     dashoffset () {
       return this.dasharray * (1 - this.normalizedValue / 100)
+    },
+    computedThickness () {
+      return `${this.thickness}%`
+    },
+    computedStyle () {
+      const size = parseFloat(this.size) + this.sizeUnits()
+      return { width: size, height: size }
+    },
+    computedClass () {
+      return {
+        'va-progress-circle--indeterminate': this.indeterminate,
+      }
+    },
+  },
+  methods: {
+    sizeUnits () {
+      return typeof this.size === 'number' ? 'px' : this.size.toString().match(/rem|em|ex|pt|pc|mm|cm|px/)[0]
     },
   },
 }
@@ -53,11 +75,16 @@ export default {
 
 .va-progress-circle {
   position: relative;
-  width: $progress-circle-diameter;
-  height: $progress-circle-diameter;
 
   &__progress-bar {
+    position: absolute;
+    top: 0;
+    left: 0;
     transform: rotate(-90deg);
+    stroke-linecap: center center;
+    width: inherit;
+    height: inherit;
+
     @at-root {
       .va-progress-circle--indeterminate & {
         animation: va-progress-circle__progress-bar--indeterminate 2s linear infinite;
@@ -79,7 +106,6 @@ export default {
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
-    font-size: $progress-value-font-size;
   }
 }
 
