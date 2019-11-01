7d97157cb84c88cb0315d93c8dd89e245aeacdfe
Kuchuk Andrey <kreezag@gmail.com>
2019-11-01 14:33:19 +0300

    add va-content component

---
+++ src/components/vuestic-components/va-content/VaContent.demo.vue
@@ -0,0 +1,220 @@
+<template>
+  <VbDemo>
+    <VbCard title="with content class (h1-h6)">
+      <va-content>
+        <h1>h1 title</h1>
+        <h2>h2 title</h2>
+        <h3>h3 title</h3>
+        <h4>h4 title</h4>
+        <h5>h5 title</h5>
+        <h6>h6 title</h6>
+      </va-content>
+    </VbCard>
+    <VbCard title="without content class (h1-6)">
+      <div>
+        <h1>h1 title</h1>
+        <h2>h2 title</h2>
+        <h3>h3 title</h3>
+        <h4>h4 title</h4>
+        <h5>h5 title</h5>
+        <h6>h6 title</h6>
+      </div>
+    </VbCard>
+    <VbCard title="with content class (pre)">
+      <va-content>
+        <pre>pre text must be here</pre>
+        <p>class: code</p>
+        <pre class="code">code must be here</pre>
+      </va-content>
+    </VbCard>
+    <VbCard title="without content class (pre)">
+      <div>
+        <pre>pre text must be here</pre>
+      </div>
+    </VbCard>
+    <VbCard title="with content class (lists)">
+      <va-content>
+        <ol>
+          <li>item 1</li>
+          <li>
+            item 2
+            <ol>
+              <li>item 2.1</li>
+              <li>item 2.2</li>
+            </ol>
+          </li>
+          <li>item 3</li>
+          <li>item 4</li>
+        </ol>
+        <ul>
+          <li>item 1</li>
+          <li>
+            item 2
+            <ul>
+              <li>item 2.1</li>
+              <li>item 2.2</li>
+            </ul>
+          </li>
+          <li>item 3</li>
+          <li>item 4</li>
+        </ul>
+      </va-content>
+    </VbCard>
+    <VbCard title="without content class (lists)">
+      <ol>
+        <li>item 1</li>
+        <li>
+          item 2
+          <ol>
+            <li>item 2.1</li>
+            <li>item 2.2</li>
+          </ol>
+        </li>
+        <li>item 3</li>
+        <li>item 4</li>
+      </ol>
+      <ul>
+        <li>item 1</li>
+        <li>
+          item 2
+          <ul>
+            <li>item 2.1</li>
+            <li>item 2.2</li>
+          </ul>
+        </li>
+        <li>item 3</li>
+        <li>item 4</li>
+      </ul>
+    </VbCard>
+    <VbCard title="with content (blockquote)">
+      <va-content>
+        <blockquote>
+          <p>this is great blockquote</p>
+          <p>author</p>
+        </blockquote>
+      </va-content>
+    </VbCard>
+    <VbCard title="without content (blockquote)">
+      <blockquote>
+        <p>this is great blockquote</p>
+        <p>author</p>
+      </blockquote>
+    </VbCard>
+    <VbCard title="with content (figure)">
+      <va-content>
+        <figure>
+          <img src="https://i.imgur.com/NLrdqsk.png">
+          <figcaption>Name of picture</figcaption>
+        </figure>
+      </va-content>
+    </VbCard>
+    <VbCard title="without content (figure)">
+      <figure>
+        <img src="https://i.imgur.com/NLrdqsk.png">
+        <figcaption>Name of picture</figcaption>
+      </figure>
+    </VbCard>
+    <VbCard title="with content (table)">
+      <va-content>
+        <table>
+          <thead>
+            <tr>
+              <th
+                v-for="(data, index) in tableDataHead"
+                :key="index"
+              >
+                {{ data }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(data, index) in tableData"
+              :key="index"
+            >
+              <td
+                v-for="(i) in data"
+                :key="i"
+              >
+                {{ i }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>striped</p>
+        <table class="striped">
+          <thead>
+            <tr>
+              <th
+                v-for="(data, index) in tableDataHead"
+                :key="index"
+              >
+                {{ data }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(data, index) in tableData"
+              :key="index"
+            >
+              <td
+                v-for="(i) in data"
+                :key="i"
+              >
+                {{ i }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </va-content>
+    </VbCard>
+    <VbCard title="without content (table)">
+      <table>
+        <thead>
+          <tr>
+            <th
+              v-for="(data, index) in tableDataHead"
+              :key="index"
+            >
+              {{ data }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(data, index) in tableData"
+            :key="index"
+          >
+            <td
+              v-for="(i) in data"
+              :key="i"
+            >
+              {{ i }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import VaContent from './VaContent'
+
+export default {
+  components: {
+    VaContent,
+  },
+  data () {
+    return {
+      tableDataHead: ['Id', 'FooBar type', 'Actions'],
+      tableData: [
+        ['1', 'Zebra', 'Delete'],
+        ['2', 'Not Zebra', 'Remove'],
+        ['3', 'Very Zebra', 'Eradicate'],
+      ],
+    }
+  },
+}
+</script>
---
+++ src/components/vuestic-components/va-content/VaContent.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="content">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VaContent',
+}
+</script>
+
+<style lang="scss">
+  @import '../../vuestic-sass/resources/resources';
+  @import '../../vuestic-sass/global/typography';
+  @import '../../vuestic-sass/global/utility-global';
+
+  .content {
+    h1 {
+      @include va-display(1);
+      @include va-header-margin(1);
+    }
+
+    h2 {
+      @include va-display(2);
+      @include va-header-margin(2);
+    }
+
+    h3 {
+      @include va-display(3);
+      @include va-header-margin(3);
+    }
+
+    h4 {
+      @include va-display(4);
+      @include va-header-margin(4);
+    }
+
+    h5 {
+      @include va-display(5);
+      @include va-header-margin(5);
+    }
+
+    h6 {
+      @include va-display(6);
+      @include va-header-margin(6);
+    }
+
+    i {
+      font-style: italic;
+    }
+
+    hr.separator {
+      height: 2px;
+      background-color: #eeeeee;
+      border: none;
+    }
+
+    p {
+      margin-top: 0;
+      margin-bottom: 1rem;
+    }
+
+    pre {
+      margin-bottom: 1rem;
+      overflow-x: auto;
+      white-space: pre;
+      word-wrap: normal;
+
+      &.code {
+        @include va-code-snippet();
+      }
+    }
+
+    // NOTE Ideally we want this to work with mixins too, but no idea how to achieve that :/.
+    ol {
+      @extend .va-ordered;
+    }
+
+    ul {
+      @extend .va-unordered;
+    }
+
+    blockquote {
+      @extend .va-blockquote;
+    }
+
+    figure {
+      border-radius: 0;
+      border: none;
+      box-sizing: border-box;
+      box-shadow: $widget-box-shadow;
+      word-wrap: break-word;
+
+      figcaption {
+        flex: 1 1 auto;
+        padding: 1.25rem;
+      }
+
+      p:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    table {
+      @extend .va-table;
+    }
+
+    a {
+      @extend .link;
+    }
+  }
+
+</style>
