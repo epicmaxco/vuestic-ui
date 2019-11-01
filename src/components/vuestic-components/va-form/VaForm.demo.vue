b0b9b1a9886068198cf22579d5114f27d0601f61
Kuchuk Andrey <kreezag@gmail.com>
2019-11-01 18:11:20 +0300

    add base component va-form and demo

---
+++ src/components/vuestic-components/va-form/VaForm.demo.vue
@@ -0,0 +1,56 @@
+<template>
+  <VbDemo>
+    <VbCard title="default">
+      <va-form>
+        <va-input
+          v-model="input"
+          label="input"
+        />
+        <va-radio
+          v-model="radio"
+          label="radio"
+        />
+        <va-checkbox
+          v-model="checkbox"
+          label="checkbox"
+        />
+        <va-button
+          type="submit"
+          @click="submit"
+        >
+          Submit
+        </va-button>
+      </va-form>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import VaForm from './VaForm'
+import VaInput from '../va-input/VaInput'
+import VaCheckbox from '../va-checkbox/VaCheckbox'
+import VaRadio from '../va-radio/VaRadio'
+import VaButton from '../va-button/VaButton'
+
+export default {
+  components: {
+    VaForm,
+    VaInput,
+    VaRadio,
+    VaCheckbox,
+    VaButton,
+  },
+  data () {
+    return {
+      input: 'input',
+      checkbox: false,
+      radio: false,
+    }
+  },
+  methods: {
+    submit (val) {
+      console.log('submit', val)
+    },
+  },
+}
+</script>
---
+++ src/components/vuestic-components/va-form/VaForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form
+    class="va-form"
+    @submit="handleSubmit"
+  >
+    <slot />
+  </form>
+</template>
+
+<script>
+
+export default {
+  name: 'VaForm',
+  mixins: [],
+  components: {
+  },
+  props: {
+  },
+  methods: {
+    handleSubmit (result) {
+      console.log('result', result)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.va-form {
+}
+</style>
