27ed881b7eae67eb3ea84f78c02b51da6950215f
Kuchuk Andrey <kreezag@gmail.com>
2019-11-12 20:27:34 +0300

    update component va-radio

--- src/components/vuestic-components/va-radio/VaRadio.vue
+++ src/components/vuestic-components/va-radio/VaRadio.vue
@@ -9,7 +9,7 @@
       :checked="isActive"
       :disabled="disabled"
       :name="name"
-      @change="click"
+      @change="onClick"
       @focus="focus"
       :tabindex="tabindex"
     >
@@ -101,7 +101,7 @@ export default {
     computedClass () {
       return {
         'va-radio--disabled': this.disabled,
-        'va-radio--leftLabel': this.leftLabel,
+        'va-radio--left-label': this.leftLabel,
       }
     },
     iconBackgroundComputedStyles () {
@@ -131,7 +131,7 @@ export default {
     },
   },
   methods: {
-    click (e) {
+    onClick (e) {
       if (!this.disabled) {
         this.$emit('input', this.option, e)
       }
@@ -152,120 +152,120 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "../../vuestic-sass/resources/resources";
+@import '../../vuestic-sass/resources/resources';
 
-  .va-radio {
+.va-radio {
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  margin-top: 0;
+  margin-right: 0.5rem;
+  transition: $transition-primary;
+
+  & + & {
+    margin-top: 0.5rem;
+  }
+
+  &--disabled {
+    cursor: default;
+  }
+
+  &--left-label {
+    flex-direction: row-reverse;
     display: inline-flex;
-    flex-direction: row;
-    align-items: center;
-    cursor: pointer;
-    position: relative;
-    margin-top: 0;
-    margin-right: 0.5rem;
-    transition: $transition-primary;
 
     & + & {
-      margin-top: 0.5rem;
+      margin-top: 0;
+    }
+  }
+
+  &__input {
+    width: 0;
+    height: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    z-index: -1;
+  }
+
+  &__icon {
+    transition: $transition-primary;
+    display: inline-flex;
+    align-items: center;
+    width: 1.4rem;
+    height: 1.4rem;
+    border-radius: 100%;
+    position: relative;
+    border: $gray solid 0.125rem;
+    box-sizing: border-box;
+
+    .va-radio__input:disabled + & {
+      opacity: $radio-content-opacity;
     }
 
-    &--disabled {
-      cursor: default;
-    }
-
-    &--leftLabel {
-      flex-direction: row-reverse;
-      display: inline-flex;
-
-      & + & {
-        margin-top: 0;
-      }
-    }
-
-    &__input {
-      width: 0;
-      height: 0;
-      position: absolute;
-      top: 0;
-      left: 0;
-      opacity: 0;
-      z-index: -1;
-    }
-
-    &__icon {
+    &__dot {
       transition: $transition-primary;
-      display: inline-flex;
-      align-items: center;
-      width: 1.4rem;
-      height: 1.4rem;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      right: 50%;
+      bottom: 50%;
       border-radius: 100%;
-      position: relative;
-      border: $gray solid 0.125rem;
-      box-sizing: border-box;
+      background-color: inherit;
+      opacity: 0;
 
-      .va-radio__input:disabled + & {
-        opacity: 0.4;
-      }
-
-      &__dot {
-        transition: $transition-primary;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        right: 50%;
-        bottom: 50%;
-        border-radius: 100%;
-        background-color: inherit;
-        opacity: 0;
-
-        .va-radio__input:checked + .va-radio__icon & {
-          opacity: 1;
-          top: 0.25rem;
-          left: 0.25rem;
-          right: 0.25rem;
-          bottom: 0.25rem;
-        }
-      }
-
-      &__background {
-        transition: $transition-primary;
-        position: absolute;
-        top: -0.35rem;
-        left: -0.35rem;
-        right: -0.35rem;
-        bottom: -0.35rem;
-        background-color: $gray;
-        border-radius: 100%;
-        z-index: 0;
-        opacity: 0;
-
-        .va-radio__input:focus + .va-radio__icon & {
-          opacity: 0.2;
-        }
-
-        .va-radio:hover & {
-          opacity: 0.2;
-        }
-
-        .va-radio--disabled:hover & {
-          opacity: 0;
-        }
+      .va-radio__input:checked + .va-radio__icon & {
+        opacity: 1;
+        top: 0.25rem;
+        left: 0.25rem;
+        right: 0.25rem;
+        bottom: 0.25rem;
       }
     }
 
-    &__text {
-      display: inline-flex;
-      margin-left: 0.5rem;
-      margin-right: 0;
+    &__background {
+      transition: $transition-primary;
+      position: absolute;
+      top: -0.35rem;
+      left: -0.35rem;
+      right: -0.35rem;
+      bottom: -0.35rem;
+      background-color: $gray;
+      border-radius: 100%;
+      z-index: 0;
+      opacity: 0;
 
-      .va-radio--disabled & {
-        opacity: 0.4;
+      .va-radio__input:focus + .va-radio__icon & {
+        opacity: 0.2;
       }
 
-      .va-radio--leftLabel & {
-        margin-left: 0;
-        margin-right: 0.5rem;
+      .va-radio:hover & {
+        opacity: 0.2;
+      }
+
+      .va-radio--disabled:hover & {
+        opacity: 0;
       }
     }
   }
+
+  &__text {
+    display: inline-flex;
+    margin-left: 0.5rem;
+    margin-right: 0;
+
+    .va-radio--disabled & {
+      opacity: $radio-content-opacity;
+    }
+
+    .va-radio--left-label & {
+      margin-left: 0;
+      margin-right: 0.5rem;
+    }
+  }
+}
 
 </style>
