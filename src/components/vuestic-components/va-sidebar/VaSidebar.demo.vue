406303ce6c87d0b1e16390e7b73848ec3c1fdd71
Kuchuk Andrey <kreezag@gmail.com>
2019-11-29 18:01:46 +0300

    add minimize sidebar demo

--- src/components/vuestic-components/va-sidebar/VaSidebar.demo.vue
+++ src/components/vuestic-components/va-sidebar/VaSidebar.demo.vue
@@ -34,6 +34,46 @@
         </template>
       </va-sidebar>
     </VbCard>
+
+    <VbCard title="Sidebar minimised">
+      <va-sidebar
+        style="position: static; height: auto;"
+        minimized
+      >
+        <template slot="menu">
+          <template v-for="(item, index) in items">
+            <sidebar-link-group
+              :key="index"
+              :icon="[ 'sidebar-menu-item-icon vuestic-iconset', item.meta.iconClass ]"
+              v-if="item.children"
+              minimized
+            >
+              <span slot="title">{{ item.displayName }}</span>
+              <sidebar-link
+                v-for="(subMenuItem, index) in item.children"
+                :key="index"
+                :to="{ name: subMenuItem.name }"
+                minimized
+              >
+                <div slot="title">
+                  <span>{{ subMenuItem.displayName }}</span>
+                </div>
+              </sidebar-link>
+            </sidebar-link-group>
+
+            <sidebar-link
+              v-else
+              :key="index"
+              :icon="[ 'sidebar-menu-item-icon vuestic-iconset', item.meta.iconClass ]"
+              :to="{ name: item.name }"
+              minimized
+            >
+              <span slot="title">{{ item.displayName }}</span>
+            </sidebar-link>
+          </template>
+        </template>
+      </va-sidebar>
+    </VbCard>
   </VbDemo>
 </template>
 
