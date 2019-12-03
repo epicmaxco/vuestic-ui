6ecd39cceba1f38fbefdcd50a20badda2f1ec51b
AlexanderBoriskin <alexander.boriskin12@gmail.com>
2019-12-03 22:41:42 +0300

    feat(va-component-fix): va-color-picker fix

--- src/components/vuestic-components/va-color-picker/VaAdvancedColorPicker.vue
+++ src/components/vuestic-components/va-color-picker/VaAdvancedColorPicker.vue
@@ -29,17 +29,68 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../../vuestic-sass/resources/resources';
+
 .va-advanced-color-picker {
-  .vc-chrome-alpha-wrap {
-    display: none;
+  .vc-chrome-field {
+    display: flex;
+    position: relative;
+    width: 100%;
+    min-height: 2.375rem;
+    border-style: solid;
+    border-width: 0 0 thin 0;
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+    padding-left: 0 !important;
+  }
+  .vc-editable-input {
+    display: flex;
+    position: relative;
+    align-items: flex-end;
+    width: 100%;
+  }
+  .vc-input__input {
+    width: 100%;
+    height: 1.5rem;
+    margin-bottom: 0.125rem;
+    padding: 0.25rem 0.5rem;
+    color: #34495e;
+    background-color: transparent;
+    border-style: none;
+    box-shadow: none !important;
+    outline: none;
+    font-size: 1rem !important;
+    font-family: $font-family-sans-serif;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.5;
+    letter-spacing: normal;
+    text-align: left !important;
+
+    &::placeholder {
+      color: $brand-secondary;
+    }
+
+    &:placeholder-shown {
+      padding-bottom: 0.875rem;
+    }
   }
 
-  .vc-chrome-hue-wrap {
-    margin-top: 10px;
-  }
-
-  .vc-chrome-toggle-btn {
-    display: none;
+  .vc-input__label {
+    position: absolute;
+    bottom: 0.875rem;
+    left: 0.5rem;
+    margin-bottom: 0.5rem;
+    max-width: calc(100% - 0.25rem);
+    color: $vue-green !important;
+    font-size: 0.625rem;
+    letter-spacing: 0.0375rem;
+    line-height: 1.2;
+    font-weight: bold;
+    text-transform: uppercase;
+    @include va-ellipsis();
+    transform-origin: top left;
   }
 }
 </style>
