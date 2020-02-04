ce9817070547041406f392a261802f951d9d9223
mikkou <developer.0112358@gmail.com>
2020-02-04 16:36:09 +0300

    feat/made colors utilities

---
+++ src/components/vuestic-utilities/utilities.demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Background utilities</h3>
+    <p class="background--danger">
+      .background--danger
+    </p>
+
+    <h3>Text color utilities</h3>
+    <p class="text--danger">
+      .text--danger
+    </p>
+  </div>
+</template>
+
+<script>
+import './index'
+export default {
+
+}
+</script>
