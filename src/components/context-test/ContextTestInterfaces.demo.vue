b96915eeb91f79e3d4d75eba5f6eb6330ad23c5f
Yauheni Prakopchyk <ontrew@gmail.com>
2020-02-15 14:18:59 +0300

    feat:improve context interface (#177)

---
+++ src/components/context-test/ContextTestInterfaces.demo.vue
@@ -0,0 +1,38 @@
+<template>
+  <VbDemo>
+    <VbCard title="full object config">
+      <va-context
+        :config="{ ContextTestInterfaces: {value: 'full object config'} }"
+      >
+        <ContextTestInterfaces/>
+      </va-context>
+    </VbCard>
+    <VbCard focus title="per-value config">
+      <va-context
+        :config="{ ContextTestInterfaces: {value: 'full object config'} }"
+        ContextTestInterfaces::value="per-value config"
+      >
+        <ContextTestInterfaces/>
+      </va-context>
+    </VbCard>
+    <VbCard title="string access all">
+      <va-context
+        all::value="all-config"
+      >
+        <ContextTestInterfaces/>
+      </va-context>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import ContextTestInterfaces from './ContextTestInterfaces'
+import VaContext from './context-provide/VaContext'
+
+export default {
+  components: {
+    VaContext,
+    ContextTestInterfaces,
+  },
+}
+</script>
