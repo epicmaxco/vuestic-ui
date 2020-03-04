29c485500d3fe1ac563357470d15c1a107ac3862
Asva <ontrew@gmail.com>
2020-03-04 18:29:40 +0300

    wip component api

---
+++ src/components/vuestic-utilities/ApiDocs.demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <VbDemo>
+    <VbCard>
+      <ApiDocs :componentOptions="VaCheckbox"/>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import ApiDocs from './ApiDocs.vue'
+import VaCheckbox
+  from '../vuestic-components/va-checkbox/VaCheckbox'
+
+export default {
+  components: {
+    ApiDocs,
+  },
+  computed: {
+    VaCheckbox: () => VaCheckbox,
+  },
+}
+</script>
---
+++ src/components/vuestic-utilities/ApiDocs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ApiDocs">
+    <pre>{{ componentProps }}</pre>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  name: 'ApiDocs',
+  props: {
+    componentOptions: {
+      required: true,
+    },
+  },
+  computed: {
+    componentProps () {
+      console.log('this.componentOptions', this.componentOptions)
+      const testComponentInstance = new (Vue.extend(this.componentOptions))()
+      console.log('testComponentInstance', testComponentInstance)
+      return testComponentInstance.$options.props
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.ApiDocs {
+
+}
+</style>
