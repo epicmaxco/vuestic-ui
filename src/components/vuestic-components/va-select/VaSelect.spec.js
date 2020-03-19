696aa3aa0f5b583f148e0017ee39e1010d4657f0
Asva <ontrew@gmail.com>
2020-03-19 14:24:38 +0300

    minor

--- src/components/vuestic-components/va-select/VaSelect.spec.js
+++ src/components/vuestic-components/va-select/VaSelect.spec.js
@@ -1,6 +1,6 @@
-import VaSelect from './VaSelect.vue'
+// import VaSelect from './VaSelect.vue'
 
-import { testIsFormComponent } from '../../vuestic-mixins/testIsFormComponent'
+// import { testIsFormComponent } from '../../vuestic-mixins/testIsFormComponent'
 
 describe('VaSelect', () => {
   it('is Form Component', () => {
