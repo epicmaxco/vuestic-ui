7f063ef23b62655869eb839097f09ed0c9eabf42
ilya pekert <misterpekert@gmail.com>
2020-03-23 14:18:18 +0300

    move helpers to util file

--- src/components/vuestic-mixins/SelectableList/SelectableListMixin.js
+++ src/components/vuestic-mixins/SelectableList/SelectableListMixin.js
@@ -1,40 +1,5 @@
 import { FormComponentMixin } from '../FormComponent/FormComponentMixin'
-
-// Maybe we need to add some kind of fallback value to these helper methods
-// And to extract some in some kind of utility file 'cause I suppose they
-// might become useful somewhere else at some point
-
-const getNestedValue = (option, propsArray) => {
-  const lastPropIndex = propsArray.length - 1
-  if (lastPropIndex < 0) return option
-
-  let nestedObj = option[propsArray[0]]
-
-  for (let i = 1; i < lastPropIndex; i++) {
-    if (nestedObj === null) {
-      return option
-    }
-    nestedObj = nestedObj[propsArray[i]]
-  }
-
-  if (nestedObj === null) return option
-
-  return nestedObj[propsArray[lastPropIndex]]
-}
-
-const getValueByPath = (option, prop) => {
-  if (option === null || typeof prop !== 'string' || !prop) return option
-  if (option[prop] !== undefined) return option[prop]
-  prop = prop.replace(/^\./, '') // remove first point symbol
-  return getNestedValue(option, prop.split('.'))
-}
-
-const getProp = (option, prop) => {
-  if (!prop) return option
-  if (typeof prop === 'string') return getValueByPath(option, prop)
-  if (typeof prop === 'function') return prop(option)
-  return option
-}
+import { getProp } from '../../../services/utils'
 
 export const SelectableListMixin = {
   mixins: [FormComponentMixin],
