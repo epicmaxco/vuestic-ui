25c3a282c5f7e169e1df30d16b4d3c127e5fd46b
SergeiPleshkov <siarhei.pliashkou@epicmax.co>
2020-03-31 10:25:42 +0300

    feat/lerna

---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    '@vue/standard',
+    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    curly: 'error',
+    'array-bracket-spacing': ['error', 'never'],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+}
