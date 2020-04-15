72a4b5191dae1aee59a47120418dbfc98167a06b
Asva <ontrew@gmail.com>
2020-04-15 16:29:33 +0300

    fix fonts and icons

--- packages/docs/nuxt.config.js
+++ packages/docs/nuxt.config.js
@@ -1,9 +1,18 @@
 export default {
+  head: {
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:400' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
+    ],
+  },
   buildModules: ['@nuxt/typescript-build'],
   plugins: [
     { mode: 'client', src: '~/plugins/ui-plugin.ts' },
     { src: '~/plugins/context.ts' }
   ],
+
   build: {
       transpile: ['vue-instantsearch', 'instantsearch.js/es'],
   },
