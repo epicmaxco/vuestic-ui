cc5823f6efc1de46f1d1d894bcccf8f7272440cd
Raichev Dmitry <dmitry.raichev@epicmax.co>
2020-04-16 15:38:58 +0300

    feat(components): create VaAffix component (#227)
    * feat(components): create VaAffix component
    
    * refactor(VaAffix): use `$vb.lorem()` in demos
    
    * feat(VaAffix): add ContextableMixin to the component
    
    * refactor(VaAffix): add comment about the custom target
    
    * refactor(VaAffix): use fallback values from the context
    
    * style(VaPagination): minor fixes
    
    * style: minor
    
    * lint
    
    Co-authored-by: Asva <ontrew@gmail.com>

--- packages/ui/src/components/context-test/context-provide/testIsContextableComponent.ts
+++ packages/ui/src/components/context-test/context-provide/testIsContextableComponent.ts
@@ -1,10 +1,11 @@
+// @ts-nocheck
 import { mount } from '@vue/test-utils'
 import VaContext from './VaContext'
 
 // Check passed props are contextable.
 // componentProps could be used to pass down required props.
 // contextConfigProps are for declaring global configs, such as icons and sizes.
-export function testIsContextableComponent (componentOptions, contextProps, componentProps, contextConfigProps) {
+export function testIsContextableComponent (componentOptions, contextProps, componentProps?, contextConfigProps?) {
   const wrapperComponentOptions = {
     render (createElement) {
       return createElement(
---
+++ packages/ui/src/components/vuestic-components/va-affix/tests/VaAffix.spec.ts
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+
+import { testIsContextableComponent } from '../../../context-test/context-provide/testIsContextableComponent'
+import VaAffix from '../VaAffix.vue'
+
+describe('VaAffix', () => {
+  let logError: any
+  beforeEach(() => {
+    // TODO:
+    // disable the Vue prop types checking in the test
+    // to get rid of the error:
+    // type check failed for prop "target". Expected , Window, got Window`
+
+    // just ignore `console.error` yet
+    logError = console.error
+    console.error = () => undefined
+  })
+
+  afterEach(() => {
+    console.error = logError
+  })
+
+  it('should render without an error', () => {
+    const wrapper = mount(VaAffix)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  it('should throw if prop tag does not exist in the context', () => {
+    const props = {
+      tag: 'a',
+    }
+    expect(() => testIsContextableComponent(VaAffix, props)).toThrow()
+  })
+  it('is contextable', () => {
+    const props = {
+      offsetTop: undefined,
+      offsetBottom: undefined,
+      target: window,
+    }
+    expect(() => testIsContextableComponent(VaAffix, props)).not.toThrow()
+  })
+})
