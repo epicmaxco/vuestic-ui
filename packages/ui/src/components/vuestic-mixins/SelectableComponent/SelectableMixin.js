08fd39a4940b4a4820e096a23c0e629dc8a57bea
Kushich <kushich.dmitry@gmail.com>
2020-04-28 20:40:20 +0300

    add methods

--- packages/ui/src/components/vuestic-mixins/SelectableComponent/SelectableMixin.js
+++ packages/ui/src/components/vuestic-mixins/SelectableComponent/SelectableMixin.js
@@ -41,4 +41,52 @@ export const SelectableMixin = {
       return Array.isArray(this.value)
     },
   },
+  methods: {
+    onFocus () {
+      this.KeyboardOnlyFocusMixin_onFocus()
+
+      this.$emit('focus')
+    },
+    onBlur (event) {
+      if (this.$refs.input === event.target && !this.isElementRelated(event.relatedTarget)) {
+        this.ValidateMixin_onBlur()
+        this.isKeyboardFocused = false
+        this.$emit('blur', event)
+      }
+    },
+    isElementRelated (element) {
+      return [this.$refs.label, this.$refs.container].includes(element)
+    },
+    clickWrapper () {
+      if (this.isElementRelated(document.activeElement)) {
+        this.$refs.input.focus()
+        this.isKeyboardFocused = false
+      }
+      this.toggleSelection()
+    },
+    toggleSelection () {
+      if (this.c_readonly || this.c_disabled) {
+        return
+      }
+      if (this.modelIsArray) {
+        if (!this.value) {
+          this.$emit('input', [this.c_arrayValue])
+        } else if (this.value.includes(this.c_arrayValue)) {
+          this.$emit('input', this.value.filter(option => option !== this.c_arrayValue))
+        } else {
+          this.$emit('input', this.value.concat(this.c_arrayValue))
+        }
+        return
+      }
+      // fix emit of value
+      if (this.isTrue) {
+        this.$emit('input', this.falseValue)
+      } else if (this.isFalse) {
+        this.$emit('input', this.trueValue)
+      } else {
+        this.$emit('input', false)
+      }
+      this.$emit('input', !this.c_value)
+    },
+  },
 }
