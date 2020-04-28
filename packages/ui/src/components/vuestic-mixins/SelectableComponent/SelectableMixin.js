2d786392b4ebabbfd46ebc6f49af27c0322d729d
Kushich <kushich.dmitry@gmail.com>
2020-04-28 19:30:46 +0300

    add mixins and computed

--- packages/ui/src/components/vuestic-mixins/SelectableComponent/SelectableMixin.js
+++ packages/ui/src/components/vuestic-mixins/SelectableComponent/SelectableMixin.js
@@ -1,6 +1,16 @@
-import { makeContextablePropsMixin } from '../../context-test/context-provide/ContextPlugin'
+import {
+  ContextPluginMixin,
+  makeContextablePropsMixin,
+} from '../../context-test/context-provide/ContextPlugin'
+import { FormComponentMixin } from '../../vuestic-mixins/FormComponent/FormComponentMixin'
+import { StatefulMixin } from '../../vuestic-mixins/StatefullMixin/StatefulMixin'
+import { ColorThemeMixin } from '../../../services/ColorThemePlugin'
 export const SelectableMixin = {
   mixins: [
+    FormComponentMixin,
+    ColorThemeMixin,
+    StatefulMixin,
+    ContextPluginMixin,
     makeContextablePropsMixin({
       arrayValue: { type: [String, Object], default: '' },
       label: { type: String, default: '' },
@@ -14,5 +24,19 @@ export const SelectableMixin = {
       throw new Error('Props trueValue and falseValue are the same')
     }
     this.isSelectableComponent = true
+  },
+  computed: {
+    isTrue () {
+      return this.modelIsArray ? this.value.includes(this.arrayValue) : this.value === this.trueValue
+    },
+    isFalse () {
+      return this.modelIsArray ? !this.value.includes(this.arrayValue) : this.value === this.falseValue
+    },
+    isChecked () {
+      return this.modelIsArray ? this.value && this.value.includes(this.c_arrayValue) : this.value
+    },
+    modelIsArray () {
+      return Array.isArray(this.value)
+    },
   },
 }
