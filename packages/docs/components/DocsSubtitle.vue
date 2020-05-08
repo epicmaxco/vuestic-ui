59b36959ab51194d332cf556faa0c303de68e0e4
SergeiPleshkov <azrailomgpro@gmail.com>
2020-05-08 10:54:24 +0300

    review fixes

--- packages/docs/components/DocsSubtitle.vue
+++ packages/docs/components/DocsSubtitle.vue
@@ -4,22 +4,21 @@
   <a :id="anchor" :style="{ color: primaryColor }" :href="`#${anchor}`">#</a>
 </h3>
 </template>
-<script>
+<script lang='ts'>
 import { kebabCase } from 'lodash'
+import { TranslationString } from '../types/configTypes'
+import { Component, Prop, Mixins } from 'vue-property-decorator'
+import { ColorThemeMixin } from '../../ui/src/services/ColorThemePlugin'
 
-export default {
-  props: {
-    text: {
-      type: String,
-    },
-  },
-  computed: {
-    anchor () {
-      return kebabCase(this.$t(this.text))
-    },
-    primaryColor () {
-      return this.$themes?.primary
-    },
-  },
+@Component({})
+export default class DocsSubtitle extends Mixins(ColorThemeMixin) {
+  @Prop({ type: String }) text!: TranslationString
+  get anchor () {
+    return kebabCase(this.$t(this.text))
+  }
+
+  get primaryColor () {
+    return this.computeColor('primary')
+  }
 }
 </script>
