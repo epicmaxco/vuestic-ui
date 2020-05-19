b526d6ccb29e8cfa7574c69e56f8a4b174967c0b
Kushich <kushich.dmitry@gmail.com>
2020-05-19 20:04:51 +0300

    fix styles

--- packages/ui/src/components/vuestic-components/va-checkbox/VaCheckbox.vue
+++ packages/ui/src/components/vuestic-components/va-checkbox/VaCheckbox.vue
@@ -18,7 +18,10 @@
       @blur="onBlur"
       ref="container"
     >
-      <div class="va-checkbox__square">
+      <div
+        class="va-checkbox__square"
+        :style="inputStyle"
+      >
         <input
           ref="input"
           :id="id"
@@ -32,15 +35,15 @@
           @keypress.prevent="toggleSelection()"
           :disabled="c_disabled"
           :indeterminate="c_indeterminate"
-          :style="inputStyle"
         >
         <va-icon
-          class="va-checkbox__icon-selected"
+          class="va-checkbox__icon"
           :name="computedIconName"
+          size="20px"
         />
       </div>
       <div
-        class="va-checkbox__label-text"
+        class="va-checkbox__label"
         :style="labelStyle"
         ref="label"
         tabindex="-1"
@@ -70,15 +73,9 @@ export default {
       indeterminate: { type: Boolean, default: false },
       checkedIcon: { type: String, default: 'check' },
       indeterminateIcon: { type: String, default: 'remove' },
+      color: { type: String, default: 'Primary' },
     }),
   ],
-  watch: {
-    value (newValue) {
-      if (newValue) {
-        this.ValidateMixin_onBlur()
-      }
-    },
-  },
   computed: {
     computedClass () {
       return {
@@ -87,7 +84,6 @@ export default {
         'va-checkbox--disabled': this.c_disabled,
         'va-checkbox--indeterminate': this.c_indeterminate,
         'va-checkbox--error': this.computedError,
-        'va-checkbox--on-keyboard-focus': this.isKeyboardFocused,
         'va-checkbox--left-label': this.leftLabel,
       }
     },
@@ -101,12 +97,12 @@ export default {
     inputStyle () {
       if (this.computedError) {
         if (this.isChecked) {
-          return { background: this.$themes.danger }
+          return { background: this.colorComputed }
         } else {
           return { borderColor: this.$themes.danger }
         }
       } else {
-        if (this.isChecked) { return { background: this.colorComputed } }
+        if (this.isChecked) { return { background: this.colorComputed, borderColor: this.colorComputed } }
       }
 
       return {}
@@ -148,90 +144,52 @@ export default {
     }
   }
 
-  #{&}__square {
+  &__square {
     @include flex-center();
 
-    width: 2rem;
-    height: 2rem;
+    width: 1.35rem;
+    min-width: 1.35rem;
+    height: 1.35rem;
     position: relative;
-    flex: 0 0 2rem;
-
-    @at-root {
-      .va-checkbox--on-keyboard-focus#{&} {
-        background-color: $light-gray;
-        transition: all, 0.6s, ease-in;
-        border-radius: 5rem;
-      }
-    }
-  }
-
-  #{&}__input {
-    box-sizing: border-box;
-    height: 1.375rem;
-    width: 1.375rem;
-    cursor: inherit;
-    color: $white;
     background-color: $white;
     border: solid 0.125rem $gray-light;
     border-radius: 0.25rem;
-
-    &:focus {
-      outline: none;
-    }
-
-    @at-root {
-      .va-checkbox--selected#{&} {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 1.4rem;
-        width: 1.4rem;
-        color: $white;
-        background-color: $vue-green;
-        border: 0;
-      }
-
-      .va-checkbox--error#{&} {
-        border-color: $theme-red;
-      }
-    }
   }
 
-  #{&}__label-text {
+  &__input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  &__label {
     display: inline-block;
     position: relative;
-    margin-left: 0.25rem;
-
-    @at-root {
-      .va-checkbox--error#{&} {
-        color: $theme-red;
-      }
-    }
+    margin: 0 0.25rem;
   }
 
-  &__error-message {
-    vertical-align: middle;
-    color: $theme-red;
-    font-size: $font-size-mini;
-  }
-
-  &__icon-selected {
+  &__icon {
     pointer-events: none;
     position: absolute;
     color: $white;
   }
 
-  &__error-message-container {
-    flex: 0 0 100%;
-    margin-left: 0.3rem; // To fit with checkbox.
+  &--selected {
+    .va-checkbox {
+      &__square {
+        background-color: $vue-green;
+        border-color: $vue-green;
+      }
+    }
   }
 
-  &__label-container {
-    margin-left: 2rem;
-  }
-
-  &__content {
-    flex-direction: row;
+  &--error {
+    .va-checkbox {
+      &__square {
+        border-color: $theme-red;
+      }
+    }
   }
 }
+
 </style>
