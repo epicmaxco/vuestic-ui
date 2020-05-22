3759939ebf21ae9606750861cb80112d8bc0b0f6
raichev-dima <dmitry.raichev@epicmax.co>
2020-05-22 17:58:53 +0300

    chore: create boilerplate code generator

--- packages/docs/components/sidebar/NavigationRoutes.ts
+++ packages/docs/components/sidebar/NavigationRoutes.ts
@@ -1,4 +1,5 @@
 import { TranslationString } from '../../../ui/src/services/api-docs/ManualApiOptions'
+import scheme from './navigationScheme.json'
 
 export type NavigationRoute = {
   name: string,
@@ -15,61 +16,4 @@ export type NavigationScheme = {
   routes: NavigationRoute[],
 }
 
-export const navigationRoutes: NavigationScheme = {
-  root: {
-    name: '/',
-    displayName: 'navigationRoutes.home',
-  },
-  routes: [
-    {
-      name: 'ui',
-      displayName: 'menu.uiElements',
-      meta: {
-        iconClass: 'vuestic-iconset-ui-elements',
-      },
-      disabled: true,
-      children: [
-        {
-          name: 'affix',
-          displayName: 'menu.affix',
-        },
-        {
-          name: 'breadcrumbs',
-          displayName: 'menu.breadcrumbs',
-        },
-        {
-          name: 'badge',
-          displayName: 'menu.badge',
-        },
-        {
-          name: 'fileUpload',
-          displayName: 'menu.fileUpload',
-        },
-        {
-          name: 'modal',
-          displayName: 'menu.modal',
-        },
-        {
-          name: 'pagination',
-          displayName: 'menu.pagination',
-        },
-        {
-          name: 'rating',
-          displayName: 'menu.rating',
-        },
-        {
-          name: 'slider',
-          displayName: 'menu.slider',
-        },
-        {
-          name: 'switch',
-          displayName: 'menu.switch',
-        },
-        {
-          name: 'tabs',
-          displayName: 'menu.tabs',
-        },
-      ],
-    },
-  ],
-}
+export const navigationRoutes: NavigationScheme = scheme
