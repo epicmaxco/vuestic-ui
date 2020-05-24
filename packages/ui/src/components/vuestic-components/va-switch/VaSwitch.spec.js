48fdf3fbb9a9e0f8f0188e6bc3b8f872f7f5458d
Kushich <kushich.dmitry@gmail.com>
2020-05-24 14:00:41 +0300

    add tests

--- packages/ui/src/components/vuestic-components/va-switch/VaSwitch.spec.js
+++ packages/ui/src/components/vuestic-components/va-switch/VaSwitch.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import VaSwitch from './VaSwitch'
 
 import { testIsSelectableComponent } from '../../vuestic-mixins/testIsSelectableComponent'
+import { testIsContextableComponent } from '../../context-test/context-provide/testIsContextableComponent'
 
 describe('VaSwitch', () => {
   it('default', () => {
@@ -18,5 +19,17 @@ describe('VaSwitch', () => {
   })
   it('is Selectable Component', () => {
     expect(() => testIsSelectableComponent(VaSwitch)).not.toThrow()
+  })
+  it('is contextable component', () => {
+    const props = {
+      value: true,
+      readonly: false,
+      disabled: false,
+      size: 'test-size',
+      loading: false,
+      color: 'test-color',
+
+    }
+    expect(() => testIsContextableComponent(VaSwitch, props).not.toThrow())
   })
 })
