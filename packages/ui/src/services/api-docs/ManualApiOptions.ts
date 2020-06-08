993c766a3d2425d7d393e5d7f8089c1328b4e13d
Kushich <45711229+Kushich@users.noreply.github.com>
2020-06-08 19:03:01 +0300

    (add) manual props type (#350)

--- packages/ui/src/services/api-docs/ManualApiOptions.ts
+++ packages/ui/src/services/api-docs/ManualApiOptions.ts
@@ -13,6 +13,7 @@ export type ManualApiOptionsItem = {
 
 export type ManualPropApiOptions = ManualApiOptionsItem & {
   hidden?: boolean; // Won't appear in documentation. Intended for internal usage props, such as icon and color configs.
+  types?: string;
   // add more here
 }
 
