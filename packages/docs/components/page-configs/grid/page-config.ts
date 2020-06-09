ef276dbbb6d9b9eed40a66feb76278890b6e90fe
Kushich <kushich.dmitry@gmail.com>
2020-06-09 16:00:44 +0300

    (add) basic structure

---
+++ packages/docs/components/page-configs/grid/page-config.ts
@@ -0,0 +1,12 @@
+import { BlockType, ApiDocsBlock } from '../../../types/configTypes'
+
+export default [
+  {
+    type: BlockType.TITLE,
+    translationString: 'grid.title',
+  },
+  {
+    type: BlockType.PARAGRAPH,
+    translationString: 'grid.summaryText',
+  },
+] as ApiDocsBlock[]
---
+++ packages/docs/examples/grid/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
