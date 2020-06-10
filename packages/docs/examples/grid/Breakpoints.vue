09e3d891fe56cc4e3b43bd2b5a207f99db2b77de
Kushich <kushich.dmitry@gmail.com>
2020-06-10 16:39:47 +0300

    (fix) breakpoints  example

--- packages/docs/examples/grid/Breakpoints.vue
+++ packages/docs/examples/grid/Breakpoints.vue
@@ -2,25 +2,25 @@
   <va-card color="secondary" style="padding: 0.75rem;">
     <div class="row">
       <div class="flex md12">
-        <div class="flex-center">md12</div>
+        <div class="item">md12</div>
       </div>
       <div class="flex md6">
-        <div class="flex-center">md6</div>
+        <div class="item">md6</div>
       </div>
       <div class="flex md6">
-        <div class="flex-center">md6</div>
+        <div class="item">md6</div>
       </div>
     </div>
     <br />
     <div class="row">
       <div class="flex md12">
-        <div class="flex-center">md12</div>
+        <div class="item">md12</div>
       </div>
       <div class="flex md9">
-        <div class="flex-center">md9</div>
+        <div class="item">md9</div>
       </div>
       <div class="flex md3">
-        <div class="flex-center">md3</div>
+        <div class="item">md3</div>
       </div>
     </div>
   </va-card>
@@ -36,8 +36,9 @@ export default {
 <style lang="scss" scoped>
 @import 'vuestic-ui/src/components/vuestic-sass/resources/resources';
 
-.flex-center {
+.item {
   border: 1px solid $gray;
   background-color: $white;
+  text-align: center;
 }
 </style>
