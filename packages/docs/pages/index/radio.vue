20a31c5646c0c0f07bc420dd4f1e1960f96c129b
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-07-02 09:47:39 +0300

    docs for va-radio (#363)
    * add docs
    
    * add props description

--- packages/docs/pages/index/radio.vue
+++ packages/docs/pages/index/radio.vue
@@ -5,16 +5,16 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import DocsContent from '../../components/DocsContent.vue'
-import switchConfig from '../../components/page-configs/va-switch/page-config'
+import radioConfig from '../../components/page-configs/va-radio/page-config'
 
 @Component({
   components: {
     DocsContent,
   },
 })
-export default class Switch extends Vue {
+export default class Radio extends Vue {
   get configComputed () {
-    return switchConfig
+    return radioConfig
   }
 }
 </script>
