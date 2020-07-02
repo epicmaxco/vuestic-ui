7ab2f29f24ca58a162ab5d37ac511d5628974f75
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-07-02 10:07:06 +0300

    docs for va-icon (#365)
    * add docs
    
    * add translation

---
+++ packages/docs/examples/va-icon/Text.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <va-icon>close</va-icon>
+    <va-icon>&#10032;</va-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
