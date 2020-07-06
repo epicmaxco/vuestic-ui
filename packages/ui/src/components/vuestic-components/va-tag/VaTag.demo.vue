78ab67f0227d5b164ab11626a0b9d0a0cb3eaa3d
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-07-06 13:02:08 +0300

    stateful

--- packages/ui/src/components/vuestic-components/va-tag/VaTag.demo.vue
+++ packages/ui/src/components/vuestic-components/va-tag/VaTag.demo.vue
@@ -1,21 +1,36 @@
 <template>
   <VbDemo>
     <VbCard title="Default">
-      <va-tag title="vuestic" />
+      <va-tag>
+        vuestic
+      </va-tag>
+    </VbCard>
+
+    <VbCard title="Closeable">
+      <va-checkbox v-model="value" />
+      <va-tag
+        v-model="value"
+        closeable
+      >
+        vuestic
+      </va-tag>
+
     </VbCard>
   </VbDemo>
 </template>
 
 <script>
 import VaTag from './VaTag'
+import VaCheckbox from '../va-checkbox/VaCheckbox'
 
 export default {
   components: {
     VaTag,
+    VaCheckbox,
   },
   data () {
     return {
-      value: 0,
+      value: true,
     }
   },
 }
--- packages/ui/src/components/vuestic-components/va-tag/VaTag.vue
+++ packages/ui/src/components/vuestic-components/va-tag/VaTag.vue
@@ -1,40 +1,60 @@
 <template>
   <component
+    v-if="valueComputed"
     :is="computedTag"
     class="va-tag"
     :class="computedClass"
     :style="computedStyle"
     v-on="$listeners"
   >
-    <div class="va-tag__content">gfdgdfdfg</div>
+    <div class="va-tag__content">
+      <slot></slot>
+      <va-icon
+        v-if="c_closeable"
+        class="va-tag__close-icon"
+        @click="close()"
+        name="close"
+        size="18px"
+      />
+    </div>
   </component>
 </template>
 
 <script lang="ts">
+import VaIcon from '../va-icon/VaIcon.vue'
 import { ColorThemeMixin } from '../../../services/ColorThemePlugin'
 import { SizeMixin } from '../../../mixins/SizeMixin'
+import { StatefulMixin } from '../../vuestic-mixins/StatefullMixin/StatefulMixin'
 import { makeContextablePropsMixin } from '../../context-test/context-provide/ContextPlugin'
 import { Component, Mixins } from 'vue-property-decorator'
 
 const TagPropsMixin = makeContextablePropsMixin({
+  value: { type: Boolean, default: true },
+  closeable: { type: Boolean, default: false },
 })
 
 @Component({
   name: 'VaTag',
+  components: { VaIcon },
 })
 export default class VaTag extends Mixins(
+  StatefulMixin,
   ColorThemeMixin,
   SizeMixin,
   TagPropsMixin,
 ) {
   get computedTag () {
-    if (this.c_tag === 'a' || this.c_href || this.c_target) {
+    if (this.c_tag === 'a') {
       return 'a'
     }
     if (this.c_tag === 'router-link' || this.hasRouterLinkParams) {
       return 'router-link'
     }
     return 'div'
+  }
+
+  close () {
+    this.valueComputed = false
   }
 }
 </script>
@@ -45,6 +65,8 @@ export default class VaTag extends Mixins(
 .va-tag {
   display: inline-flex;
   background-color: yellowgreen;
+  border: 0.125rem solid;
+  border-color: yellowgreen;
   border-radius: 1rem;
   width: auto;
   height: auto;
@@ -52,5 +74,14 @@ export default class VaTag extends Mixins(
   min-height: initial;
   margin: 0;
   padding: 0 0.5rem;
+
+  &__content {
+    display: flex;
+    align-items: center;
+  }
+
+  &__icon {
+    cursor: pointer;
+  }
 }
 </style>
