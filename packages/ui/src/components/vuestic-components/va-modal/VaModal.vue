aa5c8fc2d8feb99e0fb9b0a528979a34a3bcad34
untael <pashamironow@gmail.com>
2020-07-07 04:24:48 +0300

    feat: transition events and refactor

--- packages/ui/src/components/vuestic-components/va-modal/VaModal.vue
+++ packages/ui/src/components/vuestic-components/va-modal/VaModal.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="va-modal">
-    <ModalElement
+    <transition
       name="va-modal__overlay--transition"
-      :is-transition="!c_withoutTransitions"
+      :duration="c_withoutTransitions ? 0 : 300"
+      :appear="c_withoutTransitions ? false : true"
     >
       <div
         v-if="valueComputed && c_overlay"
         class="va-modal__overlay"
         :style="computedOverlayStyles"
       />
-    </ModalElement>
-    <ModalElement
+    </transition>
+    <transition
       name="va-modal__container--transition"
-      :is-transition="!c_withoutTransitions"
+      :duration="c_withoutTransitions ? 0 : 300"
+      :appear="c_withoutTransitions ? false : true"
+      @beforeEnter="onBeforeEnterTransition"
+      @afterEnter="onAfterEnterTransition"
+      @beforeLeave="onBeforeLeaveTransition"
+      @afterLeave="onAfterLeaveTransition"
     >
       <div
         v-if="valueComputed"
@@ -69,7 +75,7 @@
           </div>
         </div>
       </div>
-    </ModalElement>
+    </transition>
   </div>
 </template>
 
@@ -113,25 +119,9 @@ const ModalPropsMixin = makeContextablePropsMixin({
   zIndex: { type: [Number, String], default: undefined },
 })
 
-@Component
-class ModalElement extends Vue {
-  @Prop() readonly isTransition!: boolean
-  @Prop() readonly name!: string
-
-  render (createElement: CreateElement) {
-    return this.isTransition
-      ? createElement(
-        'transition',
-        { props: { name: this.name, appear: true } },
-        this.$slots.default,
-      )
-      : this.$slots.default
-  }
-}
-
 @Component({
   name: 'VaModal',
-  components: { VaButton, VaIcon, ModalElement },
+  components: { VaButton, VaIcon },
 })
 export default class VaModal extends Mixins(
   StatefulMixin,
@@ -163,7 +153,7 @@ export default class VaModal extends Mixins(
       : {
         'background-color': `rgba(0, 0, 0, ${this.c_overlayOpacity || 0.6})`,
         'z-index':
-            this.c_zIndex != null ? parseInt(this.c_zIndex) - 10 : undefined,
+          this.c_zIndex != null ? parseInt(this.c_zIndex) - 10 : undefined,
       }
   }
 
@@ -242,10 +232,30 @@ export default class VaModal extends Mixins(
     this.$emit('ok')
   }
 
+  emitTransitionEvent (eventName: string): void {
+    this.$emit(eventName)
+  }
+
   listenKeyUp (e: KeyboardEvent) {
     if (e.code === 'Escape' && !this.c_noEscDismiss && !this.c_noDismiss) {
       this.cancel()
     }
+  }
+
+  onBeforeEnterTransition (el: HTMLElement) {
+    this.$emit('beforeOpen', el)
+  }
+
+  onAfterEnterTransition (el: HTMLElement) {
+    this.$emit('open', el)
+  }
+
+  onBeforeLeaveTransition (el: HTMLElement) {
+    this.$emit('beforeClose', el)
+  }
+
+  onAfterLeaveTransition (el: HTMLElement) {
+    this.$emit('close', el)
   }
 }
 </script>
