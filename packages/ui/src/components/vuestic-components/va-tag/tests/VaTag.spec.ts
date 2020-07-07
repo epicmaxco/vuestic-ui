6ef2343df5110032886b226cd042d8bcceb47b24
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-07-07 12:08:07 +0300

    some tests

--- packages/ui/src/components/vuestic-components/va-tag/tests/VaTag.spec.ts
+++ packages/ui/src/components/vuestic-components/va-tag/tests/VaTag.spec.ts
@@ -1,9 +1,36 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import VaTag from '../VaTag.vue'
 
 describe('VaTag', () => {
   it('should render without an error', () => {
     const wrapper = mount(VaTag)
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  /* 2. Button sizes */
+
+  it('large tag', () => {
+    const wrapper = shallowMount(VaTag, {
+      propsData: { size: 'large' },
+    })
+    expect(wrapper.find(VaTag).classes()).toContain('va-tag--large')
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('small tag', () => {
+    const wrapper = shallowMount(VaTag, {
+      propsData: { size: 'small' },
+    })
+    expect(wrapper.find(VaTag).classes()).toContain('va-tag--small')
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('a tag with defined href', () => {
+    const wrapper = shallowMount(VaTag, {
+      propsData: {
+        to: 'http://epic-spinners.epicmax.co/',
+      },
+    })
+    console.log(wrapper)
+    expect(wrapper.is('router-link')).toBe(true)
   })
 })
