7d9675fdb660b0fe4251a8187a8b843ea51a9c86
kushich <dmitry.kushnerevich@epicmax.co>
2020-07-15 13:24:16 +0300

    (docs): frame

---
+++ packages/docs/examples/va-tag/Size.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <va-tag
+        v-for="(size, index) in sizes"
+        :key="index"
+        :size="size"
+      >
+        {{size}} size
+      </va-tag>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sizes: [
+        'small',
+        'medium',
+        'large',
+      ],
+    }
+  },
+}
+</script>
