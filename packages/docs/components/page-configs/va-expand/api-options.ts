24aada619462c34a7c53f6e8cc303cc6714ae297
kushich <dmitry.kushnerevich@epicmax.co>
2020-07-24 10:15:19 +0300

    expand docs

--- packages/docs/components/page-configs/va-expand/api-options.ts
+++ packages/docs/components/page-configs/va-expand/api-options.ts
@@ -2,11 +2,36 @@ import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOpti
 
 export default {
   props: {
+    value: {
+      local: true,
+    },
+    header: {
+      local: true,
+    },
+    icon: {
+      local: true,
+    },
+    solid: {
+      local: true,
+    },
+    colorAll: {
+      local: true,
+    },
   },
   events: {
+    input: {
+      types: '(value: boolean) => void',
+      local: true,
+    },
   },
   methods: {
   },
   slots: {
+    default: {
+      local: true,
+    },
+    header: {
+      local: true,
+    },
   },
 } as ManualApiOptions
