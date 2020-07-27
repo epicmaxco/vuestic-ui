a4328fde86a578c32d082284746b7530d296bcd4
untael <pashamironow@gmail.com>
2020-07-27 18:13:22 +0300

    fix: VaDropdown wrong position fix + ts refactor

--- packages/ui/src/components/vuestic-components/va-dropdown/VaDropdown.vue
+++ packages/ui/src/components/vuestic-components/va-dropdown/VaDropdown.vue
@@ -7,308 +7,289 @@
       @click="onAnchorClick()"
       ref="anchor"
     >
-      <slot name="anchor" />
+      <slot name="anchor"/>
     </div>
-    <div
-      class="va-dropdown__content"
-      v-if="showContent"
-      @mouseover="isContentHoverable && onMouseOver()"
-      @mouseout="onMouseOut()"
-      ref="content"
-    >
+    <template v-if="showContent">
       <div
-        v-if="keepAnchorWidth"
-        ref="anchorWidthContainer"
-        class="va-dropdown__anchor-width-container"
-        :style="anchorWidthContainerStyles"
+        class="va-dropdown__content"
+        @mouseover="isContentHoverable && onMouseOver()"
+        @mouseout="onMouseOut()"
+        :style="keepAnchorWidth ? anchorWidthStyles : ''"
+        ref="content"
       >
-        <slot />
+        <slot/>
       </div>
-      <slot v-else />
-    </div>
+    </template>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop, Watch } from 'vue-property-decorator'
 import { createPopper } from '@popperjs/core'
 import { DebounceLoader } from 'asva-executors'
 
-export default {
-  name: 'VaDropdown',
-  data () {
-    return {
-      popperInstance: null,
-      isClicked: false,
-      anchorWidth: undefined,
+@Component
+export default class VaDropdown extends Vue {
 
-      isMouseHovered: false,
-      hoverOverDebounceLoader: new DebounceLoader(
-        async () => {
-          this.isMouseHovered = true
-        },
-        this.hoverOverTimeout,
-      ),
-      hoverOutDebounceLoader: new DebounceLoader(
-        async () => {
-          this.isMouseHovered = false
-        },
-        this.hoverOutTimeout,
-      ),
+  @Prop({ type: String, default: '' }) debugId!: string
+  @Prop({ type: String, default: '' }) position!: string
+  @Prop({ type: Number, default: 30 }) hoverOverTimeout!: number
+  @Prop({ type: Number, default: 200 }) hoverOutTimeout!: number
+  @Prop({ type: Boolean }) boundaryBody!: boolean
+  @Prop({ type: Boolean }) value!: boolean
+  @Prop({ type: Boolean }) disabled!: boolean
+  @Prop({ type: Boolean }) fixed!: boolean
+  // Means dropdown width should be the same as anchor's width.
+  @Prop({ type: Boolean }) keepAnchorWidth!: boolean
+  // If set to false - dropdown won't dodge outside container.
+  @Prop({ type: Boolean, default: false }) preventOverflow!: boolean
+  @Prop({ type: Boolean, default: true }) closeOnClickOutside!: boolean
+  @Prop({ type: Boolean, default: true }) closeOnAnchorClick!: boolean
+  @Prop({ type: Boolean, default: true }) isContentHoverable!: boolean
+  @Prop({ type: [Array, Number], default: () => [] }) offset!: number | number[]
+  @Prop({
+    type: [Array, Number, String],
+    default: 'click',
+    validator: (trigger: string): boolean => ['click', 'hover', 'none'].includes(trigger),
+  }) trigger!: string
+
+
+  @Watch('showContent')
+  onShowContentChange (value: boolean): void {
+    this.handlePopperInstance()
+  }
+
+  popperInstance: any = null
+  isClicked: boolean = false
+  isMouseHovered: boolean = false
+  anchorWidth: string | undefined = undefined
+  hoverOverDebounceLoader: DebounceLoader | null = null
+  hoverOutDebounceLoader: DebounceLoader | null = null
+
+  get anchorWidthStyles (): { width: string, maxWidth: string } {
+    return {
+      width: this.anchorWidth + 'px',
+      maxWidth: this.anchorWidth + 'px',
     }
-  },
-  created () {
+  }
+
+  get showContent (): boolean {
+    if (this.trigger === 'hover') {
+      return this.isMouseHovered
+    }
+    if (this.trigger === 'click') {
+      return this.isClicked
+    }
+    if (this.trigger === 'none') {
+      return this.value
+    }
+
+    return false
+  }
+
+  get scrollWidth (): number {
+    const div: HTMLElement = document.createElement('div')
+
+    div.style.overflowY = 'scroll'
+    div.style.width = '50px'
+    div.style.height = '50px'
+    div.style.visibility = 'hidden'
+
+    document.body.appendChild(div)
+    const scrollWidth: number = div.offsetWidth - div.clientWidth
+    document.body.removeChild(div)
+    return scrollWidth
+  }
+
+  handlePopperInstance (): void {
+    if (this.popperInstance) {
+      this.removePopper()
+    }
+
+    if (!this.showContent) {
+      return
+    }
+
+    this.updateAnchorWidth()
+
+    this.$nextTick(() => {
+      this.initPopper()
+    })
+  }
+
+
+  onAnchorClick (): void {
+    this.$emit('anchorClick')
+    if (this.disabled) {
+      return
+    }
+    if (this.isClicked && !this.closeOnAnchorClick) {
+      return
+    }
+    this.isClicked = !this.isClicked
+  }
+
+  // Kinda complex logic here.
+  // We want to achieve 2 things:
+  // * Fast mouse-over shouldn't trigger dropdown.
+  // * Dropdown shouldn't close when you move mouse from anchor to content (even with offset).
+  onMouseOver (): void {
+    if (this.disabled) {
+      return
+    }
+    if (!this.isMouseHovered) {
+      this.hoverOverDebounceLoader && this.hoverOverDebounceLoader.run()
+    }
+
+    this.hoverOutDebounceLoader && this.hoverOutDebounceLoader.reset()
+  }
+
+
+  onMouseOut (): void {
+    if (!this.isContentHoverable) {
+      this.isMouseHovered = false
+    }
+    this.hoverOutDebounceLoader && this.hoverOutDebounceLoader.run()
+    this.hoverOverDebounceLoader && this.hoverOverDebounceLoader.reset()
+  }
+
+
+  registerClickOutsideListener (): void {
+    document.addEventListener('click', event => this.handleDocumentClick(event), false)
+  }
+
+
+  unregisterClickOutsideListener (): void {
+    document.removeEventListener('click', event => this.handleDocumentClick(event), false)
+  }
+
+
+  handleDocumentClick (event: any): void {
+    let el = event.target
+    const clickedElements = [] // Array because dropdowns can be nested.
+    // TODO Make DOM walk-over global, so that each dropdown doesn't have to do it.
+    while (el) {
+      clickedElements.push(el)
+      el = el.parentNode
+    }
+    const isCurrentDropdownClicked = clickedElements.includes(this.$refs.anchor) || clickedElements.includes(this.$refs.content)
+    if (isCurrentDropdownClicked) {
+      return
+    }
+    this.onClickOutside()
+  }
+
+
+  onClickOutside (): void {
+    this.$emit('clickOutside')
+    if (!this.closeOnClickOutside) {
+      return
+    }
+    this.hide()
+  }
+
+
+  updateAnchorWidth (): void {
+    if (this.keepAnchorWidth) {
+      let anchorWidth = (this as any).$refs.anchor.offsetWidth
+      if (this.$refs.anchorWidthContainer && (this as any).$refs.anchorWidthContainer.scrollHeight > (this as any).$refs.anchorWidthContainer.clientHeight) {
+        anchorWidth = (this as any).$refs.anchor.offsetWidth - this.scrollWidth
+      }
+      this.anchorWidth = anchorWidth
+    }
+    if (this.popperInstance) {
+      this.popperInstance.forceUpdate()
+    }
+  }
+
+
+  // @public
+  hide (): void {
+    if (this.trigger === 'click') {
+      this.isClicked = false
+    }
+  }
+
+
+  initPopper (): void {
+    const options: any = {
+      placement: this.position || 'bottom',
+      modifiers: [],
+      strategy: this.fixed ? 'fixed' : undefined,
+      onFirstUpdate: () => {
+        this.$emit('input', true)
+      },
+    }
+
+    const preventOverflow: any = {
+      name: 'preventOverflow',
+      options: {},
+    }
+    if (this.preventOverflow) {
+      options.modifiers.push(preventOverflow)
+    }
+    if (this.boundaryBody) {
+      preventOverflow.options.boundary = document.body
+    }
+
+    if (this.offset) {
+      options.modifiers.push({
+        name: 'offset',
+        options: {
+          offset: Array.isArray(this.offset) ? this.offset : [this.offset],
+        },
+      })
+      // options.modifiers.keepTogether = { enabled: false }
+      // options.modifiers.arrow = { enabled: false }
+    }
+    this.popperInstance = createPopper(
+      this.$refs.anchor as Element,
+      this.$refs.content as HTMLElement,
+      options,
+    )
+  }
+
+
+  removePopper (): void {
+    this.$emit('input', false)
+
+    if (!this.popperInstance) {
+      return
+    }
+    this.popperInstance.destroy()
+    this.popperInstance = null
+  }
+
+  created (): void {
     // nuxt fix
     if (this.$isServer) {
       return
     }
     this.registerClickOutsideListener()
-  },
-  beforeDestroy () {
+  }
+
+  mounted (): void {
+    this.hoverOverDebounceLoader = new DebounceLoader(
+      async () => {
+        this.isMouseHovered = true
+      },
+      this.hoverOverTimeout,
+    )
+    this.handlePopperInstance()
+    this.hoverOutDebounceLoader = new DebounceLoader(
+      async () => {
+        this.isMouseHovered = false
+      },
+      this.hoverOutTimeout,
+    )
+    this.handlePopperInstance()
+  }
+
+  beforeDestroy (): void {
     this.unregisterClickOutsideListener()
     this.removePopper()
-  },
-  mounted () {
-    this.handlePopperInstance()
-  },
-  watch: {
-    showContent: {
-      handler () {
-        this.handlePopperInstance()
-      },
-    },
-  },
-  props: {
-    debugId: {
-      type: String,
-      default: '',
-    },
-    position: {
-      type: String,
-      default: '',
-    },
-    boundaryBody: Boolean,
-    value: Boolean,
-    offset: {
-      type: [Array, Number],
-      default: () => [],
-    },
-    disabled: Boolean,
-    fixed: Boolean,
-    keepAnchorWidth: Boolean, // Means dropdown width should be the same as anchor's width.
-    preventOverflow: { // If set to false - dropdown won't dodge outside container.
-      type: Boolean,
-      default: false,
-    },
-    closeOnClickOutside: {
-      type: Boolean,
-      default: true,
-    },
-    closeOnAnchorClick: {
-      type: Boolean,
-      default: true,
-    },
-    isContentHoverable: {
-      type: Boolean,
-      default: true,
-    },
-    trigger: {
-      default: 'click',
-      validator: trigger => ['click', 'hover', 'none'].includes(trigger),
-    },
-    hoverOverTimeout: {
-      type: Number,
-      default: 30,
-    },
-    hoverOutTimeout: {
-      type: Number,
-      default: 200,
-    },
-  },
-  methods: {
-    handlePopperInstance () {
-      if (this.popperInstance) {
-        this.removePopper()
-      }
-
-      if (!this.showContent) {
-        return
-      }
-
-      this.updateAnchorWidth()
-
-      // I'm not entirely sure why $nextTick is needed here.
-      this.$nextTick(() => {
-        this.initPopper()
-      })
-    },
-    onAnchorClick () {
-      this.$emit('anchorClick')
-      if (this.disabled) {
-        return
-      }
-      if (this.isClicked && !this.closeOnAnchorClick) {
-        return
-      }
-      this.isClicked = !this.isClicked
-    },
-    // Kinda complex logic here.
-    // We want to achieve 2 things:
-    // * Fast mouse-over shouldn't trigger dropdown.
-    // * Dropdown shouldn't close when you move mouse from anchor to content (even with offset).
-    onMouseOver () {
-      if (this.disabled) {
-        return
-      }
-      if (!this.isMouseHovered) {
-        this.hoverOverDebounceLoader.run()
-      }
-
-      this.hoverOutDebounceLoader.reset()
-    },
-    onMouseOut () {
-      if (!this.isContentHoverable) {
-        this.isMouseHovered = false
-      }
-      this.hoverOutDebounceLoader.run()
-      this.hoverOverDebounceLoader.reset()
-    },
-    registerClickOutsideListener () {
-      document.addEventListener('click', event => this.handleDocumentClick(event), false)
-    },
-    unregisterClickOutsideListener () {
-      document.removeEventListener('click', event => this.handleDocumentClick(event), false)
-    },
-    handleDocumentClick (event) {
-      let el = event.target
-      const clickedElements = [] // Array because dropdowns can be nested.
-      // TODO Make DOM walk-over global, so that each dropdown doesn't have to do it.
-      while (el) {
-        clickedElements.push(el)
-        el = el.parentNode
-      }
-      const isCurrentDropdownClicked = clickedElements.includes(this.$refs.anchor) || clickedElements.includes(this.$refs.content)
-      if (isCurrentDropdownClicked) {
-        return
-      }
-      this.onClickOutside()
-    },
-    onClickOutside () {
-      this.$emit('clickOutside')
-      if (!this.closeOnClickOutside) {
-        return
-      }
-      this.hide()
-    },
-    updateAnchorWidth () {
-      if (this.keepAnchorWidth) {
-        let anchorWidth = this.$refs.anchor.offsetWidth
-        if (this.$refs.anchorWidthContainer && this.$refs.anchorWidthContainer.scrollHeight > this.$refs.anchorWidthContainer.clientHeight) {
-          anchorWidth = this.$refs.anchor.offsetWidth - this.scrollWidth
-        }
-        this.anchorWidth = anchorWidth
-      }
-      if (this.popperInstance) {
-        this.popperInstance.forceUpdate()
-      }
-    },
-    // @public
-    hide () {
-      if (this.trigger === 'click') {
-        this.isClicked = false
-      }
-    },
-    initPopper () {
-      const options = {
-        placement: this.position || 'bottom',
-        modifiers: [],
-        strategy: this.fixed ? 'fixed' : undefined,
-        onFirstUpdate: () => {
-          this.$emit('input', true)
-        },
-      }
-
-      const preventOverflow = {
-        name: 'preventOverflow',
-        options: {},
-      }
-      if (this.preventOverflow) {
-        options.modifiers.push(preventOverflow)
-      }
-      if (this.boundaryBody) {
-        preventOverflow.options.boundary = document.body
-      }
-
-      if (this.offset) {
-        options.modifiers.push({
-          name: 'offset',
-          options: {
-            offset: Array.isArray(this.offset) ? this.offset : [this.offset],
-          },
-        })
-        // options.modifiers.keepTogether = { enabled: false }
-        // options.modifiers.arrow = { enabled: false }
-      }
-
-      this.popperInstance = createPopper(
-        this.$refs.anchor,
-        this.$refs.content,
-        options,
-      )
-
-      // temporary solution
-      this.updatePopper()
-    },
-    removePopper () {
-      this.$emit('input', false)
-
-      if (!this.popperInstance) {
-        return
-      }
-      this.popperInstance.destroy()
-      this.popperInstance = null
-    },
-    updatePopper () {
-      // used by select
-      if (!this.popperInstance) {
-        return
-      }
-      this.updateAnchorWidth()
-    },
-  },
-  computed: {
-    anchorWidthContainerStyles () {
-      return {
-        width: this.anchorWidth + 'px',
-        maxWidth: this.anchorWidth + 'px',
-      }
-    },
-    showContent () {
-      if (this.trigger === 'hover') {
-        return this.isMouseHovered
-      }
-      if (this.trigger === 'click') {
-        return this.isClicked
-      }
-      if (this.trigger === 'none') {
-        return this.value
-      }
-
-      return null
-    },
-    scrollWidth () {
-      const div = document.createElement('div')
-
-      div.style.overflowY = 'scroll'
-      div.style.width = '50px'
-      div.style.height = '50px'
-      div.style.visibility = 'hidden'
-
-      document.body.appendChild(div)
-      const scrollWidth = div.offsetWidth - div.clientWidth
-      document.body.removeChild(div)
-      return scrollWidth
-    },
-  },
+  }
 }
 </script>
 
