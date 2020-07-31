0a8035cbc906a19eaa143c9b641619ca6b0db194
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-07-31 10:04:55 +0300

    keyboard focus

--- packages/ui/src/components/vuestic-components/va-list/VaItem.vue
+++ packages/ui/src/components/vuestic-components/va-list/VaItem.vue
@@ -10,6 +10,9 @@
     :exact-active-class="exactActiveClass"
     class="va-item align--center no-wrap"
     :class="computedClass"
+    :tabindex="indexComputed"
+    @focus="onFocus"
+    @blur="isKeyboardFocused = false"
     @click="$emit('click')"
   >
     <slot />
@@ -20,6 +23,7 @@
 import { makeContextablePropsMixin } from '../../context-test/context-provide/ContextPlugin'
 import { Component, Mixins } from 'vue-property-decorator'
 import { RouterLinkMixin } from './../../vuestic-mixins/RouterLinkMixin'
+import { KeyboardOnlyFocusMixin } from './../../vuestic-mixins/KeyboardOnlyFocusMixin/KeyboardOnlyFocusMixin'
 const ItemPropsMixin = makeContextablePropsMixin({
   tag: { type: String, default: 'div' },
   clickable: { type: Boolean, default: false },
@@ -30,13 +34,24 @@ const ItemPropsMixin = makeContextablePropsMixin({
 })
 export default class VaItem extends Mixins(
   RouterLinkMixin,
+  KeyboardOnlyFocusMixin,
   ItemPropsMixin,
 ) {
+  get indexComputed () {
+    return this.c_disabled ? -1 : 0
+  }
+
   get computedClass () {
     return {
       'va-item--clickable': this.c_clickable || this.hasRouterLinkParams,
       'va-item--disabled': this.c_disabled,
+      'va-item--focus': this.isKeyboardFocused,
     }
+  }
+
+  onFocus () {
+    (this as any).KeyboardOnlyFocusMixin_onFocus()
+    this.$emit('focus')
   }
 }
 </script>
@@ -53,6 +68,10 @@ export default class VaItem extends Mixins(
       background-color: $light-gray3;
       cursor: pointer;
     }
+  }
+
+  &--focus {
+    box-shadow: 0 0 0.5rem 0 rgba(0, 0, 0, 0.1);
   }
 
   &--disabled {
