64bbbe64573aecd82189233c146df6e33bd4f061
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-08-10 19:29:06 +0300

    inner loading

--- packages/ui/src/components/vuestic-components/va-inner-loading/VaInnerLoading.spec.js
+++
@@ -1,9 +0,0 @@
-import VaInnerLoading from './VaInnerLoading'
-
-import { testIsLoadingMixin } from '../../vuestic-mixins/LoadingMixin/testIsLoadingMixin'
-
-describe('VaInnerLoading', () => {
-  it('has loading mixin', () => {
-    expect(() => testIsLoadingMixin(VaInnerLoading)).not.toThrow()
-  })
-})
