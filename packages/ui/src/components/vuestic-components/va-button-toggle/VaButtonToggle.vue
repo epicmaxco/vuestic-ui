af4389bbd99b53a7896c9ca3ca08c13417b2a420
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-08-11 11:42:57 +0300

    buttons

--- packages/ui/src/components/vuestic-components/va-button-toggle/VaButtonToggle.vue
+++ packages/ui/src/components/vuestic-components/va-button-toggle/VaButtonToggle.vue
@@ -19,82 +19,72 @@
   </div>
 </template>
 
-<script>
-import VaButtonGroup from '../va-button-group/VaButtonGroup'
-import { getGradientBackground } from '../../../services/color-functions'
-import VaButton from '../va-button/VaButton'
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator'
 
-export default {
+import VaButton from '../va-button/VaButton.vue'
+import VaButtonGroup from '../va-button-group/VaButtonGroup.vue'
+
+import { getGradientBackground } from '../../../services/color-functions'
+import { makeContextablePropsMixin } from '../../context-test/context-provide/ContextPlugin'
+
+const ButtonTogglePropsMixin = makeContextablePropsMixin({
+  options: { type: Array, default: () => [] },
+  value: { type: [String, Number], default: '' },
+  outline: { type: Boolean, default: false },
+  flat: { type: Boolean, default: false },
+  disabled: { type: Boolean, default: false },
+  size: {
+    type: String,
+    default: 'medium',
+    validator: (value: string) => {
+      return ['medium', 'small', 'large'].includes(value)
+    },
+  },
+  color: { type: String, default: 'success' },
+  toggleColor: { type: String, default: '' },
+})
+
+@Component({
   name: 'VaButtonToggle',
   components: {
     VaButtonGroup,
     VaButton,
   },
-  props: {
-    options: {
-      type: Array,
-      default () {
-        return []
-      },
-    },
-    value: {
-      type: [String, Number],
-      default: '',
-    },
-    outline: {
-      type: Boolean,
-    },
-    flat: {
-      type: Boolean,
-    },
-    disabled: {
-      type: Boolean,
-    },
-    size: {
-      type: String,
-      default: 'medium',
-      validator: value => {
-        return ['medium', 'small', 'large'].includes(value)
-      },
-    },
-    color: {
-      type: String,
-      default: 'success',
-    },
-    toggleColor: {
-      type: String,
-      default: '',
-    },
-  },
-  methods: {
-    buttonColor (buttonValue) {
-      return buttonValue === this.value && this.toggleColor ? this.toggleColor : this.color
-    },
-    buttonStyle (buttonValue) {
-      if (buttonValue !== this.value) {
-        return {}
-      }
+})
+export default class VaButtonToggle extends Mixins(
+  ButtonTogglePropsMixin,
+) {
+  buttonColor (buttonValue: any) {
+    return buttonValue === this.c_value && this.c_toggleColor ? this.c_toggleColor : this.c_color
+  }
 
-      if (this.outline || this.flat) {
-        return {
-          backgroundColor: this.$themes[this.toggleColor ? this.toggleColor : this.color],
-          color: '#ffffff',
-        }
-      } else {
-        return {
-          backgroundColor: getGradientBackground(this.$themes[this.color]),
-          filter: 'brightness(85%)',
-        }
-      }
-    },
-    buttonClass (buttonValue) {
+  buttonStyle (buttonValue: any) {
+    if (buttonValue !== this.c_value) {
+      return {}
+    }
+
+    if (this.c_outline || this.c_flat) {
       return {
-        'va-button--active': buttonValue === this.value,
+        backgroundColor: (this as any).$themes[this.c_toggleColor ? this.c_toggleColor : this.c_color],
+        color: '#ffffff',
       }
-    },
-    changeValue (value) {
-      this.$emit('input', value)
-    },
-  },
+    } else {
+      return {
+        backgroundColor: getGradientBackground((this as any).$themes[this.c_color]),
+        filter: 'brightness(85%)',
+      }
+    }
+  }
+
+  buttonClass (buttonValue: any) {
+    return {
+      'va-button--active': buttonValue === this.c_value,
+    }
+  }
+
+  changeValue (value: any) {
+    this.$emit('input', value)
+  }
 }
 </script>
