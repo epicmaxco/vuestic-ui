1a3945dc8caad7bba27465bae70223bcaa143fec
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-08-13 11:48:48 +0300

    minor

--- packages/ui/src/components/vuestic-components/va-sidebar/VaSidebar.demo.vue
+++ packages/ui/src/components/vuestic-components/va-sidebar/VaSidebar.demo.vue
@@ -1,13 +1,13 @@
 <template>
   <VbDemo>
-    <VbCard title="Sidebar" style="width: 100%; height: 100%;">
+    <VbCard title="Sidebar" style="width: 100%; height: 100%; background-color: red;">
       <div style="display: flex; justify-content: space-between;">
         <va-switch v-model="hoverState" label="hoverState" />
         <va-switch v-model="minimized" label="minimized" />
         <va-switch v-model="right" label="right" />
       </div>
       <div style="position: relative; width: inherit; height: inherit; background-color: green;">
-      <va-sidebar
+      <!-- <va-sidebar
         style="height: auto;"
         :minimized="minimized"
         :right="right"
@@ -42,6 +42,97 @@
             </sidebar-link>
           </template>
         </template>
+      </va-sidebar> -->
+      <va-sidebar
+        style="height: auto;"
+        :minimized="minimized"
+        :right="right"
+        color="secondary"
+      >
+        <template slot="menu">
+          <va-list>
+            <va-list-label>
+              Contacts
+            </va-list-label>
+
+            <va-list-item
+              v-for="(contact, index) in contacts"
+              :key="index"
+            >
+              <va-list-item-section avatar>
+                <va-avatar>
+                  <img :src="contact.img">
+                </va-avatar>
+              </va-list-item-section>
+
+              <va-list-item-section v-if="!minimized">
+                <va-list-item-label>
+                  {{ contact.name }}
+                </va-list-item-label>
+
+                <va-list-item-label caption>
+                  {{ contact.address }}
+                </va-list-item-label>
+              </va-list-item-section>
+
+              <va-list-item-section icon  v-if="!minimized">
+                <va-icon
+                  name="remove_red_eye"
+                  color="gray"
+                />
+              </va-list-item-section>
+            </va-list-item>
+          </va-list>
+        </template>
+      </va-sidebar>
+      <!-- expand -->
+      <va-sidebar
+        style="height: auto;"
+        :minimized="minimized"
+        :right="!right"
+        color="secondary"
+      >
+        <template slot="menu">
+          <va-list>
+            <va-list-label>
+              Contacts
+            </va-list-label>
+            <va-expand-group v-model="valueExpand">
+            <va-expand
+              v-for="(contact, index) in contacts"
+              :key="index"
+            >
+              <template slot="header">
+                <va-list-item>
+                  <va-list-item-section avatar>
+                    <va-avatar>
+                      <img :src="contact.img">
+                    </va-avatar>
+                  </va-list-item-section>
+
+                  <va-list-item-section>
+                    <va-list-item-label>
+                      {{ contact.name }}
+                    </va-list-item-label>
+
+                    <va-list-item-label caption>
+                      {{ contact.address }}
+                    </va-list-item-label>
+                  </va-list-item-section>
+
+                  <va-list-item-section icon>
+                    <va-icon
+                      name="remove_red_eye"
+                      color="gray"
+                    />
+                  </va-list-item-section>
+                </va-list-item>
+              </template>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ducimus earum optio id harum eos repellendus nobis, quidem beatae sapiente necessitatibus reiciendis voluptas. Dolores est, sequi quo voluptatum laboriosam optio!
+            </va-expand>
+            </va-expand-group>
+          </va-list>
+        </template>
       </va-sidebar>
       </div>
     </VbCard>
@@ -51,26 +142,51 @@
 <script>
 import VaSidebar from '../../vuestic-components/va-sidebar/VaSidebar'
 import VaSwitch from '../va-switch/VaSwitch'
-import SidebarLinkGroup from './VaSidebarLinkGroup'
-import SidebarLink from './VaSidebarLink'
+// import SidebarLinkGroup from './VaSidebarLinkGroup'
+// import SidebarLink from './VaSidebarLink'
+import VaExpand from '../va-expand/VaExpand'
+import VaExpandGroup from '../va-expand-group/VaExpandGroup'
+import VaList from '../va-list/VaList'
+import VaListItem from '../va-list/VaListItem'
+import VaListLabel from '../va-list/VaListLabel'
+import VaListItemSection from '../va-list/VaListItemSection'
+import VaListItemLabel from '../va-list/VaListItemLabel'
+import VaIcon from '../va-icon/VaIcon'
+import VaAvatar from '../va-avatar/VaAvatar'
 import { navigationRoutes } from './navigation-router-example'
 
 export default {
   components: {
     VaSidebar,
     VaSwitch,
-    SidebarLinkGroup,
-    SidebarLink,
+    VaIcon,
+    VaAvatar,
+    // SidebarLinkGroup,
+    // SidebarLink,
+    VaExpand,
+    VaExpandGroup,
+    VaList,
+    VaListItem,
+    VaListItemSection,
+    VaListLabel,
+    VaListItemLabel,
   },
   data () {
     return {
       value: 60,
+      valueExpand: [false, false, false, false],
       icon: 'info_outline',
       theme: 'info',
       items: navigationRoutes.routes,
       hoverState: false,
       minimized: false,
       right: false,
+      contacts: [
+        { name: 'Audrey Clay', address: '644 Vermont Court, Freelandville, Kentucky, 2619', img: 'https://randomuser.me/api/portraits/women/5.jpg' },
+        { name: 'Aguirre Klein', address: '626 Carroll Street, Roulette, Ohio, 1477', img: 'https://randomuser.me/api/portraits/men/1.jpg' },
+        { name: 'Tucker Kaufman', address: '887 Winthrop Street, Tryon, Florida, 3912', img: 'https://randomuser.me/api/portraits/men/3.jpg' },
+        { name: 'Herbert Keller', address: '286 NW. Shore St.Longwood, FL 32779', img: 'https://randomuser.me/api/portraits/men/5.jpg' },
+      ],
     }
   },
 }
