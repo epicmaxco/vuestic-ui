6303fb4db8b899329f349370faca4bb7b8e802a1
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-21 15:07:14 +0300

    fix: minor

--- packages/docs/components/page-configs/reset/page-config.ts
+++ packages/docs/components/page-configs/reset/page-config.ts
@@ -7,10 +7,6 @@ export default [
   },
   {
     type: BlockType.PARAGRAPH,
-    translationString: 'reset.summaryText',
-  },
-  {
-    type: BlockType.PARAGRAPH,
     translationString: 'reset.description',
   },
   {
