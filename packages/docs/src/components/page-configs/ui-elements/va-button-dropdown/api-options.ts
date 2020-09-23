d5ca95dcd9a0d3a13367c334b17a458cc901af27
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-23 09:53:23 +0300

    add: api text

--- packages/docs/src/components/page-configs/ui-elements/va-button-dropdown/api-options.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-button-dropdown/api-options.ts
@@ -2,11 +2,24 @@ import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOpti
 
 export default {
   props: {
+    size: { local: true },
+    value: { local: true },
+    disableButton: { local: true },
+    disableDropdown: { local: true },
+    round: { local: true },
+    split: { local: true },
+    splitTo: { local: true },
+    icon: { local: true },
+    openedIcon: { local: true },
+    position: { local: true },
   },
   events: {
+    click: { local: true, types: '`() => Event`' },
+    mainButtonClick: { local: true, types: '`() => Event`' },
   },
   methods: {
   },
   slots: {
+    default: { local: true },
   },
 } as ManualApiOptions
