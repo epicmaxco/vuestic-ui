9de2cad02636a363c71b973b4f68297d24de210e
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-23 14:36:09 +0300

    add: api text

--- packages/docs/components/page-configs/va-button-toggle/api-options.ts
+++ packages/docs/components/page-configs/va-button-toggle/api-options.ts
@@ -2,8 +2,16 @@ import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOpti
 
 export default {
   props: {
+    options: { local: true },
+    value: { local: true },
+    size: { local: true },
+    toggleColor: { local: true },
   },
   events: {
+    input: {
+      types: '(value: any) => void',
+      local: true,
+    },
   },
   methods: {
   },
