8b3c869c3a5f03e53fee7ce805cddc8e44932e7b
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-28 14:48:24 +0300

    fix: close handler

--- packages/docs/examples/va-toast/Close.vue
+++ packages/docs/examples/va-toast/Close.vue
@@ -2,7 +2,7 @@
   <div>
     <va-button @click="$vaToast.init({
       message: 'Notification with onClose handler',
-      onClose: () => closeHandler
+      onClose: closeHandler
     })">notification with custom onClose handler</va-button>
     <va-button @click="$vaToast.close('notification_4')">Close #4 toast</va-button>
     <va-button @click="$vaToast.closeAll()">Close all toasts</va-button>
