7080fb0cdf0b8b32a76d20cee8719c09ba2d1550
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-28 09:23:11 +0300

    add: examples

---
+++ packages/docs/examples/va-toast/Close.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <button @click="$vaToast.init({
+      message: 'Custom position (bottom-left)',
+      onClose: () => console.log('Handle onClose')
+    })">notification with custom onClose handler</button>
+    <button @click="$vaToast.close('notification_4')">Close #4 toast</button>
+    <button @click="$vaToast.closeAll()">Close all toasts</button>
+  </div>
+</template>
