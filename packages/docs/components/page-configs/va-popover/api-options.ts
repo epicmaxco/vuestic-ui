0d2f144622908de6131ae40342f5c6459bea1841
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-29 10:59:17 +0300

    add: api text

--- packages/docs/components/page-configs/va-popover/api-options.ts
+++ packages/docs/components/page-configs/va-popover/api-options.ts
@@ -2,6 +2,12 @@ import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOpti
 
 export default {
   props: {
+    title: { local: true },
+    message: { local: true },
+    trigger: { local: true },
+    open: { local: true },
+    placement: { local: true },
+    autoHide: { local: true },
   },
   events: {
   },
