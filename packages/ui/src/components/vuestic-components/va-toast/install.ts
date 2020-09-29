a0b2e2e34045d15ccbda2c684055a7ce149d0b17
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-29 12:45:20 +0300

    add: type declaration

--- packages/ui/src/components/vuestic-components/va-toast/install.ts
+++ packages/ui/src/components/vuestic-components/va-toast/install.ts
@@ -1,4 +1,11 @@
+import Vue from 'vue'
 import Notification from './index'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $vaToast: Notification;
+  }
+}
 
 export default {
   install (Vue: any) {
