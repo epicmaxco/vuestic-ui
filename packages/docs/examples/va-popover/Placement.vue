72d028e567a6fb59f52745b572586351e596b4f3
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-29 09:20:07 +0300

    add: examples

---
+++ packages/docs/examples/va-popover/Placement.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <va-popover
+      message="Placement Bottom"
+    >
+      <va-button>Bottom</va-button>
+    </va-popover>
+
+    <va-popover
+      placement="top"
+      message="Placement Top"
+    >
+      <va-button>Top</va-button>
+    </va-popover>
+
+    <va-popover
+      placement="left"
+      message="Placement Left"
+    >
+      <va-button>Left</va-button>
+    </va-popover>
+
+    <va-popover
+      placement="right"
+      message="Placement Right"
+    >
+      <va-button>Right</va-button>
+    </va-popover>
+  </div>
+</template>
