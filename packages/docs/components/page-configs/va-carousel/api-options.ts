fbe7033c1d8b975641571443fe5a20ec8f15bcb4
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-09-30 15:49:15 +0300

    add: api text

--- packages/docs/components/page-configs/va-carousel/api-options.ts
+++ packages/docs/components/page-configs/va-carousel/api-options.ts
@@ -2,11 +2,26 @@ import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOpti
 
 export default {
   props: {
+    autoplay: { local: true },
+    autoplayInterval: { local: true },
+    autoplayDirection: { local: true },
+    value: { local: true },
+    loop: { local: true },
+    draggable: { local: true },
+    navigation: { local: true },
+    pagination: { local: true },
+    width: { local: true },
+    height: { local: true },
+    paginationPosition: { local: true },
   },
   events: {
   },
   methods: {
   },
   slots: {
+    default: { local: true },
+    content: { local: true },
+    navigation: { local: true },
+    pagination: { local: true },
   },
 } as ManualApiOptions
