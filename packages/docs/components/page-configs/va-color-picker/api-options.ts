c8a5584101fa59eb45188eaa1ea014b338de2ef9
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-10-01 14:10:32 +0300

    add: docs

---
+++ packages/docs/components/page-configs/va-color-picker/api-options.ts
@@ -0,0 +1,13 @@
+import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOptions'
+
+export default {
+  props: {
+    value: { local: true },
+  },
+  events: {
+  },
+  methods: {
+  },
+  slots: {
+  },
+} as ManualApiOptions
