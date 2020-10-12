d1408aa04a534dadfd050e8e8dba1f959c5f66a7
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-10-12 11:46:22 +0300

    fix: update to new sidebar

--- packages/docs/pages/index/ui-elements/button-toggle.vue
+++ packages/docs/pages/index/ui-elements/button-toggle.vue
@@ -4,8 +4,8 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import DocsContent from '../../components/DocsContent.vue'
-import buttonToggleConfig from '../../components/page-configs/va-button-toggle/page-config'
+import DocsContent from '../../../components/DocsContent.vue'
+import buttonToggleConfig from '../../../components/page-configs/va-button-toggle/page-config'
 
 @Component({
   components: {
