3f5f57cc54af3c2a24fe5513d13db0f7a1c9d1fe
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-10 16:33:53 +0300

    fix: minor

--- packages/docs/components/page-configs/getting-started/nuxt-integration/page-config.ts
+++ packages/docs/components/page-configs/getting-started/nuxt-integration/page-config.ts
@@ -3,14 +3,4 @@ import { DocsHelper } from '../../../../helpers/DocsHelper'
 
 export default [
   DocsHelper.title('nuxtIntegration.title'),
-
-  DocsHelper.subtitle('all.examples'),
-
-  // examples
-  // ...DocsHelper.exampleBlock(
-  //   'nuxtIntegration.examples.yourExample.title',
-  //   'nuxtIntegration.examples.yourExample.text',
-  //   'va-nuxt-integration/Example',
-  // ),
-
 ] as ApiDocsBlock[]
