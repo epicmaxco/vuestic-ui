30dec6e58f89e778130b42fb98ec96323d70fe89
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-11 15:44:21 +0300

    minor

---
+++ packages/docs/components/languages.ts
@@ -0,0 +1,35 @@
+export const languages = [
+  {
+    code: 'en',
+    name: 'English',
+  },
+  {
+    code: 'es',
+    name: 'Español',
+  },
+  {
+    code: 'ru',
+    name: 'Русский',
+  },
+  {
+    code: 'by',
+    name: 'Беларуская',
+  },
+  {
+    code: 'de',
+    name: 'Deutsch',
+  },
+  {
+    code: 'fr',
+    name: 'Français',
+  },
+  {
+    code: 'pl',
+    name: 'Polski',
+  },
+  {
+    code: 'cn',
+    name: '中文',
+  },
+  // GENERATOR_ADD - language
+]
