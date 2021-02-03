35117ed36587afe3e04fdb826b86036f60243997
raichev-dima <dmitry.raichev@epicmax.co>
2021-02-03 14:56:56 +0300

    feat: export global config hooks

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -1,5 +1,11 @@
 import { VuesticPlugin } from './components/vuestic-plugin'
+import { useColor } from './services/ColorMixin'
+import { useGlobalConfig } from './services/GlobalConfigPlugin'
+import { useTheme } from './services/Theme'
 
 export default {
   VuesticPlugin,
+  useColor,
+  useGlobalConfig,
+  useTheme,
 }
--- packages/vue-cli-example/src/views/Home.vue
+++ packages/vue-cli-example/src/views/Home.vue
@@ -3,11 +3,13 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <va-button outline>some</va-button>
+    <va-chip color="secondary">vuestic</va-chip>
     <va-chip>vuestic</va-chip>
   </div>
 </template>
 
 <script>
+import { useGlobalConfig, useTheme } from 'vuestic-ui'
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -16,5 +18,21 @@ export default {
   components: {
     HelloWorld
   },
+  setup () {
+    const { setGlobalConfig } = useGlobalConfig()
+    const { setTheme } = useTheme()
+
+    setTheme({
+      primary: '#000fa0',
+      secondary: '#fa0000'
+    })
+
+    setGlobalConfig(config => ({
+      ...config,
+      VaChip: {
+        color: 'primary'
+      }
+    }))
+  }
 }
 </script>
