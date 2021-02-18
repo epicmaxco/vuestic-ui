13c7f1f3b6ad7f224a0c23d84fec1f43c20fd5a4
m0ksem <m0ksem1337@gmail.com>
2021-02-18 15:14:07 +0200

    created navbar demo

---
+++ packages/ui/src/components/vuestic-components/va-navbar/VaNavbar.vdemo.vue
@@ -0,0 +1,36 @@
+<template>
+  <VbDemo>
+    <VbCard style="width: 100%;">
+      <va-navbar>
+        <template #selector>
+          SELECTOR
+        </template>
+        <template #logo>
+          LOGO
+        </template>
+        <template #center>
+          CENTER
+        </template>
+        <div>
+          WITHOUT SLOT
+        </div>
+      </va-navbar>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import VaNavbar from './VaNavbar'
+
+export default {
+  components: {
+    VaNavbar,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .va-navbar {
+    color:white;
+  }
+</style>
