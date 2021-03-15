5ccd3f8f3ad32524a57762198c544d99c296861a
damirJa <zs.keda@gmail.com>
2021-03-15 23:58:36 +0500

    feat: starting installation docs

--- packages/vue-cli-docs/src/components/page-configs/getting-started/installation/page-config.ts
+++ packages/vue-cli-docs/src/components/page-configs/getting-started/installation/page-config.ts
@@ -1,9 +1,20 @@
-import { ApiDocsBlock } from '../../../../types/configTypes'
-import { DocsHelper } from '../../../../helpers/DocsHelper'
+import { ApiDocsBlock } from '@/types/configTypes'
+import { DocsHelper } from '@/helpers/DocsHelper'
+
+const config = (path: string) => {
+  return `installation.${path}`
+}
 
 export default [
-  DocsHelper.title('installation.title'),
-
+  DocsHelper.title(config('title')),
+  DocsHelper.paragraph(config('subtitle')),
+  DocsHelper.subtitle(config('cli.title')),
+  DocsHelper.paragraph(config('cli.desc')),
+  DocsHelper.paragraph(config('cli.attention')),
+  DocsHelper.paragraph(config('cli.linkToOfficial')),
+  DocsHelper.paragraph(config('cli.codeAnnotation')),
+  DocsHelper.code('vue add vuestic-ui'),
+  DocsHelper.subtitle(config('manual.title')),
   // examples
   // ...DocsHelper.exampleBlock(
   //   'installation.examples.yourExample.title',
