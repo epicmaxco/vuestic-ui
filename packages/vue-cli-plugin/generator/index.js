ea20dc96ef5a17e0835509df3653a0cb720f377a
damirJa <zs.keda@gmail.com>
2021-03-15 17:57:53 +0500

    feat: improve generator to add imports in the main.js(ts)

--- packages/vue-cli-plugin/generator/index.js
+++ packages/vue-cli-plugin/generator/index.js
@@ -1,8 +1,19 @@
 module.exports = (api, options) => {
   api.extendPackage({
     dependencies: {
-      // TODO Should use ^1.0.0 once it's out
-      'vuestic-ui': '*'
+      'vuestic-ui': '^1.0.0-alpha.15'
     }
-  })
+  });
+
+  const importString = "import { VuesticPlugin } from 'vuestic-ui';"
+  api.injectImports(api.entryFile, importString);
+  api.afterInvoke(() => {
+    const fs = require('fs');
+    const { EOL } = require('os');
+    const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' });
+    const lines = contentMain.split(/\r?\n/g);
+    const renderIndex = lines.findIndex(line => line.match(/vuestic-ui/));
+    lines[renderIndex] += `\n\nVue.use(VuesticPlugin);`;
+    fs.writeFileSync(api.resolve(api.entryFile), lines.join(EOL), { encoding: 'utf-8' });
+  });
 }
