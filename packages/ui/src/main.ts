b2512b69f735a7c4791594cad61813c8685bf30e
Pavel Mironov <pashamironow@gmail.com>
2021-03-25 17:08:05 +0300

    minor

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -2,7 +2,7 @@ import './components/vuestic-sass/resources/_css-variables.scss'
 export { VuesticPlugin } from './components/vuestic-plugin'
 export { useColor } from './services/color-config/ColorMixin'
 export { useGlobalConfig } from './services/GlobalConfigPlugin'
-export { setupColors as useTheme } from './services/color-config/color-config'
+export { setupColors } from './services/color-config/color-config'
 export {
   IconMixin,
   setupIcons,
