7ac768160c6880b2f4f9656273e2ce896ed15558
Asva <ontrew@gmail.com>
2021-03-25 16:12:18 +0300

    app-bar demo tweaks

--- packages/ui/src/components/vuestic-components/va-app-bar/VaAppBar.demo.vue
+++ packages/ui/src/components/vuestic-components/va-app-bar/VaAppBar.demo.vue
@@ -4,16 +4,7 @@
       <div class="frame">
         <div class="display">
           <va-app-bar>
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
-            <va-button-dropdown size="small" icon="ellipsis">
-              Content
-            </va-button-dropdown>
+            <AppBarTestContent/>
           </va-app-bar>
         </div>
       </div>
@@ -23,13 +14,7 @@
       <div class="frame">
         <div class="display">
           <va-app-bar color="secondary">
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
+            <AppBarTestContent/>
           </va-app-bar>
         </div>
       </div>
@@ -38,17 +23,8 @@
     <VbCard title="Bottom">
       <div class="frame">
         <div class="display">
-            <va-app-bar bottom>
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
-            <va-button-dropdown size="small" icon="ellipsis">
-              Content
-            </va-button-dropdown>
+          <va-app-bar bottom>
+            <AppBarTestContent/>
           </va-app-bar>
         </div>
       </div>
@@ -61,13 +37,7 @@
             target="#scrollable-element2"
             hide-on-scroll
           >
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
+            <AppBarTestContent/>
           </va-app-bar>
           <div
             class="content"
@@ -87,13 +57,7 @@
             hide-on-scroll
             target="#scrollable-element"
           >
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
+            <AppBarTestContent/>
           </va-app-bar>
           <div
             class="content"
@@ -112,13 +76,7 @@
             target="#scrollable-element3"
             shadow-on-scroll
           >
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
+            <AppBarTestContent/>
           </va-app-bar>
           <div
             class="content"
@@ -138,13 +96,7 @@
             shadow-on-scroll
             target="#scrollable-element4"
           >
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
+            <AppBarTestContent/>
           </va-app-bar>
           <div
             class="content"
@@ -164,13 +116,7 @@
             shadow-color="danger"
             target="#scrollable-element5"
           >
-            <va-button icon="home" :color="color" flat :round="false" />
-            <va-button icon="info" :color="color" flat :round="false" />
-            <va-button icon="battery" :color="color" flat :round="false" />
-            <va-spacer />
-            <va-button :color="color" flat :round="false">
-              Login
-            </va-button>
+            <AppBarTestContent/>
           </va-app-bar>
           <div
             class="content"
@@ -186,44 +132,35 @@
 
 <script>
 import VaAppBar from '../va-app-bar/VaAppBar'
-import VaButton from '../va-button/VaButton'
-import VaSpacer from '../va-spacer/VaSpacer'
-import VaButtonDropdown from '../va-button-dropdown/VaButtonDropdown'
+import AppBarTestContent from './AppBarTestContent.vue'
 
 export default {
   components: {
+    AppBarTestContent,
     VaAppBar,
-    VaButton,
-    VaSpacer,
-    VaButtonDropdown,
-  },
-  data () {
-    return {
-      color: '#fff',
-    }
   },
 }
 </script>
 
 <style>
-  .frame {
-    border-radius: 1rem;
-    background: #2c2c2c;
-    padding: 3rem 0.5rem;
-    display: block;
-  }
+.frame {
+  border-radius: 1rem;
+  background: #2c2c2c;
+  padding: 3rem 0.5rem;
+  display: block;
+}
 
-  .display {
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    background: linear-gradient(90deg, aqua, yellow);
-    width: 314px;
-    height: 536px;
-    overflow: hidden;
-  }
+.display {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background: linear-gradient(90deg, aqua, yellow);
+  width: 314px;
+  height: 536px;
+  overflow: hidden;
+}
 
-  .content {
-    overflow: auto;
-  }
+.content {
+  overflow: auto;
+}
 </style>
