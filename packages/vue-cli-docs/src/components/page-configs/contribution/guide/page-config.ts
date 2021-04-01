4df48e38bf182a5a6eb9d0da396b13dba7a638f8
Pavel Mironov <pashamironow@gmail.com>
2021-04-01 13:24:45 +0300

    add: contribution guide page

--- packages/vue-cli-docs/src/components/page-configs/contribution/guide/page-config.ts
+++ packages/vue-cli-docs/src/components/page-configs/contribution/guide/page-config.ts
@@ -1,32 +1,78 @@
 import { ApiDocsBlock } from '../../../../types/configTypes'
 import { DocsHelper } from '../../../../helpers/DocsHelper'
 
+const npmScripts = `
+# Run vue-book dev server
+$ yarn serve:book
+
+# Generate all needed files for component (component itself, docs, tests)
+$ yarn generate:component
+
+# Build vue-book bundle
+$ yarn build:book
+
+# Lint everything
+$ yarn lint
+
+# Run tests
+$ yarn test:unit
+
+# Run vuepress dev server
+$ yarn serve:docs
+
+# Build vuepress bundle
+$ yarn build:docs
+`
+
+const componentFolderStructure = `
+  va-checkbox // component directory
+  |- VaCheckbox.vue // component itself
+  |- VaCheckbox.demo.vue // component demo
+  |- VaCheckbox.spec.js // component tests
+`
+
 export default [
   DocsHelper.title('guide.title'),
-  DocsHelper.paragraph('guide.title'),
+  DocsHelper.paragraph('guide.description'),
 
-  DocsHelper.title('guide.pullRequests.title'),
-  DocsHelper.paragraph('guide.pullRequests.description'),
+  DocsHelper.subtitle('guide.pullRequests.title'),
+  DocsHelper.paragraph('guide.pullRequests.description.localBranches'),
+  DocsHelper.paragraph('guide.pullRequests.description.dontSubmitToMaster'),
+  DocsHelper.paragraph('guide.pullRequests.description.checkoutFeat'),
+  DocsHelper.paragraph('guide.pullRequests.description.multipleSmallCommits'),
+  DocsHelper.paragraph('guide.pullRequests.description.fixBugSteps'),
 
-  DocsHelper.title('guide.branches.title'),
-  DocsHelper.paragraph('guide.branches.description'),
+  DocsHelper.subtitle('guide.branches.title'),
+  DocsHelper.paragraph('guide.branches.publicBranches'),
+  DocsHelper.paragraph('guide.branches.localBranches'),
 
-  DocsHelper.title('guide.fileNaming.title'),
-  DocsHelper.paragraph('guide.fileNaming.description'),
+  DocsHelper.subtitle('guide.fileNaming.title'),
+  DocsHelper.paragraph('guide.fileNaming.camelCase'),
+  DocsHelper.paragraph('guide.fileNaming.kebabCase'),
+  DocsHelper.paragraph('guide.fileNaming.pascalCase'),
 
-  DocsHelper.title('guide.forCoreContributors.title'),
-  DocsHelper.paragraph('guide.forCoreContributors.description'),
+  DocsHelper.subtitle('guide.forCoreContributors.title'),
+  DocsHelper.paragraph('guide.forCoreContributors.linkPr'),
+  DocsHelper.paragraph('guide.forCoreContributors.assignYourself'),
+  DocsHelper.paragraph('guide.forCoreContributors.smallIssues'),
+  DocsHelper.paragraph('guide.forCoreContributors.singlePrPerIssue'),
+  DocsHelper.paragraph('guide.forCoreContributors.onePersonPerIssue'),
+  DocsHelper.paragraph('guide.forCoreContributors.checkYourCode'),
+  DocsHelper.paragraph('guide.forCoreContributors.weUseYarn'),
+  DocsHelper.paragraph('guide.forCoreContributors.beProactive'),
+  DocsHelper.paragraph('guide.forCoreContributors.recommendedTools'),
+  DocsHelper.paragraph('guide.forCoreContributors.workInBook'),
 
-  DocsHelper.title('guide.componentFolderStructure.title'),
-  DocsHelper.paragraph('guide.componentFolderStructure.description'),
+  DocsHelper.subtitle('guide.componentFolderStructure.title'),
+  DocsHelper.code(componentFolderStructure),
 
-  DocsHelper.title('guide.beforeReleaseWorkflow.title'),
+  DocsHelper.subtitle('guide.beforeReleaseWorkflow.title'),
   DocsHelper.paragraph('guide.beforeReleaseWorkflow.description'),
 
-  DocsHelper.title('guide.npmScripts.title'),
-  DocsHelper.paragraph('guide.npmScripts.description'),
+  DocsHelper.subtitle('guide.npmScripts.title'),
+  DocsHelper.code(npmScripts),
 
-  DocsHelper.title('guide.credits.title'),
+  DocsHelper.subtitle('guide.credits.title'),
   DocsHelper.paragraph('guide.credits.description'),
 
 ] as ApiDocsBlock[]
