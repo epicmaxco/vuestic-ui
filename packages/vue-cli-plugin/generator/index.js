1e1ce9c26c809169461d8743efb30dbd8a65efcb
Pavel Mironov <pashamironow@gmail.com>
2021-04-02 16:07:46 +0300

    fix: vue-cli-plugin

--- packages/vue-cli-plugin/generator/index.js
+++ packages/vue-cli-plugin/generator/index.js
@@ -1,19 +1,23 @@
 module.exports = (api, options) => {
+  const fs = require('fs')
+  const { EOL } = require('os')
+
   api.extendPackage({
     dependencies: {
-      'vuestic-ui': '^1.0.0-alpha.15'
-    }
-  });
-
-  const importString = "import { VuesticPlugin } from 'vuestic-ui';"
-  api.injectImports(api.entryFile, importString);
+      'vuestic-ui': require('../../ui/package').version,
+    },
+  })
+  const importString = `
+  import { VuesticPlugin } from \'vuestic-ui\';
+  import \'vuestic-ui/dist/vuestic-ui.css\'
+  `
+  api.injectImports(api.entryFile, importString)
   api.afterInvoke(() => {
-    const fs = require('fs');
-    const { EOL } = require('os');
-    const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' });
-    const lines = contentMain.split(/\r?\n/g);
-    const renderIndex = lines.findIndex(line => line.match(/vuestic-ui/));
-    lines[renderIndex] += `\n\nVue.use(VuesticPlugin);`;
-    fs.writeFileSync(api.resolve(api.entryFile), lines.join(EOL), { encoding: 'utf-8' });
-  });
+
+    const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' })
+    const lines = contentMain.split(/\r?\n/g)
+    const renderIndex = lines.findIndex(line => line.match(/app\.mount/))
+    lines[renderIndex - 1] += `\n\napp.use(VuesticPlugin);`
+    fs.writeFileSync(api.resolve(api.entryFile), lines.join(EOL), { encoding: 'utf-8' })
+  })
 }
