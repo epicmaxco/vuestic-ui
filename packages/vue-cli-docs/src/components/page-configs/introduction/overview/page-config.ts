e37c15ef64e48b8e8ef8182f96e0b9cdced790a2
Pavel Mironov <pashamironow@gmail.com>
2021-04-08 20:24:39 +0300

    docs: introduction skeleton

--- packages/vue-cli-docs/src/components/page-configs/introduction/overview/page-config.ts
+++ packages/vue-cli-docs/src/components/page-configs/introduction/overview/page-config.ts
@@ -3,4 +3,12 @@ import { DocsHelper } from '../../../../helpers/DocsHelper'
 
 export default [
   DocsHelper.title('overview.title'),
+  DocsHelper.paragraph('overview.description'),
+  DocsHelper.subtitle('overview.whyVuestic.title'),
+  DocsHelper.paragraph('overview.whyVuestic.vueThreeCompatible'),
+  DocsHelper.paragraph('overview.whyVuestic.featureRich'),
+  DocsHelper.paragraph('overview.whyVuestic.themesPreset'),
+  DocsHelper.paragraph('overview.whyVuestic.configurations'),
+  DocsHelper.paragraph('overview.whyVuestic.responsive'),
+  DocsHelper.paragraph('overview.whyVuestic.translatable'),
 ] as ApiDocsBlock[]
