1711e606787f73151e3d94b155ab3464cf271d17
m0ksem <m0ksem1337@gmail.com>
2021-04-08 11:14:25 +0300

    Added vuestic config for docs. Move docs icon aliases to docs package.

---
+++ packages/vue-cli-docs/src/config/icons-config/aliases.ts
@@ -0,0 +1,32 @@
+import { IconsConfig } from 'vuestic-ui/src/main'
+
+export const aliases: IconsConfig = [
+  {
+    name: 'account_box',
+    to: 'account_box',
+  },
+  {
+    name: 'arrow_forward_ios',
+    to: 'arrow_forward_ios',
+  },
+  {
+    name: 'face',
+    to: 'face',
+  },
+  {
+    name: 'info',
+    to: 'info',
+  },
+  {
+    name: 'remove_red_eye',
+    to: 'remove_red_eye',
+  },
+  {
+    name: 'search',
+    to: 'fa4-search',
+  },
+  {
+    name: 'warning',
+    to: 'warning',
+  },
+]
