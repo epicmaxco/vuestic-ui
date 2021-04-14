fccd82404a2dfe0ace3a4392b08341d22d9e3f07
m0ksem <m0ksem1337@gmail.com>
2021-04-14 15:28:58 +0300

    Improve config typing.

--- packages/ui/src/services/component-config/component-config.ts
+++ packages/ui/src/services/component-config/component-config.ts
@@ -1,2 +1,2 @@
 // TODO ideally we want to type component props instead  of any
-export type ComponentConfig = Record<string, Record<string, any>>
+export type ComponentConfig = { [componentName: string]: { [componentProps: string]: any } }
