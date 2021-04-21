c8842d1c978512928438a2ef1be09ff048361d6c
m0ksem <m0ksem1337@gmail.com>
2021-04-22 02:00:20 +0300

    Added lorem text to docs popups.

--- packages/vue-cli-docs/src/examples/va-modal/Overview.vue
+++ packages/vue-cli-docs/src/examples/va-modal/Overview.vue
@@ -6,13 +6,15 @@
     <va-modal v-model="showModal" :message="message" title="Overview" />
   </p>
 </template>
+
 <script>
+import message from './popup-message'
+
 export default {
   data () {
     return {
       showModal: false,
-      message:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aspernatur atque blanditiis, consequatur corporis deleniti exercitationem expedita facilis fugit inventore laborum nam nobis odit, quae quas repudiandae vitae voluptates.',
+      message,
     }
   },
 }
