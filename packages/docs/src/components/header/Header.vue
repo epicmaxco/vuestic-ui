230bb0e0f71d0391fbfa3b81716941a5de4c4130
Nikhil Sharma <58226527+NikhilSharma03@users.noreply.github.com>
2021-05-16 16:40:14 +0530

    Removed mr2

--- packages/docs/src/components/header/Header.vue
+++ packages/docs/src/components/header/Header.vue
@@ -116,9 +116,6 @@ export default class Header extends mixins(PropsMixin) {
       flex: 1 1 !important;
       justify-content: space-between;
 
-      & .mr-2{
-        padding-left:20px;
-      } 
 
       .language-dropdown{
         display:flex;
