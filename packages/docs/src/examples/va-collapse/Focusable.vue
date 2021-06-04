4bd0311ede616bb50afd583b0e6c13a98bb35e67
Matthew Mamonov <g.smellyshovel@gmail.com>
2021-06-04 17:19:55 +0300

    docs: update the va-collapse's focusable prop's docs

--- packages/docs/src/examples/va-collapse/Focusable.vue
+++ packages/docs/src/examples/va-collapse/Focusable.vue
@@ -2,49 +2,19 @@
   <div>
     <va-collapse
       v-model="value"
-      header="A default one"
-      class="mb-4"
-    >
-      <div>
-        Collapse content
-      </div>
-    </va-collapse>
-
-    <va-collapse
-      v-model="value"
-      :focusable="false"
-      header=":focusable='false'"
-      class="mb-4"
-    >
-      <div>
-        Collapse content
-      </div>
-    </va-collapse>
-
-    <span>For the next one you'll see that the button is not focused right after the default one</span>
-    <va-collapse
-      v-model="value"
+      :focusable="isFocusable"
     >
       <template #header>
-        <va-button>A button</va-button>
+        <va-button>
+          Focus me with the tab key!
+        </va-button>
       </template>
-      <div>
-        Collapse content
-      </div>
     </va-collapse>
 
-    <span>And now it is (with <code>focusable</code> prop set to <code>false</code>)</span>
-    <va-collapse
-      v-model="value"
-      :focusable="false"
-    >
-      <template #header>
-        <va-button>A button inside&nbsp;<code>&lt;va-collapse :focusable="false"&gt;</code></va-button>
-      </template>
-      <div>
-        Collapse content
-      </div>
-    </va-collapse>
+    <va-switch
+      v-model="isFocusable"
+      :label="':focusable=\'' + isFocusable + '\''"
+    />
   </div>
 </template>
 
@@ -53,6 +23,7 @@ export default {
   data () {
     return {
       value: false,
+      isFocusable: true,
     }
   },
 }
