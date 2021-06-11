a8e47066f1cf0b52d8b0f7560b81555df936f4c6
Asva <ontrew@gmail.com>
2021-06-11 19:06:47 +0300

    docs: update releases in docs

--- packages/docs/src/components/page-configs/introduction/roadmap/page-config.ts
+++ packages/docs/src/components/page-configs/introduction/roadmap/page-config.ts
@@ -7,11 +7,14 @@ export default [
   DocsHelper.subtitle('roadmap.inDevelopment.title'),
   DocsHelper.paragraph('roadmap.inDevelopment.description'),
 
-  DocsHelper.headline('roadmap.1-1.title'),
-  DocsHelper.paragraph('roadmap.1-1.description'),
+  DocsHelper.headline('roadmap.1-2.title'),
+  DocsHelper.paragraph('roadmap.1-2.description'),
 
   DocsHelper.subtitle('roadmap.released.title'),
   DocsHelper.paragraph('roadmap.released.description'),
+
+  DocsHelper.headline('roadmap.1-1.title'),
+  DocsHelper.paragraph('roadmap.1-1.description'),
 
   DocsHelper.headline('roadmap.1-0.title'),
   DocsHelper.paragraph('roadmap.1-0.description'),
