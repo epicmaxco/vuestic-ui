3f04c7870612d60c86dafb395b1fe3e705138d0f
Denys Osadchyi <osadchyidenys@gmail.com>
2021-06-22 12:52:03 +0300

    Simple renaming for better readability.

--- packages/ui/src/services/color-config/tests/color-functions.spec.ts
+++ packages/ui/src/services/color-config/tests/color-functions.spec.ts
@@ -2,27 +2,27 @@ import { setHSLAColor } from '../color-functions'
 
 describe('ColorFunctions', () => {
   describe('setHSLAColor', () => {
-    it('changeHEXhue', () => {
+    it('Changing hue of HEX color', () => {
       expect(setHSLAColor('#0000ff', { h: 100 }))
         .toEqual('hsla(100,100%,50%,1)')
     })
-    it('changeHEXsaturation', () => {
+    it('Changing saturation of HEX color', () => {
       expect(setHSLAColor('#0000ff', { s: 50 }))
         .toEqual('hsla(240,50%,50%,1)')
     })
-    it('changeHEXlightness', () => {
+    it('Changing lightness of HEX color', () => {
       expect(setHSLAColor('#0000ff', { l: 10 }))
         .toEqual('hsla(240,100%,10%,1)')
     })
-    it('changeHEXalpha', () => {
+    it('Changing alpha of HEX color', () => {
       expect(setHSLAColor('#0000ff', { a: 0 }))
         .toEqual('hsla(240,100%,50%,0)')
     })
-    it('setHEX', () => {
+    it('Changing whole HSLA at once(using HEX color)', () => {
       expect(setHSLAColor('#0000ff', { h: 200, s: 34, l: 14, a: 0.7 }))
         .toEqual('hsla(200,34%,14%,0.7)')
     })
-    it('setHSLA', () => {
+    it('Changing whole HSLA at once(using HSLA color)', () => {
       expect(setHSLAColor('hsla(25,30%,10%,0.5)', { h: 200, s: 34, l: 14, a: 0.7 }))
         .toEqual('hsla(200,34%,14%,0.7)')
     })
