5c71bbebd29ec19e1a92a44ebbc3fc7e89249e01
Maxim Vishnevsky <maximvishnevsky@ya.ru>
2021-07-01 16:51:57 +0400

    Fix #877: Docs. Languages table bind with languages.ts
    * Added status fields to languages
    
    * Added status field to language generator
    
    * Added translations status mapper for languages table
    
    * Updated translations docs

--- packages/docs/src/components/languages.ts
+++ packages/docs/src/components/languages.ts
@@ -2,14 +2,17 @@ export const languages = [
   {
     code: 'en',
     name: 'English',
+    status: 'Fully',
   },
   // {
   //   code: 'es',
   //   name: 'Español',
+  //   status: 'Partially',
   // },
   {
     code: 'ru',
     name: 'Русский',
+    status: 'Fully',
   },
   // GENERATOR_ADD - language
 ]
--- packages/docs/src/components/page-configs/contribution/translation/page-config.ts
+++ packages/docs/src/components/page-configs/contribution/translation/page-config.ts
@@ -1,14 +1,15 @@
-import { ApiDocsBlock } from '../../../../types/configTypes'
-import { DocsHelper } from '../../../../helpers/DocsHelper'
+import { ApiDocsBlock } from '@/types/configTypes'
+import { DocsHelper } from '@/helpers/DocsHelper'
 import { t } from '@/helpers/I18nHelper'
+import { languages } from '@/components/languages'
+
+const translationStatuses = languages.map((lang) => [`${lang.name} (${lang.code})`, lang.status])
 
 export default [
   DocsHelper.title('translation.title'),
   DocsHelper.paragraph('translation.description'),
   DocsHelper.table(
     [t('translation.table.language'), t('translation.table.supported')],
-    [
-      ['English (en)', '+'],
-      ['Spain (es)', 'partial'],
-    ]),
+    translationStatuses
+  ),
 ] as ApiDocsBlock[]
