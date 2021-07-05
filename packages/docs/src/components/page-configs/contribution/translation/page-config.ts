97d8929ee4cb5fedc64ce82f8947ea57f407ed21
Maxim Vishnevsky <maximvishnevsky@ya.ru>
2021-07-05 14:27:37 +0400

    Fixed column/data generator

--- packages/docs/src/components/page-configs/contribution/translation/page-config.ts
+++ packages/docs/src/components/page-configs/contribution/translation/page-config.ts
@@ -1,15 +1,18 @@
 import { ApiDocsBlock } from '@/types/configTypes'
 import { DocsHelper } from '@/helpers/DocsHelper'
-import { t } from '@/helpers/I18nHelper'
 import { languages } from '@/components/languages'
 
-const translationStatuses = languages.map((lang) => [`${lang.name} (${lang.code})`, lang.status])
+const translationStatuses = languages.map((lang) => [
+  `translation.language.${lang.code}`,
+  lang.code,
+  `translation.status.${lang.status}`
+])
 
 export default [
   DocsHelper.title('translation.title'),
   DocsHelper.paragraph('translation.description'),
   DocsHelper.table(
-    [t('translation.table.language'), t('translation.table.supported')],
+    ['translation.table.language', 'translation.table.code', 'translation.table.supported'],
     translationStatuses
   ),
 ] as ApiDocsBlock[]
