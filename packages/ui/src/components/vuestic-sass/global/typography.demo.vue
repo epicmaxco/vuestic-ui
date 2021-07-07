ecfe77cd0a663db516b9465be8733e72cf732975
Sergey Kravcov <sergey.kravcov@epicmax.co>
2021-07-07 17:27:11 +0300

    add selectors for text styles

--- packages/ui/src/components/vuestic-sass/global/typography.demo.vue
+++ packages/ui/src/components/vuestic-sass/global/typography.demo.vue
@@ -1,34 +1,42 @@
 <template>
   <VbDemo>
-    <VbCard>
+    <VbCard title="Links">
       <a
         href="/default-link"
         class="link"
       >Default Link</a>
-    </VbCard>
-    <VbCard>
+      <br>
       <a
         href="/not-visited-link"
         class="link-secondary"
       >Secondary Link</a>
-    </VbCard>
-    <VbCard>
+      <br>
       <a
         href="#"
         class="link"
       >Default link Visited</a>
-    </VbCard>
-    <VbCard>
+      <br>
       <a
         href="#"
         class="link-secondary"
       >Secondary link Visited</a>
     </VbCard>
+    <VbCard title="Text styles">
+      <div>Default Text</div>
+      <div v-for="color in textStyleColors" :key="color" :class="`text--${color}`">{{ color }} text</div>
+    </VbCard>
   </VbDemo>
 </template>
 
 <script>
-export default {}
+export default {
+  setup () {
+    const textStyleColors = ['primary', 'secondary', 'success', 'info', 'danger', 'warning', 'gray', 'dark']
+    return {
+      textStyleColors,
+    }
+  },
+}
 </script>
 
 <style scoped lang="scss">
