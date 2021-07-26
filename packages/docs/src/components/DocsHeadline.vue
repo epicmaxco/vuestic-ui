c865d080832cf91dd4a550786778472557a238ac
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-07-26 17:35:31 +0300

    feat: tweak components to create anchors hash in default language (en) locale

--- packages/docs/src/components/DocsHeadline.vue
+++ packages/docs/src/components/DocsHeadline.vue
@@ -1,7 +1,7 @@
 <template>
   <h5>
     <MarkdownView tag="span" inline :value="textComputed" />
-    <DocsAnchor :text="textComputed" />
+    <DocsAnchor :text="linkTextComputed" />
   </h5>
 </template>
 <script lang='ts'>
@@ -23,6 +23,9 @@ const PropsMixin = Vue.with(Props)
 export default class DocsHeadline extends mixins(PropsMixin) {
   get textComputed () {
     return this.$t(this.text)
+  }
+  get linkTextComputed () {
+    return this.$t(this.text, 'en')
   }
 }
 </script>
