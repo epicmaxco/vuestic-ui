3f65f28eded9b697455733a8b44bfbbeea4aaedc
Shellby <shellby666@gmail.com>
2021-07-26 04:40:55 +0300

    Composition API Select, refactor & new Composable files
    Feat: useFormComponent update
    
    WIP: New composable methods (Max, Form, List)
    
    Minor

---
+++ packages/ui/src/composables/useColor.ts
@@ -0,0 +1,35 @@
+import { PropType, computed } from "vue";
+import { getColor } from "../services/color-config/color-config";
+
+/**
+ * You could add these props to any component by destructuring them inside props option.
+ * @example
+ * props: { ...useColorProps, componentsOwnProp, etc. }
+ * It's better to add props at the beginning, to make sure that Component own props will be used instead in case of collision
+ */
+export const useColorProps = {
+  color: {
+    type: String as PropType<string>,
+    default: ''
+  }
+}
+
+export function useColor (props: any) {
+  const hasColorTheme = true
+  const theme = { getColor }
+
+  const colorComputed = computed(() => {
+    return theme.getColor(props.color)
+  })
+
+  const computeColor = (prop: string, defaultColor?: string) => {
+    return theme.getColor(prop, defaultColor)
+  }
+
+  return {
+    hasColorTheme,
+    theme,
+    colorComputed,
+    computeColor,
+  }
+}
---
+++ packages/ui/src/composables/useLoading.ts
@@ -0,0 +1,19 @@
+import { PropType } from 'vue'
+
+/**
+ * You could add these props to any component by destructuring them inside props option.
+ * @example
+ * props: { ...useLoadingProps, componentsOwnProp, etc. }
+ * It's better to add props at the beginning, to make sure that Component own props will be used instead in case of collision
+ */
+export const useLoadingProps = {
+  loading: { type: Boolean as PropType<boolean>, default: false },
+}
+
+export const useLoading = () => {
+  const hasLoading = true
+
+  return {
+    hasLoading,
+  }
+}
