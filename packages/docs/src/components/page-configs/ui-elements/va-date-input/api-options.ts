56f7389361523d5e2f4e8dd8252d9c449cadb371
m0ksem <m0ksem1337@gmail.com>
2021-07-27 19:11:20 +0300

    Fix VaDateInput api-options.

--- packages/docs/src/components/page-configs/ui-elements/va-date-input/api-options.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-date-input/api-options.ts
@@ -1,39 +1,12 @@
-import { ManualApiOptions } from 'vuestic-ui/src/services/api-docs/ManualApiOptions'
+import { defineManualApi } from '../../../DocsApi/ManualApiOptions'
 import InputApiOptions from '../va-input/api-options'
 
-const api: ManualApiOptions = {
+export default defineManualApi({
   props: {
-    modelValue: { local: true, types: 'Date | Date[] | { start: Date | null, end: Date | null }' },
-    monthNames: { local: true },
-    weekdayNames: { local: true },
-    firstWeekday: { local: true, types: "'Monday' | 'Sunday'" },
-    hideWeekDays: { local: true },
-    view: { local: true },
-    showOtherMonths: { local: true },
-    allowedDays: { local: true },
-    allowedMonths: { local: true },
-    allowedYears: { local: true },
-    weekends: { local: true },
-    highlightWeekend: { local: true },
-    highlightToday: { local: true },
-    startYear: { local: true },
-    endYear: { local: true },
-    mode: { local: true, types: "'single' | 'multiple' | 'range'" },
-    type: { local: true },
-    weekendsColor: { local: true },
-
-    format: { local: true },
-    isOpen: { local: true },
-    resetOnClose: { local: true },
+    modelValue: { types: 'Date | Date[] | { start: Date | null, end: Date | null }' },
+    firstWeekday: { types: "'Monday' | 'Sunday'" },
+    mode: { types: "'single' | 'multiple' | 'range'" },
 
     ...InputApiOptions.props,
   },
-  events: {
-  },
-  methods: {
-  },
-  slots: {
-  },
-}
-
-export default api
+})
