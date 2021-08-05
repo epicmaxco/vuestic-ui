a8066b250c842dc7a7e55532b4502c677742480d
m0ksem <m0ksem1337@gmail.com>
2021-08-05 15:27:28 +0300

    Move vuestic-components to separate file.

---
+++ packages/ui/src/components/vuestic-components.ts
@@ -0,0 +1,70 @@
+export { default as VaAccordion } from './vuestic-components/va-accordion'
+export { default as VaAffix } from './vuestic-components/va-affix'
+export { default as VaAlert } from './vuestic-components/va-alert'
+export { default as VaAppBar } from './vuestic-components/va-app-bar'
+export { default as VaAvatar } from './vuestic-components/va-avatar'
+export { default as VaBacktop } from './vuestic-components/va-backtop'
+export { default as VaBadge } from './vuestic-components/va-badge'
+export { default as VaButton } from './vuestic-components/va-button'
+export { default as VaButtonDropdown } from './vuestic-components/va-button-dropdown'
+export { default as VaButtonGroup } from './vuestic-components/va-button-group'
+export { default as VaButtonToggle } from './vuestic-components/va-button-toggle'
+export { default as VaBreadcrumbs, VaBreadcrumbsItem } from './vuestic-components/va-breadcrumbs'
+export {
+  default as VaCard,
+  VaCardContent,
+  VaCardTitle,
+} from './vuestic-components/va-card'
+export { default as VaCheckbox } from './vuestic-components/va-checkbox'
+export { default as VaChip } from './vuestic-components/va-chip'
+export { default as VaCollapse } from './vuestic-components/va-collapse'
+export { default as VaColorIndicator } from './vuestic-components/va-color-indicator'
+export { VaColorPalette } from './vuestic-components/va-color-palette'
+export { default as VaColorInput } from './vuestic-components/va-color-input'
+export { default as VaConfig } from './vuestic-components/va-config'
+export { default as VaContent } from './vuestic-components/va-content'
+export { default as VaDivider } from './vuestic-components/va-divider'
+// import VaDatePicker from './vuestic-components/wip-va-date-picker'
+export { default as VaDropdown, VaDropdownContent } from './vuestic-components/va-dropdown'
+export { default as VaFileUpload } from './vuestic-components/va-file-upload'
+export { default as VaHover } from './vuestic-components/va-hover'
+export { default as VaIcon } from './vuestic-components/va-icon'
+export { default as VaImage } from './vuestic-components/va-image'
+export { default as VaInnerLoading } from './vuestic-components/va-inner-loading'
+export { default as VaInput, VaInputWrapper } from './vuestic-components/va-input'
+export {
+  default as VaList,
+  VaListItem,
+  VaListItemLabel,
+  VaListItemSection,
+  VaListLabel,
+  VaListSeparator,
+} from './vuestic-components/va-list'
+export { default as VaModal } from './vuestic-components/va-modal'
+export { default as VaNavbar, VaNavbarItem } from './vuestic-components/va-navbar'
+export { default as VaPagination } from './vuestic-components/va-pagination'
+export { default as VaParallax } from './vuestic-components/va-parallax'
+export { default as VaPopover } from './vuestic-components/va-popover'
+export {
+  VaProgressBar,
+  VaProgressCircle,
+} from './vuestic-components/va-progress-bar'
+export { default as VaRadio } from './vuestic-components/va-radio'
+export { default as VaRating } from './vuestic-components/va-rating'
+export { default as VaScrollbar } from './vuestic-components/va-scrollbar'
+export { default as VaSelect } from './vuestic-components/va-select'
+export { default as VaSeparator } from './vuestic-components/va-separator'
+export { default as VaSidebar, VaSidebarItem, VaSidebarItemContent, VaSidebarItemTitle } from './vuestic-components/va-sidebar'
+export { default as VaSlider } from './vuestic-components/va-slider'
+export { default as VaSwitch } from './vuestic-components/va-switch'
+export { default as VaTabs, VaTab } from './vuestic-components/va-tabs'
+export { default as VaTimeline, VaTimelineItem } from './vuestic-components/va-timeline'
+export {
+  default as VaTreeRoot,
+  VaTreeCategory,
+  VaTreeNode,
+} from './vuestic-components/va-tree-view'
+export { default as VaSpacer } from './vuestic-components/va-spacer'
+export { default as VaForm } from './vuestic-components/va-form'
+export { default as VaOptionList } from './vuestic-components/va-option-list'
+export { default as VaInfiniteScroll } from './vuestic-components/va-infinite-scroll'
