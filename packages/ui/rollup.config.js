558b57899c13ea3cfe6f2dcbeeb767868bf08a83
m0ksem <m0ksem1337@gmail.com>
2021-08-05 18:52:49 +0300

    Turn on declarations and mifiny for esm builds.

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -3,8 +3,8 @@ import { createCJSConfig, createESMConfig, createIIFEConfig } from './build/roll
 const defaultBuildParams = { input: './src/main.ts', minify: true, sourcemap: true }
 
 export default [
-  createESMConfig({ ...defaultBuildParams, outDir: 'dist/esm', declaration: false }),
-  createESMConfig({ ...defaultBuildParams, outDir: 'dist/esm-ssr', ssr: true, declaration: false }),
+  createESMConfig({ ...defaultBuildParams, outDir: 'dist/esm', declaration: true, minify: true }),
+  createESMConfig({ ...defaultBuildParams, outDir: 'dist/esm-ssr', ssr: true, declaration: true, minify: true }),
   createIIFEConfig({ ...defaultBuildParams, outDir: 'dist/iife' }),
   createCJSConfig({ ...defaultBuildParams, outDir: 'dist/cjs' }),
 ]
