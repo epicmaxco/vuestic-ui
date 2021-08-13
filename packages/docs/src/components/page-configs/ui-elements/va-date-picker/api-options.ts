6e3f2d802c441f448445b6130a108eb643e05412
VasylHryha <vatres@ukr.net>
2021-08-13 13:07:53 +0300

    - feat: add datepicker events types

--- packages/docs/src/components/page-configs/ui-elements/va-date-picker/api-options.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-date-picker/api-options.ts
@@ -6,6 +6,29 @@ export default defineManualApi({
     firstWeekday: { types: "'Monday' | 'Sunday'" },
     mode: { types: "'single' | 'multiple' | 'range'" },
   },
+  events: {
+    'update:view': {
+      types: '`() => Object`',
+    },
+    'hover:year': {
+      types: '`() => Date | undefined`',
+    },
+    'click:year': {
+      types: '`() => Date`',
+    },
+    'hover:month': {
+      types: '`() => Date | undefined`',
+    },
+    'click:month': {
+      types: '`() => Date`',
+    },
+    'hover:day': {
+      types: '`() => Date | undefined`',
+    },
+    'click:day': {
+      types: '`() => Date`',
+    },
+  },
   slots: {
     buttonPrev: {},
     buttonNext: {},
