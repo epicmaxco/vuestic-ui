37ef04cab2489f15b5dd1493187537b70f60b77d
Albert Allagulov <aallagulov@fix.ru>
2021-08-16 10:08:53 +0300

    card

--- packages/docs/src/examples/va-card/Actions.vue
+++ packages/docs/src/examples/va-card/Actions.vue
@@ -2,19 +2,31 @@
   <div class="row row">
     <div class="flex md6 lg4">
       <va-card>
-        <va-card-title>Between horizontal alignment</va-card-title>
+        <va-card-title>
+          {{ $t('api.VaCard.examples.actions.firstTitle') }}
+        </va-card-title>
         <va-card-actions align="between">
-          <va-button>Action 1</va-button>
-          <va-button>Action 2</va-button>
+          <va-button>
+            {{ $t('api.VaCard.examples.actions.firstButton') }}
+          </va-button>
+          <va-button>
+            {{ $t('api.VaCard.examples.actions.secondButton') }}
+          </va-button>
         </va-card-actions>
       </va-card>
     </div>
     <div class="flex md6 lg4">
       <va-card>
-        <va-card-title>Stretch vertical alignment</va-card-title>
+        <va-card-title>
+          {{ $t('api.VaCard.examples.actions.secondTitle') }}
+        </va-card-title>
         <va-card-actions align="stretch" vertical>
-          <va-button>Action 1</va-button>
-          <va-button>Action 2</va-button>
+          <va-button>
+            {{ $t('api.VaCard.examples.actions.firstButton') }}
+          </va-button>
+          <va-button>
+            {{ $t('api.VaCard.examples.actions.secondButton') }}
+          </va-button>
         </va-card-actions>
       </va-card>
     </div>
