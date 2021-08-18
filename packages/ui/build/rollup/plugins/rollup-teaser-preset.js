4c64b060bc927a1dc355d8cd3b98585a0a95775e
VasylHryha <vatres@ukr.net>
2021-08-18 13:26:55 +0300

    feat/ remove JSDoc

--- packages/ui/build/rollup/plugins/rollup-teaser-preset.js
+++ packages/ui/build/rollup/plugins/rollup-teaser-preset.js
@@ -1,6 +1,5 @@
 import { terser } from 'rollup-plugin-terser'
 
-/** @param { Options } options  */
 export const terserPlugin = (options) => terser({
   keep_classnames: true,
   keep_fnames: true,
