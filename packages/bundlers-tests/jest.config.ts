248a617e20b53bbbe63d51cef7171ffd27d31bf1
m0ksem <m0ksem1337@gmail.com>
2021-08-19 12:44:59 +0300

    Init bundlers-tests package.

---
+++ packages/bundlers-tests/jest.config.ts
@@ -0,0 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+};
