60efe07be7ab157202e15e4dac79099c38316d8a
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-08-27 17:46:03 +0300

    feat: rework card page-config

--- packages/docs/src/components/page-configs/ui-elements/va-card/page-config.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-card/page-config.ts
@@ -1,140 +1,59 @@
-import { BlockType, ApiDocsBlock } from '../../../../types/configTypes'
+import { ApiDocsBlock } from '../../../../types/configTypes'
 import { DocsHelper } from '../../../../helpers/DocsHelper'
 import VaCard from 'vuestic-ui/src/components/va-card/VaCard.vue'
 import apiOptions from './api-options'
 
 const config: ApiDocsBlock[] = [
-  {
-    type: BlockType.TITLE,
-    translationString: 'card.title',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.summaryText',
-  },
-  {
-    type: BlockType.SUBTITLE,
-    translationString: 'all.examples',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.default.title',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.examples.default.text',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Default',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.colorAndGradient.title',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/ColorAndGradient',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.tag.title',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.examples.tag.text',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Tag',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.borderAndShape.title',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/BorderAndShape',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.disabled.title',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Disabled',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.link.title',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Link',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.stripe.title',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Stripe',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.image.title',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Image',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.examples.actions.title',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.examples.actions.purpose',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.examples.actions.props',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.examples.actions.values',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-card/Actions',
-  },
-  {
-    type: BlockType.SUBTITLE,
-    translationString: 'all.api',
-  },
+  DocsHelper.title('card.title'),
+  DocsHelper.paragraph('card.summaryText'),
 
+  DocsHelper.subtitle('all.examples'),
+
+  ...DocsHelper.exampleBlock(
+    'card.examples.default.title',
+    'card.examples.default.text',
+    'va-card/Default',
+  ),
+
+  DocsHelper.headline('card.examples.colorAndGradient.title'),
+  DocsHelper.example('va-card/ColorAndGradient'),
+
+  ...DocsHelper.exampleBlock(
+    'card.examples.tag.title',
+    'card.examples.tag.text',
+    'va-card/Tag',
+  ),
+
+  DocsHelper.headline('card.examples.borderAndShape.title'),
+  DocsHelper.example('va-card/BorderAndShape'),
+
+  DocsHelper.headline('card.examples.disabled.title'),
+  DocsHelper.example('va-card/Disabled'),
+
+  DocsHelper.headline('card.examples.link.title'),
+  DocsHelper.example('va-card/Link'),
+
+  DocsHelper.headline('card.examples.stripe.title'),
+  DocsHelper.example('va-card/Stripe'),
+
+  DocsHelper.headline('card.examples.image.title'),
+  DocsHelper.example('va-card/Image'),
+
+  DocsHelper.headline('card.examples.actions.title'),
+  DocsHelper.paragraph('card.examples.actions.purpose'),
+  DocsHelper.paragraph('card.examples.actions.props'),
+  DocsHelper.paragraph('card.examples.actions.values'),
+  DocsHelper.example('va-card/Actions'),
+
+  DocsHelper.subtitle('all.api'),
   DocsHelper.api(VaCard, apiOptions),
 
-  {
-    type: BlockType.SUBTITLE,
-    translationString: 'all.faq',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.faq.questions[0].question',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.faq.questions[0].answer',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'card.faq.questions[1].question',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'card.faq.questions[1].answer',
-  },
+  DocsHelper.subtitle('all.faq'),
+  DocsHelper.headline('card.faq.questions[0].question'),
+  DocsHelper.paragraph('card.faq.questions[0].answer'),
+
+  DocsHelper.headline('card.faq.questions[1].question'),
+  DocsHelper.paragraph('card.faq.questions[1].answer'),
 ]
 
 export default config
