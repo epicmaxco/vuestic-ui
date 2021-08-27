c314d646165332ed2540f3d0e95c1bb6b3d952ae
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-08-27 14:54:03 +0300

    feat: rework backtop page-config

--- packages/docs/src/components/page-configs/ui-elements/va-backtop/page-config.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-backtop/page-config.ts
@@ -1,38 +1,20 @@
-import { BlockType, ApiDocsBlock } from '../../../../types/configTypes'
+import { ApiDocsBlock } from '../../../../types/configTypes'
 import { DocsHelper } from '../../../../helpers/DocsHelper'
 import VaBacktop from 'vuestic-ui/src/components/va-backtop/VaBacktop.vue'
 import apiOptions from './api-options'
 
 const config: ApiDocsBlock[] = [
-  {
-    type: BlockType.TITLE,
-    translationString: 'backtop.title',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'backtop.summaryText',
-  },
-  {
-    type: BlockType.SUBTITLE,
-    translationString: 'all.examples',
-  },
-  {
-    type: BlockType.HEADLINE,
-    translationString: 'backtop.examples.default.title',
-  },
-  {
-    type: BlockType.PARAGRAPH,
-    translationString: 'backtop.examples.default.text',
-  },
-  {
-    type: BlockType.EXAMPLE,
-    component: 'va-backtop/Default',
-  },
-  {
-    type: BlockType.SUBTITLE,
-    translationString: 'all.api',
-  },
+  DocsHelper.title('backtop.title'),
+  DocsHelper.paragraph('backtop.summaryText'),
 
+  DocsHelper.subtitle('all.examples'),
+  ...DocsHelper.exampleBlock(
+    'backtop.examples.default.title',
+    'backtop.examples.default.text',
+    'va-backtop/Default',
+  ),
+
+  DocsHelper.subtitle('all.api'),
   DocsHelper.api(VaBacktop, apiOptions),
 ]
 
