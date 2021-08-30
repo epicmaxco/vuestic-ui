bfb21a25bc4ca571aaf907a7824ec77b64788a06
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-08-30 17:43:07 +0300

    feat: rework sidebar-item page-config

--- packages/docs/src/components/page-configs/ui-elements/va-sidebar-item/examples/active.ts
+++
@@ -1,7 +0,0 @@
-import { DocsHelper } from '../../../../../helpers/DocsHelper'
-
-export default DocsHelper.exampleBlock(
-  'sidebarItem.examples.active.title',
-  'sidebarItem.examples.active.text',
-  'va-sidebar-item/Active',
-)
--- packages/docs/src/components/page-configs/ui-elements/va-sidebar-item/page-config.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-sidebar-item/page-config.ts
@@ -2,23 +2,40 @@ import { ApiDocsBlock } from '../../../../types/configTypes'
 import { DocsHelper } from '../../../../helpers/DocsHelper'
 import VaSidebarItem from 'vuestic-ui/src/components/va-sidebar/VaSidebarItem/VaSidebarItem.vue'
 import apiOptions from './api-options'
-import { icons, simple, components, active } from './examples'
 
 const config: ApiDocsBlock[] = [
   DocsHelper.title('sidebarItem.title'),
   DocsHelper.paragraph('sidebarItem.summaryText'),
 
   DocsHelper.subtitle('all.examples'),
-  ...simple,
+
+  ...DocsHelper.exampleBlock(
+    'sidebarItem.examples.simple.title',
+    'sidebarItem.examples.simple.text',
+    'va-sidebar-item/Simple',
+  ),
   ...DocsHelper.exampleBlock(
     'sidebarItem.examples.colors.title',
     'sidebarItem.examples.colors.text',
     'va-sidebar-item/Colors',
   ),
-  ...active,
-  ...icons,
-  ...components,
+  ...DocsHelper.exampleBlock(
+    'sidebarItem.examples.active.title',
+    'sidebarItem.examples.active.text',
+    'va-sidebar-item/Active',
+  ),
+  ...DocsHelper.exampleBlock(
+    'sidebarItem.examples.icons.title',
+    'sidebarItem.examples.icons.text',
+    'va-sidebar-item/Icons',
+  ),
+  ...DocsHelper.exampleBlock(
+    'sidebarItem.examples.components.title',
+    'sidebarItem.examples.components.text',
+    'va-sidebar-item/Components',
+  ),
 
+  DocsHelper.subtitle('all.api'),
   DocsHelper.api(VaSidebarItem, apiOptions),
 ]
 
