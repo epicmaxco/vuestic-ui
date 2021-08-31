e23cfe02e0b07406c700d74917c65d58b1f9fec2
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-08-31 10:53:52 +0300

    feat: rework toast page-config

--- packages/docs/src/examples/va-toast/Default.vue
+++ packages/docs/src/examples/va-toast/Default.vue
@@ -1,4 +1,4 @@
 <template>
   <va-button class="mr-2 mb-2" @click="$vaToast.init('Hello')">Basic notification</va-button>
-  <va-button @click="$vaToast.init('\<span style=\'font-weight: bold\'>Bold message</span>')">HTML code inside</va-button>
+  <va-button class="mr-2 mb-2" @click="$vaToast.init('\<span style=\'font-weight: bold\'>Bold message</span>')">HTML code inside</va-button>
 </template>
