ef0872cfe32878df440a0fba7e1e2f83a128b32d
Albert Allagulov <aallagulov@fix.ru>
2021-09-03 14:34:37 +0300

    translate

--- packages/docs/src/components/page-configs/styles/ag-grid-wrapper/page-config.ts
+++ packages/docs/src/components/page-configs/styles/ag-grid-wrapper/page-config.ts
@@ -1,16 +1,33 @@
 import { ApiDocsBlock } from '../../../../types/configTypes'
 import { DocsHelper } from '../../../../helpers/DocsHelper'
 
+const installCommand = `
+npm install ag-grid-vue3 ag-grid-community vue-class-component@next
+// $t('all.code.or')
+yarn add ag-grid-vue3 ag-grid-community vue-class-component@next`
+
+const setClass = `<template>
+  <ag-grid-vue class='ag-grid-vuestic' ... />
+</template>`
+
+const importStyles = `<style lang="scss">
+  @import "ag-grid-community/dist/styles/ag-grid.css";
+</style>`
+
 const config: ApiDocsBlock[] = [
-  DocsHelper.title('ag-grid-wrapper.title'),
-  DocsHelper.paragraph('Если нужен более гибкая настройка отображения таблиц, то мы рекомендуем воспользоваться библиотекой <a href="https://www.ag-grid.com/vue-data-grid/getting-started">AG Grid</a>. Vuestic UI предостовляет готовую тему стилей для этой библиотеки.'),
-  DocsHelper.headline('Установка AG Grid'),
-  DocsHelper.paragraph('Для того чтобы начать пользоваться AG Grid, установите зависимости в свой проект.'),
-  DocsHelper.code('npm install ag-grid-vue3 ag-grid-community vue-class-component@next\n// $t(\'all.code.or\')\nyarn add ag-grid-vue3 ag-grid-community vue-class-component@next', 'bash'),
-  DocsHelper.paragraph('Затем сделайте импорт стилей, которые задают каркас таблиц и примените тему `ag-grid-vuestic` на компоненте таблицы.'),
-  DocsHelper.code('<template>\n  <ag-grid-vue class="ag-grid-vuestic" ... />\n</template>', 'html'),
-  DocsHelper.code('<style lang="scss">\n  @import "ag-grid-community/dist/styles/ag-grid.css";\n</style>', 'scss'),
-  ...DocsHelper.exampleBlock('Стилизация простой таблицы', 'Для правильного рендеринга размеров ячеек необходимо передать компоненту таблицы минимальный набор свойст: rowHeight, headerHeight и minColWidth.', 'grid/AgGrid'),
+  DocsHelper.title('ag-grid.title'),
+  DocsHelper.paragraph('ag-grid.description'),
+  DocsHelper.headline('ag-grid.installation'),
+  DocsHelper.paragraph('ag-grid.dependencies'),
+  DocsHelper.code(installCommand, 'bash'),
+  DocsHelper.paragraph('ag-grid.importStyles'),
+  DocsHelper.code(setClass, 'html'),
+  DocsHelper.code(importStyles, 'scss'),
+  ...DocsHelper.exampleBlock(
+    'ag-grid.simpleTableTitle',
+    'ag-grid.setOfParams',
+    'grid/AgGrid',
+  ),
 ]
 
 export default config
