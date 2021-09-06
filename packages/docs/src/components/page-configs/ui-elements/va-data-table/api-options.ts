86516a8175fccb854b88d64efd31c2dbaa9589ec
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-09-06 16:17:40 +0300

    feat: add translations to props, events and slots

--- packages/docs/src/components/page-configs/ui-elements/va-data-table/api-options.ts
+++ packages/docs/src/components/page-configs/ui-elements/va-data-table/api-options.ts
@@ -1,3 +1,36 @@
 import { defineManualApi } from '../../../DocsApi/ManualApiOptions'
 
-export default defineManualApi({})
+export default defineManualApi({
+  events: {
+    filter: {
+      types: '`() => Number`',
+    },
+    selectionChange: {
+      types: '`() => Object`',
+    },
+    sort: {
+      types: '`() => Object`',
+    },
+    'update:sortBy': {
+      types: '`() => String`',
+    },
+    'update:sortingOrder': {
+      types: '`() => TSortingOrder`',
+    },
+  },
+  slots: {
+    colgroup: {},
+    headPrepend: {},
+    head: {},
+    'head(key)': {},
+    headAppend: {},
+    bodyPrepend: {},
+    cell: {},
+    'cell(key)': {},
+    bodyAppend: {},
+    footPrepend: {},
+    foot: {},
+    'foot(key)': {},
+    footAppend: {},
+  },
+})
