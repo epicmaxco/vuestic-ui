6b8c970362b21fc1ded472bcb6ced83c24192f12
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2021-09-17 10:53:44 +0300

    Show correct vuestic ui version on docs page (#1093)

--- packages/docs/src/components/header/components/VersionDropdown.vue
+++ packages/docs/src/components/header/components/VersionDropdown.vue
@@ -1,27 +1,13 @@
 <template>
-  <!-- <va-button-dropdown
-    color="primary"
-    :label="version.value"
-    class="version-dropdown"
-    flat
-  >
-    <div
-      v-for="(option, index) in version.options"
-      :key="index"
-      @click="selectOption(option)"
-    >
-      {{ option }}
-    </div>
-  </va-button-dropdown> -->
   <div style="color: var(--va-primary);">
-    {{ version.value }}
+    v{{ uiVersion }}
   </div>
 </template>
 
 <script lang="ts">
 // @ts-nocheck
 import { Options, Vue } from 'vue-class-component'
-import VaIcon from 'vuestic-ui/src/components/va-icon'
+import packageUi from 'vuestic-ui/package.json'
 
 @Options({
   name: 'DocsVersionDropdown',
@@ -30,19 +16,11 @@ import VaIcon from 'vuestic-ui/src/components/va-icon'
 export default class VersionDropdown extends Vue {
   data () {
     return {
-      version: { options: ['v1.0'], value: 'v1.0' },
     }
   }
 
-  selectOption (option: string) {
-    this.version.value = option
+  get uiVersion () {
+    return `${packageUi.version}`
   }
 }
 </script>
-
-<style lang="scss">
-  .version-dropdown .va-button__content {
-    font-weight: bold;
-    padding-right: 1rem !important;
-  }
-</style>
