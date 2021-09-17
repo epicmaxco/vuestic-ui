a9c55a88088cb5cd4350c8128916d2cc8b83fca0
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2021-09-17 10:52:52 +0300

    remove comments

--- packages/docs/src/components/header/components/VersionDropdown.vue
+++ packages/docs/src/components/header/components/VersionDropdown.vue
@@ -1,20 +1,5 @@
 <template>
-  <!-- <va-button-dropdown
-    color="primary"
-    :label="version.value"
-    class="version-dropdown"
-    flat
-  >
-    <div
-      v-for="(option, index) in version.options"
-      :key="index"
-      @click="selectOption(option)"
-    >
-      {{ option }}
-    </div>
-  </va-button-dropdown> -->
   <div style="color: var(--va-primary);">
-<!--    {{ version.value }}-->
     v{{ uiVersion }}
   </div>
 </template>
@@ -22,7 +7,6 @@
 <script lang="ts">
 // @ts-nocheck
 import { Options, Vue } from 'vue-class-component'
-import VaIcon from 'vuestic-ui/src/components/va-icon'
 import packageUi from 'vuestic-ui/package.json'
 
 @Options({
@@ -32,23 +16,11 @@ import packageUi from 'vuestic-ui/package.json'
 export default class VersionDropdown extends Vue {
   data () {
     return {
-      // version: { options: ['v1.0'], value: 'v1.0' },
     }
   }
 
   get uiVersion () {
     return `${packageUi.version}`
   }
-
-  // selectOption (option: string) {
-  //   this.version.value = option
-  // }
 }
 </script>
-
-<style lang="scss">
-  //.version-dropdown .va-button__content {
-  //  font-weight: bold;
-  //  padding-right: 1rem !important;
-  //}
-</style>
