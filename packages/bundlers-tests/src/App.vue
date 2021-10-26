6523a012f7a35d9ca5b4317c93549fe304411520
m0ksem <m0ksem1337@gmail.com>
2021-10-26 15:24:32 +0300

    Added CSS tests to bundlers-tests.

--- packages/bundlers-tests/src/App.vue
+++ packages/bundlers-tests/src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>Hello world!</div>
+  <div class="text--uppercase display-1">This text is in default canse, but should be in uppercase because it has text--uppercase class</div>
   <va-input :modelValue="'test'" />
+
+  <div class="row justify--center">
+    <div> We </div>
+    <div> are </div>
+    <div> in row class </div>
+    But grid is not imported so we not.
+  </div>
 </template>
 
 <script lang="ts">
