6b96d00405c4a5223dba2d3d3800d8926904ef73
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2021-10-27 18:38:07 +0300

    fix utils

--- packages/docs/src/utilities/utils.ts
+++ packages/docs/src/utilities/utils.ts
@@ -4,19 +4,19 @@ export const readTemplate = async (path: string, fileName: string): Promise<any>
   return await import(
     /* webpackChunkName: "examples" */
     /* webpackMode: "lazy-once" */
-    `!raw-loader!../components/page-configs/${path}/examples/${fileName}.vue`)
+    `!raw-loader!../page-configs/${path}/examples/${fileName}.vue`)
 }
 
 export const readComponent = async (path: string, fileName: string): Promise<any> => {
   return await import(
     /* webpackChunkName: "examples" */
     /* webpackMode: "lazy-once" */
-    `./src/page-configs/${path}/examples/${fileName}.vue`)
+    `../page-configs/${path}/examples/${fileName}.vue`)
 }
 
 export const readDocsComponent = async (path: string, fileName: string): Promise<any> => {
   return await import(
     /* webpackChunkName: "docs-components" */
     /* webpackMode: "lazy-once" */
-    `./src/page-configs/${path}/components/${fileName}.vue`)
+    `../page-configs/${path}/components/${fileName}.vue`)
 }
