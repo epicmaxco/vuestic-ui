d2c7ef3006c40d3726fe3abd992478305b0ee4ce
Alexander <shellby666@gmail.com>
2021-10-27 20:32:21 +0300

    Minor

--- packages/bundlers-tests/src/App.vue
+++ packages/bundlers-tests/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="text--uppercase display-1">This text is in default canse, but should be in uppercase because it has text--uppercase class</div>
+  <div class="text--uppercase display-1">This text is in default case, but should be in uppercase because it has text--uppercase class</div>
   <va-input :modelValue="'test'" />
 
   <div class="row justify--center">
     <div> We </div>
     <div> are </div>
-    <div> in row class </div>
-    But grid is not imported so we not.
+    <div> inside the row class </div>
+    But grid is not imported so we are not.
   </div>
 </template>
 
