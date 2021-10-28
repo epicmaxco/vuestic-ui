39e8d8e3c79914a41e442d03490c653ca305e36a
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2021-10-28 11:43:29 +0300

    add docsexample class

--- packages/docs/src/helpers/DocsHelper.ts
+++ packages/docs/src/helpers/DocsHelper.ts
@@ -4,7 +4,6 @@ import {
   BlockType,
   ApiDocsBlock,
   TextBlock,
-  PathToDirectory,
   CodeString,
   LinkOptions,
   ExampleOptions, CodeLanguage, ListBlock,
@@ -13,96 +12,112 @@ import { DefineComponent } from 'vue'
 import { VueConstructor } from 'vue-class-component'
 import { TableData, TableColumn } from './../components/DocsTable/DocsTable'
 
-export const DocsHelper = {
-  title: (translationString: TranslationString): TextBlock => {
+export class DocsHelper {
+  path: string
+
+  constructor (path: string) {
+    this.path = path
+  }
+
+  title (translationString: TranslationString): TextBlock {
     return {
       type: BlockType.TITLE,
       translationString,
     }
-  },
-  subtitle: (translationString: TranslationString): TextBlock => {
+  }
+
+  subtitle (translationString: TranslationString): TextBlock {
     return {
       type: BlockType.SUBTITLE,
       translationString,
     }
-  },
-  paragraph: (translationString: TranslationString): TextBlock => {
+  }
+
+  paragraph (translationString: TranslationString): TextBlock {
     return {
       type: BlockType.PARAGRAPH,
       translationString,
     }
-  },
-  headline: (translationString: TranslationString): TextBlock => {
+  }
+
+  headline (translationString: TranslationString): TextBlock {
     return {
       type: BlockType.HEADLINE,
       translationString,
     }
-  },
-  example: (path: PathToDirectory, component: string, exampleOptions: ExampleOptions = {}): ApiDocsBlock => {
+  }
+
+  example (component: string, exampleOptions: ExampleOptions = {}): ApiDocsBlock {
     return {
       type: BlockType.EXAMPLE,
-      path,
+      path: this.path,
       component,
       exampleOptions: exampleOptions,
     }
-  },
-  component: (path: PathToDirectory, component: string): ApiDocsBlock => {
+  }
+
+  component (component: string): ApiDocsBlock {
     return {
       type: BlockType.COMPONENT,
-      path,
+      path: this.path,
       component,
     }
-  },
-  code: (code: CodeString, language: CodeLanguage = 'javascript'): ApiDocsBlock => {
+  }
+
+  code (code: CodeString, language: CodeLanguage = 'javascript'): ApiDocsBlock {
     return {
       type: BlockType.CODE,
       code,
       language,
     }
-  },
-  api: (componentOptions: DefineComponent<any, any, any, any, any, any, any, any, any, any, any> | VueConstructor, apiOptions: ManualApiOptions): ApiDocsBlock => {
+  }
+
+  api (componentOptions: DefineComponent<any, any, any, any, any, any, any, any, any, any, any> | VueConstructor, apiOptions: ManualApiOptions): ApiDocsBlock {
     return {
       type: BlockType.API,
       componentOptions,
       apiOptions,
     }
-  },
-  table: (columns: TableColumn[], tableData: TableData): ApiDocsBlock => {
+  }
+
+  table (columns: TableColumn[], tableData: TableData): ApiDocsBlock {
     return {
       type: BlockType.TABLE,
       columns,
       tableData,
     }
-  },
-  link: (text: string, href: string, options: LinkOptions = {}): ApiDocsBlock => {
+  }
+
+  link (text: string, href: string, options: LinkOptions = {}): ApiDocsBlock {
     return {
       type: BlockType.LINK,
       text,
       href,
       options,
     }
-  },
-  alert: (translationString: TranslationString, color: string): ApiDocsBlock => {
+  }
+
+  alert (translationString: TranslationString, color: string): ApiDocsBlock {
     return {
       type: BlockType.ALERT,
       translationString,
       color,
     }
-  },
-  list: (translationStringList: TranslationString[]): ListBlock => {
+  }
+
+  list (translationStringList: TranslationString[]): ListBlock {
     return {
       type: BlockType.LIST,
       translationStringList,
     }
-  },
+  }
 
   // ********** Higher level helpers ****************
-
-  exampleBlock: (title: TranslationString, description: TranslationString, path: PathToDirectory, example: string): ApiDocsBlock[] => {
+  exampleBlock (title: TranslationString, description: TranslationString, example: string): ApiDocsBlock[] {
     return [
-      DocsHelper.headline(title),
-      DocsHelper.paragraph(description),
-      DocsHelper.example(path, example),
+      this.headline(title),
+      this.paragraph(description),
+      this.example(example),
     ]
-  },
+  }
 }
