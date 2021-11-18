208290f1cf1f5da3b38e82e6799752850d479d61
Vitaly Raicheu <rvitaly1978@gmail.com>
2021-11-18 11:23:55 +0300

    Fix#1206 Fix VaToast examples (#1225)
    * fix: update toast examples
    
    * feat: minor
    
    * feat: add html prop, update examples
    
    Co-authored-by: Vitaly Raichev <vitaly.raichev@epicmax.co>

--- packages/ui/src/components/va-toast/VaToast.vue
+++ packages/ui/src/components/va-toast/VaToast.vue
@@ -13,7 +13,8 @@
         <h2 v-if="title" class="va-toast__title" v-text="title"></h2>
 
         <div class="va-toast__content" v-show="message">
-          <p v-text="message"></p>
+          <p v-if="html" v-html="computedMessage"></p>
+          <p v-else v-text="computedMessage"></p>
         </div>
 
         <div class="va-toast__content" v-if="render">
@@ -33,7 +34,7 @@
 </template>
 
 <script lang="ts">
-import { h, watch } from 'vue'
+import { h } from 'vue'
 import { prop, mixins, Vue, Options } from 'vue-class-component'
 import VaIcon from '../va-icon/VaIcon.vue'
 
@@ -45,6 +46,7 @@ class ToastProps {
   offsetY = prop<number>({ type: Number, default: 16 })
   offsetX = prop<number>({ type: Number, default: 16 })
   message = prop<string | Function>({ type: [String, Function] as any, default: '' })
+  html = prop<boolean>({ type: Boolean, default: false })
   icon = prop<string>({ type: String, default: 'close' })
   customClass = prop<string>({ type: String, default: '' })
   duration = prop<number>({ type: Number, default: 5000 })
@@ -110,6 +112,10 @@ export default class VaToast extends mixins(
       [this.positionX]: `${this.offsetX}px`,
       backgroundColor: this.colorComputed,
     }
+  }
+
+  get computedMessage () {
+    return (typeof this.message === 'function') ? this.message() : this.message
   }
 
   destroyElement () {
