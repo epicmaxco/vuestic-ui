77183eefa54c9757ed080f8589c0c4318ecbf419
Vitaly Raichev <vitaly.raichev@epicmax.co>
2021-11-23 17:15:21 +0300

    feat: add flip prop to VaIcon

--- packages/docs/src/page-configs/ui-elements/icon/examples/Rotation.vue
+++ packages/docs/src/page-configs/ui-elements/icon/examples/Rotation.vue
@@ -1,19 +1,62 @@
 <template>
+  <va-button-toggle
+    v-model="flip"
+    :options="flipOptions"
+    :rounded="false"
+    outline
+    class="mb-4"
+  />
+
+  <va-slider
+    v-model="rotation"
+    :step="5"
+    :min="-180"
+    :max="180"
+    class="mb-4"
+  >
+    <template #prepend>
+      <va-input
+        v-model.number="rotation"
+        readonly
+        style="width: 65px; flex-shrink: 0;"
+      />
+    </template>
+  </va-slider>
+
   <div>
     <va-icon
-      name="home"
-      :rotation="180"
-      class="mr-2"
+      name="thumb_up"
+      size="large"
+      :rotation="0"
+      class="mr-3"
     />
     <va-icon
-      name="home"
-      rotation="90"
-      class="mr-2"
-    />
-    <va-icon
-      name="home"
-      :rotation="-90"
-      class="mr-2"
+      name="thumb_up"
+      size="large"
+      color="#ed6c02"
+      :rotation="rotation"
+      :flip="flip"
     />
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    const flipOptions = [
+      { label: 'off', value: 'off' },
+      { label: 'horizontal', value: 'horizontal' },
+      { label: 'vertical', value: 'vertical' },
+      { label: 'both', value: 'both' },
+    ]
+
+    return {
+      flipOptions,
+      flip: flipOptions[0].value,
+      rotation: 0,
+    }
+  },
+})
+</script>
