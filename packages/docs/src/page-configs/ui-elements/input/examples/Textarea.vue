2e59d2b9f514f52facb6fe0f7a62478018e973b2
Shellby <shellby@MacBook-Pro-Shellby.local>
2021-12-20 17:53:28 +0300

    feat: update docs examples

--- packages/docs/src/page-configs/ui-elements/input/examples/Textarea.vue
+++ packages/docs/src/page-configs/ui-elements/input/examples/Textarea.vue
@@ -2,26 +2,26 @@
   <div style="width: 300px;">
     <va-input
       class="mb-4"
-      v-model="value"
+      v-model="valueDefault"
       type="textarea"
       placeholder="Basic textarea"
     />
     <va-input
       class="mb-4"
-      v-model="value"
+      v-model="valueLabel"
       type="textarea"
       label="With label"
     />
     <va-input
       class="mb-4"
-      v-model="value"
+      v-model="valueAutosize"
       type="textarea"
       label="With autosize"
       autosize
     />
     <va-input
       class="mb-4"
-      v-model="value"
+      v-model="valueLimits"
       type="textarea"
       label="With limits from 2 to 5 rows"
       :min-rows="2"
@@ -35,7 +35,10 @@ export default {
   name: 'Textarea',
   data () {
     return {
-      value: '',
+      valueDefault: '',
+      valueLabel: '',
+      valueAutosize: '',
+      valueLimits: '',
     }
   },
 }
