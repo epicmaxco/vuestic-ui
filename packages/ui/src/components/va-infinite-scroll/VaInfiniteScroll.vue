23d398496e7acb9c3dc9e29d333bbada8b13d839
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2021-12-27 17:53:37 +0300

    feat: update component, add useScroll hook

--- packages/ui/src/components/va-infinite-scroll/VaInfiniteScroll.vue
+++ packages/ui/src/components/va-infinite-scroll/VaInfiniteScroll.vue
@@ -1,30 +1,26 @@
 <template>
   <component
-    :is="$props.tag"
+    :is="tag"
     class="va-infinite-scroll"
-    :class="{'va-infinite-scroll--reversed': $props.reverse}"
+    :class="{ 'va-infinite-scroll--reversed': reverse }"
+    ref="element"
   >
-    <slot
-      name="default"
-      ref="content"
-    />
+    <slot name="default" />
+
     <div
       class="va-infinite-scroll__spinner"
-      :class="{'va-infinite-scroll__spinner--invisible': !fetching}"
-      ref="loadingSlotContainer"
+      :class="{ 'va-infinite-scroll__spinner--invisible': !fetching }"
+      ref="spinnerSlotContainer"
     >
       <slot
         name="loading"
-        v-if="!$props.disabled"
+        v-if="!disabled"
       >
-        <div
-          ref="defaultSpinner"
-          class="va-infinite-scroll__spinner__default"
-        >
+        <div class="va-infinite-scroll__spinner__default">
           <va-progress-circle
             size="small"
             :thickness="0.15"
-            :color="error ? colors.danger : colors.primary"
+            :color="spinnerColor"
             indeterminate
           />
         </div>
@@ -35,172 +31,173 @@
 
 <script lang="ts">
 import { debounce } from 'lodash-es'
-import { watch } from 'vue'
-import { mixins, Options, prop, Vue } from 'vue-class-component'
+import { computed, defineComponent, ref, watch } from 'vue'
 
 import { sleep } from '../../services/utils'
-import ColorMixin from '../../services/color-config/ColorMixin'
+import { useColor } from '../../composables/useColor'
+import { useScroll } from './hooks/useScroll'
 import { VaProgressCircle } from '../va-progress-bar'
 
-class InfiniteScrollProps {
-  load = prop<Function>({ type: Function, required: true })
-  offset = prop<number>({ type: Number, default: 500 })
-  reverse = prop<boolean>({ type: Boolean, default: false })
-  disabled = prop<boolean>({ type: Boolean, default: false })
-  scrollTarget = prop<Element | string>({ type: [Object, String], default: null })
-  debounce = prop<number>({ type: Number, default: 100 })
-  tag = prop<string>({ type: String, default: 'div' })
-}
-
-const InfiniteScrollPropsMixin = Vue.with(InfiniteScrollProps)
-
-@Options({
+export default defineComponent({
   name: 'VaInfiniteScroll',
-  components: { VaProgressCircle },
-})
-export default class VaInfiniteScroll extends mixins(
-  ColorMixin,
-  InfiniteScrollPropsMixin,
-) {
-  index = 0
-  fetching = false
-  error = false
-  initialHeight: any = null
-  debouncedLoad!: any
-  scrollTop!: number
 
-  created () {
-    watch(() => this.$props.disabled, (value) => {
+  components: { VaProgressCircle },
+
+  props: {
+    load: { type: Function, required: true },
+    offset: { type: Number, default: 500 },
+    reverse: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
+    scrollTarget: { type: [HTMLElement, String], default: null },
+    debounce: { type: Number, default: 100 },
+    tag: { type: String, default: 'div' },
+  },
+
+  setup (props) {
+    const element = ref<HTMLElement>()
+    const spinnerSlotContainer = ref<HTMLDivElement>()
+
+    const fetching = ref(false)
+    const error = ref(false)
+    const forcedScrolling = ref(false)
+    const debouncedLoad = ref()
+    const scrollPositionBeforeLoad = ref(0)
+    const prevScrollTop = ref(0)
+
+    const scrollTargetElement = computed<HTMLElement>(() => {
+      return ((typeof props.scrollTarget === 'string')
+        ? document.querySelector(props.scrollTarget)
+        : props.scrollTarget || element.value?.parentElement) as HTMLElement
+    })
+
+    const {
+      addListener,
+      removeListener,
+    } = useScroll(props, scrollTargetElement, debouncedLoad)
+
+    const { computeColor } = useColor(props)
+
+    const spinnerColor = computed(() => {
+      return error.value ? computeColor('danger') : computeColor('primary')
+    })
+
+    const spinnerHeight = computed(() => {
+      return spinnerSlotContainer.value?.offsetHeight || 0
+    })
+
+    const computedOffset = computed(() => {
+      return props.offset + spinnerHeight.value
+    })
+
+    const stop = () => {
+      if (props.disabled) { return }
+
+      fetching.value = false
+      removeListener()
+    }
+
+    const resume = () => {
+      if (props.disabled) { return }
+
+      addListener()
+    }
+
+    const onLoad = () => {
+      const { scrollTop, scrollHeight, clientHeight } = scrollTargetElement.value
+      scrollPositionBeforeLoad.value = scrollHeight - scrollTop
+      const scrollDelta = scrollTop - prevScrollTop.value
+      prevScrollTop.value = scrollTop
+
+      if (props.disabled || error.value || fetching.value) { return }
+
+      if (forcedScrolling.value) {
+        forcedScrolling.value = false
+        return
+      }
+
+      const isReverseDirection = (props.reverse && scrollDelta > 0) || (!props.reverse && scrollDelta < 0)
+      if (isReverseDirection) { return }
+
+      const offset = props.reverse ? scrollTop : scrollHeight - scrollTop - clientHeight
+      if (offset > computedOffset.value) { return }
+
+      fetching.value = true
+
+      props.load()
+        .then(finishLoading)
+        .catch(onError)
+    }
+
+    const setScrollTop = (value: number) => {
+      forcedScrolling.value = true
+      scrollTargetElement.value.scrollTop = value
+    }
+
+    const updateTargetElementScrollTop = () => {
+      const { scrollTop, scrollHeight, clientHeight } = scrollTargetElement.value
+
+      if (props.reverse) {
+        const isReverseScroll = scrollHeight - scrollTop > scrollPositionBeforeLoad.value
+
+        if (isReverseScroll || scrollTop < spinnerHeight.value) {
+          (scrollHeight - scrollPositionBeforeLoad.value > spinnerHeight.value)
+            ? setScrollTop(scrollHeight - scrollPositionBeforeLoad.value)
+            : setScrollTop(spinnerHeight.value)
+        }
+      } else if (scrollHeight - scrollTop - clientHeight <= spinnerHeight.value) {
+        setScrollTop(scrollHeight - clientHeight - spinnerHeight.value)
+      }
+    }
+
+    const finishLoading = () => {
+      updateTargetElementScrollTop()
+      fetching.value = false
+    }
+
+    const stopErrorDisplay = () => {
+      updateTargetElementScrollTop()
+      forcedScrolling.value = false
+      error.value = false
+      fetching.value = false
+    }
+
+    const onError = () => {
+      stop()
+      error.value = true
+
+      sleep(1200)
+        .then(stopErrorDisplay)
+        .then(resume)
+    }
+
+    const setDebounce = (value: number) => {
+      debouncedLoad.value = debounce(onLoad, value)
+    }
+
+    watch(() => props.debounce, (value) => {
+      setDebounce(value)
+    }, { immediate: true })
+
+    watch(() => props.disabled, (value) => {
       if (value) {
-        this.stop()
+        stop()
       } else {
-        this.resume()
+        resume()
       }
     })
 
-    watch(() => this.$props.debounce, (value) => {
-      this.setDebounce(value as number)
-    })
-  }
-
-  get scrollAmount () {
-    return this.$props.offset as number + 1 + this.$el.offsetHeight
-  }
-
-  get scrollTargetElement () {
-    return typeof this.$props.scrollTarget === 'string'
-      ? document.querySelector(this.$props.scrollTarget)
-      : this.$props.scrollTarget || this.$el.parentElement
-  }
-
-  get colors () {
     return {
-      primary: this.theme.getColor('primary', '#23e066'),
-      danger: this.theme.getColor('danger', '#e34b4a'),
+      element,
+      spinnerSlotContainer,
+
+      spinnerColor,
+      fetching,
     }
-  }
-
-  onLoad () {
-    if (this.$props.disabled || this.error || this.fetching) {
-      return
-    }
-
-    const { scrollTop, scrollHeight } = this.scrollTargetElement
-    const containerHeight = this.scrollTargetElement.offsetHeight
-    const isLoadingRequired = this.$props.reverse
-      ? scrollTop < this.scrollAmount
-      : scrollTop + containerHeight + this.scrollAmount >= scrollHeight
-    if (!isLoadingRequired) { return }
-
-    this.fetching = true
-    this.scrollTop = this.$props.reverse ? 0 : (this as any).$el.offsetHeight
-    this.initialHeight = this.$el.offsetHeight
-    this.$props.load()
-      .then(this.finishLoading).catch(this.onError)
-  }
-
-  onError () {
-    this.stop()
-    this.error = true
-    this.fetching = true
-    sleep(1200)
-      .then(this.stopErrorDisplay)
-      .then(this.resume)
-  }
-
-  stopErrorDisplay () {
-    this.scrollTargetElement.scrollTop = this.$props.reverse
-      ? this.scrollAmount
-      : this.scrollTargetElement.scrollTop - this.scrollTargetElement.offsetHeight - this.scrollAmount
-    this.error = false
-    this.fetching = false
-  }
-
-  finishLoading () {
-    this.fetching = false
-    if (this.$props.reverse) {
-      this.scrollTargetElement.scrollTop = this.$el.offsetHeight - this.initialHeight
-    }
-  }
-
-  resume () {
-    if (!this.$props.disabled) {
-      this.scrollTargetElement.addEventListener(
-        'scroll',
-        this.debouncedLoad,
-        {
-          passive: true,
-        },
-      )
-    }
-  }
-
-  stop () {
-    if (this.$props.disabled) {
-      return
-    }
-
-    this.fetching = false
-    this.scrollTargetElement.removeEventListener(
-      'scroll',
-      this.debouncedLoad,
-      { passive: true },
-    )
-  }
-
-  setDebounce (value: number) {
-    this.debouncedLoad = debounce(this.onLoad, value)
-  }
-
-  mounted () {
-    if (!this.scrollTargetElement) {
-      return
-    }
-    this.scrollTargetElement.style.overflowY = 'scroll'
-
-    if (this.$props.reverse) {
-      this.scrollTargetElement.scrollTop = this.scrollTargetElement.scrollHeight
-    }
-
-    this.setDebounce(this.$props.debounce as number)
-    this.scrollTargetElement.addEventListener('scroll', this.debouncedLoad, {
-      passive: true,
-    })
-  }
-
-  beforeUnmount () {
-    if (!this.$props.disabled) {
-      this.scrollTargetElement.removeEventListener(
-        'scroll',
-        this.debouncedLoad,
-        { passive: true },
-      )
-    }
-  }
-}
+  },
+})
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 @import "../../styles/resources";
 @import 'variables';
 
---
+++ packages/ui/src/components/va-infinite-scroll/hooks/useScroll.ts
@@ -0,0 +1,51 @@
+import { Ref, onMounted, onBeforeUnmount } from 'vue'
+
+interface ScrollTargetProps {
+  scrollTarget: HTMLElement | string;
+  disabled: boolean;
+  reverse: boolean;
+}
+
+export const useScroll = (
+  props: ScrollTargetProps,
+  element: Ref<HTMLElement | undefined>,
+  handler: Ref<any>,
+) => {
+  const addListener = () => {
+    element.value?.addEventListener(
+      'scroll',
+      handler.value,
+      { passive: true },
+    )
+  }
+
+  const removeListener = () => {
+    element.value?.removeEventListener(
+      'scroll',
+      handler.value,
+    )
+  }
+
+  onMounted(() => {
+    if (!element.value) { return }
+
+    element.value.style.overflowY = 'scroll'
+
+    if (props.reverse) {
+      element.value.scrollTop = element.value.scrollHeight
+    }
+
+    addListener()
+  })
+
+  onBeforeUnmount(() => {
+    if (props.disabled) { return }
+
+    removeListener()
+  })
+
+  return {
+    addListener,
+    removeListener,
+  }
+}
