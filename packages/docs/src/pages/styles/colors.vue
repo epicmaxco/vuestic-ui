74385ae4bfdf9cbc8984cdfed8f4188a4c0808a7
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2021-12-28 12:44:52 +0300

    add docs

---
+++ packages/docs/src/pages/styles/colors.vue
@@ -0,0 +1,21 @@
+<template>
+  <docs-content :config="configComputed" />
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+import DocsContent from '../../components/DocsContent.vue'
+import gridConfig from '../../page-configs/styles/colors/page-config'
+
+@Options({
+  name: 'DocsStylesColors',
+  components: {
+    DocsContent,
+  },
+})
+export default class Colors extends Vue {
+  get configComputed () {
+    return gridConfig
+  }
+}
+</script>
