2e8902bfc9479827c2fe00f87697418b4bace667
Maksim Nedoshev <m0ksem1337@gmail.com>
2021-12-30 12:11:57 +0200

    Tree-shaking contribution docs (#1159)
    * raw
    
    * Added en docs.

---
+++ packages/docs/src/pages/contribution/tree-shaking-requirements.vue
@@ -0,0 +1,20 @@
+<template>
+  <docs-content :config="configComputed" />
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+import DocsContent from '../../components/DocsContent.vue'
+import treeShakingRequirementsConfig from '../../components/page-configs/contribution/tree-shaking-requirements/page-config'
+
+@Options({
+  components: {
+    DocsContent,
+  },
+})
+export default class TreeShakingRequirements extends Vue {
+  get configComputed () {
+    return treeShakingRequirementsConfig
+  }
+}
+</script>
