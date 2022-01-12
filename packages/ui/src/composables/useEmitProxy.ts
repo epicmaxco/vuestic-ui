e316f109492a47f9720d0b363a67c53ac2312d69
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-01-12 15:53:48 +0300

    feat: update useEmitProxy hook

--- packages/ui/src/composables/useEmitProxy.ts
+++ packages/ui/src/composables/useEmitProxy.ts
@@ -1,21 +1,33 @@
-export const useEmitProxy = <T extends string>(events: T[]) => {
-  const createEmits = () => {
-    return events
-  }
+type Events = string | [string, string]
+
+const getEvent = (event: Events) => Array.isArray(event) ? event[0] : event
+const getEmit = (event: Events) => Array.isArray(event) ? event[1] : event
+
+export const useEmitProxy = (events: Events[]) => {
+  const createEmits = () => events.map(event => getEmit(event))
 
   const eventToListenerName = (event: string) => {
     const eventName = event.charAt(0).toUpperCase() + event.slice(1)
     return `on${eventName}`
   }
 
-  const createListeners = (emit: (event: T, ...args: any[]) => void) => {
+  const createListeners = (emit: (event: string, ...args: any[]) => void) => {
     return events.reduce((acc, key) => ({
-      ...acc, [eventToListenerName(key)]: (...args: any[]) => emit(key, ...args),
+      ...acc,
+      [eventToListenerName(getEvent(key))]: (...args: any[]) => emit(getEmit(key), ...args),
+    }), {} as Record<string, any>)
+  }
+
+  const createVOnListeners = (emit: (event: string, ...args: any[]) => void) => {
+    return events.reduce((acc, key) => ({
+      ...acc,
+      [getEvent(key)]: (...args: any[]) => emit(getEmit(key), ...args),
     }), {} as Record<string, any>)
   }
 
   return {
     createListeners,
+    createVOnListeners,
     createEmits,
   }
 }
