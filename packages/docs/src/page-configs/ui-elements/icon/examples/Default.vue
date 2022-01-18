abdd68d41169cac8c1115fbad2f40f8af5633409
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-01-18 16:02:52 +0300

    feat: update va-icon doc example

--- packages/docs/src/page-configs/ui-elements/icon/examples/Default.vue
+++ packages/docs/src/page-configs/ui-elements/icon/examples/Default.vue
@@ -1,23 +1,40 @@
 <template>
   <div>
-      <p>Material Design</p>
-      <va-icon class="material-icons">home</va-icon>
-      <va-icon name="home" class="ml-4" />
+      <p>
+        Material Design Icons<br>
+        <va-icon class="material-icons">home</va-icon>
+        <va-icon name="home" />
+      </p>
 
-      <p>Font Awesome 4</p>
-      <va-icon class="fa fa-home" />
-      <va-icon name="fa4-home" class="ml-4" />
+      <p>
+        Font Awesome 4<br>
+        <va-icon class="fa fa-home" />
+        <va-icon name="fa4-home" />
+      </p>
 
-      <p>Font Awesome 5</p>
-      <va-icon class="fas fa-home" />
-      <va-icon name="fas-home" class="ml-4" />
+      <p>
+        Font Awesome 5<br>
+        <va-icon class="fas fa-home" />
+        <va-icon name="fas-home" />
+      </p>
 
-      <p>Entypo</p>
-      <va-icon class="entypo-home" />
-      <va-icon name="entypo-home" class="ml-4" />
+      <p>
+        Entypo<br>
+        <va-icon class="entypo-home" />
+        <va-icon name="entypo-home" />
+      </p>
 
-      <p>Ionic</p>
-      <va-icon class="icon ion-md-home" />
-      <va-icon name="ion-home" class="ml-4" />
+      <p>
+        Ionic<br>
+        <va-icon class="icon ion-md-home" />
+        <va-icon name="ion-home" />
+      </p>
   </div>
 </template>
+<style lang="scss">
+p {
+  i + i {
+    margin-left: 2rem;
+  }
+}
+</style>
