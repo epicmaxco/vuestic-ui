37345e2b5ca98935a7878176b21432c9e07e84ae
Syahrizal Setiawan <47208687+syahrizalxs@users.noreply.github.com>
2022-01-25 19:10:33 +0700

    FIx #1435 VaCheckbox Id is Undefined (#1437)
    * fix issues where VACheckbox id and name undefined
    
    * add name attribut to component
    
    Co-authored-by: syahrizal.setiawan <syahrizal.setiawan@mncgroup.com>

--- packages/ui/src/components/va-checkbox/VaCheckbox.vue
+++ packages/ui/src/components/va-checkbox/VaCheckbox.vue
@@ -23,11 +23,11 @@
       >
         <input
           ref="input"
-          :id="String(id)"
-          :name="String(name)"
           type="checkbox"
           role="checkbox"
           readonly
+          :id="id"
+          :name="name"
           v-on="SetupContext.keyboardFocusListeners"
           @focus="onFocus"
           @blur="onBlur($event)"
@@ -78,6 +78,8 @@ class CheckboxProps {
   color = prop<string>({ type: String, default: 'primary' })
   checkedIcon = prop<string>({ type: String, default: 'check' })
   indeterminateIcon = prop<string>({ type: String, default: 'remove' })
+  id = prop<string>({ type: String, default: '' })
+  name = prop<string>({ type: String, default: '' })
 }
 
 const CheckboxPropsMixin = Vue.with(CheckboxProps)
