52d1be00d5e3150c894673cad5440c9df5273114
LighthouseKeeperYN <freesaber@yandex.ru>
2022-01-27 12:07:24 +0300

    refactor:  packages\ui\src\components\va-affix\VaAffix.vue (#1442)
    * refactor:  packages\ui\src\components\va-affix\VaAffix.vue

--- packages/ui/src/components/va-affix/VaAffix.vue
+++ packages/ui/src/components/va-affix/VaAffix.vue
@@ -16,168 +16,143 @@
   </div>
 </template>
 <script lang="ts">
+import { defineComponent, computed, PropType, ref, Ref, nextTick, onMounted, onBeforeUnmount } from 'vue'
 import { noop } from 'lodash-es'
-import { Options, prop, mixins, Vue } from 'vue-class-component'
 
 import {
   handleThrottledEvent,
   useEventsHandlerWithThrottle,
   getWindowHeight,
-  State, Context,
+  State,
+  Context,
 } from './VaAffix-utils'
 
-class AffixProps {
-  offsetTop = prop<number>({ type: Number, default: undefined })
-  offsetBottom = prop<number>({ type: Number, default: undefined })
-  target = prop({ type: Function, default: () => () => window })
-}
-
-const AffixPropsMixin = Vue.with(AffixProps)
-
-@Options({
+export default defineComponent({
   name: 'VaAffix',
   emits: ['change'],
-})
-export default class VaAffix extends mixins(
-  AffixPropsMixin,
-) {
-  private state: State = {
-    isTopAffixed: false,
-    isBottomAffixed: false,
-  }
+  props: {
+    offsetTop: { type: Number as PropType<number>, default: undefined },
+    offsetBottom: { type: Number as PropType<number>, default: undefined },
+    target: { type: Function as PropType<() => HTMLElement | Window>, default: () => () => window },
+  },
+  setup (props, { emit }) {
+    const getTargetElement = () => props.target()
 
-  private initialPosition?: undefined | DOMRect
-  private clearEventListeners = noop
+    const isAffixed = computed(() => state.value.isTopAffixed || state.value.isBottomAffixed)
 
-  get computedClass () {
-    return [
-      {
-        'va-affix--affixed': this.isAffixed,
-      },
-    ]
-  }
+    const state: Ref<State> = ref({
+      isTopAffixed: false,
+      isBottomAffixed: false,
+    })
+    const getState = () => state.value
+    const setState = (newState: State) => {
+      state.value = newState
+      emit('change', isAffixed)
+    }
 
-  getTargetElement () {
-    // a custom target may get rendered later than
-    // a component gets a property from the context
-    const { target } = this
-    return target()
-  }
-
-  get computedStyle () {
     const calculateTop = () => {
-      const target = this.getTargetElement()
+      const target = getTargetElement()
 
-      if (this.offsetTop === undefined) {
-        return
+      if (props.offsetTop === undefined) { return }
+
+      if (!(target instanceof Window)) {
+        const { top } = target.getBoundingClientRect()
+        return top + props.offsetTop
       }
 
-      if (target !== window) {
-        const { top } = (target as HTMLElement).getBoundingClientRect()
-        return top + this.offsetTop
-      }
-
-      return this.offsetTop
+      return props.offsetTop
     }
 
     const calculateBottom = () => {
-      const target = this.getTargetElement()
-      if (this.offsetBottom === undefined) {
-        return
+      const target = getTargetElement()
+
+      if (props.offsetBottom === undefined) { return }
+
+      if (!(target instanceof Window)) {
+        const { bottom } = target.getBoundingClientRect()
+        const { borderTopWidth, borderBottomWidth } = getComputedStyle(target)
+        const { offsetHeight, clientHeight } = target
+
+        const scrollBarHeight = offsetHeight - clientHeight - parseInt(borderTopWidth) - parseInt(borderBottomWidth)
+
+        return getWindowHeight() - (bottom - props.offsetBottom) + scrollBarHeight
       }
 
-      if (target !== window) {
-        const { bottom } = (target as HTMLElement).getBoundingClientRect()
-        const { offsetHeight, clientHeight } = (target as HTMLElement)
-        const scrollBarHeight = offsetHeight - clientHeight
-        const windowHeight = getWindowHeight()
-        return windowHeight - (bottom - this.offsetBottom) + scrollBarHeight
-      }
-
-      return this.offsetBottom
+      return props.offsetBottom
     }
 
     const convertToPixels = (calculate: () => number | undefined) => {
       const result = calculate()
+      return result === undefined ? undefined : `${result}px`
+    }
 
-      if (result === undefined) {
-        return
+    const computedClass = computed(() => [{ 'va-affix--affixed': isAffixed }])
+    const computedStyle = computed(() => ({
+      top: state.value.isTopAffixed ? convertToPixels(calculateTop) : undefined,
+      bottom: state.value.isBottomAffixed ? convertToPixels(calculateBottom) : undefined,
+      width: `${state.value.width}px`,
+    }),
+    )
+
+    const initialPosition: Ref<undefined | DOMRect> = ref()
+    const element: Ref<HTMLElement | null> = ref(null)
+    const throttledEventHandler = (eventName: string | null, event?: Event) => {
+      const context: Context = {
+        ...props,
+        initialPosition: initialPosition.value,
+        element: element.value!,
+        target: getTargetElement(),
+        setState,
+        getState,
       }
 
-      return `${result}px`
+      if (!eventName || eventName === 'resize') {
+        handleThrottledEvent(eventName, context)
+      } else if (event && event.target) {
+        const target = getTargetElement()
+
+        if (target === event.target || target instanceof Window) {
+          handleThrottledEvent(eventName, context)
+        } else {
+          // if we have a custom target but keep scrolling on another element,
+          // so just disable the affixed state
+          setState({
+            isBottomAffixed: false,
+            isTopAffixed: false,
+          })
+        }
+      }
     }
+
+    const clearEventListeners: Ref<() => any> = ref(noop)
+    onBeforeUnmount(clearEventListeners.value)
+    onMounted(() => {
+      const events = ['scroll', 'resize']
+
+      initialPosition.value = element.value!.getBoundingClientRect()
+
+      clearEventListeners.value = useEventsHandlerWithThrottle(events, {
+        handler: throttledEventHandler,
+      })
+
+      nextTick(() => {
+        // pass `null` here to make sure it is an initial call
+        throttledEventHandler(null)
+      })
+    })
 
     return {
-      top: this.state.isTopAffixed ? convertToPixels(calculateTop) : null,
-      bottom: this.state.isBottomAffixed ? convertToPixels(calculateBottom) : null,
-      width: `${this.state.width}px`,
+      computedClass,
+      computedStyle,
+      isAffixed,
+      element,
     }
-  }
-
-  get isAffixed (): boolean {
-    return this.state.isTopAffixed || this.state.isBottomAffixed
-  }
-
-  handleThrottledEvent (eventName: string | null, event?: Event) {
-    const context: Context = {
-      ...this.$data,
-      ...this.$props,
-      element: this.$refs.element as any,
-      target: this.getTargetElement(),
-      setState: this.setState.bind(this),
-      getState: this.getState.bind(this),
-    }
-
-    if (!eventName || eventName === 'resize') {
-      handleThrottledEvent(eventName, context)
-    } else if (event && event.target) {
-      const target = this.getTargetElement()
-
-      if ((target as HTMLElement) === event.target || target === window) {
-        handleThrottledEvent(eventName, context)
-      } else {
-        // if we have a custom target but keep scrolling on another element,
-        // so just disable the affixed state
-        this.setState({
-          isBottomAffixed: false,
-          isTopAffixed: false,
-        })
-      }
-    }
-  }
-
-  setState (state: State) {
-    this.state = state
-    this.$emit('change', this.isAffixed)
-  }
-
-  getState () {
-    return this.state
-  }
-
-  mounted () {
-    const events = ['scroll', 'resize']
-
-    this.initialPosition = (this.$refs.element as HTMLElement).getBoundingClientRect()
-
-    this.clearEventListeners = useEventsHandlerWithThrottle(events, {
-      handler: this.handleThrottledEvent,
-    })
-
-    this.$nextTick(() => {
-      // pass `null` here to make sure it is an initial call
-      this.handleThrottledEvent(null)
-    })
-  }
-
-  beforeUnmount () {
-    this.clearEventListeners()
-  }
-}
+  },
+})
 </script>
 
 <style lang="scss">
-@import 'variables';
+@import "variables";
 
 .va-affix {
   font-family: var(--va-font-family);
