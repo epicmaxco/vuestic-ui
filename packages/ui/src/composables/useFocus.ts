41c68fe9159c6c8fba72586b12712dd8a89735af
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-02-01 10:38:04 +0300

    feat: update useFocus

--- packages/ui/src/composables/useFocus.ts
+++ packages/ui/src/composables/useFocus.ts
@@ -1,10 +1,30 @@
 import { ref, onMounted, onBeforeUnmount, Ref } from 'vue'
 
-export function useFocus (el?: Ref<HTMLElement | null | undefined>) {
+export const useFocusEmits = ['focused', 'blurred']
+
+export function useFocus (
+  el?: Ref<HTMLElement | null | undefined>,
+  emit?: (event: 'focused' | 'blurred') => void,
+) {
   const isFocused = ref(false)
 
-  const onFocus = () => { isFocused.value = true }
-  const onBlur = () => { isFocused.value = false }
+  const onFocus = () => {
+    isFocused.value = true
+    if (emit) { emit('focused') }
+  }
+
+  const onBlur = () => {
+    isFocused.value = false
+    if (emit) { emit('blurred') }
+  }
+
+  const focus = (): void => {
+    el?.value?.focus()
+  }
+
+  const blur = (): void => {
+    el?.value?.blur()
+  }
 
   if (el) {
     onMounted(() => {
@@ -21,5 +41,7 @@ export function useFocus (el?: Ref<HTMLElement | null | undefined>) {
     isFocused,
     onFocus,
     onBlur,
+    focus,
+    blur,
   }
 }
