527dac9edc7454aff59e127de661c2ddef18d110
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-02-03 17:55:46 +0300

    feat: update input, textarea and examples

--- packages/ui/src/components/va-input/VaInput.demo.vue
+++ packages/ui/src/components/va-input/VaInput.demo.vue
@@ -481,6 +481,25 @@
         :max-rows="4"
       />
     </VbCard>
+    <VbCard title="Textarea readonly">
+      <va-input
+        id="textarea-id-example"
+        placeholder="Try to type in ..."
+        label="Readonly"
+        type="textarea"
+        readonly
+      />
+    </VbCard>
+    <VbCard title="Textarea disabled">
+      <va-input
+        name="textarea-name-example"
+        aria-label="textarea-aria-example"
+        placeholder="Try to focus"
+        label="Disabled"
+        type="textarea"
+        disabled
+      />
+    </VbCard>
     <VbCard title="Masked input">
       <va-input
         v-model="maskCreditCardValue"
