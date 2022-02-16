cb064aa7e11e390c0ed4a98054e3b2b28dabc109
Vitaly Raicheu <rvitaly1978@gmail.com>
2022-02-16 10:58:28 +0300

    Fix#1484 Fix slider track label (#1498)
    * fix: tweak va-button-toggle
    
    * feat: improve track-label
    
    * feat: update locales, api-options
    
    * feat: update examples
    
    * feat: remove strings type from track-label
    
    Co-authored-by: Vitaly Raicheu <vitaly.raichev@epicmax.co>

--- packages/docs/src/page-configs/ui-elements/slider/examples/TrackLabel.vue
+++ packages/docs/src/page-configs/ui-elements/slider/examples/TrackLabel.vue
@@ -1,7 +1,38 @@
 <template>
   <div>
-    <va-slider v-model="value1" track-label-visible />
-    <va-slider v-model="value2" track-label-visible :track-label="value2+'%'" />
+    <va-slider class="mb-3"
+      v-model="value[0]"
+      track-label-visible
+    />
+
+    <va-slider class="mb-3 mt-4"
+      v-model="value[1]"
+      track-label-visible
+    >
+      <template #trackLabel="{ value }">
+        <va-chip color="warning" size="small">{{ value }}</va-chip>
+      </template>
+    </va-slider>
+
+    <va-slider class="mb-3"
+      v-model="value"
+      range
+      track-label-visible
+      :track-label="processTrackLabel"
+    />
+
+    <va-slider class="mt-4"
+      v-model="value"
+      range
+      track-label-visible
+    >
+      <template #trackLabel="{ value, order }">
+        <va-chip
+          size="small"
+          :color="order === 0 ? 'success' : 'danger'"
+        >{{ value }}</va-chip>
+      </template>
+    </va-slider>
   </div>
 </template>
 
@@ -9,9 +40,15 @@
 export default {
   data () {
     return {
-      value1: 23,
-      value2: 75,
+      value: [45, 65],
     }
+  },
+  methods: {
+    processTrackLabel (value, order) {
+      return order === 0
+        ? `min ${value}$`
+        : `max ${value}$`
+    },
   },
 }
 </script>
