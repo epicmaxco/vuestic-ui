8394054f4168ad551f3d36d4ad9f1323946a734c
m0ksem <m0ksem1337@gmail.com>
2022-03-08 18:55:50 +0200

    feat: carousel (manual squash merge)
    commit 5801369767e1f2c6b51a6b6a1a77ff882317e9c8
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Mon Feb 7 22:27:23 2022 +0200
    
        VaHover: made stateful prop true by default.
    
    commit 5a495a17067ce72a7a802f864e6a61e73386dfa8
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Mon Feb 7 22:23:39 2022 +0200
    
        Removed loop prop from carousel. Added animation order.
    
    commit ac285042846eb5d8e67980438ae7a162bf809b6d
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Wed Jan 26 11:43:58 2022 +0200
    
        Added css variables.
    
    commit 2fd618e4c1c4122cbdc844d74221e973f0396c5d
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Wed Jan 26 11:36:18 2022 +0200
    
        Refactor VaHover. Updated carousel colors.
    
    commit a16141cbc1716c2f7da84e0eaacd8ad60ba2dd5c
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Wed Jan 26 10:11:03 2022 +0200
    
        Fix loop bug.
    
    commit 54fdd430a3403894f069f57de55ee2929296819f
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Wed Jan 26 10:01:21 2022 +0200
    
        Fix fade animation.
    
    commit 47e3144b5dfc623413c26bf57d14a12590ee77e8
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Wed Jan 26 09:59:35 2022 +0200
    
        Fix demo and CSS variables.
    
    commit a28d5552cb49db5c5bf10e68b38797013282a40f
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Sun Jan 23 05:43:39 2022 +0200
    
        Fix active slide button. Added translations. Fix infinite scroll animation.
    
    commit df51812d8a36b9def43c6bf86ad5c2857d439d6d
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 13:47:41 2022 +0200
    
        Added css variables.
    
    commit 0a71051a8d2175083b6906cd4922d52b2aab46ef
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 13:40:39 2022 +0200
    
        Bug fixed and ux improments.
    
    commit f7835346befdb660c6ced3818f3da4084839fe66
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 13:18:23 2022 +0200
    
        Addded translations.
    
    commit 9d02d831fddce4709d422c33fbb5a358468e0465
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 12:41:42 2022 +0200
    
        Added docs demo.
    
    commit 3801793c7599f24c11c835c948b73d75464eff8f
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 11:00:27 2022 +0200
    
        Added fade effect animation.
    
    commit 80629e4a999301ee77a5bcc7b612494a888f8f76
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 08:40:13 2022 +0200
    
        Added infinite scroll.
    
    commit 181bfe41730b9d352e2af7cea46273b303ade80e
    Author: m0ksem <m0ksem1337@gmail.com>
    Date:   Thu Jan 20 06:46:27 2022 +0200
    
        Init va carousel.

---
+++ packages/docs/src/page-configs/ui-elements/carousel/examples/Slots.vue
@@ -0,0 +1,30 @@
+<template>
+  <va-carousel :items="items" v-model="value" arrows indicators>
+    <template #default="{ item }">
+      <va-avatar :src="item" />
+    </template>
+
+    <template #prev-arrow> Go back! </template>
+
+    <template #prev-next> Go forward! </template>
+
+    <template #indicator="{ index }"> Go to {{ index }} </template>
+  </va-carousel>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 0,
+      items: [
+        'https://picsum.photos/1500',
+        'https://picsum.photos/1501',
+        'https://picsum.photos/1502',
+        'https://picsum.photos/1503',
+        'https://picsum.photos/1504',
+      ],
+    }
+  },
+}
+</script>
