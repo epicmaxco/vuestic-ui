a533bc9ad306e32dd50834f57e6178259c5cfaa7
Maksim Nedoshev <m0ksem1337@gmail.com>
2022-03-16 17:13:44 +0200

    Added folders to component folder structure. (#1502)

--- packages/docs/src/page-configs/contribution/guide/code-examples/folder-structure.ts
+++ packages/docs/src/page-configs/contribution/guide/code-examples/folder-structure.ts
@@ -1,6 +1,16 @@
 export const componentFolderStructure = `
 va-checkbox // component directory
-|- VaCheckbox.vue // component itself
-|- VaCheckbox.demo.vue // component demo
-|- VaCheckbox.spec.js // component tests
+|- 📁 components // inner components specific to this component
+|- 📁 hooks // hooks specific to this component
+|---- useSomething.ts 
+|- 📁 plugins // plugins that required for this component
+|---- index.ts // here should be exported all plugins using named exports 
+|- 📁 tests
+|---- 📝 VaCheckbox.spec.js // component tests
+|- 📁 types // specific types for this component
+|- 📁 utils
+|- 📝 _variables.scss // component css variables
+|- 📝 index.ts // all components, that should be exported wrapped with withTransportConfig
+|- 📝 VaCheckbox.demo.vue // component demo
+|- 📝 VaCheckbox.vue // component itself
 `
