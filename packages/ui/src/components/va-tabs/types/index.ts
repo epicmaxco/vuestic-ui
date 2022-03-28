c5298bbff41897e9f80fa5ebc87e47a59f8f079d
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-03-28 11:46:41 +0500

    Remove redundant types and keys

--- packages/ui/src/components/va-tabs/types/index.ts
+++ packages/ui/src/components/va-tabs/types/index.ts
@@ -1,13 +1,2 @@
-import { ComputedRef } from 'vue'
-
 export const TabsViewKey = Symbol('TabsView')
 export const TabKey = Symbol('Tab')
-export const TabsServiceKey = Symbol('TabsService')
-
-export type ClassComputed = {
-  [prop: string]: boolean
-}
-
-export type ComputedStyle = {
-  [prop: string]: string | ComputedRef<string> | undefined
-}
