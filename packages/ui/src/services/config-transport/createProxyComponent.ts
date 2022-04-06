b788f5daa41dd28bcf8090830bc10b40e7b549d0
m0ksem <m0ksem1337@gmail.com>
2022-04-06 13:09:07 +0300

    refactor: createProxyComponent.

--- packages/ui/src/services/config-transport/createProxyComponent.ts
+++ packages/ui/src/services/config-transport/createProxyComponent.ts
@@ -1,85 +1,61 @@
-import { getCurrentInstance, ComponentInternalInstance, DefineComponent, SetupContext, ComponentPublicInstance, shallowReactive, watchEffect } from 'vue'
+import { getCurrentInstance, ComponentInternalInstance, DefineComponent, SetupContext, Ref } from 'vue'
 import { useComponentConfigProps } from '../component-config/component-config'
-import { withCache, getFromCache } from './cachedFabric'
-import { getPropsDefaultValue } from './propsDefaultValue'
 
+/** Compiled and reactive props. By default they passed to setup fn */
 type Props = Record<string, unknown>;
+/** Raw props */
+type RawProps = Record<string, unknown>;
 
-const getComponentPublicInstance = (): ComponentPublicInstance | null => (getCurrentInstance() as any)?.ctx || null
-const getComponentInternalInstance = (): ComponentInternalInstance | null => getCurrentInstance() || null
+/**
+ * @param propsFromConfig Ref of custom props. Required to be ref so vue can rerender component on custom props change.
+ * @returns new props object, where some props replaced with props from config.
+ */
+const createPropsWithCustomConfig = (instance: ComponentInternalInstance, propsFromConfig: Ref<Props>) => {
+  /**
+   * Reactive and compiled props. Compiled props considering default value, Boolean transformation etc.
+   * It is a default props that passed to setup function.
+   */
+  const instanceProps: Props = instance.props
 
-/** This function return `shallowReactive` that binded with callback result  */
-const computedShallowReactive = <T extends Record<any, any>>(cb: () => T) => {
-  const props = shallowReactive({}) as T
-  watchEffect(() => {
-    const p = cb()
-    Object.keys(p).forEach((key: keyof T) => { props[key] = p[key] })
+  return new Proxy(instanceProps, {
+    get (target, key: string) {
+      /**
+       * Props passed to VNode. Not compiled at all and not reactive.
+       * VNode props contained only props passed from parent.
+       */
+      const incommingProps: RawProps = instance.vnode.props || {}
+
+      // Return prop from config only if user didn't pass props manually
+      if (incommingProps[key] === undefined && propsFromConfig.value[key] !== undefined) {
+        return propsFromConfig.value[key]
+      }
+
+      return target[key]
+    },
   })
-  return props
+}
+
+/**
+ * Patch instance props with Proxy.
+ * This will change props object during render and in Devtools.
+ */
+const patchInstanceProps = (instance: ComponentInternalInstance, props: Props) => {
+  instance.props = props
 }
 
 export const createProxyComponent = <T extends DefineComponent>(component: T) => {
-  const componentProps = component.props as Record<string, any>
-  const componentPropNames = Array.isArray(componentProps) ? componentProps : Object.keys(componentProps)
-
-  const defaultProps = getPropsDefaultValue(component)
-
-  const createProps = () => {
-    const instance = getComponentInternalInstance()!
-    const customProps = useComponentConfigProps(component)
-
-    return computedShallowReactive(() => {
-      const incommingProps = instance.vnode.props || {}
-      const instanceProps = instance.props
-
-      return Object.keys(defaultProps).reduce((acc, key) => {
-        if (incommingProps[key] !== undefined) {
-          acc[key] = instanceProps[key] // Use instance props, because incommingProps are not compiled.
-        } else if (customProps.value[key] !== undefined) {
-          acc[key] = customProps.value[key]
-        } else {
-          acc[key] = instanceProps[key]
-        }
-
-        return acc
-      }, {} as Props)
-    })
-  }
-
-  const createRenderingContext = (props: Props) => {
-    const publicInstance = getComponentPublicInstance()!
-
-    return new Proxy({}, {
-      get (target, key: string) {
-        if (componentPropNames.includes(key)) { return props[key] }
-        if (key === '$props') { return props }
-
-        return publicInstance[key as keyof ComponentPublicInstance]
-      },
-    })
-  }
-
-  const patchCurrentInstance = (props: Props) => {
-    getComponentInternalInstance()!.props = props
-  }
-
   const customSetup = (originalProps: Props, ctx: SetupContext) => {
-    const props = withCache(createProps, getComponentInternalInstance()!.uid)
-    patchCurrentInstance(props)
-    return component.setup!(props, ctx)
-  }
+    const instance = getCurrentInstance()! // Not null during setup call
+    const propsFromConfig = useComponentConfigProps(component)
 
-  const customRender = (ctx: ComponentPublicInstance, cache: any, $props: Props, ...args: any[]) => {
-    const props = withCache(createProps, getComponentInternalInstance()!.uid)
-    return component.render!(createRenderingContext(props), cache, props, ...args)
+    const props = createPropsWithCustomConfig(instance, propsFromConfig)
+    patchInstanceProps(instance, props)
+    return component.setup?.(props, ctx)
   }
 
   return new Proxy(component, {
     get (target, key: any) {
-      if (key === 'setup') {
-        return customSetup
-      }
-      // if (key === 'render') { return customRender }
+      if (key === 'setup') { return customSetup }
 
       return target[key]
     },
