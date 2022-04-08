2fc4e756e057c12def8506fe7ebace1e642f4f6c
m0ksem <m0ksem1337@gmail.com>
2022-04-08 16:26:11 +0300

    Fix: Use proxy instead of shallowReactive for instance props.

--- packages/ui/src/services/config-transport/createProxyComponent.ts
+++ packages/ui/src/services/config-transport/createProxyComponent.ts
@@ -1,6 +1,5 @@
 import { getCurrentInstance, ComponentInternalInstance, DefineComponent, SetupContext, Ref } from 'vue'
 import { useComponentConfigProps } from '../component-config/component-config'
-import { computedShallowReactive } from './computedShallowReactive'
 
 /** Compiled and reactive props. By default they passed to setup fn */
 type Props = Record<string, unknown>;
@@ -18,23 +17,21 @@ const createPropsWithCustomConfig = (instance: ComponentInternalInstance, propsF
    */
   const instanceProps: Props = instance.props
 
-  return computedShallowReactive(() => {
-    /**
-     * Props passed to VNode. Not compiled at all and not reactive.
-     * VNode props contained only props passed from parent.
-     */
-    const incommingProps: RawProps = instance.vnode.props || {}
+  return new Proxy(instanceProps, {
+    get: (target, key: string) => {
+      /**
+       * Props passed to VNode. Not compiled at all and not reactive.
+       * VNode props contained only props passed from parent.
+       */
+      const incommingProps: RawProps = instance.vnode.props || {}
 
-    return Object.keys(instanceProps).reduce((acc, key) => {
       // Return prop from config only if user didn't pass props manually
       if (incommingProps[key] === undefined && propsFromConfig.value[key] !== undefined) {
-        acc[key] = propsFromConfig.value[key]
-      } else {
-        acc[key] = instanceProps[key]
+        return propsFromConfig.value[key]
       }
 
-      return acc
-    }, {} as Props)
+      return target[key]
+    },
   })
 }
 
@@ -52,7 +49,9 @@ export const createProxyComponent = <T extends DefineComponent>(component: T) =>
     const propsFromConfig = useComponentConfigProps(component)
 
     const props = createPropsWithCustomConfig(instance, propsFromConfig)
+
     patchInstanceProps(instance, props)
+
     return component.setup?.(props, ctx)
   }
 
