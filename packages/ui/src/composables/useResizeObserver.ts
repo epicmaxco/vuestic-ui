4641799b4112d2688f643249b824cc4945abe666
m0ksem <m0ksem1337@gmail.com>
2022-04-12 20:20:23 +0300

    Fix resize observer on SSR.

--- packages/ui/src/composables/useResizeObserver.ts
+++ packages/ui/src/composables/useResizeObserver.ts
@@ -3,26 +3,31 @@ import { onBeforeUnmount, onMounted, Ref, unref, watch } from 'vue'
 type MaybeRef<T> = T | Ref<T>
 
 export const useResizeObserver = <T extends HTMLElement | null>(elementsList: MaybeRef<T>[], cb: () => void) => {
-  const resizeObserver = new ResizeObserver(cb)
+  // Make it null during setup hook, because ResizeObserver is not available on SSR.
+  let resizeObserver: ResizeObserver | null = null
 
   const observeAll = (elementsList: MaybeRef<T>[]) => {
     elementsList.forEach((element: MaybeRef<T>) => {
       const unrefedElement = unref(element)
 
-      unrefedElement && resizeObserver.observe(unrefedElement)
+      unrefedElement && resizeObserver!.observe(unrefedElement)
     })
   }
 
   watch(elementsList, (newValue, oldValue) => {
+    if (!resizeObserver) { return }
     resizeObserver.disconnect()
     observeAll(newValue)
   })
 
   onMounted(() => {
+    if (!resizeObserver) {
+      resizeObserver = new ResizeObserver(cb)
+    }
     observeAll(elementsList)
   })
 
-  onBeforeUnmount(() => resizeObserver.disconnect())
+  onBeforeUnmount(() => resizeObserver && resizeObserver.disconnect())
 
   return resizeObserver
 }
