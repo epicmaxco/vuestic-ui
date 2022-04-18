65ca98f4c29c1e3c1cbd707a3c9a32ed36752249
m0ksem <m0ksem1337@gmail.com>
2022-04-18 20:55:49 +0300

    Removed junk from name page.

--- packages/nuxt-docs/pages/[name].vue
+++ packages/nuxt-docs/pages/[name].vue
@@ -6,9 +6,4 @@
 
 <script setup lang="ts">
 import DocsPage from '~/components/docs/DocsPage.vue'
-const { t } = useI18n()
-
-// TODO: Nuxt don't change layout after nuxt link click for some reason.
-//       Investigate or create bug report!
-definePageMeta({ layout: 'default' })
 </script>
