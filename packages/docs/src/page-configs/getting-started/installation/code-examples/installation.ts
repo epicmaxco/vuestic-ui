41c0815f3c147c72a4b3bf1063cd30d5574b4581
xiongmao86 <xiongmao86dev@sina.com>
2022-04-23 16:45:20 +0800

    Remove code string variable installation.

--- packages/docs/src/page-configs/getting-started/installation/code-examples/installation.ts
+++ packages/docs/src/page-configs/getting-started/installation/code-examples/installation.ts
@@ -1,9 +1,3 @@
-export const installation = `
-npm install vuestic-ui
-// {{ $t('all.code.or') }}
-yarn add vuestic-ui
-`
-
 export const installationObject = {
   npm: 'npm install vuestic-ui',
   yarn: 'yarn add vuestic-ui',
