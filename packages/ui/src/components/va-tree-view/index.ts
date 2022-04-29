cb407150248d79e04f6ba0b3d9766d41b8757b5e
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-04-29 16:49:13 +0500

    feat: remove unnecessary component export/import

--- packages/ui/src/components/va-tree-view/index.ts
+++ packages/ui/src/components/va-tree-view/index.ts
@@ -2,6 +2,5 @@ import withConfigTransport from '../../services/config-transport/withConfigTrans
 import VaTreeView from './VaTreeView.vue'
 
 export { default as VaTreeNode } from './VaTreeNode'
-export { default as VaTreeCategory } from './VaTreeCategory'
 
 export default withConfigTransport(VaTreeView as any)
