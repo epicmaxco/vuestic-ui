0977480556d94be098bf91109329f7f612b290f4
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-04-29 17:02:32 +0500

    feat: add tree view provide and tree node interfaces

--- packages/ui/src/components/va-tree-view/types/index.ts
+++ packages/ui/src/components/va-tree-view/types/index.ts
@@ -1,43 +1,16 @@
-import { ComponentPublicInstance } from 'vue'
-import VaTreeNode from '../VaTreeNode/VaTreeNode.vue'
-
 export const TreeViewKey = Symbol('TreeView')
-export const TreeCategoryKey = Symbol('TreeCategory')
-export const TreeNodeKey = Symbol('TreeNode')
 
-export interface TreeNodeCommon<T> {
-  onChildMounted: (value: T) => void,
-  onChildUnmounted: (value: T) => void,
+export interface TreeNode {
+  id: number | string
+  children: TreeNode[]
+  expanded: boolean
+  selected: boolean
+  [key: string]: unknown
 }
 
-export interface TreeViewMethods<T> {
-  onChildMounted: (value: T) => void,
-  onChildUnmounted: (value: T) => void,
-}
-
-export interface TreeCategory {
-  treeView: TreeViewMethods<TreeCategory>,
-  nodes: ComponentPublicInstance<TreeCategory | typeof VaTreeNode>[],
-  isOpenCached: boolean | undefined,
-  collapse: () => void,
-  expand: () => void,
-  toggle: (e: MouseEvent) => void,
-  onChildMounted: (node: ComponentPublicInstance<TreeCategory | typeof VaTreeNode>) => void,
-  onChildUnmounted: (removableNode: ComponentPublicInstance<TreeCategory | typeof VaTreeNode>) => void,
-}
-
-export type TreeNodeComponent = ComponentPublicInstance<TreeCategory | typeof VaTreeNode>
-
-export interface TreeViewProps {
-  color: string
-}
-
-export interface TreeView {
-  collapse: () => void,
-  expand: () => void,
-}
-
-export interface TreeViewProvide extends TreeViewProps {
-  onChildMounted: (category: TreeCategory) => void
-  onChildUnmounted: (removableCategory: TreeCategory) => void
+export interface TreeViewProvide {
+  nodeKey: string
+  selectable: boolean
+  toggleNode: (node: TreeNode) => void
+  toggleSelect: (isSelected: boolean, node: TreeNode) => void
 }
