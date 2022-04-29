e29389c02b16e645b481f31f11534fb5be1d0e0e
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-04-29 18:29:46 +0500

    feat: update demo

--- packages/ui/src/components/va-tree-view/VaTreeView.demo.vue
+++ packages/ui/src/components/va-tree-view/VaTreeView.demo.vue
@@ -1,47 +1,21 @@
 <template>
   <VbDemo>
-    <VbCard>
-      <va-button @click="$refs.treeView.expand()">
-        EXPAND ALL
-      </va-button>
-      <va-button @click="$refs.treeView.collapse()">
-        COLLAPSE ALL
-      </va-button>
-
-      <va-tree-view ref="treeView">
-        <va-tree-category
-          label="Category text"
-          is-open
-        >
-          <va-tree-node>
-            Node text
-          </va-tree-node>
-          <va-tree-category
-            label="Category text"
-            is-open
-          >
-            <va-tree-node>
-              Node text
-            </va-tree-node>
-          </va-tree-category>
-        </va-tree-category>
-      </va-tree-view>
-    </VbCard>
-    <VbCard title="Color">
+    <VbCard title="default">
       <va-tree-view
-        color="info"
+        :nodes="defaultNodes"
+        node-key="text"
+      />
+    </VbCard>
+    <VbCard title="With custom body">
+      <va-tree-view
+        :nodes="customBodyNodes"
+        node-key="text"
+        expand-all
+        selectable
       >
-        <va-tree-category
-          label="Category text"
-          is-open
-        >
-          <va-tree-node>
-            Node text
-          </va-tree-node>
-          <va-tree-node>
-            Node text
-          </va-tree-node>
-        </va-tree-category>
+        <template #node-body="prop">
+          <p>{{prop.body}}</p>
+        </template>
       </va-tree-view>
     </VbCard>
   </VbDemo>
@@ -49,11 +23,61 @@
 
 <script>
 import VaTreeView from './VaTreeView.vue'
-import VaTreeNode from './VaTreeNode/VaTreeNode'
-import VaTreeCategory from './VaTreeCategory/VaTreeCategory'
-import VaButton from '../va-button/VaButton'
 
 export default {
-  components: { VaButton, VaTreeCategory, VaTreeNode, VaTreeView },
+  name: 'VaTreeView.demo',
+
+  components: {
+    VaTreeView,
+  },
+
+  data: () => ({
+    defaultNodes: [
+      {
+        id: 1,
+        text: 'Text 1',
+        children: [
+          {
+            id: 2,
+            text: 'Text 2',
+            children: [
+              { id: 3, text: 'Text 3' },
+              { id: 4, text: 'Text 4' },
+            ],
+          },
+          { id: 5, text: 'Text 5' },
+        ],
+      },
+      { id: 6, text: 'Text 6' },
+    ],
+    customBodyNodes: [
+      {
+        id: 1,
+        text: 'Text 1',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        children: [
+          {
+            id: 2,
+            text: 'Text 2',
+            body: 'Sed semper, tellus id lacinia faucibus, nisi magna scelerisque lorem, eu finibus quam erat vel augue.',
+            children: [
+              { id: 3, text: 'Text 3' },
+              { id: 4, text: 'Text 4' },
+            ],
+          },
+          {
+            id: 5,
+            text: 'Text 5',
+            body: 'In auctor malesuada augue ac elementum.',
+          },
+        ],
+      },
+      { id: 6, text: 'Text 6' },
+    ],
+  }),
 }
 </script>
+
+<style scoped>
+
+</style>
