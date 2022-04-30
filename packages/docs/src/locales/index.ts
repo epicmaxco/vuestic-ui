1d34161817c1c3dbf5006625896cda182f9d019a
dsabyrov <sabyrov.d@rahmetapp.kz>
2022-04-30 10:28:44 +0600

    deleted unused files

--- packages/docs/src/locales/index.ts
+++ packages/docs/src/locales/index.ts
@@ -1,5 +1,3 @@
-import en from './en/en.json'
-import ru from './ru/ru.json'
 export type TranslationStatusPath = { [key in 'full' | 'part']: string }
 
 export const languages = [
@@ -7,26 +5,24 @@ export const languages = [
     code: 'en',
     name: 'English',
     status: 'full',
-    en,
     translationPath: 'translation.language.en',
   },
   {
     code: 'ru',
     name: 'Русский',
     status: 'full',
-    ru,
     translationPath: 'translation.language.ru',
   },
   // GENERATOR_ADD - language
 ]
 
-export const messages = {
-  en, ru,
-}
+export const messages = languages.reduce((result, { code, name }) => ({
+  ...result,
+  [code]: require(`./${code}/${code}.json`),
+}), {})
 
 const extractLanguageCode = (languageInISOFormat: string) => languageInISOFormat.slice(0, 2)
 
-const getLanguageCode = () => extractLanguageCode('en-EN')
-//
+const getLanguageCode = () => extractLanguageCode(window.navigator.language)
 
-export const DEFAULT_LANGUAGE = getLanguageCode() || 'en'
+export const DEFAULT_LANGUAGE = localStorage.language || getLanguageCode() || 'en'
