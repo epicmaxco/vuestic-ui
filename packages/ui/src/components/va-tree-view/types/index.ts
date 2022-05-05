b760303467360ce8fecb41566685b054cf8e7da8
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-05-05 13:26:59 +0500

    feat: add to the tree view provide colors support

--- packages/ui/src/components/va-tree-view/types/index.ts
+++ packages/ui/src/components/va-tree-view/types/index.ts
@@ -1,3 +1,5 @@
+import { ComputedRef } from 'vue'
+
 export const TreeViewKey = Symbol('TreeView')
 
 export interface TreeNode {
@@ -12,14 +14,16 @@ export interface TreeNode {
 export interface TreeViewProvide {
   nodeKey: string
   selectable: boolean
+  iconColor: ComputedRef<string>
+  colorComputed: ComputedRef<string>
   toggleNode: (node: TreeNode) => void
   toggleSelect: (node: TreeNode, isSelected: boolean) => void
   treeItems: TreeNode[]
 }
 
 export type CreateNodeProps = {
-  node: TreeNode,
-  children?: TreeNode[],
+  node: TreeNode
+  children?: TreeNode[]
   parent?: number | string | null
   expandAll?: boolean
 }
