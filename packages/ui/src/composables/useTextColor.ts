c89a16ffd23147271979ac334f147b1a1a389f18
Darya Volkova <kequeue.8@gmail.com>
2022-05-11 17:16:32 +0300

    Changed name of argument in useTextColor hook.

--- packages/ui/src/composables/useTextColor.ts
+++ packages/ui/src/composables/useTextColor.ts
@@ -1,14 +1,14 @@
 import { computed, getCurrentInstance, Ref, unref } from 'vue'
 import { useColors } from '../services/color-config/color-config'
 
-export const useTextColor = (background: string | Ref<string>, isTransparent: boolean | Ref<boolean> = false) => {
+export const useTextColor = (componentColor: string | Ref<string>, isTransparent: boolean | Ref<boolean> = false) => {
   const { props } = getCurrentInstance()!
   const { getColor, getTextColor } = useColors()
   const textColorComputed = computed(() => {
     if (unref(isTransparent)) {
-      return 'background'
+      return getColor(unref(componentColor))
     }
-    return props.textColor ? getColor(props.textColor as string) : getTextColor(getColor(unref(background)))
+    return props.textColor ? getColor(props.textColor as string) : getTextColor(getColor(unref(componentColor)))
   })
 
   return { textColorComputed }
