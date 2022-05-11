d0cf93a3468fa92a374a8f6688612956c637cfca
Darya Volkova <kequeue.8@gmail.com>
2022-05-11 15:36:35 +0300

    Added isTransparent to useTextColor hook.

--- packages/ui/src/composables/useTextColor.ts
+++ packages/ui/src/composables/useTextColor.ts
@@ -1,10 +1,15 @@
 import { computed, getCurrentInstance, Ref, unref } from 'vue'
 import { useColors } from '../services/color-config/color-config'
 
-export const useTextColor = (background: string | Ref<string>) => {
+export const useTextColor = (background: string | Ref<string>, isTransparent: boolean | Ref<boolean> = false) => {
   const { props } = getCurrentInstance()!
   const { getColor, getTextColor } = useColors()
-  const textColorComputed = computed(() => props.textColor ? getColor(props.textColor as string) : getTextColor(getColor(unref(background))))
+  const textColorComputed = computed(() => {
+    if (unref(isTransparent)) {
+      return 'background'
+    }
+    return props.textColor ? getColor(props.textColor as string) : getTextColor(getColor(unref(background)))
+  })
 
   return { textColorComputed }
 }
