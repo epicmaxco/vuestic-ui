75799029161dca131f7d415023318c58a1bb678c
m0ksem <m0ksem1337@gmail.com>
2022-05-12 16:35:26 +0300

    Create vuestic component type helper.

--- packages/ui/src/vuestic-plugin/index.ts
+++ packages/ui/src/vuestic-plugin/index.ts
@@ -1,5 +1,6 @@
 export * from './vuestic-plugins'
 export * from './create-vuestic'
+export * from './global-components'
 
 // TODO: Remove deperecated plugins after v1.5.0
 export * from './old/vuestic-plugin'
