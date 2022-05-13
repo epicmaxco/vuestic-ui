6e65650d6455abc575c45b0eafeff4abe14f66e1
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-05-13 17:38:31 +0300

    fix: button-group

--- packages/docs/src/page-configs/ui-elements/button-group/examples/Sizes.vue
+++ packages/docs/src/page-configs/ui-elements/button-group/examples/Sizes.vue
@@ -1,27 +1,15 @@
 <template>
   <div>
-    <va-button-group class="mb-4">
-      <va-button size="large">
-        One
-      </va-button>
-      <va-button size="large">
-        Two
-      </va-button>
-      <va-button size="large">
-        Three
-      </va-button>
+    <va-button-group class="mb-4" size="large">
+      <va-button>L1</va-button>
+      <va-button>L2</va-button>
+      <va-button>L3</va-button>
     </va-button-group>
 
-    <va-button-group class="mb-4">
-      <va-button size="small">
-        One
-      </va-button>
-      <va-button size="small">
-        Two
-      </va-button>
-      <va-button size="small">
-        Three
-      </va-button>
+    <va-button-group class="mb-4" size="small">
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
     </va-button-group>
   </div>
 </template>
