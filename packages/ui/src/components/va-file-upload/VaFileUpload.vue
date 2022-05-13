a1624d186fc725669c4d74ce2aef2f6e4676ec1e
xiongmao86 <xiongmao86dev@sina.com>
2022-05-13 21:15:26 +0800

    Emit removed-file for VaFileUpload

--- packages/ui/src/components/va-file-upload/VaFileUpload.vue
+++ packages/ui/src/components/va-file-upload/VaFileUpload.vue
@@ -93,7 +93,7 @@ export default defineComponent({
     },
   },
 
-  emits: ['update:modelValue', 'removedFile'],
+  emits: ['update:modelValue', 'removed-file'],
 
   setup (props, { emit }) {
     const modal = ref(false)
@@ -160,7 +160,7 @@ export default defineComponent({
       if (index in files.value) {
         const removedFile = files.value[index]
         files.value = files.value.filter((item, idx) => idx !== index)
-        emit('removedFile', removedFile)
+        emit('removed-file', removedFile)
       }
     }
 
@@ -168,7 +168,7 @@ export default defineComponent({
       if (files.value.length > 0) {
         const removedFile = files.value[0]
         files.value = []
-        emit('removedFile', removedFile)
+        emit('removed-file', removedFile)
       }
     }
 
