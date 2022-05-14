ff52a12914b1f9b8a707ad14415f0f684d220ff7
Darya Volkova <kequeue.8@gmail.com>
2022-05-14 12:50:26 +0300

    Refactor useTextColor hook.

--- packages/ui/src/composables/useTextColor.ts
+++ packages/ui/src/composables/useTextColor.ts
@@ -4,11 +4,16 @@ import { useColors } from '../services/color-config/color-config'
 export const useTextColor = (componentColor: string | Ref<string>, isTransparent: boolean | Ref<boolean> = false) => {
   const { props } = getCurrentInstance()!
   const { getColor, getTextColor } = useColors()
+
   const textColorComputed = computed(() => {
+    const hexColor = getColor(unref(componentColor))
+    const textColorProp = props.textColor as string
+
     if (unref(isTransparent)) {
-      return getColor(unref(componentColor))
+      return hexColor
     }
-    return props.textColor ? getColor(props.textColor as string) : getColor(getTextColor(getColor(unref(componentColor))))
+
+    return props.textColor ? getColor(textColorProp) : getColor(getTextColor(hexColor))
   })
 
   return { textColorComputed }
