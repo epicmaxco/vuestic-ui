f4de230c6897ad141b8eae88f09a1045f00b9b65
Alexander <shellby666@gmail.com>
2022-05-17 18:52:39 +0300

    Fix #1787: VaDatePicker opens selected date by default

--- packages/ui/src/components/va-date-picker/types/index.ts
+++ packages/ui/src/components/va-date-picker/types/index.ts
@@ -1,5 +1,5 @@
 export type VaDatePickerRange = { start?: Date | null, end?: Date | null }
-export type VaDatePickerModelValue = Date | Date[] | VaDatePickerRange
+export type VaDatePickerModelValue = Date | Date[] | VaDatePickerRange | undefined
 export type VaDatePickerViewType = 'year' | 'month' | 'day'
 export type VaDatePickerView = { type: VaDatePickerViewType, year: number, month: number }
 export type VaDatePickerViewProp = { [T in keyof VaDatePickerView]+?: VaDatePickerView[T] }
