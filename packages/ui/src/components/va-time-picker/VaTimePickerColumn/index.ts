dc6e0371ca67379d9748fbdb7fa230c7046ecd42
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-05-20 16:23:51 +0500

    feat: update component export/import

---
+++ packages/ui/src/components/va-time-picker/VaTimePickerColumn/index.ts
@@ -0,0 +1,4 @@
+import withConfigTransport from '../../../services/config-transport/withConfigTransport'
+import VaTimePickerColumn from './VaTimePickerColumn.vue'
+
+export default withConfigTransport(VaTimePickerColumn)
