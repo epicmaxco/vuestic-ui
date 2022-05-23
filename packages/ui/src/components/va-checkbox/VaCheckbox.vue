1cba4ee9a29b2adbac4642d9c00a2f57ebdbd721
aluarius <ruseomaster@gmail.com>
2022-05-23 18:41:45 +0300

    wip

--- packages/ui/src/components/va-checkbox/VaCheckbox.vue
+++ packages/ui/src/components/va-checkbox/VaCheckbox.vue
@@ -10,11 +10,10 @@
     :error-count="errorCount"
   >
     <div
+      ref="container"
       class="va-checkbox__input-container"
       @click="toggleSelection"
-      tabindex="-1"
       @blur="onBlur"
-      ref="container"
     >
       <div
         class="va-checkbox__square"
@@ -24,36 +23,37 @@
         <input
           ref="input"
           type="checkbox"
-          role="checkbox"
-          readonly
-          :id="id"
-          :name="name"
+          class="va-checkbox__input"
+          :id="computedId"
+          :name="computedName"
+          :tabindex="computedTabIndex"
+          :disabled="disabled"
+          :indeterminate="indeterminate"
+          :value="label"
           v-on="keyboardFocusListeners"
           @focus="onFocus"
           @blur="onBlur"
-          class="va-checkbox__input"
           @click.stop.prevent
           @keypress.prevent="toggleSelection"
-          :disabled="disabled"
-          :indeterminate="indeterminate"
         >
         <va-icon
-          class="va-checkbox__icon"
-          :name="computedIconName"
-          size="20px"
-          :color="textColorComputed"
           v-show="isActive"
+          class="va-checkbox__icon"
+          size="20px"
+          :name="computedIconName"
+          :color="textColorComputed"
         />
       </div>
-      <div
-        class="va-checkbox__label"
-        :style="labelStyle"
+      <label
+        v-if="label || $slots.label"
         ref="label"
-        tabindex="-1"
+        class="va-checkbox__label"
+        :for="computedId"
+        :style="labelStyle"
         @blur="onBlur"
       >
         <slot name="label">{{ label }}</slot>
-      </div>
+      </label>
     </div>
   </VaMessageListWrapper>
 </template>
@@ -68,6 +68,7 @@ import { useColors } from '../../composables/useColor'
 import useKeyboardOnlyFocus from '../../composables/useKeyboardOnlyFocus'
 import { useSelectable, useSelectableProps, useSelectableEmits } from '../../composables/useSelectable'
 import { useTextColor } from '../../composables/useTextColor'
+import { generateUniqueId } from '../../services/utils'
 
 const vaCheckboxValueType = [Boolean, Array, String, Object] as PropType<boolean | null | string | number | Record<any, unknown> | unknown[]>
 
@@ -102,7 +103,7 @@ export default defineComponent({
       onBlur,
       onFocus,
     } = useSelectable(props, emit, elements)
-    const { getColor, getTextColor } = useColors()
+    const { getColor } = useColors()
     const { hasKeyboardFocus, keyboardFocusListeners } = useKeyboardOnlyFocus()
 
     const { textColorComputed } = useTextColor()
@@ -148,6 +149,8 @@ export default defineComponent({
       : props.checkedIcon,
     )
 
+    const uniqueId = computed(() => generateUniqueId())
+
     return {
       isActive,
       computedClass,
@@ -161,6 +164,9 @@ export default defineComponent({
       toggleSelection,
       onBlur,
       onFocus,
+      computedTabIndex: computed(() => props.disabled || props.readonly ? -1 : 0),
+      computedId: computed(() => props.name || uniqueId.value),
+      computedName: computed(() => props.name || uniqueId.value),
     }
   },
 })
@@ -218,9 +224,7 @@ export default defineComponent({
   }
 
   &__input {
-    opacity: 0;
-    width: 0;
-    height: 0 !important;
+    @include visually-hidden;
   }
 
   &__label {
