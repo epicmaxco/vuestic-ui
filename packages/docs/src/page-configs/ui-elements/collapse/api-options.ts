fa7cc9b14f5f5c3a10e129e240ee891b7947c952
xiongmao86 <xiongmao86dev@sina.com>
2022-05-23 18:33:11 +0800

    Finish changing api-options.

--- packages/docs/src/page-configs/ui-elements/collapse/api-options.ts
+++ packages/docs/src/page-configs/ui-elements/collapse/api-options.ts
@@ -1,11 +1,6 @@
 import { defineManualApi } from '../../../components/DocsApi/ManualApiOptions'
 
 export default defineManualApi({
-  events: {
-    input: {
-      types: '(value: boolean) => void',
-    },
-  },
   slots: {
     default: { },
     header: { },
