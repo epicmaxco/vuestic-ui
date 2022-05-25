d4f1648319ac17375959d2232d5a9841db4b12ac
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-05-25 18:33:17 +0300

    fix: update useModal to throw error when nullish appContext

--- packages/ui/src/components/va-modal/useModal.ts
+++ packages/ui/src/components/va-modal/useModal.ts
@@ -6,6 +6,10 @@ import { ModalOptions } from './types'
 export const useModal = () => {
   const appContext = getCurrentInstance()?.appContext
 
+  if (!appContext) {
+    throw new Error('useModal can be used only in setup function. You can use app.config.globalProperties.$vaModal outside setup function')
+  }
+
   /**
    * @param options can be message string or options object
    */
