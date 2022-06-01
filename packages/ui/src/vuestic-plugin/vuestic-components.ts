5bbe7c8cb72a759565abc18191b8f118b9c4b64b
m0ksem <m0ksem1337@gmail.com>
2022-06-01 18:45:45 +0300

    Minor. Improve re-exports.

--- packages/ui/src/vuestic-plugin/vuestic-components.ts
+++ packages/ui/src/vuestic-plugin/vuestic-components.ts
@@ -1,76 +1,77 @@
-export { VaAccordion } from '../components/va-accordion'
-export { VaAffix } from '../components/va-affix'
-export { VaAlert } from '../components/va-alert'
-export { VaAppBar } from '../components/va-app-bar'
-export { VaAvatar } from '../components/va-avatar'
-export { VaBacktop } from '../components/va-backtop'
-export { VaBadge } from '../components/va-badge'
-export { VaButton } from '../components/va-button'
-export { VaButtonDropdown } from '../components/va-button-dropdown'
-export { VaButtonGroup } from '../components/va-button-group'
-export { VaButtonToggle } from '../components/va-button-toggle'
-export { VaBreadcrumbs, VaBreadcrumbsItem } from '../components/va-breadcrumbs'
 export {
+  VaAccordion,
+  VaAffix,
+  VaAlert,
+  VaAppBar,
+  VaAvatar,
+  VaBacktop,
+  VaBadge,
+  VaBreadcrumbs,
+  VaBreadcrumbsItem,
+  VaButton,
+  VaButtonDropdown,
+  VaButtonGroup,
+  VaButtonToggle,
   VaCard,
+  VaCardActions,
   VaCardContent,
   VaCardTitle,
-  VaCardActions,
-} from '../components/va-card'
-export { VaCheckbox } from '../components/va-checkbox'
-export { VaChip } from '../components/va-chip'
-export { VaCollapse } from '../components/va-collapse'
-export { VaColorIndicator } from '../components/va-color-indicator'
-export { VaColorPalette } from '../components/va-color-palette'
-export { VaColorInput } from '../components/va-color-input'
-export { VaConfig } from '../components/va-config'
-export { VaContent } from '../components/va-content'
-export { VaDivider } from '../components/va-divider'
-export { VaDataTable } from '../components/va-data-table'
-export { VaDateInput } from '../components/va-date-input'
-export { VaDatePicker } from '../components/va-date-picker'
-export { VaDropdown, VaDropdownContent } from '../components/va-dropdown'
-export { VaFileUpload } from '../components/va-file-upload'
-export { VaHover } from '../components/va-hover'
-export { VaIcon } from '../components/va-icon'
-export { VaImage } from '../components/va-image'
-export { VaInnerLoading } from '../components/va-inner-loading'
-export { VaInput } from '../components/va-input'
-export { VaMessageList } from '../components/va-input/components/VaMessageList'
-export {
+  VaCarousel,
+  VaCheckbox,
+  VaChip,
+  VaCollapse,
+  VaColorIndicator,
+  VaColorInput,
+  VaColorPalette,
+  VaConfig,
+  VaContent,
+  VaCounter,
+  VaDataTable,
+  VaDateInput,
+  VaDatePicker,
+  VaDivider,
+  VaDropdown,
+  VaDropdownContent,
+  VaFileUpload,
+  VaForm,
+  VaHover,
+  VaIcon,
+  VaImage,
+  VaInfiniteScroll,
+  VaInnerLoading,
+  VaInput,
   VaList,
   VaListItem,
   VaListItemLabel,
   VaListItemSection,
   VaListLabel,
   VaListSeparator,
-} from '../components/va-list'
-export { VaModal } from '../components/va-modal'
-export { VaNavbar, VaNavbarItem } from '../components/va-navbar'
-export { VaPagination } from '../components/va-pagination'
-export { VaParallax } from '../components/va-parallax'
-export { VaPopover } from '../components/va-popover'
-export { VaProgressBar } from '../components/va-progress-bar'
-export { VaProgressCircle } from '../components/va-progress-circle'
-export { VaRadio } from '../components/va-radio'
-export { VaRating } from '../components/va-rating'
-export { VaSelect } from '../components/va-select'
-export { VaSeparator } from '../components/va-separator'
-export { VaSidebar, VaSidebarItem, VaSidebarItemContent, VaSidebarItemTitle } from '../components/va-sidebar'
-export { VaSlider } from '../components/va-slider'
-export { VaSwitch } from '../components/va-switch'
-export { VaTabs, VaTab } from '../components/va-tabs'
-export { VaTimeline, VaTimelineItem } from '../components/va-timeline'
-export {
-  VaTreeView,
-  VaTreeView as VaTreeRoot,
+  VaMessageList,
+  VaModal,
+  VaNavbar, VaNavbarItem,
+  VaOptionList,
+  VaPagination,
+  VaParallax,
+  VaPopover,
+  VaProgressBar,
+  VaProgressCircle,
+  VaRadio,
+  VaRating,
+  VaSelect,
+  VaSeparator,
+  VaSidebar,
+  VaSidebarItem,
+  VaSidebarItemContent,
+  VaSidebarItemTitle,
+  VaSlider,
+  VaSpacer,
+  VaSwitch,
+  VaTabs, VaTab,
+  VaTimeInput,
+  VaTimeline, VaTimelineItem,
+  VaTimePicker,
   VaTreeCategory,
   VaTreeNode,
-} from '../components/va-tree-view'
-export { VaSpacer } from '../components/va-spacer'
-export { VaForm } from '../components/va-form'
-export { VaOptionList } from '../components/va-option-list'
-export { VaInfiniteScroll } from '../components/va-infinite-scroll'
-export { VaTimePicker } from '../components/va-time-picker'
-export { VaTimeInput } from '../components/va-time-input'
-export { VaCarousel } from '../components/va-carousel'
-export { VaCounter } from '../components/va-counter'
+  VaTreeView as VaTreeRoot, // Re-export as VaTreeRoot, idk why, maybe some legacy support
+  VaTreeView,
+} from '../components'
