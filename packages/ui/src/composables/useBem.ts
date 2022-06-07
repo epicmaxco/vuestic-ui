0377ab11a0eac1dc9d236cc2cab58b5a6868581d
aluarius <ruseomaster@gmail.com>
2022-06-07 16:25:39 +0300

    updates

--- packages/ui/src/composables/useBem.ts
+++ packages/ui/src/composables/useBem.ts
@@ -1,23 +1,24 @@
-import { computed, Ref, unref, reactive } from 'vue'
+import { computed, Ref, unref, reactive, ComputedRef } from 'vue'
 import { __DEV__ } from '../utils/global-utils'
 
-type ComputedClasses = Record<string, any> & {
-  get asObject(): Record<string, true>
-  get asArray(): string[]
-  get asString(): string
+type ComputedClasses = Ref<Record<string, any>> & {
+  asObject: ComputedRef<Record<string, true>>
+  asArray: ComputedRef<string[]>
+  asString: ComputedRef<string>
 }
 
 /**
- * @description
+ * @description creates BEM modifiers based on transfered prefix (base BEM class) & .
  * @param prefix string that classes start with (base BEM class).
  * @param modifiers list of options that will serve as state BEM modifiers.
  * @returns computed classes starting with "prefix" and ending with form state BEM modifier.
  * @example
  *  const result = useBem('va-component', computed(() => pick(props, ['success'])))
- *  // if success is true
- *  result = { 'va-component--success': true }
- *  result.asArray = ['va-component--success']
- *  result.asString = 'va-component--success'
+ *  // if success is `true`
+ *  result.value: { 'va-component--success': true }
+ *  result.asObject.value: { 'va-component--success': true }
+ *  result.asArray.value: ['va-component--success']
+ *  result.asString.value: 'va-component--success'
  */
 export const useBem = (
   prefix = '',
@@ -40,24 +41,18 @@ export const useBem = (
 
   const computedBemClassesString = computed(() => computedBemClassesArray.value.join(' '))
 
-  const proxy = new Proxy({}, {
+  return new Proxy({}, {
     get (_, key: string) {
-      const isKeyPassed = ['asObject', 'asArray', 'asString'].includes(key)
-
-      let value
       switch (key) {
         case 'asArray':
-          value = computedBemClassesArray
-          break
+          return computedBemClassesArray
         case 'asString':
-          value = computedBemClassesString
-          break
+          return computedBemClassesString
+        case 'asObject':
+          return computedBemClassesObject
         default:
-          value = computedBemClassesObject
+          return computedBemClassesObject.value
       }
-      return isKeyPassed ? value : value.value
     },
-  }) as unknown as ComputedClasses
-
-  return reactive(proxy)
+  }) as ComputedClasses
 }
