e73c4b166d04eaeff92f4c810da4f71181987a74
Damir Sadykov <zs.keda@gmail.com>
2022-06-08 17:44:38 +0500

    Added spacer component to the docs

---
+++ packages/docs/src/page-configs/ui-elements/spacer/examples/Default.vue
@@ -0,0 +1,20 @@
+<template>
+  <va-card color="background" style="padding: 0.75rem;">
+    <div class="d-flex" style="width: 100%;">
+      <va-button>Button</va-button>
+      <va-spacer />
+      <va-button>Button</va-button>
+    </div>
+  </va-card>
+</template>
+
+<style>
+  .spacer {
+    text-align: center;
+    border: 2px dashed #847979;
+  }
+
+  .spacer::after {
+    content: "Va-Spacer is here!";
+  }
+</style>
---
+++ packages/docs/src/pages/ui-elements/spacer.vue
@@ -0,0 +1,20 @@
+<template>
+  <docs-content :config="configComputed" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import DocsContent from '../../components/DocsContent.vue'
+import spacerConfig from '../../page-configs/ui-elements/spacer/page-config'
+
+@Options({
+  components: {
+    DocsContent,
+  },
+})
+export default class Spacer extends Vue {
+  get configComputed () {
+    return spacerConfig
+  }
+}
+</script>
