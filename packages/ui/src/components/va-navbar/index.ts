c2ab97b1e1b8db154404ad22d522f06c6117c03c
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-06-14 17:41:13 +0300

    fix: va-navbar

--- packages/ui/src/components/va-navbar/index.ts
+++ packages/ui/src/components/va-navbar/index.ts
@@ -1,6 +1,6 @@
 import withConfigTransport from '../../services/config-transport/withConfigTransport'
 import _VaNavbar from './VaNavbar.vue'
-import VaNavbarItemComponent from './VaNavbarItem/VaNavbarItem.vue'
+import _VaNavbarItem from './VaNavbarItem/VaNavbarItem.vue'
 
 export const VaNavbar = withConfigTransport(_VaNavbar)
-export const VaNavbarItem = withConfigTransport(VaNavbarItemComponent)
+export const VaNavbarItem = withConfigTransport(_VaNavbarItem)
