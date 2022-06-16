3f6d0eb02dbf0e338c5b8f97fae5969ede54104e
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-06-16 11:03:28 +0300

    fix: va-progress-circle

--- packages/docs/src/page-configs/ui-elements/progress-circle/examples/Thickness.vue
+++ packages/docs/src/page-configs/ui-elements/progress-circle/examples/Thickness.vue
@@ -2,8 +2,10 @@
   <div class="flex lg6 xs12">
     <div class="my-2 text--bold muted">0.1</div>
     <va-progress-circle :thickness="0.1" :modelValue="value" />
+
     <div class="my-2 text--bold muted">0.6</div>
     <va-progress-circle :thickness="0.6" :modelValue="value" />
+
     <div class="my-2 text--bold muted">0.6 + intermediate</div>
     <va-progress-circle :thickness="0.6" indeterminate />
   </div>
