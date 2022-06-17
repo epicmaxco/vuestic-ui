40d1e1d3536bea0f95e2daf6a925673525520909
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-06-17 16:21:22 +0300

    fix: va-sidebar

--- packages/docs/src/page-configs/ui-elements/sidebar/examples/MinimizedWidth.vue
+++ packages/docs/src/page-configs/ui-elements/sidebar/examples/MinimizedWidth.vue
@@ -1,29 +1,22 @@
 <template>
-  <div
-    class="demo-content"
-    style="height: 13rem; border: 2px solid var(--va-primary);"
-  >
+  <div style="height: 13rem; border: 1px solid var(--va-secondary);">
     <va-sidebar :minimized="minimized" textColor="dark" minimizedWidth="0">
       <va-sidebar-item
         v-for="item in items"
-        :key="item.to"
+        :key="item.title"
         :active="item.active"
       >
         <va-sidebar-item-content>
           <va-icon :name="item.icon" />
-          <va-sidebar-item-title v-if="!minimized" style="height: 24px;">
+          <va-sidebar-item-title>
             {{ item.title }}
           </va-sidebar-item-title>
         </va-sidebar-item-content>
       </va-sidebar-item>
     </va-sidebar>
   </div>
-  <div
-    class="demo-controls px-2 py-2"
-    style="background: var(--va-background); width: 100%;"
-  >
-    <va-checkbox v-model="minimized" label="Minimized" />
-  </div>
+
+  <va-checkbox class="mt-2" v-model="minimized" label="Minimized" />
 </template>
 
 <script>
--- packages/ui/src/components/va-sidebar/VaSidebarItem/VaSidebarItem.vue
+++ packages/ui/src/components/va-sidebar/VaSidebarItem/VaSidebarItem.vue
@@ -8,7 +8,6 @@
     :href="hrefComputed"
     :to="$props.to"
     :is="tagComputed"
-    v-bind="$attrs"
     v-on="keyboardFocusListeners"
   >
     <slot />
@@ -16,9 +15,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref, computed } from 'vue'
-import { RouteLocationRaw } from 'vue-router'
-import { useColors } from '../../../services/color-config/color-config'
+import { defineComponent, computed, shallowRef, StyleValue } from 'vue'
+
+import { useColors } from '../../../composables/useColor'
 import useKeyboardOnlyFocus from '../../../composables/useKeyboardOnlyFocus'
 import { useHover } from '../../../composables/useHover'
 import { useRouterLink, useRouterLinkProps } from '../../../composables/useRouterLink'
@@ -27,8 +26,6 @@ import { useSidebarItem } from '../hooks/useSidebar'
 
 export default defineComponent({
   name: 'VaSidebarItem',
-
-  inheritAttrs: false,
 
   props: {
     ...useRouterLinkProps,
@@ -40,7 +37,7 @@ export default defineComponent({
   },
 
   setup (props) {
-    const anchor = ref<HTMLAnchorElement>()
+    const anchor = shallowRef<HTMLAnchorElement>()
 
     const { isHovered } = useHover(anchor)
     const { getColor, getHoverColor, getFocusColor } = useColors()
@@ -66,7 +63,7 @@ export default defineComponent({
     const { textColorComputed } = useTextColor(backgroundColorComputed)
 
     const computedStyle = computed(() => {
-      const style: Record<string, string> = {}
+      const style: StyleValue = {}
 
       style.color = textColorComputed.value
 
@@ -103,7 +100,6 @@ export default defineComponent({
   padding-right: var(--va-sidebar-item-active-border-size);
   display: inline-block;
   width: 100%;
-  color: inherit !important;
   font-family: var(--va-font-family);
 }
 </style>
