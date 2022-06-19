31a5bac79d21adf0a1da75fe07916a51a5273505
m0ksem <m0ksem1337@gmail.com>
2022-06-20 00:55:18 +0300

    Fix#1718. Use capture in useClickOutside.

--- packages/ui/src/composables/useCaptureEvent.ts
+++ packages/ui/src/composables/useCaptureEvent.ts
@@ -2,10 +2,6 @@ import { onMounted, onBeforeUnmount } from 'vue'
 
 /** Register globally event catcher. SSR safe */
 export const useCaptureEvent = (event: string, cb: (...args: any[]) => void) => {
-  onMounted(() => {
-    window.addEventListener(event, cb, true)
-  })
-  onBeforeUnmount(() => {
-    window.removeEventListener(event, cb)
-  })
+  onMounted(() => window.addEventListener(event, cb, { capture: true }))
+  onBeforeUnmount(() => window.removeEventListener(event, cb, { capture: true }))
 }
