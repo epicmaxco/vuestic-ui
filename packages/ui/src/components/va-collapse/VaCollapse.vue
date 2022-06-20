9c493a302dc207032164da01b79b59aca1b5180e
Maksim Nedoshev <m0ksem1337@gmail.com>
2022-06-21 01:40:22 +0300

    fix#1957 Remove collapse margin. Beautify accordion. (#1964)
    * Fix accordion margin.
    
    * Remove extra css variables.

--- packages/ui/src/components/va-collapse/VaCollapse.vue
+++ packages/ui/src/components/va-collapse/VaCollapse.vue
@@ -152,6 +152,7 @@ export default defineComponent({
       tabIndexComputed,
 
       computedClasses: computed(() => ({
+        'va-collapse--expanded': computedModelValue.value,
         'va-collapse--disabled': props.disabled,
         'va-collapse--solid': props.solid,
         'va-collapse--active': props.solid && computedModelValue.value,
@@ -192,7 +193,6 @@ export default defineComponent({
   &__body {
     transition: var(--va-collapse-body-transition);
     overflow: var(--va-collapse-body-overflow);
-    margin-top: var(--va-collapse-body-margin-top);
   }
 
   &__header {
