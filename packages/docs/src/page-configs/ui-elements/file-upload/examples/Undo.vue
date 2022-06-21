192bf2508b3de3ac90f5f128de5c727d5977f443
rustem-nasyrov <nasyroff.rus@gmail.com>
2022-06-21 19:50:24 +0500

    feat: add undoing the removing action example

---
+++ packages/docs/src/page-configs/ui-elements/file-upload/examples/Undo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="width: 300px;">
+    <va-switch
+      v-model="isGalleryViewEnabled"
+      :label="$t('fileUpload.examples.undo.galleryViewLabel')"
+      class="mb-4"
+    />
+    <va-input
+      v-model="undoDuration"
+      :label="$t('fileUpload.examples.undo.inputLabel')"
+      class="mb-4"
+    />
+  </div>
+  <va-file-upload
+    v-model="basic"
+    undo
+    :type="galleryType"
+    :undo-duration="undoDuration"
+  />
+</template>
+
+<script>
+import { VaFileUpload, VaInput, VaSwitch } from 'vuestic-ui/src/components'
+
+export default {
+  components: { VaFileUpload, VaInput, VaSwitch },
+
+  data: () => ({
+    isGalleryViewEnabled: false,
+    undoDuration: 1000,
+    basic: [{ name: 'example 1.png', url: 'https://picsum.photos/100' }],
+  }),
+
+  computed: {
+    galleryType () { return this.isGalleryViewEnabled ? 'gallery' : 'list' },
+  },
+}
+</script>
