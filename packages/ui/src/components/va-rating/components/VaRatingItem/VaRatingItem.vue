dafd923c7820acbf304b160743f23661abfeac44
Mattew Mamonov <g.smellyshovel@gmail.com>
2022-06-22 16:30:23 +0300

    fix va-rating: hover+disabled, hover+readonly: ignore hover

--- packages/ui/src/components/va-rating/components/VaRatingItem/VaRatingItem.vue
+++ packages/ui/src/components/va-rating/components/VaRatingItem/VaRatingItem.vue
@@ -44,6 +44,7 @@ export default defineComponent({
     hover: { type: Boolean, default: false },
     tabindex: { type: Number, default: 0 },
     disabled: { type: Boolean, default: false },
+    readonly: { type: Boolean, default: false },
     size: { type: [String, Number], default: 'medium' },
     emptyIconColor: { type: String },
     color: { type: String, default: 'primary' },
@@ -57,7 +58,7 @@ export default defineComponent({
     const hoveredValue = ref<number | null>(null)
 
     const visibleValue = computed(() => {
-      if (props.hover) {
+      if (props.hover && !props.disabled && !props.readonly) {
         return hoveredValue.value || modelValue.value
       }
       return modelValue.value
