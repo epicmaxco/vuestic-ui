f77b741d02872691817aaffa381d3944f9b7a4b1
m0ksem <m0ksem1337@gmail.com>
2022-06-23 14:29:17 +0300

    Refactor useDomRect.

--- packages/ui/src/composables/useCaptureEvent.ts
+++ packages/ui/src/composables/useCaptureEvent.ts
@@ -1,7 +1,7 @@
 import { onMounted, onBeforeUnmount } from 'vue'
 
 /** Register globally event catcher. SSR safe */
-export const useCaptureEvent = (event: string, cb: (...args: any[]) => void) => {
-  onMounted(() => window.addEventListener(event, cb, { capture: true }))
-  onBeforeUnmount(() => window.removeEventListener(event, cb, { capture: true }))
+export const useCaptureEvent = (event: string, cb: (...args: any[]) => void, options: AddEventListenerOptions = {}) => {
+  onMounted(() => window.addEventListener(event, cb, { capture: true, ...options }))
+  onBeforeUnmount(() => window.removeEventListener(event, cb, { capture: true, ...options }))
 }
