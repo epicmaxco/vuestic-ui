ee3d7df8047aeccce04565f70fe6c0feb142265d
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2022-06-30 19:29:08 +0500

    Feat/improve time picker behavior (#1827)
    * feat: add borders to the center of the picker that's visualize selected values
    
    * feat: add selection that's always centered
    
    feat: add auto selection on scroll
    
    * feat: rename function
    
    * feat: refactor debounced scroll function
    
    feat: remove animated scroll
    
    * feat: add component property description
    
    * feat: add props translation
    
    * feat: add translation to framed property
    
    * feat: add framed property usage example
    
    * feat: rename VaTimePickerColumn directory
    
    * feat: update component export/import
    
    * fix: fix scroll flickering
    
    * feat: remove nested computed properties
    
    feat: set to the frame cell height from a variable
    
    * feat: add column vertical gap variable, remove cell height variable
    
    * feat: add time picker cell height variable
    
    * feat: refactoring
    
    * feat: remove redundant datasets
    
    * feat: move types to the separate file and folder
    
    * feat: rewrite height constants to the component props
    
    * fix: remove v-bind, set variable cell height through css variable in the root tag
    
    * feat: restore animation
    
    * feat: refactor styles
    
    * feat: move child component to the components folder
    
    * feat: refactoring
    
    * feat: refactor semicolons
    
    * feat: automatically calculate height of the component
    
    * feat: add new prop description
    
    feat: fix typo
    
    * feat: add prop usage example
    
    * feat: add new property usage example to the docs
    
    * feat: refactor demo examples
    
    feat: add framed prop usage example
    
    feat: add visible cells count prop usage example
    
    * feat: add spacing between exports declarations
    
    * feat: refactoring
    
    * feat: add selected values background
    
    * feat: fix scroll behavior
    
    * feat: fix scroll
    
    * feat: refactor demo
    
    * feat: handle filtered values
    
    * feat: add passive flag to the listeners
    
    * feat: refactor

---
+++ packages/ui/src/components/va-time-picker/components/VaTimePickerColumn/index.ts
@@ -0,0 +1,4 @@
+import withConfigTransport from '../../../../services/config-transport/withConfigTransport'
+import _VaTimePickerColumn from './VaTimePickerColumn.vue'
+
+export const VaTimePickerColumn = withConfigTransport(_VaTimePickerColumn)
