7f706b170f192389b9f1bdbe010fa81c01afb91f
aluarius <ruseomaster@gmail.com>
2022-07-02 21:24:19 +0600

    fix: new button implementation no-label state problem, deleted redundant no-label class feat: docs updates

---
+++ packages/docs/src/page-configs/ui-elements/button/examples/WithRound.vue
@@ -0,0 +1,5 @@
+<template>
+  <va-button size="large" class="mr-4 mb-2" round>Label</va-button>
+  <va-button size="small" class="mr-4 mb-2" round icon="close" />
+  <va-button class="mr-4 mb-2" round icon="close">Label</va-button>
+</template>
