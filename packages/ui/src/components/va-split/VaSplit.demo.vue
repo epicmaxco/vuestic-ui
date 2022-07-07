cf7769afa9bcc996aa62207ee2f5373b07a6979a
aluarius <ruseomaster@gmail.com>
2022-07-07 13:36:22 +0600

    wip

---
+++ packages/ui/src/components/va-split/VaSplit.demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <VbDemo>
+    <VbCard>
+      <va-split>
+        <template #start>
+          <div style="border: 1px solid red;">{{$vb.lorem(100)}}</div>
+        </template>
+        <template #end>
+          <div style="border: 1px solid red;">{{$vb.lorem(100)}}</div>
+        </template>
+      </va-split>
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import { VaSplit } from './'
+
+export default {
+  components: {
+    VaSplit,
+  },
+}
+</script>
+
+<style lang="scss"></style>
--- packages/ui/src/components/va-split/VaSplit.vue
+++ packages/ui/src/components/va-split/VaSplit.vue
@@ -1,50 +1,88 @@
 <template>
-<section ref="splitContainer" aria-label="split panels">
-  <slot name="start" />
-  <div>|</div>
-  <slot name="end" />
-</section>
+  <section
+    ref="splitContainer"
+    aria-label="split panels"
+    @mousemove="processDragging"
+    @mouseup="stopDragging"
+    @mouseleave="stopDragging"
+    :style="`display: flex`">
+    <div :style="`width: ${splitterPositionComputed}%`">
+      <slot name="start" />
+    </div>
+    <div
+      style="min-width: 10px;"
+      @mousedown="startDragging">+++</div>
+    <div :style="`width: ${100 - splitterPositionComputed}%`">
+      <slot name="end" />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref, shallowRef } from 'vue'
+import { defineComponent, PropType, ref, shallowRef, computed } from 'vue'
 
 export default defineComponent({
   name: 'VaSplit',
 
   props: {
-    direction: {
-      type: String as PropType<'horizontal' | 'vertical'>,
-      default: 'horizontal',
-      validator: (v: string) => ['horizontal', 'vertical'].includes(v),
-    },
     paneSize: {
       type: Number,
       default: 50,
       validator: (v: number) => v <= 100,
     },
     gutterSize: { type: Number, default: 15 },
+    vertical: { type: Boolean, default: false },
     resizable: { type: Boolean, default: false },
     maximizeWithDblClick: { type: Boolean, default: false },
     leftPaneMaximization: { type: Boolean, default: false },
-    limits: { type: Array as any as PropType<[number, number]>, default: undefined },
+    limits: { type: Array as any as PropType<[number, number]>, default: () => [30, 70] },
   },
 
   emits: ['update:modelValue'],
 
-  setup: () => {
+  setup: (props) => {
     const splitContainer = shallowRef<HTMLElement>()
-    const isDrugging = ref(false)
+    const containerSizeComputed = computed(() => {
+      if (!splitContainer.value) { return undefined }
 
-    const startDrugging = (e: MouseEvent | TouchEvent) => {
-      isDrugging.value = true
+      return props.vertical ? splitContainer.value.offsetHeight : splitContainer.value.offsetWidth
+    })
+
+    const isDragging = ref(false)
+    const dragStartPosition = ref(0)
+
+    const splitterPosition = ref(50)
+    const splitterPositionComputed = computed({
+      get: () => splitterPosition.value,
+      set: (v) => {
+        if (v < props.limits[0]) {
+          splitterPosition.value = props.limits[0]
+        } else if (v > props.limits[1]) {
+          splitterPosition.value = props.limits[1]
+        } else {
+          splitterPosition.value = v
+        }
+      },
+    })
+
+    const startDragging = (e: MouseEvent) => {
+      isDragging.value = true
+      dragStartPosition.value = props.vertical ? e.pageY : e.pageX
     }
 
-    const stopDrugging = (e: MouseEvent | TouchEvent) => {
-      isDrugging.value = false
+    const processDragging = (e: MouseEvent) => {
+      if (!isDragging.value || !containerSizeComputed.value) { return }
+
+      const dragEndPosition = props.vertical ? e.pageY : e.pageX
+      const distance = dragEndPosition - dragStartPosition.value
+      console.log(distance, dragEndPosition, dragStartPosition.value)
+
+      splitterPositionComputed.value = splitterPositionComputed.value + Math.floor((distance / containerSizeComputed.value) * 100)
     }
 
-    return { splitContainer }
+    const stopDragging = () => (isDragging.value = false)
+
+    return { splitContainer, startDragging, processDragging, stopDragging, splitterPositionComputed }
   },
 })
 </script>
