62293d695b7e2184f7fbdac44a115a2f16fa0d9a
Damir Sadykov <zs.keda@gmail.com>
2022-07-07 23:34:28 +0500

    page update on SW change (#2050)

--- packages/docs/src/App.vue
+++ packages/docs/src/App.vue
@@ -6,7 +6,7 @@
 <script>
 import { defineComponent } from '@vue/runtime-core'
 import emitter from 'tiny-emitter/instance'
-import { useColors } from '../../ui/src/main'
+import { useColors, useToast } from '../../ui/src/main'
 import { COLOR_THEMES, ThemeName } from './config/theme-config'
 
 const eventBus = {
@@ -24,6 +24,16 @@ export default defineComponent({
     }
   },
   setup () {
+    const { init } = useToast()
+    window.page_reload_notification = () => {
+      init({
+        message: 'New content is available. Page will be reloaded in 3 sec.',
+        color: 'success',
+        position: 'bottom-right',
+        closeable: false,
+        duration: 3000,
+      })
+    }
     useColors().setColors(COLOR_THEMES[ThemeName.DEFAULT])
   },
 })
