249ffbc390f5ebfd74140d60e9e0d496e5bd9111
m0ksem <m0ksem1337@gmail.com>
2022-07-07 22:06:13 +0300

    Remove extra styling from docs sidebar.

--- packages/docs/src/components/sidebar/Sidebar.vue
+++ packages/docs/src/components/sidebar/Sidebar.vue
@@ -162,17 +162,6 @@ export default defineComponent({
     line-height: 20px;
     cursor: pointer;
 
-    ::before {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background-color: var(--va-primary);
-      opacity: 0;
-    }
-
     &:hover {
       ::before {
         opacity: 0.2;
