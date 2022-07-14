2459d5d8ebe3c224bbee295147467d99a83b8458
brunorabelo <brunorabelow@gmail.com>
2022-07-14 13:57:26 +0200

    remove root div from examples (#2091)
    * removed root div from alert examples
    
    * remove root div tag from all examples
    
    * [#2073] `VaSlider` range static `modelValue` fix (#2087)
    
    Co-authored-by: Oleg <64714442+aluarius@users.noreply.github.com>
    Co-authored-by: aluarius <ruseomaster@gmail.com>

--- packages/docs/src/page-configs/ui-elements/backtop/examples/Default.vue
+++ packages/docs/src/page-configs/ui-elements/backtop/examples/Default.vue
@@ -1,77 +1,75 @@
 <template>
-  <div>
-    <va-switch v-model="positionVertical"
-      true-value="top"
-      false-value="bottom"
-      true-inner-label="top"
-      false-inner-label="bottom"
-      label="Vertical Position:"
-      left-label
-      class="py-2"
-    />
-    <br />
-    <va-switch v-model="positionHorizontal"
-      true-value="left"
-      false-value="right"
-      true-inner-label="left"
-      false-inner-label="right"
-      label="Horizontal Position:"
-      left-label
-      class="pt-2 pb-4"
-    />
-    <br />
-    <va-slider
-      label="Vertical offset:"
-      track-label-visible
-      v-model="verticalOffset"
-      :track-label="`${verticalOffset}%`"
-      :min="1"
-      :max="100"
-    />
-    <br />
-    <va-slider
-      label="Horizontal offset:"
-      track-label-visible
-      v-model="horizontalOffset"
-      :track-label="`${horizontalOffset}%`"
-      :min="1"
-      :max="100"
-    />
-    <br />
-    <va-slider
-      label="Visibility height:"
-      track-label-visible
-      v-model="visibilityHeight"
-      :track-label="`${visibilityHeight}`"
-      :min="0"
-      :max="600"
-    />
-    <br />
-    <va-slider
-      track-label-visible
-      v-model="scrollSpeed"
-      label="Scroll speed:"
-      :track-label="`${scrollSpeed}`"
-      :min="1"
-      :max="500"
-    />
-    <br />
-    <va-checkbox
-      v-model="isSlot"
-      :label="`Active Slot: ${isSlot}`" />
+  <va-switch v-model="positionVertical"
+             true-value="top"
+             false-value="bottom"
+             true-inner-label="top"
+             false-inner-label="bottom"
+             label="Vertical Position:"
+             left-label
+             class="py-2"
+  />
+  <br />
+  <va-switch v-model="positionHorizontal"
+             true-value="left"
+             false-value="right"
+             true-inner-label="left"
+             false-inner-label="right"
+             label="Horizontal Position:"
+             left-label
+             class="pt-2 pb-4"
+  />
+  <br />
+  <va-slider
+    label="Vertical offset:"
+    track-label-visible
+    v-model="verticalOffset"
+    :track-label="`${verticalOffset}%`"
+    :min="1"
+    :max="100"
+  />
+  <br />
+  <va-slider
+    label="Horizontal offset:"
+    track-label-visible
+    v-model="horizontalOffset"
+    :track-label="`${horizontalOffset}%`"
+    :min="1"
+    :max="100"
+  />
+  <br />
+  <va-slider
+    label="Visibility height:"
+    track-label-visible
+    v-model="visibilityHeight"
+    :track-label="`${visibilityHeight}`"
+    :min="0"
+    :max="600"
+  />
+  <br />
+  <va-slider
+    track-label-visible
+    v-model="scrollSpeed"
+    label="Scroll speed:"
+    :track-label="`${scrollSpeed}`"
+    :min="1"
+    :max="500"
+  />
+  <br />
+  <va-checkbox
+    v-model="isSlot"
+    :label="`Active Slot: ${isSlot}`" />
 
-    <va-backtop
-      target=".base-layout__content"
-      :vertical-offset="verticalOffset + '%'"
-      :horizontal-offset="horizontalOffset + '%'"
-      :horizontal-position="positionHorizontal"
-      :vertical-position="positionVertical"
-      :visibility-height="visibilityHeight"
-      :speed="scrollSpeed"
-    >
-      <p v-if="isSlot">Slot: back to top</p>
-    </va-backtop>
-  </div>
+  <va-backtop
+    target=".base-layout__content"
+    :vertical-offset="verticalOffset + '%'"
+    :horizontal-offset="horizontalOffset + '%'"
+    :horizontal-position="positionHorizontal"
+    :vertical-position="positionVertical"
+    :visibility-height="visibilityHeight"
+    :speed="scrollSpeed"
+  >
+    <p v-if="isSlot">Slot: back to top</p>
+  </va-backtop>
 </template>
 
 <script>
