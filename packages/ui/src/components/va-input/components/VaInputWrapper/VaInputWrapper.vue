98e96013ce41c492227449979636cb23eb8eec02
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-07-27 11:12:57 +0300

    fix: replace min-width to wrapper

--- packages/ui/src/components/va-input/components/VaInputWrapper/VaInputWrapper.vue
+++ packages/ui/src/components/va-input/components/VaInputWrapper/VaInputWrapper.vue
@@ -192,12 +192,12 @@ export default defineComponent({
   font-family: var(--va-font-family);
   display: var(--va-input-wrapper-display);
   vertical-align: var(--va-input-wrapper-vertical-align);
+  min-width: var(--va-input-wrapper-min-width);
 
   &__field {
     position: relative;
     display: flex;
     align-items: center;
-    min-width: var(--va-input-wrapper-min-width);
     width: 100%;
     min-height: var(--va-input-min-height);
     border-color: var(--va-input-wrapper-border-color);
