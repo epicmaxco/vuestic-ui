9aed6679f98c197604a2a80419128f2ed05661ac
Vitaly <vitaly.raichev@epicmax.co>
2022-08-04 20:37:15 +0300

    [#2190] Fix `VaButtonDropdown` dropdown appearance and alignment for `split` and `left-icon` (#2201)
    * fix: add prevent-overflow
    
    * fix: set vertical-align to middle

--- packages/ui/src/components/va-button-dropdown/VaButtonDropdown.vue
+++ packages/ui/src/components/va-button-dropdown/VaButtonDropdown.vue
@@ -50,6 +50,8 @@
         :placement="$props.placement"
         :offset="$props.offset"
         :stateful="$props.stateful"
+        :close-on-content-click="$props.closeOnContentClick"
+        prevent-overflow
         v-model="valueComputed"
       >
         <template #anchor>
@@ -208,6 +210,7 @@ export default defineComponent({
 .va-button-dropdown {
   display: inline-block;
   font-family: var(--va-font-family);
+  vertical-align: middle;
 
   .va-button {
     margin: var(--va-button-dropdown-button-margin);
