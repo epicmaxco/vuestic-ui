d82d9b4236b8ae7b5c60da41799f6323e79641a8
aluarius <ruseomaster@gmail.com>
2022-08-08 16:23:06 +0600

    feat: docs updates

---
+++ packages/docs/src/page-configs/ui-elements/app-bar/examples/Fixed.vue
@@ -0,0 +1,21 @@
+<template>
+  <va-app-bar v-if="enabled" bottom fixed hide-on-scroll>
+    <va-button icon="home" color="#fff" flat :rounded="false" />
+    <va-button icon="info" color="#fff" flat :rounded="false" />
+    <va-button icon="battery" color="#fff" flat :rounded="false" />
+    <va-spacer />
+    <va-button color="#fff" flat :rounded="false">Login</va-button>
+    <va-button-dropdown size="small" flat color="#fff" icon="ellipsis">
+      Content
+    </va-button-dropdown>
+  </va-app-bar>
+  <va-button @click="enabled = !enabled">Enable/Disable fixed app bar example</va-button>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    enabled: false,
+  }),
+}
+</script>
