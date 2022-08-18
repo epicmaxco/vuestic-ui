b9f1ed69fef559141fd1cd8c1d202b0941495bd2
azgiliat <azgiliat@azgiliats-MacBook-Pro.local>
2022-08-18 16:09:26 +0300

    use composition api, change some styles, use current locale if replace url

--- packages/docs/src/components/header/components/algolia-search/AlgoliaSearch.vue
+++ packages/docs/src/components/header/components/algolia-search/AlgoliaSearch.vue
@@ -3,42 +3,45 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
+import docsearch from '@docsearch/js'
+import { onMounted, defineComponent } from 'vue'
+import { __DEV__ } from 'vuestic-ui/src/utils/global-utils'
+import { useI18n } from 'vue-i18n'
 
-@Options({
-  name: 'Search',
-})
-export default class Search extends Vue {
-  initialize () {
-    Promise.all([
-      import(/* webpackChunkName: "docsearch" */ '@docsearch/js'),
-    ]).then(([{ default: docsearch }]) => {
+export default defineComponent({
+  name: 'AlgoliaSearch',
+  setup () {
+    const { locale } = useI18n()
+
+    onMounted(() => {
+      const changeUrlToLocalhost: Parameters<typeof docsearch>[0]['transformItems'] = (items) => items.map((item) => ({
+        ...item,
+        url: item.url.replace(/^https.*\/en/, `${window.location.origin}/${locale.value}`),
+      }))
+      const transformItems = __DEV__ ? changeUrlToLocalhost : undefined
+
       docsearch({
         container: '#docsearch',
         appId: 'DVNV64RN9R',
         indexName: 'vuestic',
         apiKey: 'cd8e70cb466bf6df138543a38c33ea5e',
-        transformItems: process.env.NODE_ENV === 'development'
-          ? (items) => items.map((item) => ({
-            ...item,
-            url: item.url.replace(/^https.*\/en/, `${window.location.origin}/ru`),
-          }))
-          : undefined,
+        transformItems,
       })
     })
-  }
-
-  mounted () {
-    this.initialize()
-  }
-}
+  },
+})
 </script>
 
 <style lang="scss">
-
 @import '@docsearch/css';
 @import '~vuestic-ui/src/styles/index.scss';
 @import '@/assets/smart-grid.scss';
+
+:root {
+  --docsearch-primary-color: var(--va-primary);
+  --docsearch-searchbox-shadow: inset 0 0 0 var(--va-input-border-width) var(--va-primary);
+  --docsearch-text-color: var(--va-secondary);
+}
 
 #docsearch {
   flex-grow: 1;
@@ -48,6 +51,8 @@ export default class Search extends Vue {
     margin-left: 0;
     flex-grow: 1;
     max-width: 320px;
+    border-radius: var(--va-input-border-radius);
+    background-color: var(--va-input-wrapper-background);
   }
 
   .DocSearch-Button-Placeholder {
@@ -68,5 +73,4 @@ export default class Search extends Vue {
     @include xs(display, none);
   }
 }
-
 </style>
