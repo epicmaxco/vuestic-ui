3a5c4f19700ebcc2703e4212d3a3388345cc69e7
m0ksem <m0ksem1337@gmail.com>
2022-08-18 16:16:11 +0300

    feat(nuxt-docs): added i18n module

--- packages/nuxt-docs/nuxt.config.ts
+++ packages/nuxt-docs/nuxt.config.ts
@@ -2,5 +2,5 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['./modules/vuestic', './modules/define-page-config'],
+  modules: ['./modules/vuestic', './modules/define-page-config', './modules/i18n'],
 })
