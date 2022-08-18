0c9de3e26b6c5a1ed60b5116605af09265676dc6
azgiliat <azgiliat@azgiliats-MacBook-Pro.local>
2022-08-18 08:48:59 +0300

    update docsearch version

--- packages/docs/src/components/header/components/algolia-search/AlgoliaSearch.vue
+++ packages/docs/src/components/header/components/algolia-search/AlgoliaSearch.vue
@@ -1,56 +1,35 @@
 <template>
   <form id="search-form">
-    <va-input
-      id="algolia-search-input"
-      class="search-input"
-      v-model="value"
-      placeholder="Search..."
-      @focus="onFocusHandler('dark', 'block')"
-      @blur="onFocusHandler('gray', 'none')"
-    >
-      <template #prependInner>
-        <va-icon
-          class="search-icon mr-2"
-          name="search"
-          :color="color"
-          size="100%"
-        />
-      </template>
-    </va-input>
+    <div id="docsearch" />
   </form>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 
+import '@docsearch/css'
+
 @Options({
   name: 'Search',
 })
 export default class Search extends Vue {
-  value = ''
-  color = 'gray'
-
-  onFocusHandler (colorValue: string, displayValue: string): void {
-    this.color = colorValue
-    const el: HTMLElement | null = document.querySelector('.ds-dropdown-menu')
-    if (el) { el.style.display = displayValue }
-  }
-
   initialize () {
     Promise.all([
-      // @ts-ignore
-      import(/* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.js'),
-      // @ts-ignore
-      import(/* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.css'),
-    ]).then(([docsearch]) => {
-      docsearch = docsearch.default
-      docsearch(Object.assign({
-        apiKey: 'be3528055c92da2ea5133b93ed548c6d',
+      import(/* webpackChunkName: "docsearch" */ '@docsearch/js'),
+    ]).then(([{ default: docsearch }]) => {
+      docsearch({
+        container: '#docsearch',
+        appId: 'DVNV64RN9R',
         indexName: 'vuestic',
-      }, {
-        inputSelector: '#algolia-search-input',
-        debug: true,
-      }))
+        apiKey: 'cd8e70cb466bf6df138543a38c33ea5e',
+        // TODO in dev mode transform url to localhost
+        // transformItems (items) {
+        //   return items.map((item) => {
+        //     console.log(item)
+        //     return item
+        //   })
+        // },
+      })
     })
   }
 
@@ -73,163 +52,33 @@ export default class Search extends Vue {
   width: 100%;
   z-index: 1;
 
-  &:focus-within {
-    @include xs(position, absolute);
-    @include xs(left, 0);
-    @include xs(padding, 0 calc(1.2rem + 4px));
+  #docsearch {
+    flex-grow: 1;
+    display: flex;
+  }
 
-    .ds-dataset-1 {
-      @include xs(width, 100vw);
-      @include xs(left, 0);
+  .DocSearch-Button {
+    margin-left: 0;
+    flex-grow: 1;
+    max-width: 320px;
+  }
+
+  .DocSearch-Button-Placeholder {
+    @media (max-width: 768px) {
+      display: block;
+    }
+    //@include xs(display, none);
+    @media (max-width: 480px) {
+      display: block;
     }
   }
 
-  .va-input-wrapper__field {
-    padding: 0.25rem;
-    padding-left: 0.75rem;
-    background: var(--va-input-wrapper-background);
-    border-radius: 0.25rem;
-
-    @include xs(width, 5rem);
-
-    &:focus-within {
-      @include xs(width, var(--va-form-element-default-width));
-    }
-  }
-
-  .va-input__container {
-    background-color: transparent !important;
-    border: none;
-  }
-
-  .va-input-wrapper__prepend-inner {
-    margin: 0;
-  }
-
-  .search-icon {
-    transition: background-color, 0.2s;
-  }
-
-  .search-input {
-    background: none;
-    border: none;
-    font-size: 100%;
-    width: 100%;
-    max-width: 20rem;
-    padding-left: 0.1rem;
-
-    &:focus-within {
-      @include xs(max-width, none);
-    }
-  }
-
-  #algolia-search-input::placeholder {
-    color: var(--va-secondary);
-  }
-
-  .ds-dropdown-menu {
-    &::before {
-      display: none;
+  .DocSearch-Button-Keys {
+    @media (max-width: 768px) {
+      display: flex;
     }
 
-    > div {
-      position: fixed;
-      border: none;
-      box-shadow: 0 2px 8px rgba(122, 139, 173, 0.2);
-      border-radius: 4px;
-      padding: 0;
-
-      .ds-suggestion {
-        .algolia-docsearch-suggestion {
-          padding: 0;
-        }
-
-        &:first-child {
-          .algolia-docsearch-suggestion--category-header {
-            border-top: none;
-          }
-        }
-
-        .algolia-docsearch-suggestion--category-header {
-          padding: 0.25rem 1rem;
-          border-bottom: none;
-          border-top: 1px solid $prism-background;
-
-          &-lvl0 {
-            font-family: Source Sans Pro;
-            font-style: normal;
-            font-weight: bold;
-            font-size: 10px;
-            line-height: 14px;
-            letter-spacing: 0.6px;
-            text-transform: uppercase;
-            color: $default-gray;
-
-            .algolia-docsearch-suggestion--highlight {
-              box-shadow: inset 0 -2px 0 0 rgba(44, 130, 224, 0.1);
-            }
-          }
-        }
-
-        .algolia-docsearch-suggestion--wrapper {
-          padding-top: 0;
-        }
-
-        .algolia-docsearch-suggestion--subcategory-column {
-          display: none;
-        }
-
-        .algolia-docsearch-suggestion--content {
-          padding: 0.5rem 1rem;
-          float: left;
-          width: 100%;
-
-          &::before {
-            display: none;
-          }
-
-          .algolia-docsearch-suggestion--title {
-            font-family: Source Sans Pro;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 16px;
-            line-height: 20px;
-            color: var(--va-dark);
-
-            .algolia-docsearch-suggestion--highlight {
-              padding: 0;
-              color: var(--va-primary);
-              background: rgba(44, 130, 224, 0.1);
-            }
-          }
-
-          .algolia-docsearch-suggestion--text {
-            font-family: Source Sans Pro;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 15px;
-            line-height: 20px;
-            color: $default-gray;
-
-            .algolia-docsearch-suggestion--highlight {
-              color: var(--va-primary);
-              background: rgba(44, 130, 224, 0.1);
-              box-shadow: none;
-            }
-          }
-        }
-      }
-
-      .ds-cursor {
-        .algolia-docsearch-suggestion--content {
-          background-color: $prism-background;
-        }
-      }
-
-      .algolia-docsearch-footer {
-        margin: 1rem;
-      }
-    }
+    @include xs(display, none);
   }
 }
 </style>
