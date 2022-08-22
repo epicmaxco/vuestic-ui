88ac77870b61216f9faf673984cf51a4eb147195
aluarius <ruseomaster@gmail.com>
2022-08-22 21:11:18 +0600

    feat:pagination documentation update

---
+++ packages/docs/src/page-configs/ui-elements/pagination/examples/Gapped.vue
@@ -0,0 +1,15 @@
+<template>
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="default" gapped class="mb-4" />
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="primary" gapped class="mb-4" />
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="secondary" gapped />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 3,
+    }
+  },
+}
+</script>
---
+++ packages/docs/src/page-configs/ui-elements/pagination/examples/Presets.vue
@@ -0,0 +1,17 @@
+<template>
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="default" class="mb-4" />
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="primary" class="mb-4" />
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="secondary" class="mb-4" />
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="plain" class="mb-4" />
+  <va-pagination v-model="value" :pages="10" :visible-pages="3" preset="plainOpacity" />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 3,
+    }
+  },
+}
+</script>
