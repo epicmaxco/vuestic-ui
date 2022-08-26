c7ce9874a498db52087d76c42603ec73e7368f17
m0ksem <m0ksem1337@gmail.com>
2022-08-26 17:42:07 +0300

    tests: added nuxt tree-shaking tests to bundlers-tests

--- packages/bundlers-tests/nuxt.config.ts
+++ packages/bundlers-tests/nuxt.config.ts
@@ -1,6 +1,5 @@
 import { defineNuxtConfig } from 'nuxt'
 import { resolve } from 'pathe'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -16,24 +15,14 @@ export default defineNuxtConfig({
     config: {
       // Config here
     },
+    css: ['typography', 'reset']
   },
 
   rootDir: './src/',
 
   vite: {
-    resolve: {
-      alias: {
-        // 'vuestic-ui/css': '../ui/css',
-        'vuestic-ui': resolve(__dirname, '../../node_modules/vuestic-ui')
-      }
+    build: {
+      sourcemap: false,
     },
-    plugins: [
-      visualizer({
-        filename: './dist/nuxt.stats.html',
-        title: 'Vuestic Nuxt App',
-        template: 'sunburst',
-        sourcemap: true,
-      })
-    ]
   }
 })
