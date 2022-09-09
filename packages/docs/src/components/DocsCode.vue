97c77159cf5cb37277de427bd25dc9116df6bd72
m0ksem <m0ksem1337@gmail.com>
2022-09-09 16:04:12 +0300

    fix(#2380): syntax-highlight in docs

--- packages/docs/src/components/DocsCode.vue
+++ packages/docs/src/components/DocsCode.vue
@@ -1,10 +1,7 @@
 <template>
   <va-tabs v-if="!isString" v-model="index">
     <template #tabs>
-      <va-tab
-        v-for="tab in tabs"
-        :key="tab"
-      >
+      <va-tab v-for="tab in tabs" :key="tab">
         {{ tab }}
       </va-tab>
     </template>
@@ -26,17 +23,27 @@ export default defineComponent({
   components: { PrismWrapper },
   props: {
     language: { type: String as PropType<string>, default: 'javascript' },
-    code: { type: [Object, String] as PropType<Record<string, string> | string>, default: '' },
+    code: {
+      type: [Object, String] as PropType<Record<string, string> | string>,
+      default: '',
+    },
   },
   setup (props) {
     const isString = computed(() => typeof props.code === 'string')
 
-    const tabs = computed(() => isString.value ? [] : Object.keys(props.code))
+    const tabs = computed(() =>
+      isString.value ? [] : Object.keys(props.code),
+    )
 
-    const contents = computed(() => isString.value
-      ? [applyTranslations((props.code as string).trim())]
-      : tabs.value.map(tab => applyTranslations((props.code as Record<string, string>)[tab].trim()),
-      ))
+    const contents = computed(() =>
+      isString.value
+        ? [applyTranslations((props.code as string).trim())]
+        : tabs.value.map((tab) =>
+          applyTranslations(
+            (props.code as Record<string, string>)[tab].trim(),
+          ),
+        ),
+    )
 
     const index = ref(0)
 
@@ -44,7 +51,9 @@ export default defineComponent({
 
     const forceUpdate = () => {
       doShowCode.value = false
-      nextTick(() => { doShowCode.value = true }) // nextTick() triggers v-if, that causes re-rendering of component.
+      nextTick(() => {
+        doShowCode.value = true
+      }) // nextTick() triggers v-if, that causes re-rendering of component.
     }
 
     // Hack: Remove this with alias when moving to vite.
@@ -75,10 +84,9 @@ export default defineComponent({
   // The structure is temporarily saved.
   // Notably it has structure like this: pre.DocsCode > code.DocsCode.
   // Here class is being applied twice, while it should have been applied only on external container
-  pre.DocsCode {
-    margin-bottom: 0.2rem;
+  .DocsCode {
+    margin-bottom: 0.2rem !important;
     background: var(--va-background-secondary);
-    padding-top: 1.3rem;
     font-size: calc(1rem / 1.4);
 
     code[class*="language-"],
@@ -98,95 +106,89 @@ export default defineComponent({
       border-radius: 0.25rem;
     }
 
+    :not(pre) > code[class*="language-"],
     pre[class*="language-"] {
-      padding: 1rem;
-      margin: 0;
-      overflow: auto;
+      background: $prism-background;
+    }
 
-      :not(pre) > code[class*="language-"],
-      pre[class*="language-"] {
-        background: $prism-background;
-      }
+    /* Inline code */
+    :not(pre) > code[class*="language-"] {
+      padding: 0.1rem;
+      border-radius: 0.3rem;
+      white-space: normal;
+    }
 
-      /* Inline code */
-      :not(pre) > code[class*="language-"] {
-        padding: 0.1rem;
-        border-radius: 0.3rem;
-        white-space: normal;
-      }
+    .token.comment,
+    .token.block-comment,
+    .token.prolog,
+    .token.doctype,
+    .token.cdata {
+      color: slategray;
+    }
 
-      .token.comment,
-      .token.block-comment,
-      .token.prolog,
-      .token.doctype,
-      .token.cdata {
-        color: slategray;
-      }
+    .token.punctuation {
+      color: #999999;
+    }
 
-      .token.punctuation {
-        color: #999999;
-      }
+    .token.namespace {
+      opacity: 0.7;
+    }
 
-      .token.namespace {
-        opacity: 0.7;
-      }
+    .token.property,
+    .token.tag,
+    .token.boolean,
+    .token.number,
+    .token.constant,
+    .token.symbol,
+    .token.deleted {
+      color: #990055;
+    }
 
-      .token.property,
-      .token.tag,
-      .token.boolean,
-      .token.number,
-      .token.constant,
-      .token.symbol,
-      .token.deleted {
-        color: #990055;
-      }
+    .token.selector,
+    .token.attr-name,
+    .token.string,
+    .token.char,
+    .token.builtin,
+    .token.inserted {
+      color: #669900;
+    }
 
-      .token.selector,
-      .token.attr-name,
-      .token.string,
-      .token.char,
-      .token.builtin,
-      .token.inserted {
-        color: #669900;
-      }
+    .token.operator,
+    .token.entity,
+    .token.url,
+    .language-css .token.string,
+    .style .token.string {
+      color: #9a6e3a;
+    }
 
-      .token.operator,
-      .token.entity,
-      .token.url,
-      .language-css .token.string,
-      .style .token.string {
-        color: #9a6e3a;
-      }
+    .token.atrule,
+    .token.attr-value,
+    .token.keyword {
+      color: #0077aa;
+    }
 
-      .token.atrule,
-      .token.attr-value,
-      .token.keyword {
-        color: #0077aa;
-      }
+    .token.function,
+    .token.class-name {
+      color: #dd4a68;
+    }
 
-      .token.function,
-      .token.class-name {
-        color: #dd4a68;
-      }
+    .token.regex,
+    .token.important,
+    .token.variable {
+      color: #ee9900;
+    }
 
-      .token.regex,
-      .token.important,
-      .token.variable {
-        color: #ee9900;
-      }
+    .token.important,
+    .token.bold {
+      font-weight: $font-weight-bold;
+    }
 
-      .token.important,
-      .token.bold {
-        font-weight: $font-weight-bold;
-      }
+    .token.italic {
+      font-style: italic;
+    }
 
-      .token.italic {
-        font-style: italic;
-      }
-
-      .token.entity {
-        cursor: help;
-      }
+    .token.entity {
+      cursor: help;
     }
   }
 </style>
