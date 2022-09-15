854730b5d214ea998043449bc77882c416b31000
Azgiliat <46869767+Azgiliat@users.noreply.github.com>
2022-09-15 15:11:59 +0300

    Fix/unit tests (#2281)
    fix some tests

--- packages/ui/src/composables/tests/useHover.spec.ts
+++ packages/ui/src/composables/tests/useHover.spec.ts
@@ -3,28 +3,27 @@ import { mount } from '@vue/test-utils'
 import { ref } from 'vue'
 import { describe, it, expect } from 'vitest'
 
-// TODO Test broken. Getting error.
-describe.skip('useHover', () => {
-  it(
-    'mouse enter & leave should change isHovered value',
-    async () => {
-      const TestComponent = {
-        template: '<p ref="content"></p>',
-        setup () {
-          const content = ref(null)
-          const { isHovered } = useHover(content)
-          return { isHovered, content }
-        },
-      }
+describe('useHover', () => {
+  it('mouse enter & leave should change isHovered value', async () => {
+    const TestComponent = {
+      template: '<p ref="content">test test</p>',
+      setup () {
+        const content = ref(null)
+        const { isHovered } = useHover(content)
+        return { isHovered, content }
+      },
+    }
 
-      const wrapper = mount(TestComponent)
-      expect(wrapper.exists()).toBeTruthy()
+    const wrapper = mount(TestComponent)
+    expect(wrapper.exists()).toBeTruthy()
+    await wrapper.vm.$nextTick()
 
-      await wrapper.trigger('mouseenter')
-      await wrapper.vm.$nextTick(() => expect(wrapper.vm.isHovered).toBe(true))
+    await wrapper.trigger('mouseenter')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.isHovered).toBe(true)
 
-      await wrapper.trigger('mouseleave')
-      await wrapper.vm.$nextTick(() => expect(wrapper.vm.isHovered).toBe(false))
-    },
-  )
+    await wrapper.trigger('mouseleave')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.isHovered).toBe(false)
+  })
 })
