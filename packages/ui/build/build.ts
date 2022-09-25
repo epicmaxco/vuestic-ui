286731cea93bd36d620572bac94f43c8346081e8
m0ksem <m0ksem1337@gmail.com>
2022-09-25 05:08:02 +0300

    chore: bump version to v1.4.12

--- packages/ui/build/build.ts
+++ packages/ui/build/build.ts
@@ -7,14 +7,15 @@ defineVuesticBuild(async () => {
   buildStart()
 
   await Promise.all([
-    $('npm run build:types', { successMessage: 'types built' }),
+    // $('npm run build:types', { successMessage: 'types built' }),
     $('vite build --config ./build/configs/vite.cjs.ts', { successMessage: 'cjs built' }),
     $('vite build --config ./build/configs/vite.iife.ts', { successMessage: 'iife built' }),
     $('vite build --config ./build/configs/vite.es.ts', { successMessage: 'esm built' }),
     $('vite build --config ./build/configs/vite.mjs.ts', { successMessage: 'esm-node built' }),
-    $('vite build --config ./build/configs/vite.styles.ts', { successMessage: 'styles built' }),
-    $('vite build --config ./build/configs/vite.styles-essential.ts', { successMessage: 'essential styles built' }),
   ])
+
+  await $('vite build --config ./build/configs/vite.styles-essential.ts', { successMessage: 'essential styles built' })
+  await $('vite build --config ./build/configs/vite.styles.ts', { successMessage: 'styles built' })
 
   buildEnd()
 })
