68254d11e9c01a52ac469dadd4947bbe7b5ccdef
Kamil <46625964+skyroses@users.noreply.github.com>
2022-09-27 19:25:01 +0300

    New slots for custom boundary and direction links (#2388)

--- packages/docs/src/page-configs/ui-elements/pagination/api-options.ts
+++ packages/docs/src/page-configs/ui-elements/pagination/api-options.ts
@@ -1,3 +1,10 @@
 import { defineManualApi } from '../../../components/DocsApi/ManualApiOptions'
 
-export default defineManualApi({})
+export default defineManualApi({
+  slots: {
+    firstPageLink: { },
+    prevPageLink: { },
+    nextPageLink: { },
+    lastPageLink: { },
+  },
+})
