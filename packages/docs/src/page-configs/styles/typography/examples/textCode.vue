f3097773aca7c42fbb0467be714ca637a513d5d5
Maksim Nedoshev <m0ksem1337@gmail.com>
2022-10-14 21:40:47 +0300

    fix: typography double dash (#2504)
    * fix: typography double dash
    
    * fix: va-title class names
    
    * chore: removed useless selector in va-link

--- packages/docs/src/page-configs/styles/typography/examples/textCode.vue
+++ packages/docs/src/page-configs/styles/typography/examples/textCode.vue
@@ -1,4 +1,4 @@
 <template>
-  <span class="va-text--code">currentColor</span> none has a greater influence on life on planet Earth than it’s own
+  <span class="va-text-code">currentColor</span> none has a greater influence on life on planet Earth than it’s own
   satellite, the moon.
 </template>
