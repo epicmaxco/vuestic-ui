7d46fa1ad57a884b85ba276cf5ab17069c7c4b2e
m0ksem <m0ksem1337@gmail.com>
2022-11-01 07:57:37 +0200

    fix(docs): old create-vuestic api scheme

--- packages/docs/src/page-configs/services/i18n/code-examples/setup.ts
+++ packages/docs/src/page-configs/services/i18n/code-examples/setup.ts
@@ -3,14 +3,16 @@ import { createVuestic } from 'vuestic-ui'
 
 createApp(App)
   .use(createVuestic({
-    // ...
-
-    i18n: {
-      ok: 'Добре',
-      cancel: 'Скасувати',
-      search: 'Пошук',
-
+    config: {
       // ...
+
+      i18n: {
+        ok: 'Добре',
+        cancel: 'Скасувати',
+        search: 'Пошук',
+
+        // ...
+      }
     }
   }))
   .mount('#app')
