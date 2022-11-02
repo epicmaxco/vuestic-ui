04b74b0519bd4023e9a9b87078ddc0288dc44b05
m0ksem <m0ksem1337@gmail.com>
2022-11-02 23:45:22 +0200

    fix(#2618): add app.mount to installation code examples

--- packages/docs/src/page-configs/styles/colors/code-examples/scheme.ts
+++ packages/docs/src/page-configs/styles/colors/code-examples/scheme.ts
@@ -1,13 +1,14 @@
 export const scheme = `
 const app = createApp(App)
-app.use(createVuestic({
-  config: {
-    colors: {
-      variables: {
-        primary: '#B456C0',
-        custom: '#A35600',
-      }
+  .use(createVuestic({
+    config: {
+      colors: {
+        variables: {
+          primary: '#B456C0',
+          custom: '#A35600',
+        }
+      },
     },
-  },
-}))
+  }))
+  .mount('#app')
 `
