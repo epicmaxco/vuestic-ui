9141b4a81a082dad343dcb318c3349c4903e0f9d
Maksim Nedoshev <m0ksem1337@gmail.com>
2022-11-04 08:59:59 +0200

    fix/nuxt package for 1.5.0 (#2610)
    * fix(nuxt): dev build
    
    * chore: added new components and composables to nuxt plugin

--- packages/nuxt/nuxt.shim.d.ts
+++ packages/nuxt/nuxt.shim.d.ts
@@ -1,3 +1,8 @@
 declare module '#app' {
   export * from 'nuxt/app'
 }
+
+// Path to vuestic-ui main, so it is not required to build vuestic before working with packages/nuxt
+declare module 'vuestci-ui' {
+  export * from 'vuestic-ui/src/main'
+}
