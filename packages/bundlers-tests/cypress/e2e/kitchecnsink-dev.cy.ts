231b3f1e347c3bd5b84f667fd95320f3a1e147b7
azgiliat <toxa2088@gmail.com>
2022-11-08 19:04:34 +0300

    fix typos in test cases

--- packages/bundlers-tests/cypress/e2e/kitchecnsink-dev.cy.ts
+++ packages/bundlers-tests/cypress/e2e/kitchecnsink-dev.cy.ts
@@ -1,7 +1,6 @@
 describe("Kitchensink page", () => {
   beforeEach(() => {
     cy.visit("/en/getting-started/kitchensink", {
-      timeout: 20000,
       onBeforeLoad(win) {
         cy.stub(win.console, "error").as("errorLogged");
       },
@@ -9,7 +8,7 @@ describe("Kitchensink page", () => {
   })
 
   it("Check h1 text", () => {
-    cy.get("h1").should("contain", "Kitchesink");
+    cy.get("h1").should("contain", "Kitchensink");
   });
 
   it('Should be no errors in console', () => {
