e4a8a38c56da3906707c394135179ba7b02b1b11
Rustem Nasyrov <nasyroff.rus@gmail.com>
2022-11-18 19:18:34 +0400

    Update examples

--- packages/docs/src/page-configs/ui-elements/navbar/examples/Colors.vue
+++ packages/docs/src/page-configs/ui-elements/navbar/examples/Colors.vue
@@ -1,49 +1,59 @@
 <template>
-  <va-navbar color="primary" class="mb-2">
+  <va-navbar color="primary" class="mb-3">
     <template #left>
-      <va-navbar-item>LOGO HERE</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>Primary</va-navbar-item>
+      <va-navbar-item class="logo">LOGO</va-navbar-item>
     </template>
     <template #right>
-      <va-navbar-item>Auto font color</va-navbar-item>
+      <va-navbar-item>Dashboard</va-navbar-item>
+      <va-navbar-item>Reports</va-navbar-item>
+      <va-navbar-item>Users</va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-magnify" color="textInverted" />
+      </va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-account-circle-outline" color="textInverted" />
+      </va-navbar-item>
     </template>
   </va-navbar>
 
-  <va-navbar color="secondary" class="mb-2">
+  <va-navbar color="warning" class="mb-3">
     <template #left>
-      <va-navbar-item>LOGO HERE</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>Secondary</va-navbar-item>
+      <va-navbar-item class="logo">LOGO</va-navbar-item>
     </template>
     <template #right>
-      <va-navbar-item>Auto font color</va-navbar-item>
+      <va-navbar-item>Dashboard</va-navbar-item>
+      <va-navbar-item>Reports</va-navbar-item>
+      <va-navbar-item>Users</va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-magnify" color="textInverted" />
+      </va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-account-circle-outline" color="textInverted" />
+      </va-navbar-item>
     </template>
   </va-navbar>
 
-  <va-navbar color="#ff00ff" class="mb-2">
+  <va-navbar color="#111111" class="mb-3" text-color="#BAFFC5">
     <template #left>
-      <va-navbar-item>LOGO HERE</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>#ff00ff</va-navbar-item>
+      <va-navbar-item class="logo">LOGO</va-navbar-item>
     </template>
     <template #right>
-      <va-navbar-item>Auto font color</va-navbar-item>
-    </template>
-  </va-navbar>
-
-  <va-navbar color="#111111" text-color="#cdcdcd" class="mb-2">
-    <template #left>
-      <va-navbar-item>Dark background</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>#111111</va-navbar-item>
-    </template>
-    <template #right>
-      <va-navbar-item>#cdcdcd text color</va-navbar-item>
+      <va-navbar-item>Dashboard</va-navbar-item>
+      <va-navbar-item>Reports</va-navbar-item>
+      <va-navbar-item>Users</va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-magnify" color="#BAFFC5" />
+      </va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-account-circle-outline" color="#BAFFC5" />
+      </va-navbar-item>
     </template>
   </va-navbar>
 </template>
+
+<style scoped>
+.logo {
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+</style>
--- packages/docs/src/page-configs/ui-elements/navbar/examples/Shape.vue
+++ packages/docs/src/page-configs/ui-elements/navbar/examples/Shape.vue
@@ -1,36 +1,25 @@
 <template>
   <va-navbar color="primary" shape class="mb-2">
     <template #left>
-      <va-navbar-item>LOGO HERE</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>Primary</va-navbar-item>
+      <va-navbar-item class="logo">LOGO</va-navbar-item>
     </template>
     <template #right>
-      <va-navbar-item>Right</va-navbar-item>
-    </template>
-  </va-navbar>
-
-  <va-navbar color="textPrimary" shape class="mb-2">
-    <template #left>
-      <va-navbar-item>LOGO HERE</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>Dark</va-navbar-item>
-    </template>
-    <template #right>
-      <va-navbar-item>Right</va-navbar-item>
-    </template>
-  </va-navbar>
-  <va-navbar color="#ff00ff" shape class="mb-2">
-    <template #left>
-      <va-navbar-item>LOGO HERE</va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item>#ff00ff</va-navbar-item>
-    </template>
-    <template #right>
-      <va-navbar-item>Right</va-navbar-item>
+      <va-navbar-item>Dashboard</va-navbar-item>
+      <va-navbar-item>Reports</va-navbar-item>
+      <va-navbar-item>Users</va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-magnify" />
+      </va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-account-circle-outline" />
+      </va-navbar-item>
     </template>
   </va-navbar>
 </template>
+
+<style scoped>
+.logo {
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+</style>
