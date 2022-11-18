1a525446515bcb8febc4d54ab412d7f97c70cfec
Valar103769 <36926073+Valar103769@users.noreply.github.com>
2022-11-18 11:16:46 +0800

    docs(styles): override inline style use important

--- packages/docs/src/page-configs/styles/css-variables/code-examples/overriding.ts
+++ packages/docs/src/page-configs/styles/css-variables/code-examples/overriding.ts
@@ -2,5 +2,6 @@ export const overriding = `
 :root {
   --va-checkbox-square-border-radius: 50%;
   --va-input-border-radius: 99999px;
+  --va-background-primary: hsl(208deg 100% 97%) !important;
 }
 `
