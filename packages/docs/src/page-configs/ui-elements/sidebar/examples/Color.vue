39551bd1bea6fbbb0b761e240ccd995c19eb67b2
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2022-11-25 13:13:29 +0400

    Feat/sidebar improvements (#2729)
    * feat: allow to set opacity
    
    * feat: add hover opacity property, update colors examples
    
    * feat: add props descriptions
    
    * fix: active and inactive opacity

--- packages/docs/src/page-configs/ui-elements/sidebar/examples/Color.vue
+++ packages/docs/src/page-configs/ui-elements/sidebar/examples/Color.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="mb-4" style="height: 16rem;">
-    <va-sidebar color="primary" minimizedWidth="64px" width="18rem">
+  <div class="mb-4" style="height: 16rem; overflow: hidden;">
+    <va-sidebar
+      color="primary"
+      minimized-width="64px"
+      width="18rem"
+      class="colored-sidebar"
+    >
       <va-sidebar-item active-color="backgroundPrimary">
         <va-sidebar-item-content>
-          <va-sidebar-item-title>
+          <va-sidebar-item-title style="white-space: normal;">
             background primary and white text color
           </va-sidebar-item-title>
         </va-sidebar-item-content>
@@ -24,11 +29,16 @@
     </va-sidebar>
   </div>
 
-  <div class="mb-4" style="height: 16rem;">
-    <va-sidebar color="background-element" minimizedWidth="64px" width="18rem">
+  <div class="mb-4" style="height: 16rem; overflow: hidden;">
+    <va-sidebar
+      color="background-element"
+      minimized-width="64px"
+      width="18rem"
+      class="colored-sidebar"
+    >
       <va-sidebar-item>
         <va-sidebar-item-content>
-          <va-sidebar-item-title>
+          <va-sidebar-item-title style="white-space: normal;">
             background dark and auto text color
           </va-sidebar-item-title>
         </va-sidebar-item-content>
@@ -48,11 +58,17 @@
     </va-sidebar>
   </div>
 
-  <div class="mb-4" style="height: 16rem; border: 1px solid var(--va-secondary);">
-    <va-sidebar color="success" minimizedWidth="64px" width="18rem">
-      <va-sidebar-item active-color="backgroundPrimary">
+  <div class="mb-4" style="height: 16rem; overflow: hidden;">
+    <va-sidebar
+      color="#F0F7ED"
+      text-color="#009600"
+      minimized-width="64px"
+      width="18rem"
+      class="colored-sidebar"
+    >
+      <va-sidebar-item active-color="#F0F7ED">
         <va-sidebar-item-content>
-          <va-sidebar-item-title>
+          <va-sidebar-item-title style="white-space: normal;">
             background success and auto text color
           </va-sidebar-item-title>
         </va-sidebar-item-content>
@@ -61,7 +77,9 @@
         v-for="item in items"
         :key="item.title"
         :active="item.active"
-        active-color="backgroundPrimary"
+        active-color="#009600"
+        :text-color="item.active ? '#F0F7ED' : '#009600'"
+        :hover-opacity="item.active ? 0.75 : 0.2"
       >
         <va-sidebar-item-content>
           <va-icon :name="item.icon" />
@@ -74,10 +92,17 @@
   </div>
 
   <div class="mb-4" style="height: 16rem;">
-    <va-sidebar color="danger" active-color="backgroundPrimary" text-color="success" minimizedWidth="64px" width="18rem">
+    <va-sidebar
+      color="#FFD300"
+      active-color="#FFD300"
+      text-color="textPrimary"
+      minimized-width="64px"
+      width="18rem"
+      class="colored-sidebar"
+    >
       <va-sidebar-item>
         <va-sidebar-item-content>
-          <va-sidebar-item-title>
+          <va-sidebar-item-title style="white-space: normal;">
             background danger and success text color
           </va-sidebar-item-title>
         </va-sidebar-item-content>
@@ -86,6 +111,9 @@
         v-for="item in items"
         :key="item.title"
         :active="item.active"
+        active-color="textPrimary"
+        :text-color="item.active ? '#FFD300' : 'textPrimary'"
+        :hover-opacity="item.active ? 0.75 : 0.2"
       >
         <va-sidebar-item-content>
           <va-icon :name="item.icon" />
@@ -111,3 +139,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.colored-sidebar .va-sidebar__menu {
+  overflow: hidden;
+}
+</style>
