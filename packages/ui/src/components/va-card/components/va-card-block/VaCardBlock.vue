6a25856dda789c077621de45110cc79add18fe1a
Rustem Nasyrov <nasyroff.rus@gmail.com>
2022-11-28 18:21:03 +0400

    feat: add card block with horizontal prop

---
+++ packages/ui/src/components/va-card/components/va-card-block/VaCardBlock.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="va-card-block row" :class="classComputed">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  name: 'VaCardBlock',
+
+  props: {
+    horizontal: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  setup: (props) => ({
+    classComputed: computed(() => ({
+      'va-card-block--horizontal': props.horizontal,
+    })),
+  }),
+})
+</script>
+
+<style lang="scss">
+.va-card-block {
+  flex-direction: column;
+
+  & > div:first-child {
+    border-top-right-radius: var(--va-card-border-radius);
+    border-top-left-radius: var(--va-card-border-radius);
+  }
+
+  & > div:last-child {
+    border-bottom-right-radius: var(--va-card-border-radius);
+    border-bottom-left-radius: var(--va-card-border-radius);
+  }
+
+  &--horizontal {
+    flex-direction: row;
+
+    & > div:first-child {
+      border-radius: var(--va-card-border-radius) 0 0 var(--va-card-border-radius);
+    }
+
+    & > div:last-child {
+      border-radius: 0 var(--va-card-border-radius) var(--va-card-border-radius) 0;
+    }
+  }
+}
+</style>
--- packages/ui/src/components/va-card/index.ts
+++ packages/ui/src/components/va-card/index.ts
@@ -4,9 +4,11 @@ import _VaCard from './VaCard.vue'
 import VaCardContentBase from './components/va-card-content/VaCardContent.vue'
 import VaCardTitleBase from './components/va-card-title/VaCardTitle.vue'
 import VaCardActionsBase from './components/va-card-actions/VaCardActions.vue'
+import VaCardBlockBase from './components/va-card-block/VaCardBlock.vue'
 
 export const VaCardContent = withConfigTransport(VaCardContentBase)
 export const VaCardTitle = withConfigTransport(VaCardTitleBase)
 export const VaCardActions = withConfigTransport(VaCardActionsBase)
+export const VaCardBlock = withConfigTransport(VaCardBlockBase)
 
 export const VaCard = withConfigTransport(_VaCard)
