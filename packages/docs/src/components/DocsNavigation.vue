6bb43690c3fac234e110dff6191ae4e5d2344fed
aluarius <ruseomaster@gmail.com>
2022-12-09 15:28:57 +0300

    [#2564] docs codesandbox icon fix

--- packages/docs/src/components/DocsNavigation.vue
+++ packages/docs/src/components/DocsNavigation.vue
@@ -45,7 +45,14 @@
         color="secondary"
       >
         <va-icon class="docs-navigation__button__icon" size="13px">
-          <svg width="13" height="13" viewBox="0 0 88 88" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 88h88V0H0v88Zm79-9V9H9v70h70Z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="#737373" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-codesandbox" id="IconChangeColor">
+            <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" id="mainIconPathAttribute" fill="transparent" stroke="#737373"></path>
+            <polyline points="7.5 4.21 12 6.81 16.5 4.21"></polyline>
+            <polyline points="7.5 19.79 7.5 14.6 3 12"></polyline>
+            <polyline points="21 12 16.5 14.6 16.5 19.79"></polyline>
+            <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+            <line x1="12" y1="22.08" x2="12" y2="12"></line>
+          </svg>
         </va-icon>
         <span class="docs-navigation__button__text">{{ $t('docsNavigation.openCodeSandbox') }}</span>
       </va-button>
