c0e6d6130d9f9e9d193f6fa3a6787b49ded3c3c2
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2022-12-12 16:47:21 +0400

    chore: bump version to v1.5.3

--- .github/ISSUE_TEMPLATE/bug-template.md
+++ .github/ISSUE_TEMPLATE/bug-template.md
@@ -6,7 +6,7 @@ assignees: ''
 
 ---
 
-**Vuestic-ui version:** 1.5.2
+**Vuestic-ui version:** 1.5.3
 
 **Steps to reproduce**
 
--- packages/vue-cli-plugin/generator/configs/default.js
+++ packages/vue-cli-plugin/generator/configs/default.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   extendPackage: {
     dependencies: {
-      'vuestic-ui': '^1.5.2',
+      'vuestic-ui': '^1.5.3',
     }
   },
   vueUse: [
