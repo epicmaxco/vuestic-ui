5b09300f4b154eae376bcc050b7b6ca4902ff63c
aluarius <ruseomaster@gmail.com>
2022-12-20 11:35:32 +0700

    [#2795] [#2673] va-select max visible options prop, show selected options top, refactoring

---
+++ packages/ui/src/components/va-select/hooks/useMaxVisibleOptions.ts
@@ -0,0 +1,49 @@
+import { computed, ref, toRefs, watch, ExtractPropTypes } from 'vue'
+
+import type { SelectOption } from '../types'
+
+export const useMaxVisibleOptionsProps = {
+  maxVisibleOptions: { type: Number, default: 0 },
+}
+
+export const useMaxVisibleOptions = (
+  props: ExtractPropTypes<typeof useMaxVisibleOptionsProps> & { multiple: boolean, modelValue: SelectOption | SelectOption[] },
+) => {
+  const { modelValue } = toRefs(props)
+  const isAllOptionsShown = ref(false)
+  const belowLimitSelectedOptions = ref<SelectOption[]>([])
+  const hiddenSelectedOptions = ref<SelectOption[]>([])
+
+  const hiddenSelectedOptionsAmount = computed(() => hiddenSelectedOptions.value.length)
+  const allSelectedOptions = computed(() => [...belowLimitSelectedOptions.value, ...hiddenSelectedOptions.value])
+  const visibleSelectedOptions = computed(() => isAllOptionsShown.value ? allSelectedOptions.value : belowLimitSelectedOptions.value)
+
+  watch(modelValue, () => {
+    if (!props.multiple) { return }
+
+    if (!Array.isArray(modelValue.value)) {
+      belowLimitSelectedOptions.value = [modelValue.value]
+      hiddenSelectedOptions.value = []
+    }
+
+    const value = modelValue.value as SelectOption[]
+
+    if (props.maxVisibleOptions) {
+      belowLimitSelectedOptions.value = value.slice(0, props.maxVisibleOptions)
+      hiddenSelectedOptions.value = value.slice(props.maxVisibleOptions)
+    } else {
+      belowLimitSelectedOptions.value = [...value]
+      hiddenSelectedOptions.value = []
+    }
+  })
+
+  const toggleHiddenOptionsState = () => (isAllOptionsShown.value = !isAllOptionsShown.value)
+
+  return {
+    toggleHiddenOptionsState,
+    isAllOptionsShown,
+    visibleSelectedOptions,
+    hiddenSelectedOptionsAmount,
+    allSelectedOptions,
+  }
+}
