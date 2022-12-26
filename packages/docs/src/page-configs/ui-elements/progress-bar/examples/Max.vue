6b2e1f4c93e28bfdd5705f7909fe295f97cb62b2
Rustem Nasyrov <nasyroff.rus@gmail.com>
2022-12-26 17:03:21 +0400

    feat: add max property example

---
+++ packages/docs/src/page-configs/ui-elements/progress-bar/examples/Max.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex lg6 xs12">
+    <va-progress-bar :model-value="value" :max="max" />
+    <va-slider class="mt-3" v-model="value" :max="max" track-label-visible />
+    <va-input v-model="max" label="Max value" class="mt-3" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      max: 50,
+      value: 35,
+    }
+  },
+}
+</script>
