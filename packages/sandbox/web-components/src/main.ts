476cfa5517371998c817c253597329f92de33b17
m0ksem <m0ksem1337@gmail.com>
2023-01-14 11:43:57 +0200

    feat(web-components): kitchensink demo

--- packages/sandbox/web-components/src/main.ts
+++ packages/sandbox/web-components/src/main.ts
@@ -1,9 +1,18 @@
+import { createApp } from 'vue';
+// @ts-ignore
 import { registerVuesticWebComponents, VaButton, VaSelect, VaTab, VaTabs, VaConfig } from 'vuestic-ui/web-components'
 import { v } from 'vuestic-ui/dist/web-components/src/services/vue-plugin/components'
 import 'vuestic-ui/css'
 import { kebabCase } from 'lodash'
+import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
-console.log(v)
+export const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {},
+})
+
 
 registerVuesticWebComponents({
   components: v,
@@ -26,27 +35,27 @@ registerVuesticWebComponents({
   `
 })
 
+// const kitchensink = document.getElementById('kitchensink')
 
-const kitchensink = document.getElementById('kitchensink')
+// const createDemoDiv = (content: Node, title: string) => {
+//   const div = document.createElement('div')
+//   const h1 = document.createElement('h1')
+//   h1.innerText = title
 
-const createDemoDiv = (content: Node, title: string) => {
-  const div = document.createElement('div')
-  const h1 = document.createElement('h1')
-  h1.innerText = title
+//   div.appendChild(h1)
+//   div.appendChild(content)
+//   div.classList.add('kitchensink__demo')
 
-  div.appendChild(h1)
-  div.appendChild(content)
-  div.classList.add('kitchensink__demo')
+//   return div
+// }
 
-  return div
-}
+// Object.entries(v).forEach(([name, vueComponent]) => {
+//   const el = document.createElement(kebabCase(name))
+//   ;(el as any).stateful = true
 
-Object.entries(v).forEach(([name, vueComponent]) => {
-  const el = document.createElement(kebabCase(name))
-  ;(el as any).stateful = true
+//   const demo = createDemoDiv(el as any, name)
 
-  const demo = createDemoDiv(el as any, name)
+//   kitchensink?.appendChild(demo)
+// })
 
-  kitchensink?.appendChild(demo)
-})
-
+createApp(App).use(i18n).mount('#kitchensink')
