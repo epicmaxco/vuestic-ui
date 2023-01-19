c46ecf1a88fc1788d1f413c9439fdb2b3315f298
Rustem Nasyrov <nasyroff.rus@gmail.com>
2023-01-19 12:00:51 +0400

    fix: update example

--- packages/docs/src/page-configs/services/colors-custom-classes/code-examples/setup-example.ts
+++ packages/docs/src/page-configs/services/colors-custom-classes/code-examples/setup-example.ts
@@ -1,19 +1,23 @@
 export const setupExample = `
 // main.js
-import { VuesticPlugin } from 'vuestic-ui'
+import { createVuestic } from 'vuestic-ui'
 import 'vuestic-ui/dist/vuestic-ui.css'
-const app = createApp(App)
-app.use(VuesticPlugin, {
-  colors: { ... },
-  colorsCustomClasses: [
-    {
-      prefix: 'custom',
-      property: ['border', 'color'],
-    },
-    {
-      prefix: 'brand',
-      property: 'background',
-    },
-  ],
-})
+
+createApp(App)
+  .use(createVuestic({
+    config: {
+      colors: { ... },
+      colorsCustomClasses: [
+        {
+          prefix: 'custom',
+          property: ['border', 'color'],
+        },
+        {
+          prefix: 'brand',
+          property: 'background',
+        },
+      ],
+    }
+  }))
+  .mount('#app')
 `
