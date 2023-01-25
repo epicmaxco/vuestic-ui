f34d9275894d98f0179d4b1770f72519d7e82696
m0ksem <maksim.nedoshev@epicmax.co>
2023-01-25 03:19:33 +0200

    feat(nuxt): vuestic.config.ts in nuxt module

--- packages/nuxt/src/composables/use-config-file.ts
+++ packages/nuxt/src/composables/use-config-file.ts
@@ -1,18 +1,26 @@
-import { existsSync } from 'fs'
+import { existsSync, writeFileSync, readFileSync } from 'fs'
 import { resolveAlias, useNuxt } from '@nuxt/kit';
 import { resolve } from 'pathe'
 import { resolveInRuntime } from './../utils/resolve';
+import { watch } from 'chokidar'
 
 export const useConfigFile = async () => {
   const root = resolveAlias('~/')
   const configPath = resolve(root, 'vuestic.config.ts')
+  const nuxtConfigPath = resolve(root, 'nuxt.config.ts')
   const nuxt = useNuxt()
 
-  console.log('Searching for', resolve(root, 'vuestic.config.ts'))
+  const restartNuxt = () => {
+    writeFileSync(nuxtConfigPath, readFileSync(nuxtConfigPath, 'utf-8'))
+  }
+
   if (existsSync(configPath)) {
-    console.log('Found config from ', configPath)
     nuxt.options.alias['#vuestic-config'] = configPath
   } else {
     nuxt.options.alias['#vuestic-config'] = resolveInRuntime('./runtime/config.mjs')
   }
+
+  watch(configPath)
+    .on('add', restartNuxt)
+    .on('unlink', restartNuxt)
 }
