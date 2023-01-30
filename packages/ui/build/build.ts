c5be36b05b380be09ce368f698a8981474159593
m0ksem <m0ksem1337@gmail.com>
2023-01-30 23:14:20 +0200

    chore: uncomment build scripts

--- packages/ui/build/build.ts
+++ packages/ui/build/build.ts
@@ -4,14 +4,14 @@ import { buildStart } from './steps/buildStart'
 import { buildEnd } from './steps/buildEnd'
 
 defineVuesticBuild(async () => {
-  // buildStart()
+  buildStart()
 
   await Promise.all([
-    // $('npm run build:types', { successMessage: 'types built' }),
-    // $('vite build --config ./build/configs/vite.cjs.ts', { successMessage: 'cjs built' }),
-    // $('vite build --config ./build/configs/vite.iife.ts', { successMessage: 'iife built' }),
-    // $('vite build --config ./build/configs/vite.es.ts', { successMessage: 'esm built' }),
-    // $('vite build --config ./build/configs/vite.mjs.ts', { successMessage: 'esm-node built' }),
+    $('npm run build:types', { successMessage: 'types built' }),
+    $('vite build --config ./build/configs/vite.cjs.ts', { successMessage: 'cjs built' }),
+    $('vite build --config ./build/configs/vite.iife.ts', { successMessage: 'iife built' }),
+    $('vite build --config ./build/configs/vite.es.ts', { successMessage: 'esm built' }),
+    $('vite build --config ./build/configs/vite.mjs.ts', { successMessage: 'esm-node built' }),
     $('vite build --config ./build/configs/vite.web-components.ts', { successMessage: 'wc built' }),
   ])
 
