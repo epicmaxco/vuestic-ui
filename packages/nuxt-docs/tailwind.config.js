f50e0beab1315b3f62d7d9cceeefb3f0dc27f36a
Rustem Nasyrov <nasyroff.rus@gmail.com>
2023-02-02 17:45:43 +0400

    fix: add tailwind watcher to the page configs

--- packages/nuxt-docs/tailwind.config.js
+++ packages/nuxt-docs/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
+    "./page-config/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
