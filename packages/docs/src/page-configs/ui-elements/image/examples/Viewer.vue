b65a5aee99caec1cf8b281e2f32ba7d045fc5c6c
Oleg <64714442+aluarius@users.noreply.github.com>
2023-02-10 16:55:44 +0400

    [#2869] VaViewer for VaImage (#2886)

---
+++ packages/docs/src/page-configs/ui-elements/image/examples/Viewer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-start">
+    <va-viewer class="col-span-1">
+      <va-image
+        src="https://picsum.photos/600/800"
+        :max-width="200"
+        fit="contain"
+      >
+        <template #loader>
+          Loading...
+        </template>
+      </va-image>
+    </va-viewer>
+
+    <va-viewer class="col-span-1">
+      <template #anchor="{ openViewer, isOpened }">
+        <button
+          v-if="!isOpened"
+          class="mb-2 p-1 border rounded"
+          @click="openViewer"
+        >
+          Open image
+        </button>
+      </template>
+      <template #image>
+        <va-image
+          src="https://picsum.photos/600/800"
+          :max-width="200"
+          fit="contain"
+        />
+      </template>
+    </va-viewer>
+  </div>
+</template>
