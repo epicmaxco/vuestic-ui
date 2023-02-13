8758989965e4aa91a81063f39c014d540cac0ba1
Aram Sabatés <46224115+asbermudez@users.noreply.github.com>
2023-02-13 14:00:13 +0100

    feature: expose ValidationRule<T> type to consumer (#2946)
    * feat: expose ValidationRule<T> type
    
    * feat: expose ValidationRule from main.ts
    
    ---------
    
    Co-authored-by: bermudez2 <bermudez@catfp.ch>
    Co-authored-by: Maksim Nedoshev <m0ksem1337@gmail.com>

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -1,5 +1,11 @@
 export { useGlobalConfig } from './services/global-config'
-export { useColors, useBreakpoint, useI18nConfig, useIcon as useIcons } from './composables'
+export {
+  useColors,
+  useBreakpoint,
+  useI18nConfig,
+  useIcon as useIcons,
+  type ValidationRule,
+} from './composables'
 
 export * from './services/vue-plugin'
 export * from './services/web-components'
