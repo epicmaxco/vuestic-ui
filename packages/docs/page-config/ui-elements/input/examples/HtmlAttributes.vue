947efa9eea85dd263d10ba751223162d78438ba0
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-02-16 20:40:32 +0300

    docs: example fix

--- packages/docs/page-config/ui-elements/input/examples/HtmlAttributes.vue
+++ packages/docs/page-config/ui-elements/input/examples/HtmlAttributes.vue
@@ -1,41 +1,49 @@
 <template>
-  <div style="max-width: 300px">
+  <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
     <va-input
       v-model="value"
+      label="password"
       type="password"
       class="mb-6"
     />
     <va-input
       v-model="value"
+      label="disabled"
       disabled
       class="mb-6"
     />
     <va-input
       v-model="value"
+      label="readonly"
       readonly
       class="mb-6"
     />
     <va-input
-      placeholder="First name"
+      placeholder="Some text"
+      label="Placeholder"
       class="mb-6"
     />
     <va-input
       v-model="value"
       pattern="\d{4,4}"
+      label="pattern"
       class="mb-6"
     />
     <va-input
       v-model="value"
+      label="inputmode"
       inputmode="numeric"
       class="mb-6"
     />
     <va-input
       v-model="value"
+      label="minlength"
       minlength="4"
       class="mb-6"
     />
     <va-input
       v-model="value"
+      label="maxlength"
       maxlength="10"
     />
   </div>
