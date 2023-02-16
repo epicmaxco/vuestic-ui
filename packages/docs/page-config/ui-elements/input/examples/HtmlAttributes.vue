2539e3156734bd1261b1668b05a745f1e1a53345
Flying Turtle <nasyroff.rus@gmail.com>
2023-02-16 19:00:06 +0400

    feat: add support html attributes example

---
+++ packages/docs/page-config/ui-elements/input/examples/HtmlAttributes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="max-width: 300px">
+    <va-input
+      v-model="value"
+      type="password"
+      class="mb-6"
+    />
+    <va-input
+      v-model="value"
+      disabled
+      class="mb-6"
+    />
+    <va-input
+      v-model="value"
+      readonly
+      class="mb-6"
+    />
+    <va-input
+      placeholder="First name"
+      class="mb-6"
+    />
+    <va-input
+      v-model="value"
+      pattern="\d{4,4}"
+      class="mb-6"
+    />
+    <va-input
+      v-model="value"
+      inputmode="numeric"
+      class="mb-6"
+    />
+    <va-input
+      v-model="value"
+      minlength="4"
+      class="mb-6"
+    />
+    <va-input
+      v-model="value"
+      maxlength="10"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HtmlAttributes',
+  data: () => ({
+    value: '',
+  }),
+}
+</script>
