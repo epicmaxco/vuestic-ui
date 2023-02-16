84fdfce39330e3578ee851fbe467a6a6bac5f9eb
Rustem Nasyrov <nasyroff.rus@gmail.com>
2023-02-16 12:35:26 +0400

    feat: update and refactor examples

--- packages/docs/page-config/ui-elements/alert/examples/Color.vue
+++ packages/docs/page-config/ui-elements/alert/examples/Color.vue
@@ -1,41 +1,26 @@
 <template>
-  <va-alert class="mb-6">
-    {{ $t("alert.misc.primary") }}
-  </va-alert>
-  <va-alert
-    color="success"
-    class="mb-6"
-  >
-    {{ $t("alert.misc.success") }}
-  </va-alert>
-  <va-alert
-    color="info"
-    class="mb-6"
-  >
-    {{ $t("alert.misc.info") }}
-  </va-alert>
   <va-alert
     color="warning"
     class="mb-6"
   >
-    {{ $t("alert.misc.warning") }}
+    {{ $t('alert.misc.warning') }}
   </va-alert>
   <va-alert
     color="danger"
     class="mb-6"
   >
-    {{ $t("alert.misc.danger") }}
+    {{ $t('alert.misc.danger') }}
   </va-alert>
   <va-alert
-    color="#692BEB"
+    color="#ce6e67"
     class="mb-6"
   >
-    {{ $t("alert.misc.warning") }}
+    {{ $t('alert.misc.info') }}
   </va-alert>
   <va-alert
-    color="#540C0C"
-    class="mb-6"
+    color="#fdeae7"
+    text-color="#940909"
   >
-    {{ $t("alert.misc.danger") }}
+    {{ $t('alert.misc.customized') }}
   </va-alert>
 </template>
