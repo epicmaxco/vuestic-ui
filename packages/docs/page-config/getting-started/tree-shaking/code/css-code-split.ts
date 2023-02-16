d31e9d2a55378b106861b25952b6964109322089
aluarius <ruseomaster@gmail.com>
2023-02-16 20:52:03 +0600

    refactor: deleted reset from tree shaking example

--- packages/docs/page-config/getting-started/tree-shaking/code/css-code-split.ts
+++ packages/docs/page-config/getting-started/tree-shaking/code/css-code-split.ts
@@ -3,4 +3,3 @@ import "vuestic-ui/styles/essential.css";
 // Optional
 import "vuestic-ui/styles/typography.css";
 import "vuestic-ui/styles/smart-helpers.css";
-import "vuestic-ui/styles/reset.css";
