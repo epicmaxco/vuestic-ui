0748626f17f46a65736be678db237de6d88d2ec3
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-02-17 23:30:35 +0200

    fix(docs): file being minified on build

--- packages/docs/modules/page-config/blocks/file/transform.ts
+++ packages/docs/modules/page-config/blocks/file/transform.ts
@@ -1,13 +1,16 @@
 import { defineBlockTransform } from "../../compiler/define-block-transform";
 import { extname } from 'path'
+import { readFile } from 'fs/promises'
 
 export default defineBlockTransform(async function (block) {
   if (block.type !== 'file') { return }
 
   const importName = block.args[0].slice(1, -1)
   const importPath = (await this.importer.resolveAbsolutePath(`${importName}`))!
-  const importComponent = this.importer.importDefault('file', `${importPath}?raw`)
+  const content = (await readFile(importPath, 'utf-8')).toString()
+  const contentEscaped = JSON.stringify(content)
+  // const importComponent = this.importer.importDefault('file', `${importPath}?raw`)
   const importExt = block.args[1] || `'${extname(importPath).slice(1)}'`
 
-  return block.replaceArgCode(0, `${importComponent}, ${importExt}`)
+  return block.replaceArgCode(0, `${contentEscaped}, ${importExt}`)
 })
