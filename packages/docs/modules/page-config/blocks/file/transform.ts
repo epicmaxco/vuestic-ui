ffd09bf580845919b1fbb921058d13438ed627e4
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-02-20 19:36:09 +0200

    fix(docs): throw correct error if file not found

--- packages/docs/modules/page-config/blocks/file/transform.ts
+++ packages/docs/modules/page-config/blocks/file/transform.ts
@@ -6,7 +6,12 @@ export default defineBlockTransform(async function (block) {
   if (block.type !== 'file') { return }
 
   const importName = block.args[0].slice(1, -1)
-  const importPath = (await this.importer.resolveAbsolutePath(`${importName}`))!
+  const importPath = (await this.importer.resolveAbsolutePath(`${importName}`))
+
+  if (!importPath) {
+    throw new Error(`Cannot find file ${importName}`)
+  }
+
   const content = (await readFile(importPath, 'utf-8')).toString()
   const contentEscaped = JSON.stringify(content)
   const importExt = block.args[1] || `'${extname(importPath).slice(1)}'`
