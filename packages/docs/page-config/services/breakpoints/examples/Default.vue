bd5b5aa6eb022139a0d095ca85eebe27ccacc9ae
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-03-01 13:20:05 +0200

    fix(docs): incorrect import to vuestic/src/main

--- packages/docs/page-config/services/breakpoints/examples/Default.vue
+++ packages/docs/page-config/services/breakpoints/examples/Default.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { useBreakpoint } from "vuestic-ui/src/composables";
+import { useBreakpoint } from "vuestic-ui";
 
 const breakpoint = useBreakpoint();
 </script>
