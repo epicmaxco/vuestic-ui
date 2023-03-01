e22fd67e8bb5dab2c541e52c430af9ee7919b60c
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-03-01 13:00:18 +0200

    docs(data-table): improve docs demo

--- packages/docs/page-config/ui-elements/data-table/examples/ExpandableRow.vue
+++ packages/docs/page-config/ui-elements/data-table/examples/ExpandableRow.vue
@@ -2,19 +2,53 @@
   <va-data-table
     :items="items"
     :columns="columns"
-    striped
   >
     <template #cell(actions)="{ row, isExpanded }">
-      <va-button @click="row.toggleRowDetails()">{{  isExpanded ? 'Collapse': 'Expand' }}</va-button>
+      <va-button 
+        @click="row.toggleRowDetails()"
+        :icon="row.isExpanded ? 'va-arrow-up': 'va-arrow-down'"
+        preset="secondary"
+        class="w-full"
+      >
+        {{ isExpanded ? 'Hide': 'More info' }}
+      </va-button>
     </template>
 
-    <template #expandableRow="{ cells }">
-      <va-alert outline>
-        This is {{ cells[0].value }} details.
-      </va-alert>
+    <template #expandableRow="{ rowData }">
+      <div class="flex gap-2">
+        <va-avatar :src="`https://randomuser.me/api/portraits/men/${rowData.id}.jpg`"/>
+        <div class="pl-2">
+          <div class="flex gap-1">
+            <span>{{ rowData.name }}</span>
+            <span class="va-link">@{{ rowData.username }}</span>
+          </div>
+          <div class="flex items-center">
+            <va-icon size="small" name="phone" color="secondary" class="mr-2" />
+            <span>{{ rowData.phone }}</span>
+          </div>
+          <div class="flex items-center">
+            <va-icon size="small" name="email" color="secondary" class="mr-2" />
+            <span>{{ rowData.email }}</span>
+          </div>
+          <div class="flex items-center">
+            <va-icon size="small" name="language" color="secondary" class="mr-2" />
+            <span class="va-link">{{ rowData.website }}</span>
+          </div>
+        </div>
+      </div>
     </template>
   </va-data-table>
 </template>
+
+<style>
+  .va-data-table__table-tr--expanded td {
+    background-color: var(--va-background-element);
+  }
+
+  .va-data-table__table-expanded-content td {
+    background: var(--va-background-border);
+  }
+</style>
 
 <script>
 import { defineComponent } from "vue";
@@ -23,29 +57,44 @@ export default defineComponent({
   data() {
     const users = [
       {
+        id: 1,
         name: "Leanne Graham",
         username: "Bret",
         email: "Sincere@april.biz",
+        phone: "1-770-736-8031",
+        website: "hildegard.org",
       },
       {
+        id: 2,
         name: "Ervin Howell",
         username: "Antonette",
         email: "Shanna@melissa.tv",
+        phone: "010-692-6593",
+        website: "anastasia.net",
       },
       {
+        id: 3,
         name: "Clementine Bauch",
         username: "Samantha",
         email: "Nathan@yesenia.net",
+        phone: "1-463-123-4447",
+        website: "ramiro.info",
       },
       {
+        id: 4,
         name: "Patricia Lebsack",
         username: "Karianne",
         email: "Julianne.OConner@kory.org",
+        phone: "493-170-9623",
+        website: "kale.biz",
       },
       {
+        id: 5,
         name: "Chelsey Dietrich",
         username: "Kamren",
         email: "Lucio_Hettinger@annie.ca",
+        phone: "(254)954-1289",
+        website: "demarco.info",
       },
     ];
 
