eeab7be4dfa2b6a30d385b1e270dd641103dbfed
aluarius <ruseomaster@gmail.com>
2023-03-01 16:47:27 +0600

    feat: va button toggle tests

--- packages/ui/src/components/va-button-toggle/tests/VaButtonToggle.spec.ts
+++ packages/ui/src/components/va-button-toggle/tests/VaButtonToggle.spec.ts
@@ -1,15 +1,47 @@
 import { describe, it, expect } from 'vitest'
-import { mountWithGlobalConfig } from '../../../utils/unit-test-utils'
+
+import { mountWithGlobalConfig, shallowMountWithGlobalConfig } from '../../../utils/unit-test-utils'
 
 import VaButtonToggle from '../VaButtonToggle.vue'
 
+import type { VueWrapper } from '@vue/test-utils'
+
 describe('VaButtonToggle', () => {
+  const options = [{ name: 'one', value: 1 }, { name: false, value: 0 }]
+
   it('should render without an error', () => {
     const wrapper = mountWithGlobalConfig(VaButtonToggle, {
-      propsData: {
-        options: [],
+      attrs: {
+        options,
       },
     })
     expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('should interpret `useSelectableProps`', () => {
+    const entries = [
+      {
+        value: options[0],
+        expected: [true, 'one'],
+      },
+      {
+        value: options[1],
+        expected: [false, 'false'],
+      },
+    ]
+
+    entries.forEach(({ value, expected }) => {
+      const wrapper: VueWrapper<any> = shallowMountWithGlobalConfig(VaButtonToggle, {
+        attrs: {
+          options,
+          textBy: 'name',
+          valueBy: 'value',
+          modelValue: options[0].value,
+        },
+      })
+
+      expect(wrapper.vm.isToggled(value)).toBe(expected[0])
+      expect(wrapper.vm.getText(value)).toBe(expected[1])
+    })
   })
 })
