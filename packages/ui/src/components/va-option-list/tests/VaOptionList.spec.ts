28d2ede496285986c9a2f87e8b9aeaa34efbeabf
aluarius <ruseomaster@gmail.com>
2023-03-01 15:18:48 +0600

    feat: va options list tests

--- packages/ui/src/components/va-option-list/tests/VaOptionList.spec.ts
+++ packages/ui/src/components/va-option-list/tests/VaOptionList.spec.ts
@@ -1,41 +1,58 @@
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect } from 'vitest'
+
 import { mountWithGlobalConfig, shallowMountWithGlobalConfig } from '../../../utils/unit-test-utils'
 
 import VaOptionList from '../VaOptionList.vue'
 
+import type { VueWrapper } from '@vue/test-utils'
+
 describe('VaOptionList', () => {
-  it('should render without an error', () => {
+  it('should be rendered without errors', () => {
     const wrapper = mountWithGlobalConfig(VaOptionList)
     expect(wrapper.exists()).toBeTruthy()
   })
 
-  // describe('selectedValue should use default value', () => {
-  //   let wrapper
+  describe('`selectedValue` should use `defaultValue` and interpret `useSelectableProps`', () => {
+    const entries = [
+      {
+        defaultValue: { id: 1, name: 'one', value: 2, disabled: true },
+        expected: [true, 'one', 2, 2],
+      },
+      {
+        defaultValue: 'one',
+        expected: [false, 'one', 'one', 'one'],
+      },
+      {
+        defaultValue: 0,
+        expected: [false, '0', 0, 0],
+      },
+      {
+        defaultValue: false,
+        expected: [false, 'false', false, false],
+      },
+    ]
 
-  //   beforeEach(() => {
-  //     wrapper = shallowMountWithGlobalConfig(VaOptionList, {
-  //       attrs: {
-  //         options: [{ name: 'one' }, { name: 'two' }],
-  //         defaultValue: 'one',
-  //       },
-  //     })
-  //   })
+    entries.forEach(({ defaultValue, expected }) => {
+      const wrapper: VueWrapper<any> = shallowMountWithGlobalConfig(VaOptionList, {
+        attrs: {
+          stateful: true,
+          disabledBy: 'disabled',
+          textBy: 'name',
+          valueBy: 'value',
+          defaultValue,
+        },
+      })
 
-  //   it('selected value updates with default value', () => {
-  //     wrapper.setProps({ defaultValue: 'two' })
-  //     expect(wrapper.vm.selectedValue).toBe('two')
-  //   })
+      it('should return `defaultValue`', async () => {
+        expect(wrapper.vm.selectedValue).toEqual(defaultValue)
+      })
 
-  //   it('should set selectedValue with default value', () => {
-  //     expect(wrapper.vm.selectedValue).toBe('one')
-  //   })
-  // })
-
-  // describe('selectedValue should use value if provided', () => {
-  //   it('should update selectedValue', () => {
-  //     const wrapper = shallowMountWithGlobalConfig(VaOptionList)
-  //     wrapper.setProps({ value: 'three' })
-  //     expect(wrapper.vm.selectedValue).toBe('three')
-  //   })
-  // })
+      it('should correctly interpret `useSelectableProps`', async () => {
+        expect(wrapper.vm.isDisabled(wrapper.vm.selectedValue)).toBe(expected[0])
+        expect(wrapper.vm.getText(wrapper.vm.selectedValue)).toBe(expected[1])
+        expect(wrapper.vm.getValue(wrapper.vm.selectedValue)).toBe(expected[2])
+        expect(wrapper.vm.getTrackBy(wrapper.vm.selectedValue)).toBe(expected[3])
+      })
+    })
+  })
 })
