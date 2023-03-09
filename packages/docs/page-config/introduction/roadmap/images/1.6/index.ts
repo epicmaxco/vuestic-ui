c2f148099b62123a419df7d4e4932e4ed08468b0
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-03-09 14:29:35 +0200

    chore(docs): roadmap for 1.6 and 1.7 (#3114)

---
+++ packages/docs/page-config/introduction/roadmap/images/1.6/index.ts
@@ -0,0 +1,5 @@
+export { default as createVuestic } from './createVuestic.vue';
+export { default as autoComplete } from './autoComplete.vue'
+export { default as breakpoints } from './breakpoints.vue'
+export { default as stepper } from './stepper.vue'
+export { default as tailwind } from './tailwind.vue'
