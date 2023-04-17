c3078b515e36ed8a0932e8a7caf893c480219f39
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2023-04-17 12:19:43 +0300

    fix(#3304): update index

--- packages/docs/pages/index.vue
+++ packages/docs/pages/index.vue
@@ -1,45 +1,56 @@
 <template>
   <div class="landing">
-    <div class="landing__preview-wrapper">
-      <LandingHeader />
+    <LandingHeader v-model="isMobileMenuOpen" />
+    <div v-show="!isMobileMenuOpen">
       <LandingPreview />
       <LandingPartners />
+      <LandingFeatures />
+      <LandingCustomize />
+      <LandingSeamlessIntegration />
+      <LandingOpenSource />
+      <LandingAdmin />
+      <LandingFooter />
     </div>
-    <LandingCustomize />
-    <LandingSeamlessIntegration />
-    <LandingOpenSource />
-    <LandingAdmin />
-    <LandingFooter />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, onBeforeUnmount } from 'vue'
+import { useColors } from "vuestic-ui"
+
+const colorMode = useColorMode()
+const cookie = useCookie('vuestic-theme')
+
+const { applyPreset } = useColors()
+applyPreset('landing')
+
 definePageMeta({
   layout: 'landing',
 })
 
 useHead({
   title: 'Vuestic UI — Vue 3 UI framework',
-});
+  link: [
+    { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" },
+  ]
+})
+
+const isMobileMenuOpen = ref(false)
+
+onBeforeUnmount(() => {
+  applyPreset(cookie.value || colorMode.preference)
+})
 </script>
 
 <style lang="scss">
 @import "vuestic-ui/src/styles/typography/typography.scss";
-@import "vuestic-ui/src/styles/smart-helpers/smart-helpers.scss";
+@import "@/assets/variables.scss";
 
 .landing {
   font-family: Source Sans Pro;
 
   a {
     @include link(2px);
-  }
-
-  &__preview-wrapper {
-    background-image: url("/landing/pattern.svg");
-    background-size: unset;
-    background-repeat: no-repeat;
-    background-position: top;
-    background-color: var(--va-background-secondary);
   }
 }
 </style>
