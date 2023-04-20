86ab471c3228c716116931fda0892627657b596a
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-04-20 17:45:18 +0300

    fix: use provide/inject from current app

--- packages/ui/src/components/va-modal/hooks/useModal.ts
+++ packages/ui/src/components/va-modal/hooks/useModal.ts
@@ -1,14 +1,10 @@
-import { getCurrentInstance } from 'vue'
 import { createModalInstance } from '../modal'
 import { ModalOptions } from '../types'
+import { getCurrentApp } from '../../../services/current-app'
 
 /** This hook can be used without plugin used */
 export const useModal = () => {
-  const appContext = getCurrentInstance()?.appContext
-
-  if (!appContext) {
-    throw new Error('useModal can be used only in setup function. You can use app.config.globalProperties.$vaModal outside setup function')
-  }
+  const appContext = getCurrentApp()._context
 
   /**
    * @param options can be message string or options object
--- packages/ui/src/services/global-config/global-config.ts
+++ packages/ui/src/services/global-config/global-config.ts
@@ -1,19 +1,18 @@
 import cloneDeep from 'lodash/cloneDeep.js'
-import { ref, getCurrentInstance } from 'vue'
+import { Ref, ref } from 'vue'
 import { GlobalConfig, GlobalConfigUpdater, PartialGlobalConfig, ProvidedGlobalConfig } from './types'
 import { getComponentsDefaultConfig } from '../component-config'
 import { getIconDefaultConfig } from '../icon'
 import { getColorDefaultConfig } from '../color'
 import { getI18nConfigDefaults } from '../i18n'
 import { getBreakpointDefaultConfig } from '../breakpoint'
-import { getGlobalProperty } from '../vue-plugin/utils'
-import { getCurrentApp, inject } from '../current-app'
+import { inject, provide } from '../current-app'
 import { mergeDeep } from '../../utils/merge-deep'
 import { getColorsClassesDefaultConfig } from '../colors-classes'
 
 export const GLOBAL_CONFIG = Symbol('GLOBAL_CONFIG')
 
-export const createGlobalConfig = () => {
+export const createGlobalConfig = (): ProvidedGlobalConfig => {
   const globalConfig = ref<GlobalConfig>({
     colors: getColorDefaultConfig(),
     icons: getIconDefaultConfig(),
@@ -27,7 +26,7 @@ export const createGlobalConfig = () => {
      * TODO: if this try won't be success, may be remake to provide/inject
      */
     routerComponent: undefined,
-  })
+  }) as Ref<GlobalConfig>
 
   const getGlobalConfig = (): GlobalConfig => globalConfig.value
   const setGlobalConfig = (updater: GlobalConfig | GlobalConfigUpdater<GlobalConfig>) => {
@@ -48,16 +47,6 @@ export const createGlobalConfig = () => {
   }
 }
 
-const provideForCurrentApp = <T>(provide: T) => {
-  const provides = getCurrentInstance()?.appContext.provides || getCurrentApp()?._context.provides
-
-  if (!provides) { throw new Error('Vue app not found for provide') }
-
-  provides[GLOBAL_CONFIG] = provide
-
-  return provide
-}
-
 /** Use this function if you don't want to throw error if hook used outside setup function by useGlobalConfig */
 export function useGlobalConfig () {
   let injected = inject<ProvidedGlobalConfig>(GLOBAL_CONFIG) as ProvidedGlobalConfig
@@ -65,7 +54,7 @@ export function useGlobalConfig () {
   if (!injected) {
     injected = createGlobalConfig()
 
-    provideForCurrentApp(injected)
+    provide(GLOBAL_CONFIG, injected)
   }
 
   return injected
