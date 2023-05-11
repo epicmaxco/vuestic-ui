7c6a573129e02344f35d8a4c1ac9cccfdb91c188
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2023-05-11 14:07:10 +0300

    #3209: update VaButtonGroup examples

--- packages/docs/page-config/ui-elements/button-group/examples/Styles.vue
+++ packages/docs/page-config/ui-elements/button-group/examples/Styles.vue
@@ -1,62 +1,52 @@
 <template>
-  <va-button-group class="mb-6">
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+  <div class="flex flex-wrap gap-6 mb-6">
+    <va-button-group>
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
 
-  <va-button-group
-    preset="primary"
-    class="mb-6"
-  >
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+    <va-button-group preset="primary">
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
 
-  <va-button-group
-    preset="secondary"
-    class="mb-6"
-  >
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+    <va-button-group preset="secondary">
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
+  </div>
 
-  <va-button-group
-    preset="plain"
-    class="mb-6"
-  >
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+  <div class="flex flex-wrap gap-6 mb-6">
+    <va-button-group preset="plain">
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
 
-  <va-button-group
-    preset="plainOpacity"
-    class="mb-6"
-  >
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+    <va-button-group preset="plainOpacity">
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
+  </div>
 
-  <va-button-group
-    preset="secondary"
-    border-color="primary"
-    class="mb-6"
-  >
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+  <div class="flex flex-wrap gap-6 mb-6">
+    <va-button-group
+      preset="secondary"
+      border-color="primary"
+    >
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
 
-  <va-button-group
-    round
-    class="mb-6"
-  >
-    <va-button>One</va-button>
-    <va-button>Two</va-button>
-    <va-button>Three</va-button>
-  </va-button-group>
+    <va-button-group round>
+      <va-button>One</va-button>
+      <va-button>Two</va-button>
+      <va-button>Three</va-button>
+    </va-button-group>
+  </div>
 </template>
