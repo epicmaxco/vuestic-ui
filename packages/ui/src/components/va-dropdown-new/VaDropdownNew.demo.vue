eb691e1e4f2088991c93b6c6f243592f6bcc0a30
ilearnjs <20753143+ilearnjs@users.noreply.github.com>
2023-06-02 12:54:22 +0200

    Update new dropdown

--- packages/ui/src/components/va-dropdown-new/VaDropdownNew.demo.vue
+++ packages/ui/src/components/va-dropdown-new/VaDropdownNew.demo.vue
@@ -1,31 +1,113 @@
 <template>
   <VbDemo>
-    <VbCard title="Default">
+    <VbCard title="Click">
       <va-dropdown-new>
         <template #anchor>
-          Hi
+          Click me
         </template>
-        Dropdown
+        Clicked
       </va-dropdown-new>
     </VbCard>
-    <br />
-    <VbCard title="Big">
+    <VbCard title="Hover">
+      <va-dropdown-new
+        trigger="hover"
+      >
+        <template #anchor>
+          Hover me
+        </template>
+        Hovered
+      </va-dropdown-new>
+    </VbCard>
+    <VbCard title="Top">
+      <va-dropdown-new
+        placement="top"
+      >
+        <template #anchor>
+          Click me
+        </template>
+        Top
+      </va-dropdown-new>
+    </VbCard>
+    <VbCard title="Top-start">
+      <va-dropdown-new
+        placement="top-start"
+      >
+        <template #anchor>
+          Click me
+        </template>
+        TS
+      </va-dropdown-new>
+    </VbCard>
+    <VbCard title="Offset 20 40">
+      <va-dropdown-new
+        :offset="[20, 40]"
+      >
+        <template #anchor>
+          Click me
+        </template>
+        Offset
+      </va-dropdown-new>
+    </VbCard>
+    <VbCard title="Same width">
+      <va-dropdown-new
+        keepAnchorWidth
+      >
+        <template #anchor>
+          Click me
+        </template>
+        Looooong Dropdown
+      </va-dropdown-new>
+    </VbCard>
+    <VbCard title="Flip + target">
+      Scroll up
       <div class="overflow-hidden">
         <div
           class="scroll-container"
-          ref="targetBig"
+          ref="targetFlip"
+          v-scroll-to-middle-y
         >
           <div class="content-container">
             <div class="vertical-space"></div>
             <va-dropdown-new
-              :target="targetBig"
+              :model-value="true"
+              :target="targetFlip"
+              class="grid place-items-center"
             >
               <template #anchor>
-                <div class="scrollable"></div>
+                <div class="grid place-items-center h-24 w-24 border-2 border-gray-1000 border-dashed">
+                </div>
               </template>
               Dropdown
             </va-dropdown-new>
             <div class="vertical-space"></div>
+          </div>
+        </div>
+      </div>
+    </VbCard>
+    <VbCard title="Autoplacement main and cross axis + target">
+      Scroll around
+      <div class="overflow-hidden">
+        <div
+          class="scroll-container"
+          ref="targetFlipShift"
+          v-scroll-to-middle-y
+          v-scroll-to-middle-x
+        >
+          <div class="content-container">
+            <div class="vertical-space horizontal-space"></div>
+            <va-dropdown-new
+              :model-value="true"
+              :target="targetFlipShift"
+            >
+              <template #anchor>
+                <div class="grid place-items-center h-24 w-24 border-2 border-gray-1000 border-dashed">
+                </div>
+              </template>
+              <div>
+                Looooonger Dropdown
+              </div>
+            </va-dropdown-new>
+            <div class="vertical-space horizontal-space"></div>
           </div>
         </div>
       </div>
@@ -43,8 +125,23 @@ export default {
   },
   setup () {
     return {
-      targetBig: ref(null),
+      targetFlip: ref(null),
+      targetFlipShift: ref(null),
     }
+  },
+  directives: {
+    scrollToMiddleY: {
+      mounted: (el) => {
+        const { scrollHeight, clientHeight } = el
+        el.scrollTop = (scrollHeight - clientHeight) / 2
+      },
+    },
+    scrollToMiddleX: {
+      mounted: (el) => {
+        const { scrollWidth, clientWidth } = el
+        el.scrollLeft = (scrollWidth - clientWidth) / 2
+      },
+    },
   },
 }
 </script>
@@ -63,19 +160,15 @@ export default {
 }
 
 .content-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  display: grid;
+  place-items: center;
 }
 
 .vertical-space {
   height: 400px;
 }
 
-.scrollable {
-  width: 80px;
-  height: 80px;
-  border: 1px dashed black;
+.horizontal-space {
+  width: 1000px;
 }
 </style>
--- packages/ui/src/components/va-dropdown-new/VaDropdownNew.vue
+++ packages/ui/src/components/va-dropdown-new/VaDropdownNew.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     ref="anchor"
+    :class="$attrs.class"
   >
     <slot
       name="anchor"
@@ -8,7 +9,7 @@
     </slot>
   </div>
   <div
-    v-if="isMounted"
+    v-if="isMounted && valueComputed"
     ref="floating"
     :style="floatingStyles"
   >
@@ -18,39 +19,176 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, PropType, computed, watch, nextTick } from 'vue'
-import { useFloating, autoUpdate, flip } from '@floating-ui/vue'
-import { MaybeHTMLElementOrSelector, useHTMLElementSelector, useIsMounted } from '../../composables'
+import { defineComponent, PropType, computed, nextTick } from 'vue'
+import { useFloating, autoUpdate, flip, shift, Placement, offset, size } from '@floating-ui/vue'
+import kebabCase from 'lodash/kebabCase'
+import {
+  createStatefulProps,
+  MaybeHTMLElementOrSelector, useClickOutside, useHTMLElement,
+  useHTMLElementSelector,
+  useIsMounted, useStateful,
+} from '../../composables'
+import { useMouseNavigation } from '../va-dropdown/hooks/useDropdownNavigation'
+import { DropdownOffsetProp } from '../va-dropdown/types'
+import { useCursorAnchor } from '../va-dropdown/hooks/useCursorAnchor'
+
+// TODO
+// cursor
+// teleport
 
 export default defineComponent({
   name: 'VaDropdownNew',
   props: {
+    ...createStatefulProps(Boolean, true),
     trigger: {
       type: String as PropType<'click' | 'right-click' | 'hover' | 'dblclick' | 'none'>,
       default: 'click',
       validator: (value: string) => ['click', 'right-click', 'hover', 'dblclick', 'none'].includes(value),
     },
+    closeOnClickOutside: { type: Boolean, default: true },
+    placement: { type: String as PropType<Placement>, default: 'bottom' },
+    offset: { type: [Array, Number] as PropType<DropdownOffsetProp>, default: 0 },
+    keepAnchorWidth: { type: Boolean, default: false },
     target: { type: [String, Object] as PropType<MaybeHTMLElementOrSelector>, default: undefined },
+    cursor: { type: Boolean, default: false },
   },
-  setup (props) {
-    const anchor = ref(null)
-    const floating = ref(null)
+  setup (props, { emit }) {
+    const { valueComputed: statefulVal } = useStateful(props, emit)
+    const valueComputed = computed({
+      get: () => statefulVal.value, // && !props.disabled && !props.readonly,
+      set (val) {
+        statefulVal.value = val
+        if (val) {
+          emit('open')
+        } else {
+          emit('close')
+        }
+      },
+    })
 
+    const anchor = useHTMLElement('anchor')
+    // const cursorAnchor = useCursorAnchor(anchor, valueComputed)
+    const floating = useHTMLElement('floating')
     const targetElement = useHTMLElementSelector(computed(() => props.target || 'body'))
-    console.log(targetElement.value)
 
-    const { floatingStyles, update } = useFloating(anchor, floating, {
-      whileElementsMounted: autoUpdate,
-      middleware: computed(() => [
+    const onClick = (e: MouseEvent) => {
+      if ((props.trigger !== 'click' && kebabCase(props.trigger) !== 'right-click')) { return } // || props.disabled) { return }
+
+      if (valueComputed.value) {
+        // emitAndClose('anchor-click', props.closeOnAnchorClick, e)
+      } else {
+        if (props.trigger !== 'click') { return }
+        valueComputed.value = true
+        emit('anchor-click', e)
+      }
+    }
+    const onContextmenu = (e: MouseEvent) => {
+      if (kebabCase(props.trigger) !== 'right-click') { return } // || props.disabled) { return }
+      e.preventDefault()
+
+      if (valueComputed.value) {
+        // emitAndClose('anchor-right-click', props.closeOnAnchorClick, e)
+
+        if (props.cursor) {
+          nextTick(() => { valueComputed.value = true })
+        }
+      } else {
+        valueComputed.value = true
+        emit('anchor-right-click', e)
+      }
+    }
+    const onDblclick = () => { return undefined }
+    const onMouseenter = () => {
+      if (props.trigger !== 'hover') { return } // || props.disabled) { return }
+      valueComputed.value = true
+
+      // debounceHover(() => { valueComputed.value = true })
+      // cancelUnHoverDebounce()
+    }
+    const onMouseleave = () => {
+      if (props.trigger !== 'hover') { return } // || props.disabled) { return }
+      valueComputed.value = false
+
+      // if (props.isContentHoverable) {
+      //   debounceUnHover(() => { valueComputed.value = false })
+      // } else {
+      //   valueComputed.value = false
+      // }
+      // cancelHoverDebounce()
+    }
+
+    useMouseNavigation(anchor, {
+      click: onClick,
+      contextmenu: onContextmenu,
+      dblclick: onDblclick,
+      mouseenter: onMouseenter,
+      mouseleave: onMouseleave,
+    })
+
+    const emitAndClose = (eventName: Parameters<typeof emit>[0], close?: boolean, e?: Event) => {
+      emit(eventName, e)
+      if (close && props.trigger !== 'none') { valueComputed.value = false }
+    }
+
+    useClickOutside([anchor, floating], () => {
+      if (props.closeOnClickOutside && valueComputed.value) {
+        emitAndClose('click-outside', props.closeOnClickOutside)
+      }
+    })
+
+    const placementComputed = computed(() => {
+      return props.placement
+    })
+
+    const offsetComputed = computed(() => {
+      const result = { mainAxis: 0, crossAxis: 0 }
+      if (Array.isArray(props.offset)) {
+        result.mainAxis = props.offset[0]
+        result.crossAxis = props.offset[1]
+      } else {
+        result.mainAxis = props.offset
+      }
+
+      return result
+    })
+
+    const middlewareComputed = computed(() => {
+      const result = [
+        offset(offsetComputed.value),
+        // flip element, works for -start, -end
         flip({
           boundary: targetElement.value,
         }),
-      ]),
+        // shift the element into the view
+        shift(),
+      ]
+
+      if (props.keepAnchorWidth) {
+        result.push(size({
+          apply ({ elements }) {
+            const reference = elements.reference
+            const availableWidth = reference.getBoundingClientRect().width
+            Object.assign(elements.floating.style, {
+              maxWidth: `${availableWidth}px`,
+            })
+          },
+        }),
+        )
+      }
+
+      return result
+    })
+
+    const { floatingStyles } = useFloating(anchor, floating, {
+      placement: placementComputed,
+      whileElementsMounted: autoUpdate,
+      middleware: middlewareComputed,
     })
 
     const isMounted = useIsMounted()
 
     return {
+      valueComputed,
       anchor,
       floating,
       floatingStyles,
