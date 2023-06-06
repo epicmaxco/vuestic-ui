9fd4825268ae56196fd75ae343ceaa05bac5dddc
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-06-06 23:00:27 +0300

    fix(docs): fix api table event names render

--- packages/docs/modules/page-config/blocks/api/index.ts
+++ packages/docs/modules/page-config/blocks/api/index.ts
@@ -3,12 +3,12 @@ import { definePageConfigBlock } from '../../types'
 import Component from './index.vue'
 import { VisualOptions, type ManualApiOptions } from './types';
 import type { ComponentMeta } from 'vue-component-meta'
+import { parseComponent } from './component-parser'
 
 const setup = (
   componentName: string, 
   component: DefineComponent,
   cssVariables: string,
-  meta: ComponentMeta,
   manual?: ManualApiOptions,
   visualOptions?: VisualOptions,
 ) => {
@@ -17,7 +17,7 @@ const setup = (
     componentName,
     component,
     cssVariables,
-    meta,
+    meta: parseComponent(componentName),
     manual,
     visualOptions,
   }
