59c176e474ff1a521a540f65fd7cce6477ab3de1
Evobaso-J <ndrbsl@gmail.com>
2023-06-09 08:36:31 +0200

    feat(tests): add apply preset function test

--- packages/ui/src/composables/tests/useColor.spec.ts
+++ packages/ui/src/composables/tests/useColor.spec.ts
@@ -1,13 +1,44 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
+import { useColors } from '../useColors'
+import { useGlobalConfig } from '../useGlobalConfig'
+import { createTestComposable } from './createTestComposable'
+import { warn } from '../../utils/console'
 
 describe('applyPreset function', () => {
-  it.todo(
+  it(
+    'Given a non-existing preset name, the current preset name is changed and the warn function is called once',
+    () => {
+      const {
+        composableWrapper: { applyPreset, globalConfig },
+      } = createTestComposable([useColors, useGlobalConfig])
+
+      vi.mock('../../utils/console', () => ({
+        warn: vi.fn(),
+      }))
+
+      // Since this test's goal is to trigger a warning
+      // by passing the wrong value to a function, we mock
+      // useReactiveComputed to prevent it from throwing an unwanted error
+      vi.mock('../useReactiveComputed', () => ({
+        useReactiveComputed: vi.fn(),
+      }))
+      applyPreset('foo')
+      expect(globalConfig.value.colors.currentPresetName).toBe('foo')
+      expect(warn).toHaveBeenCalledTimes(1)
+      vi.doUnmock('../useReactiveComputed')
+    },
+  )
+  it(
     'Given a preset name that exists, the current preset name is changed to the given preset name',
+    () => {
+      const {
+        composableWrapper: { applyPreset, globalConfig },
+      } = createTestComposable([useColors, useGlobalConfig])
+
+      applyPreset('dark')
+      expect(globalConfig.value.colors.currentPresetName).toBe('dark')
+    },
   )
-  it.todo(
-    'Given a non-existing preset name, the current preset name is not changed',
-  )
-  it.todo('Given a non-existing preset name, the warn function is called once')
 })
 
 describe('currentPresetName computed', () => {
