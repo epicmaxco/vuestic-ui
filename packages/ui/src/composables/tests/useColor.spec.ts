6fa12042cc8b4ad7e6dd3279a8ed540a45f6b8de
Evobaso-J <ndrbsl@gmail.com>
2023-06-09 08:44:42 +0200

    refactor(tests): remove duplicates and add check

--- packages/ui/src/composables/tests/useColor.spec.ts
+++ packages/ui/src/composables/tests/useColor.spec.ts
@@ -5,17 +5,15 @@ import { createTestComposable } from './createTestComposable'
 import { warn } from '../../utils/console'
 
 describe('applyPreset function', () => {
+  const {
+    composableWrapper: { applyPreset, globalConfig },
+  } = createTestComposable([useColors, useGlobalConfig])
   it(
     'Given a non-existing preset name, the current preset name is changed and the warn function is called once',
     () => {
-      const {
-        composableWrapper: { applyPreset, globalConfig },
-      } = createTestComposable([useColors, useGlobalConfig])
-
       vi.mock('../../utils/console', () => ({
         warn: vi.fn(),
       }))
-
       // Since this test's goal is to trigger a warning
       // by passing the wrong value to a function, we mock
       // useReactiveComputed to prevent it from throwing an unwanted error
@@ -26,17 +24,15 @@ describe('applyPreset function', () => {
       expect(globalConfig.value.colors.currentPresetName).toBe('foo')
       expect(warn).toHaveBeenCalledTimes(1)
       vi.doUnmock('../useReactiveComputed')
+      vi.clearAllMocks()
     },
   )
   it(
-    'Given a preset name that exists, the current preset name is changed to the given preset name',
+    'Given a preset name that exists, the current preset name is changed to the given preset name and no warning is triggered',
     () => {
-      const {
-        composableWrapper: { applyPreset, globalConfig },
-      } = createTestComposable([useColors, useGlobalConfig])
-
       applyPreset('dark')
       expect(globalConfig.value.colors.currentPresetName).toBe('dark')
+      expect(warn).toHaveBeenCalledTimes(0)
     },
   )
 })
