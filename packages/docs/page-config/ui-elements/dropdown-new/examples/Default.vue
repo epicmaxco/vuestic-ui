f8b2188597877b828793bd54a3167f3800edb784
ilearnjs <20753143+ilearnjs@users.noreply.github.com>
2023-06-09 13:23:51 +0200

    rollback docs

--- packages/docs/page-config/ui-elements/dropdown-new/examples/Default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <va-dropdown-new>
-    <template #anchor>
-      <va-button> Dropdownable </va-button>
-    </template>
-
-    <va-dropdown-content> Dropped down! </va-dropdown-content>
-  </va-dropdown-new>
-</template>
