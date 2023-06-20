62aefa877dc0372ea8e425ef207ad3901607a224
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-06-20 15:11:47 +0300

    fix(docs): sidebar badges

--- packages/docs/components/layout/Sidebar.vue
+++ packages/docs/components/layout/Sidebar.vue
@@ -50,8 +50,8 @@
                   placement="right-center"
                   size="small"
                   offset="5px"
+                  :text="childRoute.meta && childRoute.meta.badge?.text"
                   :color="childRoute.meta && childRoute.meta.badge && badgeColors[childRoute.meta.badge.type]"
-                  :visible-empty="false"
                 >
                   {{ childRoute.displayName }}
                 </va-badge>
