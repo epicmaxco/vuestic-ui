c0110deb318a9346e0635c796320100087a58998
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-06-21 14:10:00 +0400

    minor: text tweaks

--- packages/docs/page-config/ui-elements/accordion/index.ts
+++ packages/docs/page-config/ui-elements/accordion/index.ts
@@ -36,7 +36,7 @@ export default definePageConfig({
     }),
 
     block.subtitle("Accessibility"),
-    block.paragraph("Expand/collapse of accordion item of focus is depend on `mutilple` prop, for the rest accessbility information see [VaCollapse accessibility section](ui-elements/collapse#all-accessibility)[[target=_blank]]."),
+    block.paragraph("Expand/collapse of accordion item in focus is dependent on `mutilple` prop, for other accessbility information see [VaCollapse accessibility section](ui-elements/collapse#all-accessibility)[[target=_blank]]."),
 
     block.subtitle("API"),
     block.api("VaAccordion", apiDescription, apiOptions),
