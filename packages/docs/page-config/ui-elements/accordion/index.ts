9afec792ab460c2b4fc5ca6e12669556d455773f
Rui <xiongmao86dev@sina.com>
2023-06-21 18:11:30 +0800

    Accessibility improvements on `VaAccordion` and `VaCollapse` (#3499)
    * Add role heading to header-wrapper
    
    * Add a11y docs to `VaAccordion` and `VaCollapse`
    
    * minor: text tweaks
    
    ---------
    
    Co-authored-by: Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>

--- packages/docs/page-config/ui-elements/accordion/index.ts
+++ packages/docs/page-config/ui-elements/accordion/index.ts
@@ -35,6 +35,9 @@ export default definePageConfig({
       description: "One of the interesting ways to use the component is a collapsing menu."
     }),
 
+    block.subtitle("Accessibility"),
+    block.paragraph("Expand/collapse of accordion item in focus is dependent on `mutilple` prop, for other accessbility information see [VaCollapse accessibility section](ui-elements/collapse#all-accessibility)[[target=_blank]]."),
+
     block.subtitle("API"),
     block.api("VaAccordion", apiDescription, apiOptions),
   ],
