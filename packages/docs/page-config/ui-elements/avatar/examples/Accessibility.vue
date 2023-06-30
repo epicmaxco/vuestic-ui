153ee254d701d01ddcbb7b21d1017af64c35cfcf
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2023-06-30 14:33:46 +0400

    Feat/a11y avatar (#3295)
    * refactor: remove redundant hook
    
    * feat: remove redundant a11y attributes, use filtered component props
    
    * refactor: remove redundant prop
    
    * refactor: update computeds, move props extraction out of setup func
    
    * feat: add accessibility advices example
    
    * Remove trunslation
    
    * feat(avatar): add restColor prop
    
    ---------
    
    Co-authored-by: Maksim Nedoshev <m0ksem1337@gmail.com>

---
+++ packages/docs/page-config/ui-elements/avatar/examples/Accessibility.vue
@@ -0,0 +1,30 @@
+<template>
+  <va-avatar
+    src="https://randomuser.me/api/portraits/women/1.jpg"
+    alt="Women"
+    class="mr-4"
+  />
+
+  <va-avatar
+    title="icon face"
+    icon="face"
+    class="mr-4"
+  />
+
+  <va-avatar
+    title="Jane Doe' avatar"
+    class="mr-4"
+  >
+    J.D.
+  </va-avatar>
+
+  <div class="inline-flex justify-start items-center">
+    <va-avatar
+      aria-hidden="true"
+      class="mr-4"
+    >
+      J.D.
+    </va-avatar>
+    <span>Jane Doe</span>
+  </div>
+</template>
