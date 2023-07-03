c7f16a5d62b5a1259a14098e460c4c008eb1f3bb
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-07-03 11:29:01 +0300

    fix: circular import error

--- packages/ui/src/components/va-radio/index.ts
+++ packages/ui/src/components/va-radio/index.ts
@@ -1,6 +1,5 @@
 import withConfigTransport from '../../services/config-transport/withConfigTransport'
 import _VaRadio from './VaRadio.vue'
 
+export { type VaRadioOption } from './types'
 export const VaRadio = withConfigTransport(_VaRadio)
-
-export { VaRadioOption } from './types'
