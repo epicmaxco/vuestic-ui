61567ce39b906ac2bccde9c2d45aa58f59c68e9e
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-07-07 14:00:19 +0300

    Fix#3568/provide more things to stepper slot (#3573)
    * fix(#3568): provide more data in slots
    
    * fix(stepper): dont show finish button if last step is disabled
    
    * fix(stepper): pass current step slot data to controls slot
    
    * fix(#3560): stepper hide-disabled-button prop

--- packages/ui/src/components/va-stepper/VaStepper.demo.vue
+++ packages/ui/src/components/va-stepper/VaStepper.demo.vue
@@ -1,12 +1,9 @@
 <template>
   <VbDemo>
     <VbCard title="Default">
-      <VaStepper
-        v-model="step"
-        :steps="steps"
-      >
+      <VaStepper v-model="step" :steps="steps">
         <template
-          v-for="step, i in steps"
+          v-for="(step, i) in steps"
           :key="step.label"
           #[`step-content-${i}`]
         >
@@ -15,12 +12,9 @@
       </VaStepper>
     </VbCard>
     <VbCard title="With disabled steps">
-      <VaStepper
-        v-model="step"
-        :steps="stepsWithDisabled"
-      >
+      <VaStepper v-model="step" :steps="stepsWithDisabled">
         <template
-          v-for="step, i in stepsWithDisabled"
+          v-for="(step, i) in stepsWithDisabled"
           :key="step.label"
           #[`step-content-${i}`]
         >
@@ -29,20 +23,12 @@
       </VaStepper>
     </VbCard>
     <VbCard title="Custom icons and no controls">
-      <VaStepper
-        v-model="step"
-        :steps="stepsWithCustomIcons"
-        controlsHidden
-      />
+      <VaStepper v-model="step" :steps="stepsWithCustomIcons" controlsHidden />
     </VbCard>
     <VbCard title="Navigation disabled">
-      <VaStepper
-        v-model="step"
-        :steps="steps"
-        navigationDisabled
-      >
+      <VaStepper v-model="step" :steps="steps" navigationDisabled>
         <template
-          v-for="step, i in steps"
+          v-for="(step, i) in steps"
           :key="step.label"
           #[`step-content-${i}`]
         >
@@ -51,13 +37,9 @@
       </VaStepper>
     </VbCard>
     <VbCard title="Next disabled">
-      <VaStepper
-        v-model="step"
-        :steps="steps"
-        nextDisabled
-      >
+      <VaStepper v-model="step" :steps="steps" nextDisabled>
         <template
-          v-for="step, i in steps"
+          v-for="(step, i) in steps"
           :key="step.label"
           #[`step-content-${i}`]
         >
@@ -66,13 +48,9 @@
       </VaStepper>
     </VbCard>
     <VbCard title="Vertical">
-      <VaStepper
-        v-model="step"
-        :steps="steps"
-        vertical
-      >
+      <VaStepper v-model="step" :steps="steps" vertical>
         <template
-          v-for="step, i in steps"
+          v-for="(step, i) in steps"
           :key="step.label"
           #[`step-content-${i}`]
         >
@@ -80,36 +58,40 @@
         </template>
       </VaStepper>
     </VbCard>
+
+    <VbCard title="Finish button hidden">
+      <VaStepper v-model="step" :steps="steps" finishButtonHidden> </VaStepper>
+    </VbCard>
   </VbDemo>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { VaStepper } from './index'
+import { ref } from "vue";
+import { VaStepper } from "./index";
 
-const step = ref(2)
+const step = ref(2);
 
 const steps = [
-  { label: 'One' },
-  { label: 'Two' },
-  { label: 'Three' },
-  { label: 'Four' },
-  { label: 'Five' },
-]
+  { label: "One" },
+  { label: "Two" },
+  { label: "Three" },
+  { label: "Four" },
+  { label: "Five" },
+];
 
 const stepsWithDisabled = [
-  { label: 'One' },
-  { label: 'Two', disabled: true },
-  { label: 'Three' },
-  { label: 'Four', disabled: true },
-  { label: 'Five' },
-]
+  { label: "One" },
+  { label: "Two", disabled: true },
+  { label: "Three" },
+  { label: "Four", disabled: true },
+  { label: "Five" },
+];
 
 const stepsWithCustomIcons = [
-  { label: 'One', icon: 'search' },
-  { label: 'Two', icon: 'home' },
-  { label: 'Three', icon: 'delete' },
-  { label: 'Four', icon: 'grade' },
-  { label: 'Five', icon: 'list' },
-]
+  { label: "One", icon: "search" },
+  { label: "Two", icon: "home" },
+  { label: "Three", icon: "delete" },
+  { label: "Four", icon: "grade" },
+  { label: "Five", icon: "list" },
+];
 </script>
