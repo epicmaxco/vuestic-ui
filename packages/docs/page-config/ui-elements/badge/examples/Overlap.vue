489f14960607a259bbcd371abb961d77f971862a
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-07-20 04:40:13 +0400

    feat: badge fixes

--- packages/docs/page-config/ui-elements/badge/examples/Overlap.vue
+++ packages/docs/page-config/ui-elements/badge/examples/Overlap.vue
@@ -1,36 +1,44 @@
 <template>
-  <va-badge
-    dot
-    overlap
-    placement="bottom-center"
-    class="mr-8"
-  >
-    <va-avatar src="https://randomuser.me/api/portraits/women/1.jpg" />
-  </va-badge>
+  <div class="flex items-center">
+    <va-badge
+      dot
+      overlap
+      placement="bottom-right"
+      :offset="[-7, -7]"
+      class="mr-8"
+    >
+      <va-avatar src="https://randomuser.me/api/portraits/women/1.jpg" />
+    </va-badge>
 
-  <va-badge
-    text="5"
-    overlap
-    class="mr-6"
-  >
-    <va-icon name="&#xe0cb;" />
-  </va-badge>
+    <va-badge
+      text="5"
+      overlap
+      class="mr-6"
+      :offset="[-3,3]"
+    >
+      <va-icon name="&#xe0cb;" />
+    </va-badge>
 
-  <va-badge
-    text="5"
-    overlap
-    class="mr-6"
-    style="--va-badge-text-wrapper-border-radius: 50%;"
-  >
-    <va-icon name="&#xe0cb;" />
-  </va-badge>
+    <va-badge
+      text="5"
+      overlap
+      class="mr-6"
+      :offset="[-3,3]"
+      style="--va-badge-text-wrapper-border-radius: 50%;"
+    >
+      <va-icon name="&#xe0cb;" />
+    </va-badge>
 
-  <va-badge
-    placement="bottom-end"
-    text="99+"
-    overlap
-    style="--va-badge-text-wrapper-border-radius: 40px;"
-  >
-    <va-icon name="&#xe0cb;" />
-  </va-badge>
+    <va-badge
+      placement="bottom-end"
+      text="99+"
+      overlap
+      :offset="[0,-4]"
+      style="--va-badge-text-wrapper-border-radius: 40px;"
+    >
+      <va-icon name="mail_outline" />
+    </va-badge>
+  </div>
 </template>
+<script setup>
+</script>
