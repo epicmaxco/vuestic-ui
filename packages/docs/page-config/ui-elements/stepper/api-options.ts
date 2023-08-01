422544390f7cc612468146777d1fbb884af18fb0
Keaton Lee <keaton.lee@bcbsks.com>
2023-08-01 18:46:37 -0400

    Whitespace fixes

--- packages/docs/page-config/ui-elements/stepper/api-options.ts
+++ packages/docs/page-config/ui-elements/stepper/api-options.ts
@@ -2,21 +2,21 @@ export default defineManualApi({
   props: {
     steps: {
       types:
-        "{ label: string, icon?: string, disabled?: boolean, save?: Function, hasError?: boolean, completed?: boolean }",
+        '{ label: string, icon?: string, disabled?: boolean, save?: Function, hasError?: boolean, completed?: boolean }',
     },
   },
   slots: {
-    stepButton: {},
-    stepContent: {},
-    controls: {},
-    divider: {},
+    stepButton: { },
+    stepContent: { },
+    controls: { },
+    divider: { },
   },
   events: {
-    finish: { types: "" },
+    finish: { types: '' }
   },
   methods: {
-    setStep: { types: "`(stepNumber: number) => void`" },
-    nextStep: { types: "`(stepsToSkip: number) => void`" },
-    prevStep: { types: "`(stepsToSkip: number) => void`" },
+    setStep: { types: '`(stepNumber: number) => void`' },
+    nextStep: { types: '`(stepsToSkip: number) => void`' },
+    prevStep: { types: '`(stepsToSkip: number) => void`' },
   },
 });
--- packages/docs/page-config/ui-elements/stepper/index.ts
+++ packages/docs/page-config/ui-elements/stepper/index.ts
@@ -1,49 +1,42 @@
-import apiOptions from "./api-options";
-import apiDescription from "./api-description";
+import apiOptions from './api-options';
+import apiDescription from './api-description';
 
 export default definePageConfig({
   blocks: [
-    block.title("Stepper"),
-    block.paragraph(
-      "Stepper allows users to separate content into several steps and navigate through it using buttons or timeline."
-    ),
+    block.title('Stepper'),
+    block.paragraph('Stepper allows users to separate content into several steps and navigate through it using buttons or timeline.'),
 
-    block.subtitle("Examples"),
+    block.subtitle('Examples'),
 
-    block.example("Default", { title: "Basic usage" }),
+    block.example('Default', { title: 'Basic usage' }),
 
-    block.example("Vertical", { title: "Vertical" }),
+    block.example('Vertical', { title: 'Vertical' }),
 
-    block.example("CustomSave", { title: "Custom save action for each step" }),
+    block.example('CustomSave', { title: 'Custom save action for each step' }),
 
-    block.example("Linear", { title: `Linear (Uses Form Validation)` }),
+    block.example('Linear', { title: `Linear (Uses Form Validation)` }),
 
-    block.example("Icons", {
-      title: "Custom icons",
+    block.example('Icons', {
+      title: 'Custom icons',
       description:
-        "Each step config could have icon name specified, check `va-icon` for more details.",
+        'Each step config could have icon name specified, check `va-icon` for more details.',
     }),
 
-    block.example("Custom", { title: "Customized with slots" }),
+    block.example('Custom', { title: 'Customized with slots' }),
 
-    block.alert(
-      "You can iterate through slots using template literals in slot name. \n Example: ``{'#[`step-button-${i}`]=\"{ setStep, isActive, isCompleted }\"'}``",
-      "info"
-    ),
+    block.alert('You can iterate through slots using template literals in slot name. \n Example: ``{\'#[`step-button-${i}`]=\"{ setStep, isActive, isCompleted }\"\'}``', 'info'),
 
-    block.example("Minimal", {
-      title: "Navigation only",
+    block.example('Minimal', {
+      title: 'Navigation only',
       description:
-        "You can hide controls and use stepper navigation independently",
+        'You can hide controls and use stepper navigation independently',
     }),
 
-    block.subtitle("Accessibility"),
+    block.subtitle('Accessibility'),
 
-    block.paragraph(
-      'The component itself has a [role="group"](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/group_role)[[target=_blank]] with aria attributes like an [aria-orientation](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-orientation)[[target=_blank]] attribute that depends on the `vertical` property. The current step element has the attribute [aria-current="step"](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-current#values)[[target=_blank]]. Keyboard navigation for this component is based on the [w3 tabs pattern](https://www.w3.org/WAI/ARIA/apg/patterns/tabs/)[[target=_blank]].'
-    ),
+    block.paragraph('The component itself has a [role=\"group\"](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/group_role)[[target=_blank]] with aria attributes like an [aria-orientation](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-orientation)[[target=_blank]] attribute that depends on the `vertical` property. The current step element has the attribute [aria-current=\"step\"](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-current#values)[[target=_blank]]. Keyboard navigation for this component is based on the [w3 tabs pattern](https://www.w3.org/WAI/ARIA/apg/patterns/tabs/)[[target=_blank]].'),
 
-    block.subtitle("API"),
-    block.api("VaStepper", apiDescription, apiOptions),
+    block.subtitle('API'),
+    block.api('VaStepper', apiDescription, apiOptions),
   ],
 });
