d0ab1f32844e27861dfff092160d978955f50b4e
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-08-09 16:34:03 +0300

    fix: build target even if there is no argument

--- packages/library-builder/src/cli/index.ts
+++ packages/library-builder/src/cli/index.ts
@@ -5,7 +5,8 @@ import { args } from './args'
 const buildTargets: BuildTarget[] = []
 
 BUILD_TYPES.forEach((type) => {
-  if (args[type]) {
+  // If not provided in args it must be enabled by default
+  if (args[type] === true || args[type] === 'true' || args[type] === undefined) {
     buildTargets.push(type)
   }
 })
