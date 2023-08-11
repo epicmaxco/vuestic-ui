45b1b5acea788d43e94dae997c7b17d2a22f9f35
Roman4437 <romanbabalici97@gmail.com>
2023-08-11 17:16:10 +0200

    fixed everything

--- packages/ui/src/components/va-accordion/VaAccordion.stories.ts
+++ packages/ui/src/components/va-accordion/VaAccordion.stories.ts
@@ -4,11 +4,6 @@ import { within, userEvent } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
 import { sleep } from '../../utils/sleep'
 
-async function DispatchEventWithDelay (element: HTMLElement, callback: (element: HTMLElement) => void, ms: number) {
-  callback(element)
-  await sleep(ms)
-}
-
 export default {
   title: 'VaAccordion',
   component: VaAccordion,
@@ -40,7 +35,7 @@ Default.play = async ({ canvasElement, step }) => {
     expect(collapses[0]).toHaveAttribute('aria-expanded', 'true')
   })
 
-  await step('Close on click', async () => {
+  await step('Closes on click', async () => {
     userEvent.click(collapses[0])
     await sleep()
     expect(collapses[0]).toHaveAttribute('aria-expanded', 'false')
@@ -57,13 +52,13 @@ Default.play = async ({ canvasElement, step }) => {
 export const Stateful = () => ({
   components: { VaAccordion, VaCollapse },
   template: `
-    [true]
+    <p>[true]</p>
     <va-accordion stateful>
       <va-collapse header="Collapse">
         Content
       </va-collapse>
     </va-accordion>
-    [false]
+    <p>[false]</p>
     <va-accordion>
       <va-collapse header="Collapse">
         Content
@@ -72,160 +67,90 @@ export const Stateful = () => ({
   `,
 })
 
-Stateful.play = async ({ canvasElement }) => {
+Stateful.play = async ({ canvasElement, step }) => {
   const canvas = within(canvasElement)
   const collapses = canvas.getAllByRole('button', { name: 'Collapse' }) as HTMLElement[]
-  const [lastCollapse] = collapses.slice(-1)
-  collapses.push(lastCollapse)
 
-  for (const collapse of collapses) {
-    await DispatchEventWithDelay(collapse, userEvent.click, 500)
-  }
+  await step('Should open on click', async () => {
+    userEvent.click(collapses[0])
+    await sleep()
+    expect(collapses[0]).toHaveAttribute('aria-expanded', 'true')
+  })
+
+  await step('Shouldn\'t open on click', async () => {
+    userEvent.click(collapses[1])
+    await sleep()
+    expect(collapses[1]).toHaveAttribute('aria-expanded', 'false')
+  })
 }
 
 export const Multiple = () => ({
   components: { VaAccordion, VaCollapse },
-  setup () {
-    return {
-      modelValue: [false, true, false],
-      collapses: [
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-      ],
-    }
-  },
   template: `
-    <va-accordion v-model="modelValue" multiple>
-      <va-collapse
-        v-for="(collapse, index) in collapses"
-        :key="index"
-        :header="collapse.title"
+    <va-accordion multiple>
+      <va-collapse 
+        v-for="i in 3"
+        :key="i"
+        header="Collapse"
       >
-        <div>
-          {{ collapse.content }}
-        </div>
+        Content
       </va-collapse>
     </va-accordion>
   `,
 })
 
-Multiple.play = async ({ canvasElement }) => {
+Multiple.play = async ({ canvasElement, step }) => {
   const canvas = within(canvasElement)
   const collapses = canvas.getAllByRole('button', { name: 'Collapse' }) as HTMLElement[]
-  await DispatchEventWithDelay(collapses[1], userEvent.click, 500)
 
-  for (const collapse of collapses) {
-    await DispatchEventWithDelay(collapse, userEvent.click, 500)
-  }
+  await step('Opens on click', async () => {
+    userEvent.click(collapses[0])
+    await sleep()
+    expect(collapses[0]).toHaveAttribute('aria-expanded', 'true')
+  })
 
-  for (const collapse of collapses.reverse()) {
-    await DispatchEventWithDelay(collapse, userEvent.click, 500)
-  }
-
-  await DispatchEventWithDelay(collapses[1], userEvent.click, 500)
+  await step('Opening another, doesn\'t close previous', async () => {
+    userEvent.click(collapses[1])
+    await sleep()
+    expect(collapses[0]).toHaveAttribute('aria-expanded', 'true')
+    expect(collapses[1]).toHaveAttribute('aria-expanded', 'true')
+  })
 }
 
 export const Inset = () => ({
   components: { VaAccordion, VaCollapse },
-  setup () {
-    return {
-      modelValue: [true, true, false, false, true],
-      collapses: [
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-      ],
-    }
-  },
   template: `
-    <va-accordion v-model="modelValue" inset multiple>
-      <va-collapse
-        v-for="(collapse, index) in collapses"
-        :key="index"
-        :header="collapse.title"
+    <va-accordion 
+      :modelValue="[true,true,false,false,true]" 
+      inset 
+      multiple
+    >
+      <va-collapse 
+        v-for="i in 5"
+        :key="i"
+        header="Collapse"
       >
-        <div>
-          {{ collapse.content }}
-        </div>
+        Content
       </va-collapse>
     </va-accordion>
   `,
 })
-
-Inset.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const collapses = canvas.getAllByRole('button', { name: 'Collapse' }) as HTMLElement[]
-  const skip = new Set([0, 1, 4])
-  let index = 0
-
-  for (const collapse of collapses) {
-    if (skip.has(index)) {
-      await DispatchEventWithDelay(collapse, userEvent.click, 500)
-    }
-    index++
-  }
-
-  index = 0
-
-  for (const collapse of collapses.reverse()) {
-    if (skip.has(index)) {
-      await DispatchEventWithDelay(collapse, userEvent.click, 500)
-    }
-    index++
-  }
-}
 
 export const Popout = () => ({
   components: { VaAccordion, VaCollapse },
-  setup () {
-    return {
-      modelValue: [true, true, false, false, true],
-      collapses: [
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-        { title: 'Collapse', content: 'Content' },
-      ],
-    }
-  },
   template: `
-    <va-accordion v-model="modelValue" popout multiple>
-      <va-collapse
-        v-for="(collapse, index) in collapses"
-        :key="index"
-        :header="collapse.title"
+    <va-accordion 
+      :modelValue="[true,true,false,false,true]" 
+      popout 
+      multiple
+    >
+      <va-collapse 
+        v-for="i in 5"
+        :key="i"
+        header="Collapse"
       >
-        <div>
-          {{ collapse.content }}
-        </div>
+        Content
       </va-collapse>
     </va-accordion>
   `,
 })
-
-Popout.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const collapses = canvas.getAllByRole('button', { name: 'Collapse' }) as HTMLElement[]
-  const skip = new Set([0, 1, 4])
-  let index = 0
-
-  for (const collapse of collapses) {
-    if (skip.has(index)) {
-      await DispatchEventWithDelay(collapse, userEvent.click, 500)
-    }
-    index++
-  }
-
-  index = 0
-
-  for (const collapse of collapses.reverse()) {
-    if (skip.has(index)) {
-      await DispatchEventWithDelay(collapse, userEvent.click, 500)
-    }
-    index++
-  }
-}
