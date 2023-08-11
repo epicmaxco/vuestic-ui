7f1ba5c4b166a8b8ec5620074b9827ca552bb875
Yauheni Prakopchyk <ontrew@gmail.com>
2023-08-11 17:29:04 +0400

    Feat/chromatic support (#3682)
    * feat: add chromatic dep
    
    * fix: chromatic related issues
    
    * feat: add github workflow for chromatic
    
    * chore: update codeql action
    
    ---------
    
    Co-authored-by: Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>

--- packages/ui/src/components/va-form/VaFormValidationLib.demo.vue
+++ packages/ui/src/components/va-form/VaFormValidationLib.demo.vue
@@ -7,30 +7,30 @@
       <va-form ref="vuelidateForm">
         <va-input
           label="required name"
-          v-model="$v.name.$model"
-          :error="$v.name.$error"
+          v-model="v$.name.$model"
+          :error="v$.name.$error"
           :error-messages="[
-            !$v.name.required && 'required',
-            !$v.name.minLength && `Name must have at least ${$v.name.$params.minLength.min} letters.`
+            !v$.name.required && 'required',
+            !v$.name.minLength && `Name must have at least ${v$.name.$params.minLength.min} letters.`
           ]"
         />
         <va-input
           label="required age"
-          v-model="$v.age.$model"
-          :error="$v.age.$error"
+          v-model="v$.age.$model"
+          :error="v$.age.$error"
           :error-messages="[
-            !$v.age.required && 'required',
-            !$v.age.numeric && 'Age must be a number',
-            !$v.age.between && 'Age must be between 10 and 100'
+            !v$.age.required && 'required',
+            !v$.age.numeric && 'Age must be a number',
+            !v$.age.between && 'Age must be between 10 and 100'
           ]"
         />
         <va-input
           label="required email"
-          v-model="$v.email.$model"
-          :error="$v.email.$error"
+          v-model="v$.email.$model"
+          :error="v$.email.$error"
           :error-messages="[
-            !$v.email.required && 'required',
-            !$v.email.email && 'Email must be correct'
+            !v$.email.required && 'required',
+            !v$.email.email && 'Email must be correct'
           ]"
         />
         <button @click="$refs.vuelidateForm.reset()">
@@ -41,40 +41,33 @@
   </VbDemo>
 </template>
 
-<script>
+<script setup>
 import { VaForm } from './index'
 import { VaInput } from '../va-input'
+import { reactive } from 'vue'
 
-import { validationMixin } from 'vuelidate'
-import { required, minLength, email, between, numeric } from 'vuelidate/lib/validators'
+import { required, minLength, email, between, numeric } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
 
-export default {
-  mixins: [validationMixin],
-  components: {
-    VaForm,
-    VaInput,
+const state = reactive({
+  name: '',
+  age: 0,
+  email: '',
+})
+const rules = {
+  name: {
+    required,
+    minLength: minLength(4),
   },
-  validations: {
-    name: {
-      required,
-      minLength: minLength(4),
-    },
-    email: {
-      required,
-      email,
-    },
-    age: {
-      required,
-      numeric,
-      between: between(10, 100),
-    },
+  email: {
+    required,
+    email,
   },
-  data () {
-    return {
-      name: '',
-      age: 0,
-      email: '',
-    }
+  age: {
+    required,
+    numeric,
+    between: between(10, 100),
   },
 }
+const v$ = useVuelidate(rules, state)
 </script>
