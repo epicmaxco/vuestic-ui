82cbbfd31112d6fc522bf6abaf49e7c7a79f91bb
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-08-21 00:18:55 +0300

    feat(create-vuestic): add tailwind option to nuxt and create-vue presets

--- packages/create-vuestic/src/versions.ts
+++ packages/create-vuestic/src/versions.ts
@@ -2,4 +2,8 @@ export const versions = {
   'vuestic-ui': '^1.6.1',
   '@vuestic/nuxt': '^1.0.11',
   '@vuestic/ag-grid-theme': '^1.1.4',
+  '@vuestic/tailwind': '^0.1.3',
+  'tailwindcss': '^3.3.3',
+  'postcss': '^8.4.28',
+  'autoprefixer': '^10.4.15'
 }
