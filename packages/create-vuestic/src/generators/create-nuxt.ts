4eed60f8fe6b011f9bf958b7912047d950ad8ed2
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-08-30 01:28:38 +0300

    feat(create-vuestic): tailwind option (#3718)

--- packages/create-vuestic/src/generators/create-nuxt.ts
+++ packages/create-vuestic/src/generators/create-nuxt.ts
@@ -1,6 +1,11 @@
+import { doesSatisfyNodeVersion } from '../utils/node-version';
 import { execp } from './../utils/exacp';
 
 export const createNuxt3 = (projectName: string) => {
+  if (!doesSatisfyNodeVersion('v16.10.0')) {
+    throw new Error('Nuxt 3 requires Node.js v16.10.0 or higher. Please upgrade your Node.js version.')
+  }
+
   const command =`npx nuxi init ${projectName}`
 
   return execp(command)
