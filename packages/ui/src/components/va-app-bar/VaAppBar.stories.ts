ecd97b8ab875761e1e7f59d3bf750459410ede5c
Roman4437 <romanbabalici97@gmail.com>
2023-08-30 15:22:26 +0200

    va-app-bar component and stories fixes

--- packages/ui/src/components/va-app-bar/VaAppBar.stories.ts
+++ packages/ui/src/components/va-app-bar/VaAppBar.stories.ts
@@ -1,4 +1,6 @@
 import { VaAppBar } from './'
+import { expect } from '@storybook/jest'
+import { sleep } from '../../utils/sleep'
 
 export default {
   title: 'VaAppBar',
@@ -6,24 +8,30 @@ export default {
   tags: ['autodocs'],
 }
 
-export const Default = {
-  args: {
-    default: 'App Bar',
-  },
-}
+export const Default = () => ({
+  components: { VaAppBar },
+  template: `
+    <va-app-bar>
+      App Bar 
+    </va-app-bar>
+  `,
+})
 
-export const Gradient = {
-  args: {
-    default: 'App Bar',
-    gradient: true,
-  },
-}
+export const Gradient = () => ({
+  components: { VaAppBar },
+  template: `
+    <va-app-bar gradient>
+      App Bar 
+    </va-app-bar>
+  `,
+})
 
 export const Color = () => ({
   components: { VaAppBar },
   template: `
+    [warning]
     <va-app-bar color="warning">
-      App Bar
+      App Bar 
     </va-app-bar>
   `,
 })
@@ -32,7 +40,7 @@ export const Fixed = () => ({
   components: { VaAppBar },
   template: `
     <va-app-bar fixed>
-      App Bar
+      App Bar 
     </va-app-bar>
     {{ $vb.lorem(600) }}
   `,
@@ -42,7 +50,7 @@ export const Bottom = () => ({
   components: { VaAppBar },
   template: `
     <va-app-bar fixed bottom>
-      App Bar
+      App Bar 
     </va-app-bar>
     {{ $vb.lorem(600) }}
   `,
@@ -52,14 +60,14 @@ export const HideOnScroll = () => ({
   components: { VaAppBar },
   template: `
     <div class="flex flex-col relative max-h-32 overflow-hidden">
-      <va-app-bar
-        hide-on-scroll
+      <va-app-bar 
+        hide-on-scroll 
         target="#target1"
       >
-        App Bar
+        App Bar 
       </va-app-bar>
-      <div
-        id="target1"
+      <div 
+        id="target1" 
         class="pb-[500px] overflow-auto"
       >
         {{ $vb.lorem(2500) }}
@@ -67,19 +75,30 @@ export const HideOnScroll = () => ({
     </div>
   `,
 })
+
+HideOnScroll.play = async ({ step }) => {
+  const target = document.querySelector('#target1') as HTMLElement
+  target.scrollTo({ top: target.clientHeight })
+  await sleep(500)
+  const appbar = document.querySelector('[role="toolbar"]') as HTMLElement
+
+  await step('Hidden on scroll', async () => {
+    expect(appbar.style.cssText.includes('translateY')).toBeTruthy()
+  })
+}
 
 export const ShadowOnScroll = () => ({
   components: { VaAppBar },
   template: `
     <div class="flex flex-col relative max-h-32 overflow-hidden">
-      <va-app-bar
-        shadow-on-scroll
+      <va-app-bar 
+        shadow-on-scroll 
         target="#target2"
       >
-        App Bar
+        App Bar 
       </va-app-bar>
-      <div
-        id="target2"
+      <div 
+        id="target2" 
         class="pb-[500px] overflow-auto"
       >
         {{ $vb.lorem(2500) }}
@@ -88,19 +107,31 @@ export const ShadowOnScroll = () => ({
   `,
 })
 
+ShadowOnScroll.play = async ({ step }) => {
+  const target = document.querySelector('#target2') as HTMLElement
+  target.scrollTo({ top: target.clientHeight })
+  await sleep(500)
+  const appbar = document.querySelector('[role="toolbar"]') as HTMLElement
+
+  await step('Has shadow on scroll', async () => {
+    expect(appbar.style.cssText.includes('box-shadow')).toBeTruthy()
+  })
+}
+
 export const ShadowColor = () => ({
   components: { VaAppBar },
   template: `
+    [warning]
     <div class="flex flex-col relative max-h-32 overflow-hidden">
-      <va-app-bar
-        shadow-on-scroll
+      <va-app-bar 
+        shadow-on-scroll 
         shadow-color="warning"
         target="#target3"
       >
-        App Bar
+        App Bar 
       </va-app-bar>
-      <div
-        id="target4"
+      <div 
+        id="target3" 
         class="pb-[500px] overflow-auto"
       >
         {{ $vb.lorem(2500) }}
