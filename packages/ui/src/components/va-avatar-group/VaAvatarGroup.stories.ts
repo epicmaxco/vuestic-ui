26215a675a53fcc8bbb07abc6b28e8d6f03668d7
Roman4437 <romanbabalici97@gmail.com>
2023-09-06 16:06:52 +0200

    va-avatar-group stories

--- packages/ui/src/components/va-avatar-group/VaAvatarGroup.stories.ts
+++ packages/ui/src/components/va-avatar-group/VaAvatarGroup.stories.ts
@@ -1,12 +1,88 @@
-import { defineComponent } from 'vue'
-import VaAvatarGroup from './VaAvatarGroup.demo.vue'
+import { VaAvatarGroup } from './'
 
 export default {
   title: 'VaAvatarGroup',
   component: VaAvatarGroup,
+  tags: ['autodocs'],
 }
 
-export const Default = defineComponent({
+export const Default = () => ({
   components: { VaAvatarGroup },
-  template: '<VaAvatarGroup/>',
+  setup: () => ({avatars: [
+    {
+      src: 'https://randomuser.me/api/portraits/women/1.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+    {
+      src: 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+    {
+      src: 'https://randomuser.me/api/portraits/women/5.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+  ]}),
+  template: `
+    <va-avatar-group :options="avatars" :max="3"/>
+  `,
+})
+
+export const RestColor = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({avatars: [
+    {
+      src: 'https://randomuser.me/api/portraits/women/1.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+    {
+      src: 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+    {
+      src: 'https://randomuser.me/api/portraits/women/5.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+  ]}),
+  template: `
+    <va-avatar-group :options="avatars" restColor="warning" :max="3"/>
+  `,
+})
+
+export const Vertical = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({avatars: [
+    {
+      src: 'https://randomuser.me/api/portraits/women/1.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+    {
+      src: 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+    {
+      src: 'https://randomuser.me/api/portraits/women/5.jpg',
+    },
+    {
+      icon: 'warning',
+    },
+  ]}),
+  template: `
+    <va-avatar-group :options="avatars" vertical :max="3"/>
+  `,
 })
