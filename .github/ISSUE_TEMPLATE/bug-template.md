7ec2b37d0fbd97853d37c7968c0d2174f72819a4
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-09-09 02:50:09 +0300

    chore: bump version to v1.7.8

--- .github/ISSUE_TEMPLATE/bug-template.md
+++ .github/ISSUE_TEMPLATE/bug-template.md
@@ -6,7 +6,7 @@ assignees: ''
 
 ---
 
-**Vuestic-ui version:** 1.7.6
+**Vuestic-ui version:** 1.7.8
 
 **Steps to reproduce**
 
--- packages/vue-cli-plugin/generator/configs/default.js
+++ packages/vue-cli-plugin/generator/configs/default.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   extendPackage: {
     dependencies: {
-      'vuestic-ui': '^1.7.6',
+      'vuestic-ui': '^1.7.8',
     }
   },
   vueUse: [
