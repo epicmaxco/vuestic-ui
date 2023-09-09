a8a5b6042a88e93518a2e4a302dd96b3165c3e4b
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-09-09 16:42:29 +0300

    fix(input) label slot

--- packages/ui/src/components/va-input/VaInput.stories.ts
+++ packages/ui/src/components/va-input/VaInput.stories.ts
@@ -26,3 +26,51 @@ export const Clearable = () => ({
   },
   template: '<VaInput clearable v-model="value" />',
 })
+
+export const LabelSlot = () => ({
+  components: { VaInput },
+  data: () => ({ value: 'test' }),
+  template: `
+    <VaInput clearable v-model="value">
+      <template #label="bind">
+        <span style="color: red">Custom label!</span>
+      </template>
+    </VaInput>
+  `,
+})
+
+export const InnerLabelSlot = () => ({
+  components: { VaInput },
+  data: () => ({ value: 'test' }),
+  template: `
+    <VaInput clearable v-model="value" inner-label>
+      <template #label="{ label }">
+        <span style="color: red">Custom label!</span>
+      </template>
+    </VaInput>
+  `,
+})
+
+export const MessagesSlot = () => ({
+  components: { VaInput },
+  data: () => ({ value: 'test' }),
+  template: `
+    <VaInput clearable v-model="value" :messages="['User message!']">
+      <template #messages="{ messages }">
+        <span style="color: red">{{ messages }}</span>
+      </template>
+    </VaInput>
+  `,
+})
+
+export const MessageSlot = () => ({
+  components: { VaInput },
+  data: () => ({ value: 'test' }),
+  template: `
+    <VaInput clearable v-model="value" :messages="['User message!']">
+      <template #message="{ message }">
+        <span style="color: red">{{ message }}</span>
+      </template>
+    </VaInput>
+  `,
+})
