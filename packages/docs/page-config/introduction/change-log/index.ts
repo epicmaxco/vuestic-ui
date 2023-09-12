6953e290904363e39c93d3802acf73f59ee52f56
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-09-12 04:10:04 +0300

    chore: railway deploy fix

--- packages/docs/page-config/introduction/change-log/index.ts
+++ packages/docs/page-config/introduction/change-log/index.ts
@@ -1,30 +1,42 @@
-const changelogs = import.meta.glob('@/../ui/**/CHANGELOG.md', { eager: true , as: 'raw' }) as Record<string, string>
+const changelogs = import.meta.glob('@/../ui/**/CHANGELOG.md', { as: 'raw' }) as Record<string, () => Promise<string>>
 
 
-const mergedChangelog = Object
-  .entries(changelogs)
-  .reduce((acc, [path, changelog]) => {
-    const componentName = path.match(/components\/(.*)\/CHANGELOG.md/)?.[1]
+const render = async () => {
+  const logEnteries = await Promise.all(Object.entries(changelogs)
+    .map(async ([path, changelog]) => {
+      return [
+        path,
+        await changelog()
+      ]
+    }))
 
-    const spilledByVersion = changelog.split(/# ([0-9]*\.[0-9]*\.[0-9]*)/).filter(Boolean)
+  const mergedChangelog = logEnteries
+    .reduce((acc, [path, changelog]) => {
+      const componentName = path.match(/components\/(.*)\/CHANGELOG.md/)?.[1]
 
-    for (let i = 0; i < spilledByVersion.length; i += 2) {
-      const version = spilledByVersion[i]
-      const content = spilledByVersion[i + 1]
+      const spilledByVersion = changelog.split(/# ([0-9]*\.[0-9]*\.[0-9]*)/).filter(Boolean)
 
-      acc[version] = acc[version] || {}
+      for (let i = 0; i < spilledByVersion.length; i += 2) {
+        const version = spilledByVersion[i]
+        const content = spilledByVersion[i + 1]
 
-      if (componentName) {
-        acc[version][componentName] = content.split('\n').filter(Boolean)
+        acc[version] = acc[version] || {}
+
+        if (componentName) {
+          acc[version][componentName] = content.split('\n').filter(Boolean)
+        }
       }
-    }
-    return acc
-  }, {} as Record<string, Record<string, string[]>>)
+      return acc
+    }, {} as Record<string, Record<string, string[]>>)
+
+  return [
+    block.component("ChangeLog", { changelog: mergedChangelog })
+  ]
+}
 
 export default definePageConfig({
   blocks: [
     block.title('Change Log'),
-    // block.markdown(Object.entries(mergedChangelog).map(([version, content]) => `## ${version}\n${content}`).join('\n')),
-    block.component('ChangeLog', { changeLog: mergedChangelog }),
+    block.async(render())
   ],
 });
