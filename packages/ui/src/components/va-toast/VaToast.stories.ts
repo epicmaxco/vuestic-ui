f67b96f90bd38286388972106ba0ce3a9249f2fe
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-09-14 01:58:26 +0300

    chore(toast): added textColor demo

--- packages/ui/src/components/va-toast/VaToast.stories.ts
+++ packages/ui/src/components/va-toast/VaToast.stories.ts
@@ -1,12 +1,28 @@
 import { defineComponent } from 'vue'
-import VaToast from './VaToast.demo.vue'
+import VaToastDemo from './VaToast.demo.vue'
+import VaToast from './VaToast.vue'
+import { StoryFn } from '@storybook/vue3'
+import { expect } from '@storybook/jest'
 
 export default {
   title: 'VaToast',
   component: VaToast,
 }
 
-export const Default = defineComponent({
-  components: { VaToast },
-  template: '<VaToast/>',
+export const Default = () => defineComponent({
+  components: { VaToastDemo },
+  template: '<VaToastDemo/>',
 })
+
+export const TextColor: StoryFn = () => ({
+  components: { VaToast: VaToast },
+  template: '<VaToast color="red"/>',
+})
+
+TextColor.play = async ({ canvasElement, step }) => {
+  await step('Closes on click', async () => {
+    expect(canvasElement.querySelector('.va-toast')).toHaveStyle({
+      color: '#ffffff',
+    })
+  })
+}
