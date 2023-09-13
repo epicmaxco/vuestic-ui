1b72297ac4932995988a806878a3a689fb53a450
Roman4437 <118062031+Roman4437@users.noreply.github.com>
2023-09-13 13:01:00 +0200

    Stories rewok: VaAppBar (#3714)
    * va-app-bar stories refactor
    
    * chore: tweak ap bar stories a bit
    
    * va-app-bar component and stories fixes
    
    * fix: small story fixes
    
    ---------
    
    Co-authored-by: Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>

--- packages/ui/src/components/va-app-bar/VaAppBar.stories.ts
+++ packages/ui/src/components/va-app-bar/VaAppBar.stories.ts
@@ -1,12 +1,129 @@
-import { defineComponent } from 'vue'
-import VaAppBar from './VaAppBar.demo.vue'
+import { VaAppBar } from './'
 
 export default {
   title: 'VaAppBar',
   component: VaAppBar,
+  tags: ['autodocs'],
 }
 
-export const Default = defineComponent({
+export const Default = () => ({
   components: { VaAppBar },
-  template: '<VaAppBar/>',
+  template: `
+    <va-app-bar>
+      App Bar
+    </va-app-bar>
+  `,
 })
+
+export const Gradient = () => ({
+  components: { VaAppBar },
+  template: `
+    <va-app-bar gradient>
+      App Bar
+    </va-app-bar>
+  `,
+})
+
+export const Color = () => ({
+  components: { VaAppBar },
+  template: `
+    <va-app-bar color="warning">
+      App Bar
+    </va-app-bar>
+  `,
+})
+
+export const Fixed = () => ({
+  components: { VaAppBar },
+  template: `
+    <va-app-bar fixed>
+      App Bar
+    </va-app-bar>
+    {{ $vb.lorem(2000) }}
+  `,
+})
+
+export const FixedBottom = () => ({
+  components: { VaAppBar },
+  template: `
+    <va-app-bar fixed bottom>
+      App Bar
+    </va-app-bar>
+    {{ $vb.lorem(2000) }}
+  `,
+})
+
+export const HideOnScroll = () => ({
+  components: { VaAppBar },
+  template: `
+    <div class="flex flex-col relative max-h-32 overflow-hidden">
+      <va-app-bar
+        hide-on-scroll
+        target="#target"
+      >
+        App Bar
+      </va-app-bar>
+      <div
+        id="target"
+        class="pb-[500px] overflow-auto"
+      >
+        {{ $vb.lorem(2500) }}
+      </div>
+    </div>
+  `,
+})
+
+HideOnScroll.play = async ({ canvasElement, step }) => {
+  const target = canvasElement.querySelector('#target') as HTMLElement
+  target.scrollTo({ top: target.clientHeight })
+}
+
+export const ShadowOnScroll = () => ({
+  components: { VaAppBar },
+  template: `
+    <div class="flex flex-col relative max-h-32 overflow-hidden">
+      <va-app-bar
+        shadow-on-scroll
+        target="#target"
+      >
+        App Bar
+      </va-app-bar>
+      <div
+        id="target"
+        class="pb-[500px] overflow-auto"
+      >
+        {{ $vb.lorem(2500) }}
+      </div>
+    </div>
+  `,
+})
+
+ShadowOnScroll.play = async ({ canvasElement, step }) => {
+  const target = canvasElement.querySelector('#target') as HTMLElement
+  target.scrollTo({ top: target.clientHeight })
+}
+
+export const ShadowColor = () => ({
+  components: { VaAppBar },
+  template: `
+    <div class="flex flex-col relative max-h-32 overflow-hidden">
+      <va-app-bar
+        shadow-on-scroll
+        shadow-color="danger"
+        target="#target"
+      >
+        App Bar
+      </va-app-bar>
+      <div
+        id="target"
+        class="pb-[500px] overflow-auto"
+      >
+        {{ $vb.lorem(2500) }}
+      </div>
+    </div>
+  `,
+})
+ShadowColor.play = async ({ canvasElement, step }) => {
+  const target = canvasElement.querySelector('#target') as HTMLElement
+  target.scrollTo({ top: target.clientHeight })
+}
