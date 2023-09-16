22ea3bd05629f30707e3819db714d3ec93594d4e
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-09-17 00:03:28 +0400

    feat: add dummy concept

---
+++ packages/ui/src/composables/UseElementBackgroundDummy.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useElementBackground } from './useElementBackground'
+import { useCurrentElement } from './useCurrentElement'
+import { watch, computed, defineEmits } from 'vue'
+
+const color = computed(() => useElementBackground(useCurrentElement()))
+
+const emit = defineEmits(['update:color'])
+
+watch(color, (value) => emit('update:color', value), { immediate: true })
+</script>
+
+<template>
+  <div style="width: 2rem; height: 2rem;" />
+</template>
--- packages/ui/src/composables/useElementBackground.stories.ts
+++ packages/ui/src/composables/useElementBackground.stories.ts
@@ -1,36 +1,57 @@
-import { defineComponent } from 'vue'
+import { defineComponent, computed, ref } from 'vue'
 import { useElementBackground } from './useElementBackground'
 import { useCurrentElement } from './useCurrentElement'
+import { VaButton } from '../components/va-button'
+import UseElementBackgroundDummy from './UseElementBackgroundDummy.vue'
+import { within } from '@storybook/testing-library'
+import { sleep } from '../utils/sleep'
+import { expect } from '@storybook/jest'
 
 export default {
   title: 'composables/useElementBackground',
 }
 
 export const Default = () => ({
+  components: { VaButton, UseElementBackgroundDummy },
   data () {
     return {
-      styles: {
-        background: '#000',
-        color: '#fff',
-      },
+      background: '#000',
+      color: null,
     }
   },
-
-  setup () {
-    return {
-      color: useElementBackground(useCurrentElement()),
-    }
-  },
-
   template: `
-<div :style="{ ...styles }">
-  <button @click="styles = { background: '#000', color: '#fff' }">Black</button>
-  <button @click="styles = { background: '#fff', color: '#000' }">White</button>
-
-  <div>Current color: {{ color }}</div>
-</div>
+  <UseElementBackgroundDummy
+    :style="{ background }"
+    @update:color="color = $event"
+  />
+  <p>Color detected: <span data-testid="color">{{color}}</span></p>
+  <va-button @click="background = '#000'">black</va-button>
+  <va-button @click="background = '#fff'">white</va-button>
 `,
 })
+
+Default.play = async ({ canvasElement, step }) => {
+  const canvas = within(canvasElement)
+  const color = canvas.getByTestId('color') as HTMLElement
+  const black = canvas.getByText('black') as HTMLElement
+  const white = canvas.getByText('white') as HTMLElement
+
+  await sleep()
+
+  await step('should match white', async () => {
+    white.click()
+    // Timing before update seems pretty random.
+    // That might be something we want to account for on usage.
+    // 20 ms was a sweet spot for me, 10 was too low.
+    await sleep(20)
+    expect(color.innerText).toBe('rgba255, 255, 255, 1')
+  })
+  await step('should match black', async () => {
+    black.click()
+    await sleep(20)
+    expect(color.innerText).toBe('rgba0, 0, 0, 1')
+  })
+}
 
 export const WithTransition = () => ({
   data () {
@@ -39,12 +60,6 @@ export const WithTransition = () => ({
         background: '#000',
         color: '#fff',
       },
-    }
-  },
-
-  setup () {
-    return {
-      color: useElementBackground(useCurrentElement()),
     }
   },
 
