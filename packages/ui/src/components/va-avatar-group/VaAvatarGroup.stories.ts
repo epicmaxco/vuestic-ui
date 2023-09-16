e47df89fdfb4ca957fd27ea8bcd6ea4bb3cadc04
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-09-17 02:23:27 +0400

    fix: tweak va-avatar stories

--- packages/ui/src/components/va-avatar-group/VaAvatarGroup.stories.ts
+++ packages/ui/src/components/va-avatar-group/VaAvatarGroup.stories.ts
@@ -1,4 +1,5 @@
 import { VaAvatarGroup } from './'
+import { addText } from '../../../.storybook/interaction-utils/addText'
 
 export default {
   title: 'VaAvatarGroup',
@@ -6,83 +7,79 @@ export default {
   tags: ['autodocs'],
 }
 
+const getAvatars = () => ([
+  {
+    src: 'https://randomuser.me/api/portraits/women/1.jpg',
+  },
+  {
+    icon: 'warning',
+  },
+  {
+    color: 'danger',
+  },
+])
+
 export const Default = () => ({
   components: { VaAvatarGroup },
-  setup: () => ({avatars: [
-    {
-      src: 'https://randomuser.me/api/portraits/women/1.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-    {
-      src: 'https://randomuser.me/api/portraits/men/3.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-    {
-      src: 'https://randomuser.me/api/portraits/women/5.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-  ]}),
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
   template: `
-    <va-avatar-group :options="avatars" :max="3"/>
+    <va-avatar-group :options="avatars"/>
   `,
 })
+addText(
+  Default,
+  'This looks very broken - why do we have +3 even though we have 3 avatars?',
+  'broken',
+)
 
 export const RestColor = () => ({
   components: { VaAvatarGroup },
-  setup: () => ({avatars: [
-    {
-      src: 'https://randomuser.me/api/portraits/women/1.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-    {
-      src: 'https://randomuser.me/api/portraits/men/3.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-    {
-      src: 'https://randomuser.me/api/portraits/women/5.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-  ]}),
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
   template: `
-    <va-avatar-group :options="avatars" restColor="warning" :max="3"/>
+    <va-avatar-group :options="avatars" restColor="warning"/>
   `,
 })
 
+export const Max = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    [2]
+    <va-avatar-group :options="avatars" :max="2"/>
+    [3]
+    <va-avatar-group :options="avatars" :max="3"/>
+    [4]
+    <va-avatar-group :options="avatars" :max="4"/>
+  `,
+})
+addText(
+  Max,
+  '+-3 o.o?',
+  'broken',
+)
+
 export const Vertical = () => ({
   components: { VaAvatarGroup },
-  setup: () => ({avatars: [
-    {
-      src: 'https://randomuser.me/api/portraits/women/1.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-    {
-      src: 'https://randomuser.me/api/portraits/men/3.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-    {
-      src: 'https://randomuser.me/api/portraits/women/5.jpg',
-    },
-    {
-      icon: 'warning',
-    },
-  ]}),
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
   template: `
-    <va-avatar-group :options="avatars" vertical :max="3"/>
+    <va-avatar-group :options="avatars" vertical/>
+  `,
+})
+
+export const PassAvatarProps = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    <va-avatar-group :options="avatars" square size="large"/>
   `,
 })
