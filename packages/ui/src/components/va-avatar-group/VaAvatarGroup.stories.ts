a838fe73e49e8a207b8bd91885c556a24030c902
Roman4437 <118062031+Roman4437@users.noreply.github.com>
2023-09-17 00:30:10 +0200

    Stories rework: VaAvatarGroup (#3800)
    * va-avatar-group stories
    
    * fix: tweak va-avatar stories
    
    * chore: remove demo file for va-avatar-group
    
    ---------
    
    Co-authored-by: Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>

--- packages/ui/src/components/va-avatar-group/VaAvatarGroup.stories.ts
+++ packages/ui/src/components/va-avatar-group/VaAvatarGroup.stories.ts
@@ -1,12 +1,85 @@
-import { defineComponent } from 'vue'
-import VaAvatarGroup from './VaAvatarGroup.demo.vue'
+import { VaAvatarGroup } from './'
+import { addText } from '../../../.storybook/interaction-utils/addText'
 
 export default {
   title: 'VaAvatarGroup',
   component: VaAvatarGroup,
+  tags: ['autodocs'],
 }
 
-export const Default = defineComponent({
+const getAvatars = () => ([
+  {
+    src: 'https://randomuser.me/api/portraits/women/1.jpg',
+  },
+  {
+    icon: 'warning',
+  },
+  {
+    color: 'danger',
+  },
+])
+
+export const Default = () => ({
   components: { VaAvatarGroup },
-  template: '<VaAvatarGroup/>',
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    <va-avatar-group :options="avatars"/>
+  `,
+})
+addText(
+  Default,
+  'This looks very broken - why do we have +3 even though we have 3 avatars?',
+  'broken',
+)
+
+export const RestColor = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    <va-avatar-group :options="avatars" restColor="warning"/>
+  `,
+})
+
+export const Max = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    [2]
+    <va-avatar-group :options="avatars" :max="2"/>
+    [3]
+    <va-avatar-group :options="avatars" :max="3"/>
+    [4]
+    <va-avatar-group :options="avatars" :max="4"/>
+  `,
+})
+addText(
+  Max,
+  '+-3 o.o?',
+  'broken',
+)
+
+export const Vertical = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    <va-avatar-group :options="avatars" vertical/>
+  `,
+})
+
+export const PassAvatarProps = () => ({
+  components: { VaAvatarGroup },
+  setup: () => ({
+    avatars: getAvatars(),
+  }),
+  template: `
+    <va-avatar-group :options="avatars" square size="large"/>
+  `,
 })
