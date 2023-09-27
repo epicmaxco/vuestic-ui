aa3752eef996f54e381c6dc4a33241a05cf3b6df
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-09-27 23:16:54 +0300

    fix(#2704): icon shift (#3903)

--- packages/ui/src/components/va-button/VaButton.stories.ts
+++ packages/ui/src/components/va-button/VaButton.stories.ts
@@ -1,12 +1,24 @@
 import { defineComponent } from 'vue'
-import VaButton from './VaButton.demo.vue'
+import VaButton from './VaButton.vue'
+import VaButtonDemo from './VaButton.demo.vue'
 
 export default {
   title: 'VaButton',
   component: VaButton,
 }
 
-export const Default = defineComponent({
-  components: { VaButton },
+export const Default = () => defineComponent({
+  components: { VaButton: VaButtonDemo },
   template: '<VaButton/>',
+})
+
+// See: https://github.com/epicmaxco/vuestic-ui/issues/2704
+export const RightShift = () => ({
+  components: { VaButton },
+  template: `
+    <div>
+      <VaButton>Text</VaButton>
+      <VaButton icon="phone">Text2</VaButton>
+    </div>
+  `,
 })
