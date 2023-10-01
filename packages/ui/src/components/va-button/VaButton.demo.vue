d6c155176b41deb00eb1dd47e01a25f411b6c3ed
root <root@Cattys-Laptop>
2023-10-01 14:09:04 -0400

    fix/3930

--- packages/ui/src/components/va-button/VaButton.demo.vue
+++ packages/ui/src/components/va-button/VaButton.demo.vue
@@ -172,7 +172,7 @@
           <td>
             <va-button border-color="primary" preset="primary" icon="create">Secondary</va-button>
             <va-button border-color="primary" preset="secondary" iconRight="clear">Secondary</va-button>
-            <va-button border-color="primary" preset="plain" icon="create" iconRight="clear">
+            <va-button border-color="primary" preset="primary" icon="create" iconRight="clear">
               Secondary
             </va-button>
           </td>
