dfc13e2f729af4075cfb87241ea8f899ff263995
xiongmao86 <xiongmao86dev@sina.com>
2023-10-04 20:47:45 +0800

    add interaction test for VaValue

--- packages/ui/src/components/va-value/VaValue.stories.ts
+++ packages/ui/src/components/va-value/VaValue.stories.ts
@@ -1,5 +1,8 @@
 import { VaValue } from '.'
 import { VaInput } from '../va-input'
+import { within, userEvent } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { sleep } from '../../utils/sleep'
 
 export default {
   title: 'VaValue',
@@ -18,12 +21,27 @@ export const Default = () => ({
   `,
 })
 
+Default.play = async ({ canvasElement, step }) => {
+  const canvas = within(canvasElement)
+  const button = canvas.getByRole('button')
+
+  await step('button text is initially `unclicked`', async () => {
+    expect(button.innerText).toEqual('unclicked')
+  })
+
+  await step('button text is `clicked` after clicking it', async () => {
+    userEvent.click(button)
+    await sleep()
+    expect(button.innerText).toEqual('clicked')
+  })
+}
+
 export const ReactiveObject = () => ({
   components: { VaValue, VaInput },
   template: `
     <VaValue :defaultValue="{ name: '', age: 0 }" #default="v">
       <VaInput v-model="v.name" label="name" />
-      <VaInput v-model="v.age" label="name" />
+      <VaInput v-model="v.age" label="age" />
     </VaValue>
   `,
 })
