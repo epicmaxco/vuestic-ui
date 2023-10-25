7cbb700aac4e9a3bd807ef8bf0ea10c13b3df1ad
xiongmao86 <xiongmao86dev@sina.com>
2023-10-25 20:47:19 +0800

    Turn ColorCSSVariablesUpdater from demo into story

--- packages/ui/src/services/color/ColorCSSVariablesUpdater.stories.ts
+++ packages/ui/src/services/color/ColorCSSVariablesUpdater.stories.ts
@@ -1,12 +1,32 @@
-import { defineComponent } from 'vue'
-import ColorCSSVariablesUpdater from './ColorCSSVariablesUpdater.demo.vue'
+import { VaChip, VaDivider, VaButton } from '../../components'
+import { useColors } from '../../composables'
 
 export default {
-  title: 'ColorCSSVariablesUpdater',
-  component: ColorCSSVariablesUpdater,
+  title: 'services/ColorCSSVariablesUpdater',
 }
 
-export const Default = defineComponent({
-  components: { ColorCSSVariablesUpdater },
-  template: '<ColorCSSVariablesUpdater/>',
+export const Default = () => ({
+  components: { VaChip, VaDivider, VaButton },
+  methods: {
+    change (color) {
+      useColors().setColors({ danger: color })
+    },
+  },
+  template: `
+    <div class="mb-6">
+      <a class="title title--va-danger">
+        This is title danger color
+      </a>
+      <va-chip color="danger">This is vue component</va-chip>
+    </div>
+
+    <va-divider />
+    Change danger color to:
+    <va-button @click="change('#4ab2e3')" color="#4ab2e3">
+      danger = blue
+    </va-button>
+    <va-button @click="change('#e34b4a')" color="#e34b4a">
+      danger = red
+    </va-button>
+  `,
 })
