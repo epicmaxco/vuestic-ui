17a7646b6357986d54fa1da63e7c90ad67a1c1c5
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-11-07 23:21:01 +0200

    fix: suppress extra warnings

--- packages/library-builder/src/builder/build-nuxt.ts
+++ packages/library-builder/src/builder/build-nuxt.ts
@@ -1,8 +1,6 @@
-import { existsSync } from 'fs'
 import { join } from 'pathe'
 import { buildNuxtModule } from '../nuxt/builder'
 import { strictResolve } from '../utils/strict-resolve'
-
 
 export const buildNuxt = async (options: {
   cwd: string,
@@ -19,10 +17,24 @@ export const buildNuxt = async (options: {
     return
   }
 
-  return buildNuxtModule({
+  const stdout = process.stdout.write
+
+  process.stdout.write = (buffer, cb: any): boolean => {
+    // suppress nuxt logs
+    if (buffer.toString().includes('Building vuestic-ui')) return true
+    if (buffer.toString().includes('Build succeeded for vuestic-ui')) return true
+    if (buffer.toString().includes('(total size: ')) return true
+    if (buffer.toString().includes('Σ Total dist size')) return true
+    if (buffer.toString() === '') return true
+    return stdout.call(process.stdout, buffer, cb)
+  }
+
+  await buildNuxtModule({
     nuxtDir: nuxtModulePath,
     outDir: join(cwd, outDir, '/nuxt'),
     entry,
     cwd,
   })
+
+  process.stdout.write = stdout
 }
--- packages/library-builder/src/builder/build-vite.ts
+++ packages/library-builder/src/builder/build-vite.ts
@@ -1,7 +1,18 @@
 import { UserConfig } from 'vite';
 import { build as viteBuild } from 'vite'
 
+const globalConsole = global.console
+const warn = globalConsole.warn
+globalConsole.warn = (msg, ...args) => {
+  // suppress @vue/reactivity-transform warning
+  if (msg.includes('@vue/reactivity-transform')) return
+  if (msg.includes('Generated an empty chunk')) return
+  if (msg.includes('Browserslist: caniuse-lite is outdated. Please run')) return
+  warn.call(globalConsole, msg, ...args)
+}
+
 export const buildVite = async (config: UserConfig) => {
+  config.logLevel = 'silent'
   const result = await viteBuild(config)
   return result
 }
