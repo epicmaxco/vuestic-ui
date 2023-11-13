45359644a5801418f0212db3c02b2b99971b8c0a
Daniel Roe <daniel@roe.dev>
2023-11-13 05:12:21 +0100

    fix: import nuxt composables from #imports (#4006)
    Co-authored-by: Maksim Nedoshev <m0ksem1337@gmail.com>

--- packages/docs/modules/banner/runtime/banner.ts
+++ packages/docs/modules/banner/runtime/banner.ts
@@ -1,4 +1,4 @@
-import { defineNuxtPlugin } from '#app';
+import { defineNuxtPlugin } from '#imports';
 export default defineNuxtPlugin((nuxt) => {
   if (nuxt.ssrContext) { return }
 
--- packages/docs/modules/google-analytics/runtime/plugin.ts
+++ packages/docs/modules/google-analytics/runtime/plugin.ts
@@ -1,4 +1,4 @@
-import { defineNuxtPlugin } from '#app';
+import { defineNuxtPlugin } from '#imports';
 import VueGtag from 'vue-gtag'
 
 export default defineNuxtPlugin((nuxt) => {
