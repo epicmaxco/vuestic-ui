0d1ee9fdebd3b5a4c3a629325a72d444df428366
xiongmao86 <xiongmao86dev@sina.com>
2023-11-21 17:02:33 +0800

    eslint fix inner-loading

--- packages/docs/page-config/ui-elements/inner-loading/examples/Default.vue
+++ packages/docs/page-config/ui-elements/inner-loading/examples/Default.vue
@@ -1,11 +1,11 @@
 <template>
-  <va-inner-loading loading>
-    <va-card>
-      <va-card-title>Title</va-card-title>
-      <va-card-content>
+  <VaInnerLoading loading>
+    <VaCard>
+      <VaCardTitle>Title</VaCardTitle>
+      <VaCardContent>
         Lorem ipsum dolor sit amet, consectetur adipiscing
         elit.
-      </va-card-content>
-    </va-card>
-  </va-inner-loading>
+      </VaCardContent>
+    </VaCard>
+  </VaInnerLoading>
 </template>
