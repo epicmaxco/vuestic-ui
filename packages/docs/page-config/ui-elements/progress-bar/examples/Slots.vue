4bc5feb34d6d4a6e8ff07c748c6798d56218e947
xiongmao86 <xiongmao86dev@sina.com>
2023-11-21 17:15:29 +0800

    eslint fix progress-bar

--- packages/docs/page-config/ui-elements/progress-bar/examples/Slots.vue
+++ packages/docs/page-config/ui-elements/progress-bar/examples/Slots.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="flex flex-col gap-2">
     <div>When you use a default slot:</div>
-    <va-progress-bar :model-value="value">
+    <VaProgressBar :model-value="value">
       {{ value }}
-    </va-progress-bar>
+    </VaProgressBar>
 
     <div>
       When you use a 'content-inside' prop:
     </div>
-    <va-progress-bar
+    <VaProgressBar
       :model-value="value"
       size="large"
       content-inside
     >
       {{ value }}
-    </va-progress-bar>
+    </VaProgressBar>
 
     <div>
       When you use a 'show-percent' prop:
     </div>
-    <va-progress-bar
+    <VaProgressBar
       :model-value="value"
       show-percent
     />
-    <va-progress-bar
+    <VaProgressBar
       :model-value="value"
       size="1.5rem"
       color="warning"
