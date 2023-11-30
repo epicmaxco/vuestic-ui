6e8710399aad4aa7681efed80085556fdd6ba893
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-11-30 20:15:12 +0200

    fix(stateful): use provided prop instead of default value if provided.

---
+++ packages/ui/src/composables/useUserProvidedProp.ts
@@ -0,0 +1,14 @@
+import { computed, getCurrentInstance } from 'vue'
+
+export const NOT_PROVIDED = Symbol('NOT_PROVIDED')
+
+export const useUserProvidedProp = <Name extends string, Props extends Record<Name, any>>(propName: Name, props: Props) => {
+  const vm = getCurrentInstance()!
+
+  return computed(() => {
+    if (!vm?.vnode.props) { return null }
+    const originalProp = props[propName]
+    // If vnode doesn't have this prop it mean default value is used
+    return propName in vm.vnode.props ? originalProp as Props[Name] : NOT_PROVIDED
+  })
+}
