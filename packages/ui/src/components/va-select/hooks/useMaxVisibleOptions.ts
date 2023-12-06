f847c9483a95fd4162a500f4f7c199cd54023815
ishaanparlikar <iparlika@emich.edu>
2023-12-06 10:35:41 +0530

    Va-select to accept number and string for max-visible-options

--- packages/ui/src/components/va-select/hooks/useMaxVisibleOptions.ts
+++ packages/ui/src/components/va-select/hooks/useMaxVisibleOptions.ts
@@ -5,7 +5,7 @@ import { isNilValue } from '../../../utils/isNilValue'
 import type { SelectOption } from '../types'
 
 export const useMaxVisibleOptionsProps = {
-  maxVisibleOptions: { type: Number, default: 0 },
+  maxVisibleOptions: { type: Number || String, default: 0 },
 }
 
 type UseMaxVisibleOptionsProps = ExtractPropTypes<typeof useMaxVisibleOptionsProps> & { multiple: boolean, modelValue: SelectOption | SelectOption[] }
