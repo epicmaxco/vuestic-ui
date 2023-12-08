c0d8d6e264db7a494236672bb2c84ec7cf74954f
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-12-09 01:32:52 +0200

    chore: update versions for create-vuestic

--- packages/create-vuestic/src/versions.ts
+++ packages/create-vuestic/src/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  'vuestic-ui': '^1.7.5',
-  '@vuestic/nuxt': '^1.0.14',
+  'vuestic-ui': '^1.8.8',
+  '@vuestic/nuxt': '^1.0.18',
   '@vuestic/ag-grid-theme': '^1.1.4',
   '@vuestic/tailwind': '^0.1.3',
   'tailwindcss': '^3.3.3',
