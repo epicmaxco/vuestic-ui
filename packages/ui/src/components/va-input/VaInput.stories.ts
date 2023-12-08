99018140f7174c561f14914fde4d8aa078ac1410
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-12-08 14:14:33 +0200

    fix(#3996): set input value on input event according to model value

--- packages/ui/src/components/va-input/VaInput.stories.ts
+++ packages/ui/src/components/va-input/VaInput.stories.ts
@@ -102,3 +102,27 @@ Autofocus.play = async ({ canvasElement }) => {
 
   expect(input).toHaveFocus()
 }
+
+export const InputValue: StoryFn = () => ({
+  components: { VaInput },
+  data: () => ({ value: 't' }),
+  // Must disallow writing more than 2 characters
+  template: '<VaInput :model-value="value" @update:model-value="$event.length > 2 ? void 0 : value = $event" />',
+})
+
+export const InputValueComputed: StoryFn = () => ({
+  components: { VaInput },
+  data: () => ({ value: 't' }),
+
+  computed: {
+    valueComputed: {
+      set (v) {
+        if (v.length > 2) { return }
+        this.value = v
+      },
+      get () { return this.value },
+    },
+  },
+  // Must disallow writing more than 2 characters
+  template: '<VaInput v-model="valueComputed" />',
+})
