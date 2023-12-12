8ff4c8c225d62434ecd7d21c5367560e114c6bcf
Yauheni Prakopchyk <ontrew@gmail.com>
2023-12-12 06:50:45 +0400

    Replace google analytics with gtm (#4084)
    Co-authored-by: Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>

--- packages/docs/modules/google-analytics/runtime/plugin.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtPlugin } from '#imports';
-import VueGtag from 'vue-gtag'
-
-export default defineNuxtPlugin((nuxt) => {
-  nuxt.vueApp.use(VueGtag, {
-    config: { id: "<%= options.ID %>" }
-  })
-})
