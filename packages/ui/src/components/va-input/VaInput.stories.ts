fd2e0b2b2f2f5afaa00c62a18e03afc54c5c5d27
Maksim Nedoshev <m0ksem1337@gmail.com>
2023-12-14 19:24:01 +0200

    fix(input): restore cursor on input value update (#4095)

--- packages/ui/src/components/va-input/VaInput.stories.ts
+++ packages/ui/src/components/va-input/VaInput.stories.ts
@@ -106,8 +106,8 @@ Autofocus.play = async ({ canvasElement }) => {
 export const InputValue: StoryFn = () => ({
   components: { VaInput },
   data: () => ({ value: 't' }),
-  // Must disallow writing more than 2 characters
-  template: '<VaInput :model-value="value" @update:model-value="$event.length > 2 ? void 0 : value = $event" />',
+  // Must disallow writing more than 5 characters
+  template: '<VaInput :model-value="value" @update:model-value="value = $event.slice(0, 5)" strictBindInputValue />',
 })
 
 export const InputValueComputed: StoryFn = () => ({
@@ -117,12 +117,25 @@ export const InputValueComputed: StoryFn = () => ({
   computed: {
     valueComputed: {
       set (v) {
-        if (v.length > 2) { return }
-        this.value = v
+        this.value = v.slice(0, 5)
       },
       get () { return this.value },
     },
   },
+  // Must disallow writing more than 5 characters
+  template: '<VaInput v-model="valueComputed" strictBindInputValue />',
+})
+
+export const DebounceInput: StoryFn = () => ({
+  components: { VaInput },
+  data: () => ({ value: 't' }),
+  methods: {
+    onInput (text: string) {
+      setTimeout(() => {
+        this.value = text
+      }, 1000)
+    },
+  },
   // Must disallow writing more than 2 characters
-  template: '<VaInput v-model="valueComputed" />',
+  template: '<VaInput stateful :model-value="value" @update:model-value="onInput" strictBindInputValue />',
 })
