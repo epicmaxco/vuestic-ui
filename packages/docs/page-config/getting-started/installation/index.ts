533ae9d92dbaf4a118c2dff0b1aa59d68a91e4e7
Abdou <82264545+3aluw@users.noreply.github.com>
2024-01-09 08:04:35 +0100

    Update yarn.lock for Node.js 20 Compatibility (#4124)
    * re-generated yarn lock to work with node 20
    
    * Revert "re-generated yarn lock to work with node 20"
    
    This reverts commit 0b253fa9923e80304ab51ee7da06fd36bddc7e5f.
    
    * chore: remove deprecated vue-cli from sandbox
    
    * chore(docs): replace vue-cli installation guide with link to nuxt installation
    
    ---------
    
    Co-authored-by: Maksim Nedoshev <m0ksem1337@gmail.com>

--- packages/docs/page-config/getting-started/installation/index.ts
+++ packages/docs/page-config/getting-started/installation/index.ts
@@ -16,14 +16,8 @@ export default definePageConfig({
     }, 'bash'),
     block.paragraph('`create-vuestic` provides three template: Vuestic Admin, create-vue and Nuxt. Templates can be configured to use specific features like tree-shaking or AG Grid theme.'),
 
-    block.subtitle("Vue CLI installation"),
-    block.paragraph("If you have app based on Vue CLI this is a way to install Vuestic UI. Works fine with vite or webpack. Prefer `create-vuestic` for new projects"),
-    block.alert("Installing Vuestic UI via the plugin would modify your `package.json` and `main.(js|ts)` files, so make sure you’ve committed your code before proceeding to avoid data loss.", "warning"),
-    block.paragraph("First, make sure you've got [Vue CLI](https://cli.vuejs.org/guide/installation.html)[[target=_blank]] installed globally:"),
-    block.code("cli", "bash"),
-    block.paragraph("Notice also that the minimum supported version of Vue CLI is v4.0.0. In case you've got an older version installed, please, consider [upgrading](https://cli.vuejs.org/migrating-from-v3/#migrating-from-v3)[[target=_blank]] it."),
-    block.paragraph("If everything's fine then you can finally install the plugin with the following command (**don't forget to commit**):"),
-    block.code("vue add vuestic-ui", "bash"),
+    block.subtitle("Nuxt installation"),
+    block.link('Visit nuxt integration page', '/getting-started/nuxt#manual-installation'),
 
     block.subtitle("Manual installation"),
     block.paragraph("If you decide to install Vuestic UI manually, all you need to do is to install a NPM package, a couple of other necessary assets (such as fonts and CSS styles) and slightly modify your application's entry point (most probably `index.js` or `main.js`, depending on your setup)."),
