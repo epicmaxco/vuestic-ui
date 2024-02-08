42a6c7076da347db2773cefade623d21ce0cc2a5
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-02-08 21:34:03 +0200

    docs: upgrade partners block (#4153)

--- packages/docs/components/landing/Partners.vue
+++ packages/docs/components/landing/Partners.vue
@@ -1,90 +1,12 @@
-<template>
-  <section class="partners">
-    <div class="partners__wrapper">
-      <div class="partners__inner">
-        <div class="partners__logos">
-          <a
-            href="https://flatlogic.com/"
-            target="_blank"
-            title="Flatlogic - The best way to create React, Angular and Vue full-stack web applications"
-            class="partners__logo"
-          >
-            <LandingIconsIconFlatlogic />
-          </a>
-
-          <a
-            href="https://vuejobs.com/"
-            target="_blank"
-            title="Vue.js jobs – Browse through dozens of Vue.js openings"
-            class="partners__logo logo__vue-jobs"
-          >
-            <LandingIconsIconVueJobs />
-            <span class="logo__vue-jobs-text">Hire Vue.js developers</span>
-          </a>
-
-          <a
-            href="https://epicmax.co/"
-            target="_blank"
-            title="Epicmax - Top Vue.js Development Company"
-            class="partners__logo"
-          >
-            <LandingIconsIconEpicmaxColor
-              height="19"
-              width="102"
-            />
-          </a>
-
-          <a
-            href="https://www.ag-grid.com/"
-            target="_blank"
-            title="Data Grid: AG Grid: High-Performance React Grid, Angular Grid, JavaScript Grid"
-            class="partners__logo"
-          >
-            <LandingIconsIconAgGrid />
-          </a>
-
-          <a
-            href="https://vuejsforge.com/?utm_source=Epicmax&utm_medium=partners_logo&utm_campaign=vuestic/"
-            target="_blank"
-            title="Vue.js Forge - The largest hands-on Vue.js Event"
-            class="partners__logo"
-          >
-            <LandingIconsIconVueForge />
-          </a>
-
-          <a
-            href="https://codesandbox.io/?utm_source=Epicmax&utm_medium=partners_logo&utm_campaign=vuestic"
-            target="_blank"
-            title="CodeSandbox - An online editor tailored for web applications"
-            class="partners__logo"
-          >
-            <LandingIconsIconCodeSandbox />
-          </a>
-
-          <a
-            href="https://chromatic.com/?utm_source=github&utm_medium=profile&utm_campaign=vuestic"
-            target="_blank"
-            title="Chromatic - Storybook deployment, review, and test"
-            class="partners__logo"
-          >
-            <LandingIconsIconChromatic />
-          </a>
-        </div>
-
-        <h2 class="partners__subtitle">
-          Our beloved Partners & Sponsors that support us ❤️
-        </h2>
-      </div>
-    </div>
-  </section>
-</template>
-
-<script>
-export default {
-  name: 'LandingPartners',
-}
+<script setup lang="ts">
+  import PartnersList from './partners/PartnersList.vue';
 </script>
 
+<template>
+  <section class="partners">
+    <PartnersList />
+  </section>
+</template>
 <style lang="scss" scoped>
 @import "@/assets";
 
@@ -97,68 +19,5 @@ export default {
   padding: $section-padding;
   color: var(--va-text-inverted);
   background-color: var(--bg-partners);
-
-  @include xs(padding, $section-xs-padding);
-
-  &__wrapper {
-    @include wrapper();
-  }
-
-  &__inner {
-    @include row-flex();
-
-    align-items: center;
-    flex-direction: column;
-  }
-
-  &__subtitle {
-    @include col();
-    @include size(12);
-    @include text-font();
-
-    margin-top: 1.75rem;
-    text-align: center;
-  }
-
-  &__logos {
-    @include col();
-    @include size(12);
-    @include row-flex();
-
-    justify-content: center;
-    align-items: flex-end;
-    flex-wrap: wrap;
-  }
-
-  &__logo {
-    margin: 1.3rem 1rem 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    color: inherit;
-
-    &:active,
-    &:visited,
-    &:hover {
-      color: unset;
-    }
-
-    &.logo__vue-js {
-      position: relative;
-      bottom: -6px;
-    }
-
-    &.logo__vue-jobs {
-      position: relative;
-
-      .logo__vue-jobs-text {
-        position: absolute;
-        bottom: -10px;
-        font-size: 8px;
-        color: inherit;
-      }
-    }
-  }
 }
 </style>
