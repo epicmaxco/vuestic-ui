96b527f4cca1e75d7c9f10788620f068206e94db
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-03-01 16:14:07 +0200

    chore: document useAppGlobal

--- packages/ui/src/composables/useAppGlobal.ts
+++ packages/ui/src/composables/useAppGlobal.ts
@@ -16,6 +16,11 @@ const getGlobalObject = () => {
   return globalProperties.$vaGlobalVariable
 }
 
+/**
+ * This composable must be used to make global variables. This global is shared in app context, rather then
+ * in window context. This is useful to avoid global variables in window context in multiple app mode, ssr
+ * or cjs build can mess up global variables
+ */
 export const useAppGlobal = <T>(key: string, defaultValue: T): WritableComputedRef<T> => {
   const globalObject = getGlobalObject()
 
