9778e70c3f7c7de4d01c528bd7ea486837cbad60
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-03-25 14:00:21 +0200

    feat(form-field): init component

---
+++ packages/docs/page-config/ui-elements/form-field/examples/use-form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <VaForm ref="formRef" hide-error-messages>
+      <VaFormField v-slot="{ value: v }" v-model="value" :rules="[(v) => v.length >= 2 || 'Min length is 2 characters']">
+        <input v-model="v.ref" placeholder="Write here..." class="border-2 border-sold border-grey-300 px-2">
+      </VaFormField>
+    </VaForm>
+
+    <div v-if="isDirty" class="mt-2">
+      <p v-for="error in errorMessages" :key="error">
+        {{ error }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useForm } from 'vuestic-ui'
+
+const value = ref('')
+
+const { errorMessages, isDirty } = useForm('formRef')
+</script>
---
+++ packages/ui/src/components/va-form-field/index.ts
@@ -0,0 +1,4 @@
+import { withConfigTransport } from '../../services/config-transport'
+import _VaFormField from './VaFormField.vue'
+
+export const VaFormField = withConfigTransport(_VaFormField)
