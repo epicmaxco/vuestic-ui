d35dd2cd7050c4ef44634334f2aa2aede8730c89
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-03-25 14:22:37 +0200

    fix(build): types

--- packages/ui/src/components/va-form-field/index.ts
+++ packages/ui/src/components/va-form-field/index.ts
@@ -1,4 +1,4 @@
-import { withConfigTransport } from '../../services/config-transport'
+import { withConfigTransport } from '../../services/config-transport/withConfigTransport'
 import _VaFormField from './VaFormField.vue'
 
 export const VaFormField = withConfigTransport(_VaFormField)
