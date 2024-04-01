e5406e471c8a47b5d1fb88193458ff76d7b78757
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2024-04-01 19:31:27 +0300

    fix: create-nuxt command

--- packages/create-vuestic/src/generators/create-nuxt.ts
+++ packages/create-vuestic/src/generators/create-nuxt.ts
@@ -1,12 +1,17 @@
 import { doesSatisfyNodeVersion } from '../utils/node-version';
 import { execp } from './../utils/exacp';
+import {getPackageManagerName} from "../utils/package-manager";
 
 export const createNuxt3 = (projectName: string) => {
   if (!doesSatisfyNodeVersion('v16.10.0')) {
     throw new Error('Nuxt 3 requires Node.js v16.10.0 or higher. Please upgrade your Node.js version.')
   }
 
-  const command =`npx nuxi init ${projectName}`
+  const packageManager = getPackageManagerName()
+
+  // --packageManager - provide package manager to the nuxt project
+  // --gitInit - false since we provide our own git initialization
+  const command =`npx nuxi@latest init ${projectName} --packageManager ${packageManager} --gitInit false`
 
   return execp(command)
 }
