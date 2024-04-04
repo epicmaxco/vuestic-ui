2c7d954437d7e0e691bdd9b7c882011436a3f3d3
GweesinChan <gweesin2000@gmail.com>
2024-04-04 11:45:53 +0800

    docs(VaSelect): improvement objects as options description(#4067)

--- packages/docs/page-config/ui-elements/select/index.ts
+++ packages/docs/page-config/ui-elements/select/index.ts
@@ -23,6 +23,7 @@ export default definePageConfig({
       title: "Objects as options",
       description: "You can use objects as options. Text will be showing from `text-by` property from object. Value can be also returned from select using `value-by` instead of whole object. Comparing object values is done by `track-by` prop or `value-by` if it is not provided. <br /> <em>Note: Using `text-by` without a `value-by` or `track-by` may cause inconsistencies in expected behavior.",
     }),
+    block.alert("When using `Objects as options`, it's necessary to utilize `track-by` because ObjectOptions are compared by object reference by default. `track-by` allows resolving this comparison by either literal equality.", "warning", true),
     block.example("TrackBy", {
       title: "Track by",
       description: "Track by can be used if you have multiple options with same value."
