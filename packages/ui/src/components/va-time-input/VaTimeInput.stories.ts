c7e04f2b1794fdf50a1bc7ff2cf797dc62da2824
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-04-11 08:54:34 +0300

    feat(time-input): added touched state stories

--- packages/ui/src/components/va-time-input/VaTimeInput.stories.ts
+++ packages/ui/src/components/va-time-input/VaTimeInput.stories.ts
@@ -1,7 +1,4 @@
-import { sleep } from './../../utils/sleep'
-import { expect } from '@storybook/jest'
-import { userEvent } from '@storybook/testing-library'
-import { StoryFn } from '@storybook/vue3'
+import { defineStory } from './../../../.storybook/types'
 import VaTimeInputDemo from './VaTimeInput.demo.vue'
 import VaTimeInput from './VaTimeInput.vue'
 
@@ -33,29 +30,118 @@ export const Placeholder = () => ({
   template: '<VaTimeInput v-model="value" placeholder="Please, select a time" />',
 })
 
-export const Clearable: StoryFn = () => ({
-  components: { VaTimeInput },
-  data () {
-    return {
-      value: new Date('2020-01-01T00:00:00.000Z'),
-    }
+export const Clearable = defineStory({
+  story: () => ({
+    components: { VaTimeInput },
+    data () {
+      return {
+        value: new Date('2020-01-01T00:00:00.000Z'),
+      }
+    },
+    template: '<VaTimeInput clearable v-model="value" />',
+  }),
+
+  async tests ({ canvasElement, expect, event }) {
+    const leftIcon = canvasElement.querySelector('.va-input-wrapper__field')!
+    const clearButton = canvasElement.querySelector('.va-time-input__clear-button')!
+    const input = canvasElement.querySelector('input')!
+
+    await event.click(leftIcon)
+
+    // Dropdown should be visible
+    expect(canvasElement.parentElement!.querySelector('.va-time-picker')).not.toBe(null)
+
+    await event.click(clearButton)
+
+    expect(input).toHaveValue('')
+    // Dropdown should be hidden
+    expect(canvasElement.querySelector('.va-dropdown__content')).toBe(null)
   },
-  template: '<VaTimeInput clearable v-model="value" />',
 })
 
-Clearable.play = async ({ canvasElement }) => {
-  const leftIcon = canvasElement.querySelector('.va-input-wrapper__field')!
-  const clearButton = canvasElement.querySelector('.va-time-input__clear-button')!
-  const input = canvasElement.querySelector('input')!
+export const ValidationDirtyState = defineStory({
+  story: () => ({
+    components: { VaTimeInput },
+    data () {
+      return {
+        value: new Date('2020-01-01T00:00:00.000Z'),
+      }
+    },
+    template: `
+      [isDirty]: {{ $refs.component?.isDirty }}
+      <VaTimeInput v-model="value" :rules="[(v) => v.getHours() === 4 || 'Must be 4 AM']" ref="component" />
+    `,
+  }),
 
-  await userEvent.click(leftIcon)
+  async tests ({ canvasElement, step, expect, event, sleep }) {
+    step('Expect no error when mounted even if value is incorrect', () => {
+      const error = canvasElement.querySelector('.va-input-wrapper.va-input-wrapper--error') as HTMLElement
+      expect(error).toBeNull()
+    })
 
-  // Dropdown should be visible
-  expect(canvasElement.parentElement!.querySelector('.va-time-picker')).not.toBe(null)
+    step('Expect error when value changed', async () => {
+      await event.click(canvasElement.querySelector('.va-input-wrapper__field')!)
 
-  await userEvent.click(clearButton)
+      await sleep(100)
+      const twoAm = document.body.querySelectorAll('.va-time-picker-cell')[2] as HTMLElement
 
-  expect(input).toHaveValue('')
-  // Dropdown should be hidden
-  expect(canvasElement.querySelector('.va-dropdown__content')).toBe(null)
-}
+      await event.hover(twoAm)
+      await event.click(twoAm)
+
+      await sleep(300)
+
+      const error = canvasElement.querySelector('.va-input-wrapper.va-input-wrapper--error') as HTMLElement
+      expect(error).not.toBeNull()
+    })
+  },
+})
+
+export const ValidationTouchedState = defineStory({
+  story: () => ({
+    components: { VaTimeInput },
+    data () {
+      return {
+        value: new Date('2020-01-01T00:00:00.000Z'),
+      }
+    },
+    methods: {
+      reset () {
+        this.$refs.component?.reset()
+      },
+    },
+    template: `
+      [isTouched]: {{ $refs.component?.isTouched }}
+      <VaTimeInput v-model="value" :rules="[(v) => v && v.getHours() === 4 || 'Must be 4 AM']" ref="component" />
+    `,
+  }),
+
+  async tests ({ canvasElement, step, expect, event, sleep, methods }) {
+    step('Expect no error when mounted even if value is incorrect', () => {
+      const error = canvasElement.querySelector('.va-input-wrapper.va-input-wrapper--error') as HTMLElement
+      expect(error).toBeNull()
+    })
+
+    step('Expect error when open/closed', async () => {
+      await event.hover(canvasElement.querySelector('.va-input-wrapper__field')!)
+      await event.click(canvasElement.querySelector('.va-input-wrapper__field')!)
+
+      await event.hover(canvasElement.querySelector('.va-input-wrapper__field')!)
+      await event.click(canvasElement.querySelector('.va-input-wrapper__field')!)
+
+      const error = canvasElement.querySelector('.va-input-wrapper.va-input-wrapper--error') as HTMLElement
+      expect(error).not.toBeNull()
+    })
+
+    step('Expect error input touched', async () => {
+      methods.reset()
+
+      await event.focus(canvasElement.querySelector('.va-input-wrapper__field')!)
+      await event.blur(canvasElement.querySelector('.va-input-wrapper__field')!)
+
+      await sleep(100)
+
+      const error = canvasElement.querySelector('.va-input-wrapper.va-input-wrapper--error') as HTMLElement
+      expect(error).not.toBeNull()
+    })
+  },
+})
