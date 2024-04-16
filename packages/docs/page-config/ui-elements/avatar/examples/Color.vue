8ded7d5d2eecf1a4ae310975d50ec600542b50a0
ahmadJT <ahmad.j@turing.com>
2024-04-16 20:11:35 +0100

    fixed spacing between avatars in the docs

--- packages/docs/page-config/ui-elements/avatar/examples/Color.vue
+++ packages/docs/page-config/ui-elements/avatar/examples/Color.vue
@@ -1,50 +1,52 @@
 <template>
-  <VaAvatar
-    color="success"
-    class="mr-6"
-  >
-    J.D.
-  </VaAvatar>
+  <div class="flex gap-6 flex-wrap">
+    <VaAvatar
+      color="success"
+      class="mr-6"
+    >
+      J.D.
+    </VaAvatar>
 
-  <VaAvatar
-    color="info"
-    class="mr-6"
-  >
-    J.D.
-  </VaAvatar>
+    <VaAvatar
+      color="info"
+      class="mr-6"
+    >
+      J.D.
+    </VaAvatar>
 
-  <VaAvatar
-    color="warning"
-    class="mr-6"
-  >
-    J.D.
-  </VaAvatar>
+    <VaAvatar
+      color="warning"
+      class="mr-6"
+    >
+      J.D.
+    </VaAvatar>
 
-  <VaAvatar
-    color="#fafaa0"
-    class="mr-6"
-  >
-    J.D.
-  </VaAvatar>
+    <VaAvatar
+      color="#fafaa0"
+      class="mr-6"
+    >
+      J.D.
+    </VaAvatar>
 
-  <VaAvatar
-    color="danger"
-    class="mr-6"
-  >
-    J.D.
-  </VaAvatar>
+    <VaAvatar
+      color="danger"
+      class="mr-6"
+    >
+      J.D.
+    </VaAvatar>
 
-  <VaAvatar
-    color="#692BEB"
-    class="mr-6"
-  >
-    J.D.
-  </VaAvatar>
+    <VaAvatar
+      color="#692BEB"
+      class="mr-6"
+    >
+      J.D.
+    </VaAvatar>
 
-  <VaAvatar
-    color="#540C0C"
-    text-color="#fafaa0"
-  >
-    J.D.
-  </VaAvatar>
+    <VaAvatar
+      color="#540C0C"
+      text-color="#fafaa0"
+    >
+      J.D.
+    </VaAvatar>
+  </div>
 </template>
