609e7fef81056b4a124ca6e4352ac0687d5add5a
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-05-06 19:45:16 +0300

    chore: bump version to v1.9.10

--- .github/ISSUE_TEMPLATE/bug-template.md
+++ .github/ISSUE_TEMPLATE/bug-template.md
@@ -6,7 +6,7 @@ assignees: ''
 
 ---
 
-**Vuestic-ui version:** 1.9.9
+**Vuestic-ui version:** 1.9.10
 
 ### Description
 
--- packages/vue-cli-plugin/generator/configs/default.js
+++ packages/vue-cli-plugin/generator/configs/default.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   extendPackage: {
     dependencies: {
-      'vuestic-ui': '^1.9.9',
+      'vuestic-ui': '^1.9.10',
     }
   },
   vueUse: [
