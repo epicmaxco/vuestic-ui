5509e2b8725c1ef5f82e902dba6c34c7f21aa07b
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-06-25 11:54:58 +0300

    fix: correct clone deep function

--- packages/ui/src/utils/clone-deep.ts
+++ packages/ui/src/utils/clone-deep.ts
@@ -1,3 +1,11 @@
+import { mergeDeep } from "./merge-deep"
+
+// Notice: structureClone can not copy DOM nodes and functions.
+/** Deeply merge object into new object, cloning DOM nodes and functions */
 export const cloneDeep = <T>(value: T) => {
-  return structuredClone(value)
+  if (value === null || typeof value !== 'object') {
+    return value
+  }
+
+  return mergeDeep({}, value)
 }
