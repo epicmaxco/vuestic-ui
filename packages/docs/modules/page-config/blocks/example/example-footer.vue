10e1880400a75fefdfec70b264f8ab952af804f5
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-06-26 13:49:58 +0300

    docs: add copy button to all code views

--- packages/docs/modules/page-config/blocks/example/example-footer.vue
+++ packages/docs/modules/page-config/blocks/example/example-footer.vue
@@ -229,7 +229,7 @@ const doShowPlaygroundButton = computed(() => {
 @import "@/assets";
 
 .docs-navigation {
-  background: var(--va-background-element);
+  background: var(--va-background-border);
   margin: -2px 0 0.2rem 0;
   border-radius: 0 0 0.25rem 0.25rem;
   padding: 0 calc(var(--va-card-padding) - var(--va-button-sm-content-px) / 2);
