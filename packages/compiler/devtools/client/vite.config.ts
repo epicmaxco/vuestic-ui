f614bc7014de3c2878d3f1a6671bdccdd7bf9a5c
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-07-08 16:08:55 +0300

    chore: improve bundling

--- packages/compiler/devtools/client/vite.config.ts
+++ packages/compiler/devtools/client/vite.config.ts
@@ -12,11 +12,22 @@ export default defineConfig({
   ],
 
   build: {
+    minify: false,
+    sourcemap: false,
+
     lib: {
       entry: fileURLToPath(new URL('./index.ts', import.meta.url)),
       name: 'vuestic-devtools',
       formats: ['es'],
       fileName: (format) => `vuestic-devtools.es.js`,
+    },
+
+    rollupOptions: {
+      external: ['vue', 'shiki', 'vuestic-ui'],
+
+      output: {
+        chunkFileNames: `[name].js`,
+      }
     }
   },
 })
--- packages/compiler/vite-plugin/index.ts
+++ packages/compiler/vite-plugin/index.ts
@@ -10,14 +10,17 @@ type Options = {
    * Adds CSS layers to Vuestic UI
    * Helps control the order of CSS in the final bundle
    *
-   * @default true
+   * @default false
    *
    * Add `vuestic.components` and `vuestic.styles` CSS layers
+   *
+   * @notice This will make Vuestic styles less important. Make sure you don't have any global conflicting styles.
+   * For example. tailwind have normalize css included. It may have higher priority than Vuestic styles and components might look broken.
    */
   cssLayers?: boolean,
 }
 
-export const vuestic = (options: Options): Plugin[] => {
+export const vuestic = (options: Options = {}): Plugin[] => {
   const extractOptions = (key: keyof Options) => {
     // Build fails without as Record<string, string> cast
     return typeof options[key] === 'object' ? options[key] as Record<string, string> : undefined
@@ -30,7 +33,7 @@ export const vuestic = (options: Options): Plugin[] => {
     plugins.push(devtools(extractOptions('devtools')))
   }
 
-  if (options.cssLayers !== false) {
+  if (options.cssLayers === true) {
     console.log('Using vuestic:css-layers') // TODO: Remove this log
     plugins.push(cssLayers)
   }
