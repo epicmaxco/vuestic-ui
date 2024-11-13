713a92c257050f442f10b8aa54dfdb38246ccbd7
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-11-13 15:54:31 +0200

    raw

---
+++ packages/formkit/playground/src/App.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import { useInput } from '@formkit/vue'
+import { defineComponent, ref } from 'vue';
+import Child from './Child.vue'
+
+export default defineComponent({
+  components: {
+    Child
+  },
+  setup(props, ctx) {
+    const node = useInput({
+      type: 'text',
+      label: 'Test',
+      helpText: 'This is a test',
+      validation: 'required|min:3',
+      validationVisibility: 'live'
+    } ,ctx);
+
+    const value = ref(false)
+
+    console.log(node)
+
+    return {
+      value
+    }
+  }
+})
+
+</script>
+
+<template>
+  <FormKit
+    type="button"
+    label="Test"
+  />
+  <FormKit
+    #wrapper="bind"
+    type="checkbox"
+    label="Test"
+    validation="required|min:3"
+    v-model="value"
+  >
+    <Child :bind="bind" />
+  </FormKit>
+</template>
+
---
+++ packages/formkit/playground/src/Child.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { VaCheckbox, VaButton } from 'vuestic-ui'
+
+  const components = {
+    checkbox: VaCheckbox,
+    button: VaButton
+  }
+</script>
+
+<script setup lang="ts">
+  import { FormKitFrameworkContext } from '@formkit/core';
+
+  import { computed, PropType } from 'vue';
+
+
+  const props = defineProps({
+    bind: {
+      type: Object as PropType<FormKitFrameworkContext<any>>,
+      required: true
+    }
+  });
+
+  console.log(props.bind)
+
+  const component = computed(() => {
+    return components[props.bind.type as keyof typeof components]
+  })
+</script>
+
+<template>
+  <component
+    :is="component"
+    v-model="props.bind.value"
+  />
+</template>
