d24ac8e789bb482de35cd28ca7e5b9936b572563
raichev-dima <raichevdp@gmail.com>
2024-12-05 18:21:38 +0300

    feat(FormKit): work on Text component

--- packages/formkit/playground/vite.config.ts
+++ packages/formkit/playground/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [UnoCSS(), vue()],
 })
