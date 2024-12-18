edb3b2647d43949f76997f60d9f441cd469bf136
raichev-dima <raichevdp@gmail.com>
2024-12-18 17:14:09 +0300

    feat: implement Month/Week type

--- packages/ui/src/stories/formkit/Date.stories.ts
+++ packages/ui/src/stories/formkit/Date.stories.ts
@@ -22,3 +22,36 @@ export const Default: StoryFn = () => ({
     />
   `,
 })
+
+export const Month: StoryFn = () => ({
+  setup () {
+    return {
+      types,
+    }
+  },
+  template: `
+    <FormKit
+      :type="types.month"
+      help="What month were you born?"
+      label="Birth month"
+      name="birth_month"
+      value="1965-09"
+    />
+  `,
+})
+
+export const Week: StoryFn = () => ({
+  setup () {
+    return {
+      types,
+    }
+  },
+  template: `
+    <FormKit
+      label="Vacation"
+      :type="types.week"
+      help="What week will you travel to Fiji?"
+      value="2025-W02"
+    />
+  `,
+})
