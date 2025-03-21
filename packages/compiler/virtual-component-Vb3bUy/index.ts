c80fde4ff2fa869b8f0b07a0e46aba0407d617dd
Maksim Nedoshev <m0ksem1337@gmail.com>
2025-03-21 22:50:11 +0200

    chore(virtual-components): remove junk

--- packages/compiler/virtual-component-Vb3bUy/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    props: { name: String },
-    setup(__props, { expose: __expose }) {
-        __expose();
-        const __returned__ = {};
-        Object.defineProperty(__returned__, '__isScriptSetup', { enumerable: false, value: true });
-        return __returned__;
-    }
-};
