23ea8e7817d8873c79c1b4254f619d4f91771aa6
gweesin <gweesin2000@gmail.com>
2025-06-15 21:34:30 +0800

    docs: add no-data slot documentation

---
+++ packages/docs/page-config/ui-elements/data-table/examples/NoDataSlots.vue
@@ -0,0 +1,34 @@
+<template>
+  <VaDataTable
+    :items="items"
+    :columns="columns"
+  >
+    <template #no-data>
+      <code>no-data</code> Slot
+    </template>
+  </VaDataTable>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    const columns = [
+      { key: "id" },
+      { key: "name" },
+      { key: "username" },
+      { key: "email" },
+      { key: "phone" },
+      { key: "website" },
+      { key: "address.street", name: "street", label: "Street" },
+      { key: "company.name", name: "companyName", label: "Company Name" },
+    ];
+
+    return {
+      items: [],
+      columns,
+    };
+  },
+});
+</script>
